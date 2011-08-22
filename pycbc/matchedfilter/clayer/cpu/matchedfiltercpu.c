685f469a361f715427c6fbe21f5d2ba3d542fd38
Karsten Wiesner <karsten.wiesner@aei.mpg.de>
2011-08-22 13:15:26 -0400

    intermediate update of clayer mfilt signature foer badri

--- pycbc/matchedfilter/clayer/cpu/matchedfiltercpu.c
+++ pycbc/matchedfilter/clayer/cpu/matchedfiltercpu.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include "../../../datavector/clayer/cpu/datavectorcpu_types.h"
+#include "../../../clayer/cpu/pycbccpu_types.h"
 
-int gen_snr_cpu(complex_vector_single_t* stilde, 
-                complex_vector_single_t* htilde,
-                real_vector_single_t* snr)
+
+void gen_snr_cpu(cpu_context_t* context,
+                real_vector_single_t* snr,
+                complex_vector_single_t* stilde, 
+                complex_vector_single_t* htilde)
 {    
     //printf("gen_snr_cpu called w/: complex_vector_single_t* stilde= %p\n", 
     //       stilde->data);
@@ -11,6 +14,6 @@ int gen_snr_cpu(complex_vector_single_t* stilde,
     //       htilde->data);
     //printf("gen_snr_cpu called w/: real_vector_single_t* snr= %p\n", snr->data);
     
-    return 0;
+    return;
 
 }
