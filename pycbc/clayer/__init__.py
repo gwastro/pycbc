5b3a4cc4474a01d755c489b2cbd38b75978b63e0
Duncan Brown <duncan.brown@ligo.org>
2011-10-13 09:50:20 -0400

    created an __init__.py file to make pycbc/clayer a python package

---
+++ pycbc/clayer/__init__.py
@@ -0,0 +1,14 @@
+"""
+PyCBC
+=====
+
+Provides
+  1. A collection of classes for processing objects for gravitational wave 
+     search and analysis
+  2. processing objects can be implemented at different processing 
+     architectures: On the Host CPU (C/C++), on a GPU (OpenCl, CUDA)
+  3. Bindings to other GW search and analysis frameworks (Lalsuite)
+
+"""
+
+__author__ = 'Karsten Wiesner <karsten.wiesner@ligo.org>'
