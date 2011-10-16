553478c2927282dc2203618e282db36575400815
Karsten Wiesner <karsten.wiesner@aei.mpg.de>
2011-10-16 22:35:00 +0200

    recovered the datapumps and checked them in under pycbc/tools
    Signed-off-by: Duncan Brown <duncan.brown@ligo.org>

--- pycbc/tools/datavecterm_cpu.py
+++ pycbc/tools/datavecterm_cpu.py
@@ -30,7 +30,7 @@ transfers datavectors from a GPU to the CPU
 """
 
 # import framework related parent
-from pycbc.cpu import CpuProcessingObj
+from pycbc.pycbc_cpu import CpuProcessingObj
 
 
 import logging
