7162adab7ef376a28756ba954421f3d1e5539793
Josh Willis <joshua.willis@ligo.org>
2012-03-29 23:22:33 -0500

    Add Array method to return numpy data

--- pycbc/array.py
+++ pycbc/array.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2012  Alex Nitz
+# Copyright (C) 2012  Alex Nitz, Josh Willis
 #
 # This program is free software; you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by the
@@ -24,7 +24,7 @@
 #
 """
 This modules provides a device independent Array class based on PyCUDA,
-PyOpenCL, and Numpy. 
+PyOpenCL, and Numpy.
 """
 
 import functools as _functools
@@ -41,7 +41,7 @@ if _pycbc.HAVE_CUDA:
     import pycuda.gpuarray as _cudaarray
 if _pycbc.HAVE_OPENCL:
     import pyopencl as _pyopencl
-    import pyopencl.array as _openclarray 
+    import pyopencl.array as _openclarray
 
 _ALLOWED_DTYPES = [_numpy.float32,_numpy.float64,_numpy.complex64,
                    _numpy.complex128]
@@ -49,34 +49,34 @@ _ALLOWED_SCALARS = [int,long, float]
 
 
 def _convert_to_scheme(ary):
-    if ary._scheme is not _scheme.mgr.state: 
-        converted_array = Array(ary,dtype=ary._data.dtype) 
+    if ary._scheme is not _scheme.mgr.state:
+        converted_array = Array(ary,dtype=ary._data.dtype)
         ary._data = converted_array._data
         ary._scheme = _scheme.mgr.state
 
 class Array(object):
     """Array used to do numeric calculations on a various compute
     devices. It is a convience wrapper around _numpy, _pyopencl, and
-    _pycuda. 
+    _pycuda.
     """
 
     def __init__(self,initial_array, dtype=None, copy=True):
-        """ initial_array: An array-like object as specified by NumPy, this 
-        also includes instances of an underlying data type as described in 
+        """ initial_array: An array-like object as specified by NumPy, this
+        also includes instances of an underlying data type as described in
         section 3 or an instance of the PYCBC Array class itself. This
         object is used to populate the data of the array.
 
-        dtype: A NumPy style dtype that describes the type of 
+        dtype: A NumPy style dtype that describes the type of
         encapsulated data (float32,compex64, etc)
 
-        copy: This defines whether the initial_array is copied to instantiate 
-        the array or is simply referenced. If copy is false, new data is not 
-        created, and so all arguments that would force a copy are ignored. 
-        The default is to copy the given object. 
+        copy: This defines whether the initial_array is copied to instantiate
+        the array or is simply referenced. If copy is false, new data is not
+        created, and so all arguments that would force a copy are ignored.
+        The default is to copy the given object.
         """
         self._scheme=_scheme.mgr.state
         self._data = None
-        
+
         if not copy:
             if isinstance(initial_array,Array):
                 if self._scheme is not initial_array._scheme:
@@ -92,18 +92,18 @@ class Array(object):
                 self._data = initial_array
             else:
                 raise TypeError(str(type(initial_array))+' is not supported')
-                
+
             # Check that the dtype is supported.
             if self._data.dtype not in _ALLOWED_DTYPES:
                 raise TypeError(str(self._data.dtype) + ' is not supported')
-            
-                
-        if copy:                  
+
+
+        if copy:
             #Check that a valid dtype was given.
-            if dtype is not None: 
+            if dtype is not None:
                 if dtype not in _ALLOWED_DTYPES:
-                    raise TypeError(str(dtype) + ' is not supported')   
-        
+                    raise TypeError(str(dtype) + ' is not supported')
+
             #Unwrap initial_array
             input_data = None
             if isinstance(initial_array,Array):
@@ -120,14 +120,14 @@ class Array(object):
                                              _openclarray.Array):
                     self._data = input_data.get()
                 else:
-                    self._data = _numpy.array(input_data,dtype=dtype)          
-            elif _pycbc.HAVE_CUDA and (type(self._scheme) is 
+                    self._data = _numpy.array(input_data,dtype=dtype)
+            elif _pycbc.HAVE_CUDA and (type(self._scheme) is
                                        _scheme.CUDAScheme):
                 if type(input_data) is _cudaarray.GPUArray:
                     input_data = input_data.get()
                 self._data = _cudaarray.to_gpu(_numpy.array(input_data,
-                                                            dtype=dtype))       
-            elif _pycbc.HAVE_OPENCL and (type(self._scheme) is 
+                                                            dtype=dtype))
+            elif _pycbc.HAVE_OPENCL and (type(self._scheme) is
                                          _scheme.OpenCLScheme):
                 if type(input_data) is _openclarray.Array:
                     input_data = input_data.get()
@@ -137,12 +137,12 @@ class Array(object):
             else:
                 raise TypeError('Invalid Processing scheme Type')
 
-            #If no dtype was given, default to Double, and Double Complex. 
+            #If no dtype was given, default to Double, and Double Complex.
             if dtype is None:
                 if self._data.dtype.kind == 'c':
                     self._data = self._data.astype(_numpy.complex128)
                 else:
-                       self._data = self._data.astype(_numpy.float64)             
+                       self._data = self._data.astype(_numpy.float64)
 
     def _returntype(fn):
         """ Wrapper for method functions to return a PyCBC Array class """
@@ -163,21 +163,21 @@ class Array(object):
         @_functools.wraps(fn)
         def checked(self,other):
             if type(other) not in _ALLOWED_SCALARS and self._typecheck(other) is NotImplemented:
-                return NotImplemented     
+                return NotImplemented
             if isinstance(other,Array):
                 _convert_to_scheme(other)
                 other = other._data
             return fn(self,other)
-        return checked  
+        return checked
 
     def _typecheck(self,other):
         """ Additional typechecking for other. Stops Array from avoiding
-        checks of derived types 
-        """  
+        checks of derived types
+        """
         if type(other) is not Array:
             return NotImplemented
-    
-            
+
+
     def _convert(fn):
         # Convert this array to the current processing scheme
         @_functools.wraps(fn)
@@ -185,14 +185,14 @@ class Array(object):
             _convert_to_scheme(self)
             return fn(self,*args)
         return converted
-            
+
     @_returntype
     @_convert
-    @_checkother 
+    @_checkother
     def __mul__(self,other):
         """ Multiply by an Array or a scalar and return an Array. """
         return self._data * other
-    
+
     @_returntype
     @_convert
     @_checkother
@@ -206,90 +206,90 @@ class Array(object):
         """ Multiply by an Array or a scalar and return an Array. """
         self._data *= other
         return self
-    
+
     @_returntype
     @_convert
     @_checkother
     def __add__(self,other):
         """ Add Array to Array or scalar and return an Array. """
         return self._data + other
-        
+
     @_returntype
     @_convert
     @_checkother
     def __radd__(self,other):
         """ Add Array to Array or scalar and return an Array. """
         return self._data + other
-    
+
     @_convert
     @_checkother
     def __iadd__(self,other):
         """ Add Array to Array or scalar and return an Array. """
         self._data += other
         return self
-        
+
     @_convert
     @_checkother
-    @_returntype    
+    @_returntype
     def __div__(self,other):
         """ Divide Array by Array or scalar and return an Array. """
         return self._data / other
-        
+
     @_returntype
     @_convert
     @_checkother
     def __rdiv__(self,other):
         """ Divide Array by Array or scalar and return an Array. """
         return other / self._data
-        
+
     @_convert
     @_checkother
     def __idiv__(self,other):
         """ Divide Array by Array or scalar and return an Array. """
         self._data /= other
         return self
-        
+
     @_returntype
     @_convert
     @_checkother
     def __sub__(self,other):
         """ Subtract Array or scalar from Array and return an Array. """
-        return self._data - other 
-        
+        return self._data - other
+
     @_returntype
     @_convert
     @_checkother
     def __rsub__(self,other):
         """ Subtract Array or scalar from Array and return an Array. """
         return other - self._data
-        
-    @_convert   
+
+    @_convert
     @_checkother
     def __isub__(self,other):
         """ Subtract Array or scalar from Array and return an Array. """
         self._data -= other
         return self
-        
+
     @_returntype
     @_convert
     @_checkother
     def __pow__(self,other):
         """ Exponentiate Arrary by scalar """
         return self._data ** other
-    
-    @_returntype  
+
+    @_returntype
     @_convert
     def __abs__(self):
         """ Return absolute value of Array """
         return abs(self._data)
-        
+
     def __len__(self):
         """ Return length of Array """
         return len(self._data)
-        
+
     def __str__(self):
         return str(self._data)
-    
+
     @_returntype
     @_convert
     def real(self):
@@ -301,13 +301,13 @@ class Array(object):
     def imag(self):
         """ Return imaginary part of Array """
         return Array(self._data.imag,copy=True)
-    
+
     @_returntype
     @_convert
     def conj(self):
-        """ Return complex conjugate of Array. """ 
+        """ Return complex conjugate of Array. """
         return self._data.conj()
-        
+
     @_convert
     def sum(self):
         """ Return the sum of the the array. """
@@ -317,7 +317,7 @@ class Array(object):
             return _pycuda.gpuarray.sum(self._data).get().max()
         elif _pycbc.HAVE_OPENCL and type(self._data) is _openclarray.Array:
             return _pyopencl.array.sum(self._data).get().max()
-    
+
     @_convert
     @_checkother
     def dot(self,other):
@@ -328,7 +328,7 @@ class Array(object):
             return _pycuda.gpuarray.dot(self._data,other).get().max()
         elif _pycbc.HAVE_OPENCL and type(self._data) is _openclarray.Array:
             return _pyopencl.array.dot(self._data,other).get().max()
-    
+
     @_convert
     def __getitem__(self, index):
         if isinstance(index, slice):
@@ -346,7 +346,7 @@ class Array(object):
     def data(self):
         """Returns the internal python array """
         return self._data
- 
+
     @data.setter
     def data(self,other):
         dtype = None
@@ -354,7 +354,7 @@ class Array(object):
             dtype = other.dtype
         temp = Array(other,dtype=dtype)
         self._data = temp._data
-    
+
 
     @property
     @_convert
@@ -365,15 +365,15 @@ class Array(object):
         if _pycbc.HAVE_CUDA and type(self._data) is _cudaarray.GPUArray:
             return self._data.ptr
         if _pycbc.HAVE_OPENCL and type(self._data) is _openclarray.Array:
-            return self._data.data 
-        
+            return self._data.data
+
     @property
     @_convert
     def  lal(self):
         """ Returns a LAL Object that contains this data """
         lal_data = None
         if type(self._data) is not _numpy.ndarray:
-            raise TypeError("Cannot return lal type from the GPU") 
+            raise TypeError("Cannot return lal type from the GPU")
         elif self._data.dtype == float32:
             lal_data = _swiglal.XLALCreateREAL4Vector(len(self))
         elif self._data.dtype == float64:
@@ -381,8 +381,8 @@ class Array(object):
         elif self._data.dtype == complex64:
             lal_data = _swiglal.XLALCreateCOMPLEX8Vector(len(self))
         elif self._data.dtype == complex128:
-            lal_data = _swiglal.XLALCreateCOMPLEX16Vector(len(self))    
-       
+            lal_data = _swiglal.XLALCreateCOMPLEX16Vector(len(self))
+
         lal_data.data = self._data
         self._data = lal_data.data
 
