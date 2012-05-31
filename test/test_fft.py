7f5b7610a686d117c9a3c24b9058358b2fd16fc2
Andrew Miller <andrew.miller@ligo.org>
2012-05-31 13:15:50 +0200

    trying a different import

--- test/test_fft.py
+++ test/test_fft.py
@@ -27,6 +27,7 @@ These are the unittests for the pycbc.fft subpackage
 
 import pycbc
 import pycbc.scheme
+from pycbc.scheme import *
 import pycbc.array
 import pycbc.timeseries
 import pycbc.frequencyseries
@@ -568,7 +569,7 @@ if __name__ == '__main__':
         for klass in CUDATestClasses:
             suiteCUDA.addTest(unittest.makeSuite(klass))
 
-        with pycbc.scheme.CUDAScheme():
+        with CUDAScheme():
             unittest.TextTestRunner(verbosity=2).run(suiteCUDA)
 
     if pycbc.HAVE_OPENCL:
