bc1ee57f3e72ebc2510719dc0898334bd7b4f997
Andrew Miller <andrew.miller@ligo.org>
2012-06-25 17:05:00 +0200

    added name.

--- pycbc/types/array.py
+++ pycbc/types/array.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2012  Alex Nitz, Josh Willis
+# Copyright (C) 2012  Alex Nitz, Josh Willis, Andrew Miller
 #
 # This program is free software; you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by the
