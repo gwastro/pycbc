aa82e0055b42f84e68f5d1d15901b1679d434c6f
Alexander Harvey Nitz <ahnitz@syr.edu>
2012-07-15 01:05:04 -0400

    separated the test commands in setup.py into separate sub commands

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-# Copyright (C) 2012 Alex Nitz
+# Copyright (C) 2012 Alex Nitz , Andrew Miller
 #
 # This program is free software; you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by the
@@ -30,13 +30,14 @@ from distutils import sysconfig,file_util
 from distutils.core import setup,Command,Extension
 
 
-# ======== DISTUTILS CONFIGURATION AND BUILD SCRIPTS ==========================        
+# ======== DISTUTILS CONFIGURATION AND BUILD SCRIPTS ==========================   
+test_results = []     
 # Run all of the testing scripts
-class test(Command):
-    
+class TestBase(Command):
     user_options = []
     test_modules = []
     def initialize_options(self):
+        self.scheme = None
         self.build_dir = None
     def finalize_options(self):
         #Populate the needed variables
@@ -55,34 +56,52 @@ class test(Command):
         
     def run(self):
         # Get the list of cpu test modules
-        self.test_modules+= self.find_test_modules("test*.py")     
+        self.test_modules = self.find_test_modules("test*.py")     
         # Run from the build directory
         sys.path.insert(0,self.build_dir)
 
-        results = []
-        cuda_results = []
-        opencl_results = []
+        test_results.append("\n" + (self.scheme + " tests ").rjust(30))
         for test in self.test_modules:
-            a = subprocess.call(['python','test/'+test+'.py','-s', 'cpu'])
-            results.append([test,a])
-            a = subprocess.call(['python','test/'+test+'.py','-s', 'cuda'])
-            cuda_results.append([test,a])
-            a = subprocess.call(['python','test/'+test+'.py','-s', 'opencl'])
-            opencl_results.append([test,a])
-        
-        print "\n-----CPU Results-----"
-        for test in results:
-            print test[0] + " status - "+str(test[1])
-        if len(cuda_results) != 0:
-            print "\n-----CUDA Results-----"
-            for test in cuda_results:
-                print test[0] + " status - "+str(test[1])
-        if len(opencl_results) != 0:
-            print "\n-----OpenCL Results-----"
-            for test in opencl_results:
-                print test[0] + " status - "+str(test[1])
+            a = subprocess.call(['python','test/'+test+'.py','-s', self.scheme])
+            if a != 0:
+                result_str = str(test).ljust(30) + ": Fail : " + str(a)
+            else:
+                result_str = str(test).ljust(30) + ": Pass" 
+            test_results.append(result_str)
 
+class test(Command):
+    sub_commands = [('test_cpu',None),('test_cuda',None),('test_opencl',None)]
+    user_options = []
+    description = "run the available tests for all compute schemes (cpu,cuda,opencl)"
+    def initialize_options(self):
+        pass
+    def finalize_options(self):
+        pass
+    def run(self):
+        for cmd_name in self.get_sub_commands():
+            self.run_command(cmd_name)
+        for test in test_results:
+            print test
+    
+class test_cpu(TestBase):
+    description = "run all CPU tests"
+    def initialize_options(self):
+        TestBase.initialize_options(self)
+        self.scheme = 'cpu'
+    
+class test_cuda(TestBase):
+    description = "run CUDA tests"
+    def initialize_options(self):
+        TestBase.initialize_options(self)
+        self.scheme = 'cuda'
 
+class test_opencl(TestBase):
+    description = "run OpenCL tests"
+    def initialize_options(self):
+        TestBase.initialize_options(self)
+        self.scheme = 'opencl'
+    
+    
 # do the actual work of building the package
 setup (
     name = 'PyCBC',
@@ -90,7 +109,8 @@ setup (
     description = 'Gravitational wave CBC analysis toolkit',
     author = 'Ligo Virgo Collaboration - PyCBC team',
     url = 'https://sugwg-git.phy.syr.edu/dokuwiki/doku.php?id=pycbc:home',
-    cmdclass = { 'test'  : test ,},
+    cmdclass = { 'test'  : test , 'test_cpu':test_cpu,'test_cuda':test_cuda,
+                 'test_opencl':test_opencl},
     ext_modules = [],
 	requires = ['swiglal'],
     packages = ['pycbc','pycbc.fft','pycbc.types'],
