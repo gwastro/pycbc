f002f22ecd4bbde4d438a26d1319e3262a3d283c
Alexander Harvey Nitz <ahnitz@syr.edu>
2012-08-13 22:55:54 -0400

    removed debugging statement

--- pycbc/__init__.py
+++ pycbc/__init__.py
@@ -1,6 +1,31 @@
+# Copyright (C) 2012  Alex Nitz
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 3 of the License, or (at your
+# option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+# Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+
+#
+# =============================================================================
+#
+#                                   Preamble
+#
+# =============================================================================
+#
 """PyCBC contains a toolkit for CBC gravitational wave analysis
 
 """
+
 # Check for optional components of the PyCBC Package
 
 try:
@@ -19,3 +44,11 @@ except ImportError:
 # PYCBC Specfic Constants
 
 DYN_RANGE_FAC =  5.9029581035870565e+20
+
+from pycbc.filter import *
+from pycbc.waveform import *
+from pycbc.fft import *
+from pycbc.types import *
+from pycbc.scheme import *
+from pycbc.frame import *
+
--- pycbc/waveform/TaylorF2.py
+++ pycbc/waveform/TaylorF2.py
@@ -5,6 +5,7 @@ import pycuda.tools
 from pycuda.elementwise import ElementwiseKernel
 from pycbc.types import FrequencySeries,zeros
 from pycuda.gpuarray import to_gpu
+from numpy import sqrt
 
 pntype = numpy.dtype([('pfaN', numpy.float64),
                     ('pfa2', numpy.float64),
@@ -16,27 +17,43 @@ pntype = numpy.dtype([('pfaN', numpy.float64),
                     ('pfl6', numpy.float64),
                     ('pfa7', numpy.float64),
                     ('delta_f',numpy.float64),
+                    ('FTaN',numpy.float64),
+                    ('piM',numpy.float64),
+                    ('amp0',numpy.float64),
+                    ('dEtaN',numpy.float64),
+                    ('kmin',numpy.float64),
+                    ('phi0',numpy.float64),
+                    ('tC',numpy.float64),
                     ('phase0', numpy.int64),
                      ])
 pycuda.tools.register_dtype(pntype,"pntype")
 
-def get_taylorf2_pn_coefficients(mass1,mass2,beta =0 , sigma = 0,lambda_tides=0):
+def get_taylorf2_pn_coefficients(mass1,mass2,distance,beta =0 , sigma = 0):
     
     M = float(mass1) + float(mass2)
     eta = mass1 * mass2 / (M * M)
     theta = -11831./9240.;
-
+    lambdaa = -1987./3080.0;
     pfaN = 3.0/(128.0 * eta);
     pfa2 = 5*(743.0/84 + 11.0 * eta)/9.0;
     pfa3 = -16.0*lal.LAL_PI + 4.0*beta;
     pfa4 = 5.0*(3058.673/7.056 + 5429.0/7.0 * eta + 617.0 * eta*eta)/72.0 - 10.0*sigma
     pfa5 = 5.0/9.0 * (7729.0/84.0 - 13.0 * eta) * lal.LAL_PI
     pfl5 = 5.0/3.0 * (7729.0/84.0 - 13.0 * eta) * lal.LAL_PI
-    pfa6 = (11583.231236531/4.694215680 - 640.0/3.0 * lal.LAL_PI * lal.LAL_PI - 6848.0/21.0*lal.LAL_GAMMA) + eta * (-15335.597827/3.048192 + 2255./12. * lal.LAL_PI * lal.LAL_PI - 1760./3.*theta +12320./9.*lambda_tides) + eta*eta * 76055.0/1728.0 - eta*eta*eta*  127825.0/1296.0 
+    pfa6 = (11583.231236531/4.694215680 - 640.0/3.0 * lal.LAL_PI * lal.LAL_PI - 6848.0/21.0*lal.LAL_GAMMA) + eta * (-15335.597827/3.048192 + 2255./12. * lal.LAL_PI * lal.LAL_PI - 1760./3.*theta +12320./9.*lambdaa) + eta*eta * 76055.0/1728.0 - eta*eta*eta*  127825.0/1296.0 
     pfl6 = -6848.0/21.0;
     pfa7 = lal.LAL_PI * 5.0/756.0 * ( 15419335.0/336.0 + 75703.0/2.0 * eta - 14809.0 * eta*eta)
 
-    pn_coefficients = numpy.array((pfaN,pfa2,pfa3,pfa4,pfa5,pfl5,pfa6,pfl6,pfa7,0,0),dtype=pntype)
+    FTaN = 32.0 * eta*eta / 5.0;
+
+    dETaN = -eta;
+  
+    amp0 = 4. * mass1 * mass2 / float(distance) * lal.LAL_MRSUN_SI * lal.LAL_MTSUN_SI * sqrt(lal.LAL_PI/12.0)    
+    
+    m_sec = M * lal.LAL_MTSUN_SI;
+    piM = lal.LAL_PI * m_sec; 
+
+    pn_coefficients = numpy.array((pfaN,pfa2,pfa3,pfa4,pfa5,pfl5,pfa6,pfl6,pfa7,0,FTaN,piM,amp0,dETaN,0,0,0,0),dtype=pntype)
 
     return pn_coefficients
 
@@ -54,17 +71,22 @@ double pfa6;
 double pfl6;
 double pfa7;
 double delta_f;
+double FTaN;
+double piM;
+double amp0;
+double dETaN;
+double kmin;
+double phi0;
+double tC;
 long phaseO;
 };
 """
  
 taylorf2_text = """
 
-    const double m_sec = 1 ;//m * LAL_MTSUN_SI; 
-    const double piM = LAL_PI * m_sec;
-    const double f = i * cf->delta_f;
-    const double v0 = cbrt(piM * 14.0); //FIXME
-    const double v = cbrt(piM*f);
+    const double f = (i +cf->kmin) * cf->delta_f;
+    const double v0 = cbrt(cf->piM * 14.0);
+    const double v = cbrt(cf->piM*f);
     const double v2 = v * v;
     const double v3 = v * v2;
     const double v4 = v * v3;
@@ -78,9 +100,8 @@ taylorf2_text = """
     double dEnergy = 0.;
     double flux = 0.;
     double amp;
-    double amp0 = 1; // FIXME
-    double shft = 0; // FIXME
-    double phi0 = 0; // FIXME
+    double shft = -LAL_TWOPI * cf->tC;
+    double phi0 = cf->phi0;
 
     switch (cf->phaseO)
     {
@@ -108,11 +129,11 @@ taylorf2_text = """
     dEnergy+=1.0;
 
     phasing *= cf->pfaN / v5;
-//    flux *= FTaN * v10;
-//    dEnergy *= dETaN * v;
+    flux *= cf->FTaN * v10;
+    dEnergy *= cf->dETaN * v;
 
     phasing += shft * f + phi0;
-    amp = amp0 * v; //FIXME
+    amp = cf->amp0 * sqrt(-dEnergy/flux) * v;
     htilde[i]._M_re = amp * cos(phasing + LAL_PI_4) ;
     htilde[i]._M_im = - amp * sin(phasing + LAL_PI_4);
 
@@ -121,13 +142,24 @@ taylorf2_text = """
 taylorf2_kernel = ElementwiseKernel("pycuda::complex<double> *htilde, pntype *cf",
                     taylorf2_text, "taylorf2_kernel",preamble=preamble)
 
-def taylorf2(length,delta_f,mass1,mass2,fstart,phiC=0,tC=0,distance=1,phase_order=7,beta =0 , sigma = 0,lambda_tides=0):
-    pn_const = get_taylorf2_pn_coefficients(mass1,mass2,beta,sigma,lambda_tides)
+def taylorf2(length,delta_f,mass1,mass2,fstart,phiC=0,tC=0,distance=1,phase_order=7,beta =0 , sigma = 0 ):
+    pn_const = get_taylorf2_pn_coefficients(mass1,mass2,distance,beta,sigma)
     pn_const['phase0'] = int(phase_order)
     pn_const['delta_f'] = delta_f
-    kmin = fstart / float(delta_f)
+    pn_const['phi0']=phiC
+    pn_const['tC']=tC
+
+    kmin = int(fstart / float(delta_f))
+
+    piM = pn_const['piM']
+    vISCO = 1. / sqrt(6.);
+    fISCO = vISCO * vISCO * vISCO / piM;
+    kmax = int(fISCO / delta_f)
+    pn_const['kmin'] = kmin
+    print vISCO, piM,fISCO
+    print kmin ,kmax
     htilde = FrequencySeries(zeros(length,dtype=numpy.complex128),delta_f = delta_f, copy=False)
-    taylorf2_kernel(htilde.data,to_gpu(pn_const))
+    taylorf2_kernel(htilde.data[kmin:kmax],to_gpu(pn_const))
     return htilde
     
 
