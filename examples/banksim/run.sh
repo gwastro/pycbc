99c6c335bdb07a7063dd47b1e63762a3eadb4c29
Alexander Harvey Nitz <ahnitz@syr.edu>
2012-09-14 12:20:06 -0400

    added separate amplitude and phase order input to banksim.py and faith.py

--- examples/banksim/run.sh
+++ examples/banksim/run.sh
@@ -4,10 +4,12 @@ python ../../scripts/banksim.py \
 --psd iLIGOModel \
 --match-file cpu.dat \
 --template-approximant="SpinTaylorT4" \
---template-order=7 \
+--template-phase-order=7 \
+--template-amplitude-order=7 \
 --template-start-frequency=40 \
 --signal-approximant="SpinTaylorT4" \
---signal-order=7 \
+--signal-phase-order=7 \
+--signal-amplitude-order=7 \
 --signal-start-frequency=40 \
 --filter-low-frequency=40 \
 --filter-sample-rate=4096 \
