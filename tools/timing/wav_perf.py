43d268902db1721971396d4009937549d031175b
Alexander Harvey Nitz <ahnitz@syr.edu>
2012-10-04 12:47:10 -0400

    added timing tool for tf2 cuda

---
+++ tools/timing/wav_perf.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python
+from pycbc.scheme import *
+from pycbc.types import *
+from pycbc.waveform import *
+import pycbc
+from optparse import OptionParser
+from math import sin, log
+import gc
+parser = OptionParser()
+
+parser.add_option('--scheme','-s',  type = 'choice', 
+                    choices = ('cpu','cuda','opencl'), 
+                    default = 'cpu', dest = 'scheme', 
+                    help = 'specifies processing scheme, can be cpu [default], cuda, or opencl')
+
+parser.add_option('--device-num','-d', action='store', type = 'int', 
+                    dest = 'devicenum', default=0,
+                    help = 'specifies a GPU device to use for CUDA or OpenCL, 0 by default')
+
+parser.add_option('--deltaf',type=float, help='frequency step')
+parser.add_option('--iterations', type=int, help='Number of iterations to perform')
+          
+(options, args) = parser.parse_args()   
+
+#Changing the optvalues to a dict makes them easier to read
+_options = vars(options)
+
+if _options['scheme'] == 'cpu':
+    ctx = DefaultScheme()
+if _options['scheme'] == 'cuda':
+    ctx = CUDAScheme(device_num=_options['devicenum'])
+if _options['scheme'] == 'opencl':
+    ctx = OpenCLScheme(device_num=_options['devicenum'])
+
+
+niter = options.iterations
+
+
+if type(ctx) is CUDAScheme:
+    print "RUNNING ON ", ctx.device.name()
+else:
+    print "RUNNING ON CPU"
+
+
+with ctx:
+    wf_taylor = get_fd_waveform(mass1=1, mass2=1, f_lower=14, 
+                                approximant='TaylorF2', delta_f=options.deltaf)
+
+def taylorf2():
+    with ctx:
+        for i in range(0,niter):
+            wf_taylor = get_fd_waveform(mass1=1, mass2=1, f_lower=14, 
+                                        approximant='TaylorF2', delta_f=options.deltaf)
+
+
+import timeit
+gt = timeit.Timer(taylorf2)
+t = (1000 * gt.timeit(number=1)/niter)
+print "TF2 %.2f msec" % t, " %5.1f gen/min " % (1000 *60 /t)
+
+
+
+
+
