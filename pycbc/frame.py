f2d63007c1392bfc1b1d082e96d6b8f7c919ff7d
Alexander Harvey Nitz <ahnitz@syr.edu>
2013-01-23 12:54:02 -0500

    rewrite frame.py to use lalframe, unittests now broken

--- pycbc/frame.py
+++ pycbc/frame.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2012 Andrew Miller
+# Copyright (C) 2012 Andrew Miller, Alex Nitz
 #
 # This program is free software; you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by the
@@ -23,106 +23,103 @@
 # =============================================================================
 #
 """
-This modules uses PyLAL to read in a frame or cache, and returns a pycbc TimeSeries.
+This modules contains functions for reading in data from frame files or caches
 """
+import lalframe
+import numpy
+import os.path
+from pycbc.types import TimeSeries
+import copy
 
-import pycbc
-import pycbc.types
-from glue import lal
-from pylal import Fr, frutils
-import lal as _lal
-import numpy as _numpy
+FrMap = {                                                  
+lalframe.LAL_FRAMEU_FR_VECT_2S:[lalframe.FrReadINT2TimeSeries, numpy.int16],                                                   
+lalframe.LAL_FRAMEU_FR_VECT_4S:[lalframe.FrReadINT4TimeSeries, numpy.int32],  
+lalframe.LAL_FRAMEU_FR_VECT_8S:[lalframe.FrReadINT8TimeSeries, numpy.int64],                       
+lalframe.LAL_FRAMEU_FR_VECT_4U:[lalframe.FrReadREAL4TimeSeries, numpy.float32], 
+lalframe.LAL_FRAMEU_FR_VECT_8U:[lalframe.FrReadREAL8TimeSeries, numpy.float64],                   
+lalframe.LAL_FRAMEU_FR_VECT_8C:[lalframe.FrReadCOMPLEX8TimeSeries, numpy.complex64],    
+lalframe.LAL_FRAMEU_FR_VECT_16C:[lalframe.FrReadCOMPLEX16TimeSeries, numpy.complex128],                                                           
+}
 
-def read_frame(filename, channels, start=None, end=None):
-    if start is not None and end is not None:
-        if type(start) is _lal.LIGOTimeGPS:
-            if start.gpsNanoSeconds != 0:
-                raise ValueError('start and end times must be integer valued')
-            else:
-                start = start.gpsSeconds
-        else:
-            if int(start) != start:
-                raise ValueError('start and end times must be integer valued')
-            else:
-                start = int(start)                
-        if type(end) is _lal.LIGOTimeGPS:
-            if end.gpsNanoSeconds != 0:
-                raise ValueError('start and end times must be integer valued')
-            else:
-                end = end.gpsSeconds
-        else:
-            if int(end) != end:
-                raise ValueError('start and end times must be integer valued')
-            else:
-                end = int(end)             
-        span = end - start
-        if span <= 0:
-            raise ValueError('beginning must be before end')
+FRAME_SAMPLE_RATE = 16384
+
+
+def _read_channel(channel, stream, start, duration):
+    channel_type = lalframe.FrGetTimeSeriesType(channel, stream)
+    read_func = FrMap[channel_type][0]
+    d_type = FrMap[channel_type][1]
+    data = read_func(stream, channel, start, duration, 0)
+    return TimeSeries(data.data.data, delta_t=data.deltaT, epoch=start, dtype=d_type)
+
+def read_frame(location, channels, start_time=None, end_time=None, duration=None):
+    """Read time series from frame data.
+
+    Using a the `location`, which can either be a frame file ".gwf" or a 
+    frame cache ".gwf", read in the data for the given channel(s) and output
+    as a TimeSeries or list of TimeSeries. 
+
+    Parameters
+    ----------
+    location : string
+        Either a frame filename (can include pattern) or the cache filename.  
+    channels : string or list of strings
+        Either a string that contains the channel name or a list of channel name
+        strings.
+    start_time : {None, LIGOTimeGPS}, optional
+        The gps start time of the time series. Defaults to reading from the 
+        beginning of the available frame(s). 
+    end_time : {None, LIGOTimeGPS}, optional
+        The gps end time of the time series. Defaults to the end of the frame(s).
+        Note, this argument is incompatible with `duration`.
+    duration : {None, float}, optional
+        The amount of data to read in seconds. Note, this argument is incompatible
+        with `end`.
+
+    Returns
+    -------
+    Frame Data: TimeSeries or list of TimeSeries
+        A TimeSeries or a list of TimeSeries, corresponding to the data from the
+        frame file/cache for a given channel or channels. 
+    """
+
+    dir_name, file_name = os.path.split(location)
+    base_name, file_extension = os.path.splitext(file_name)
+
+    if file_extension == ".lcf":
+        cache = lalframe.FrImportCache(location)
+        stream = lalframe.FrCacheOpen(cache)
+    elif file_extension == ".gwf": 
+        stream = lalframe.FrOpen(dir_name, file_name)
     else:
-        start = -1
-        span = -1
+        raise TypeError("Invalid location name")
 
-    if type(channels) is list:
-        ts = []
+    if end  and duration:
+        raise ValueError("end time and duration are mutually exclusive")
+
+    if start is None:
+        start = stream.epoch*1
+
+    if end is None:
+        if type(channels) is list:
+            end = start + lalframe.FrGetVectorLength(channels[0], stream) / FRAME_SAMPLE_RATE
+        else:
+            end = start + lalframe.FrGetVectorLength(channels, stream) / FRAME_SAMPLE_RATE
+
+    if duration is None:
+        duration = float(end-start)
+
+    if type(channels) is list: 
+        all_data = []
         for channel in channels:
-            frdata = Fr.frgetvect1d(filename, channel, start, span)
-            ts.append(pycbc.types.TimeSeries(initial_array=frdata[0],
-                                            delta_t=frdata[3],
-                                            epoch=_lal.LIGOTimeGPS(frdata[1]),
-                                            copy=False))
+            print float(stream.epoch), float(start)
+            channel_data = _read_channel(channel, stream, start, duration)
+            lalframe.FrSeek(stream, start)
+            all_data.append(channel_data)
+        return all_data
     else:
-        frdata = Fr.frgetvect1d(filename, channels, start, span)
-        ts = pycbc.types.TimeSeries(initial_array=frdata[0],
-                                    delta_t=frdata[3],
-                                    epoch=_lal.LIGOTimeGPS(frdata[1]),
-                                    copy=False)
-    return ts
+        return _read_channel(channels, stream, start, duration)
 
-def read_cache(filename, channels, start, end):
-    if type(start) is _lal.LIGOTimeGPS:
-        if start.gpsNanoSeconds != 0:
-            raise ValueError('start and end times must be integer valued')
-        else:
-            start = start.gpsSeconds
-    else:
-        if int(start) != start:
-            raise ValueError('start and end times must be integer valued')
-        else:
-            start = int(start)                
-    if type(end) is _lal.LIGOTimeGPS:
-        if end.gpsNanoSeconds != 0:
-            raise ValueError('start and end times must be integer valued')
-        else:
-            end = end.gpsSeconds
-    else:
-        if int(end) != end:
-            raise ValueError('start and end times must be integer valued')
-        else:
-            end = int(end)
-    span = end - start
-    if span <= 0:
-        raise ValueError('beginning must be before end')
-    with open(filename,'r') as f:
-        lal_cache = lal.Cache.fromfile(f)
-    frdata = frutils.FrameCache(lal_cache)
-    if type(channels) is list:
-        ts = []
-        for channel in channels:
-            data = frdata.fetch(channel,start,end)
-            dt = data.metadata.dt
-            # Now we actually create the new TimeSeries, and append it to the list
-            ts.append(pycbc.types.TimeSeries(initial_array=data.A,
-                                                delta_t=dt,
-                                                epoch=_lal.LIGOTimeGPS(data.metadata.segments[0][0]),
-                                                copy=False))
-    else:
-        data = frdata.fetch(channels,start,end)
-        dt = data.metadata.dt
-        ts = pycbc.types.TimeSeries(initial_array=data.A,
-                                    delta_t=dt,
-                                    epoch=_lal.LIGOTimeGPS(data.metadata.segments[0][0]),
-                                    copy=False)
-    return ts
-        
+    
+
         
     
