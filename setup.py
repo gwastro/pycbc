cbd6aea4f1c257182ce31e0fd1082db2110ed05f
Alexander Harvey Nitz <ahnitz@syr.edu>
2013-08-01 15:55:18 -0400

    fix up in pycbc_split_table

--- setup.py
+++ setup.py
@@ -238,6 +238,6 @@ setup (
     ext_modules = [lalwrap_module, testlalwrap_module],
     requires = ['lal'],
     scripts = ['bin/pycbc_banksim', 'bin/pycbc_faithsim', 'bin/pycbc_inspiral', 
-               'bin/pycbc_banksim_match_combine', 'bin/pycbc_make_banksim', 'bin/pycbc_split_table'],
+               'bin/pycbc_make_banksim', 'bin/pycbc_split_table'],
     packages = ['pycbc','pycbc.fft','pycbc.types','pycbc.filter','pycbc.psd','pycbc.waveform','pycbc.events','pycbc.noise','pycbc.vetoes'],
 )
