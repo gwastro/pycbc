4654c924f14b665ae2eef69c450ff522b4f7d79e
Ian Harry <ian.harry@ligo.org>
2014-01-31 14:29:34 -0500

    - Adding tags, if required, to injection_utils - Adding ability to use section names other than 'injections'. Changing default from 'injection' to 'injections' to match expected naming in ihope

--- pycbc/ahope/injection_utils.py
+++ pycbc/ahope/injection_utils.py
@@ -6,21 +6,25 @@ from pycbc.ahope.matchedfltr_utils import *
 from glue import segments
 
 def setup_injection_workflow(workflow, science_segs, datafind_outs, 
-                             tmplt_banks, start_time, end_time, output_dir=None):
+                             tmplt_banks, start_time, end_time, output_dir=None,
+                             injSectionName='injections', tags =[]):
     '''
     Setup matched filter with injections section of ahope workflow.
     FIXME: ADD MORE DOCUMENTATION
     '''
     logging.info("Entering injection module.")
-    inj_exe = LalappsInspinjExec('injection')
+    # FIXME: Add ability to specify different exes
+    inj_exe = LalappsInspinjExec(injSectionName)
     all_sec = workflow.cp.sections()
-    sections = [sec for sec in all_sec if sec.startswith('injection-')]
+    sections = [sec for sec in all_sec if sec.startswith(injSectionName +'-')]
     inj_tags = []
     inj_files = []
     for sec in sections:  
         segment = segments.segment(start_time, end_time)
         inj_tag = sec.split('-')[1]
-        inj_job = inj_exe.create_job(workflow.cp, tags=[inj_tag], out_dir=output_dir)
+        curr_tags = tags + [inj_tag]
+        inj_job = inj_exe.create_job(workflow.cp, tags=curr_tags,
+                                     out_dir=output_dir)
         node = inj_job.create_node(segment)
         workflow.add_node(node)
         injection_file = node.output_files[0]
