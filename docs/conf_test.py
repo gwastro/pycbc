1a977c138f0e50a9464199ce3173db95dfe8ffdb
Alexander Harvey Nitz <ahnitz@syr.edu>
2014-02-06 18:32:31 -0500

    remove mathjax module while it causes https errors

--- docs/conf_test.py
+++ docs/conf_test.py
@@ -27,7 +27,7 @@ import sys, os
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 
           'sphinx.ext.intersphinx', 'sphinx.ext.coverage', 
-              'sphinx.ext.mathjax', 'sphinx.ext.viewcode', 
+             'sphinx.ext.viewcode', 
             'sphinxcontrib.programoutput',
          'numpydoc',           'matplotlib.sphinxext.mathmpl',
           'matplotlib.sphinxext.only_directives',
