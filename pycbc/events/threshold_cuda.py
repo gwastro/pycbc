d39a937efd67fa199854cc648183fa5c59c89bf3
Alexander Harvey Nitz <ahnitz@syr.edu>
2014-02-24 13:53:22 -0500

    add new threshold method

--- pycbc/events/threshold_cuda.py
+++ pycbc/events/threshold_cuda.py
@@ -23,6 +23,7 @@
 #
 import numpy
 from pycbc.types import zeros, Array
+from pycuda import gpuarray, driver
 from pycuda.gpuarray import to_gpu, empty
 from pycuda.tools import get_or_register_dtype, dtype_to_ctype
 from pycuda.elementwise import ElementwiseKernel
@@ -154,7 +155,191 @@ def threshold_with_gpu_abs(series, value):
     pycbc.scheme.mgr.state.context.synchronize()
     tgt = numpy.where(loc == 1)[0]
     return tgt, val[tgt]
+    
+# Do the thresholding using a block-level scan operation within the kernel
+nt = 128
+gs = 32
 
+kernel2 = """
+#include <stdio.h>
+#define GS %s
+#define NT %s
+ 
+__global__ void threshold_comb(float2* in, unsigned int* outl, unsigned int* outl2, float2* outv,  unsigned int* bn, unsigned int* n){ 
+
+    // should be number of blocks in the threshold_seg kernel, we guess here instead
+    // which is ok as long as it is greater
+    __shared__ int loc[2048];
+    int li = threadIdx.x;  
+        
+    int tt = 0;
+    loc[li] = bn[li];
+    __syncthreads();   
+       
+    for (int j=0; j<blockIdx.x; j++)
+        tt += loc[j];
+    
+    int rn = loc[blockIdx.x];
+    for (int r=li; r<rn; r+=blockDim.x){
+        int loc = outl[blockIdx.x*GS*NT + r];
+        outl2[tt + r] =  loc;
+        outv[tt + r] = in[loc];
+    }
+
+    if (blockIdx.x == (gridDim.x-1) && threadIdx.x == 0)
+        n[0] = tt + rn;
+}           
+"""  % (gs, nt)
+mod = SourceModule(kernel2)
+stuff2 = mod.get_function("threshold_comb")
+
+kernel = """
+#include <stdio.h>
+
+#define GS %s
+#define NT %s
+
+__global__ void threshold_seg(float2 *in, unsigned int* outl, unsigned int* bn, float threshold, unsigned int length){  
+    float2 val;
+    int above, tmp;
+    int s = blockIdx.x * blockDim.x * GS;
+    int i = s + threadIdx.x;
+    int region = s + GS * blockDim.x;
+    int li = threadIdx.x;
+    int lio = li + NT;
+    
+    volatile __shared__ int c;
+    volatile __shared__ unsigned int loc[NT * 2];
+    
+    if (li == 0)
+        c = 0;
+   
+    for (; i<region; i+=blockDim.x){
+    
+        // Do the thresholding for a NUM_THREAD section of memory
+        // Above threshold is stored as a 1 in shared memory       
+        if (i < length){
+            val = in[i];
+            above =  (val.x*val.x + val.y*val.y > threshold)?1:0;
+        } else
+            above =0;
+        
+        loc[lio] = above;
+        loc[li]=0;
+        __syncthreads();
+              
+        // Do a cumulative sum of the shared memory above threshold flags
+        // The cumsum is used to find where to store the locations of the
+        // above threshold regions within the scope of this block
+        // Be caeful ever touching this section, the syncthreads are really
+        // important!!
+        
+        // Note that this is hardcoded to the block size (number of threads)
+              
+        tmp = loc[lio] +  loc[lio - 1 ];
+        __syncthreads();
+        loc[lio] = tmp;
+        __syncthreads();
+        
+        tmp = loc[lio] +  loc[lio - 2 ];
+        __syncthreads();
+        loc[lio] = tmp;
+        __syncthreads();
+        
+        tmp = loc[lio] +  loc[lio - 4 ];
+        __syncthreads();
+        loc[lio] = tmp;
+        __syncthreads();
+        
+        tmp = loc[lio] +  loc[lio - 8 ];
+        __syncthreads();
+        loc[lio] = tmp;
+        __syncthreads();
+        
+        tmp = loc[lio] +  loc[lio - 16 ];
+        __syncthreads();
+        loc[lio] = tmp;
+        __syncthreads();
+        
+        tmp = loc[lio] +  loc[lio - 32 ];
+        __syncthreads();
+        loc[lio] = tmp;
+        __syncthreads();
+        
+        tmp = loc[lio] +  loc[lio - 64];
+        __syncthreads();
+        loc[lio] = tmp;
+        __syncthreads();
+        
+        
+        //tmp = loc[lio] +  loc[lio - 128 ];
+        //__syncthreads();
+        //loc[lio] = tmp;
+        //__syncthreads();
+
+        // Store the index value of a trigger now that we know where to put it
+        if (above)
+            outl[c + s + loc[lio] - 1] = i;   
+        
+        if (threadIdx.x == 0)
+            c += loc[NT*2-1];
+    }
+    if (threadIdx.x == 0)
+        bn[blockIdx.x] = c;
+
+}
+""" % (gs, nt)
+mod = SourceModule(kernel)
+stuff = mod.get_function("threshold_seg")
+
+# Workspace memory needed for this kernel
+_dn = None
+_n = None
+_bn = None
+_loc_tmp = None
+_loc_out = None
+_val_out = None
+_val = None
+_loc = None
+
+def threshold_integrated(series, value):
+    global _dn, _n, _bn, _loc_tmp, _loc_out, _val_out, _loc, _val
+        
+    t = numpy.float32(value**2)
+    nb = int(numpy.ceil(float(len(series))/nt/gs))
+    
+    if _bn is None or len(_bn) < nb:
+        _bn = gpuarray.zeros(nb, dtype=numpy.uint32)
+        
+    if _n is None:
+        _n = driver.pagelocked_empty((1), numpy.uint32, mem_flags=drv.host_alloc_flags.DEVICEMAP)
+        ptr = numpy.intp(_n.base.get_device_pointer())
+        class T():
+            pass
+        _dn = T()
+        _dn.gpudata = ptr
+        _dn.flags = _n.flags
+        
+    if _loc_tmp is None or len(series) > len(_loc_tmp):
+        _loc_tmp = gpuarray.zeros(len(series), dtype=numpy.uint32)
+        _loc_out = gpuarray.zeros(len(series), dtype=numpy.uint32)
+        _val_out = gpuarray.zeros(len(series), dtype=series.dtype)
+        _val = driver.pagelocked_empty((4096*256), numpy.complex64)
+        _loc = driver.pagelocked_empty((4096*256), numpy.uint32)
+    
+    #Do the thresholding by block
+    stuff(series.data, _loc_tmp, _bn, t, numpy.uint32(len(series)), block=(nt, 1, 1), grid=(nb, 1))
+    
+    # Recombine the blocks into a final output
+    stuff2(series.data, _loc_tmp, _loc_out, _val_out, _bn, _dn, block=(nb, 1, 1), grid=(nb, 1))
+    
+    # We need to get the data back now
+    pycbc.scheme.mgr.state.context.synchronize()
+    if _n != 0: 
+        driver.memcpy_dtoh_async(_val[0:_n], _val_out.gpudata)
+        driver.memcpy_dtoh_async(_loc[0:_n], _loc_out.gpudata)
+        pycbc.scheme.mgr.state.context.synchronize()
+    return _loc[0:_n], _val[0:_n]
 
 # Do thresholding entirely on the CPU
 def threshold_on_cpu(series, value):
@@ -164,7 +349,7 @@ def threshold_on_cpu(series, value):
 
 
 # Select which of the thresholding methods we want to use out of the above.
-threshold = threshold_on_cpu
+threshold = threshold_integrated
 
 threshold_cluster_mod = SourceModule("""
 #include<pycuda-complex.hpp>
