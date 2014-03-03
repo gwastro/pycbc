2ec44a544b07a423d4b63b045e8bf99526e48701
Ian Harry <ian.harry@ligo.org>
2014-03-03 17:03:05 -0500

    Update dailt test example

--- examples/ahope/data_checker/daily_test.py
+++ examples/ahope/data_checker/daily_test.py
@@ -1,6 +1,36 @@
+#!/usr/bin/env python
+
+# Copyright (C) 2013 Ian W. Harry
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 3 of the License, or (at your
+# option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+# Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+"""
+Description of daily_test
+"""
+
+import pycbc
+import pycbc.version
+__author__  = "Ian Harry <ian.harry@astro.cf.ac.uk>"
+__version__ = pycbc.version.git_verbose_msg
+__date__    = pycbc.version.date
+__program__ = "daily_test"
+
 import os
 import copy
 import logging
+import argparse
 from glue import pipeline
 from glue import segments
 import pycbc.ahope as ahope
@@ -8,12 +38,14 @@ import pycbc.ahope as ahope
 logging.basicConfig(format='%(asctime)s:%(levelname)s : %(message)s', \
                     level=logging.INFO,datefmt='%I:%M:%S')
 
-# This is the "official" start according to Stuart
-start_time = 1073822416
-# End times set as current time - one day (86400s)
-end_time= 1077465683
+# command line options
+_desc = __doc__[1:]
+parser = argparse.ArgumentParser(description=_desc)
+parser.add_argument('--version', action='version', version=__version__)
+ahope.add_ahope_command_line_group(parser)
+args = parser.parse_args()
 
-workflow = ahope.Workflow(['./daily_er5.ini'], start_time, end_time)
+workflow = ahope.Workflow(args)
 currDir = os.getcwd()
 segDir = os.path.join(currDir,"segments")
 dfDirSYR = os.path.join(currDir,"datafindSYR")
---
+++ examples/ahope/data_checker/run_daily_test.sh
@@ -0,0 +1,1 @@
+./daily_test.py --config-files daily_er5.ini --config-overrides ahope:start-time:1073822416 ahope:end-time:1077825735
