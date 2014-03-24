603e666edeadf4a3b19d9deafe5f33a6b421c9b5
Alexander Harvey Nitz <ahnitz@syr.edu>
2014-03-24 16:40:10 -0400

    update pycbc_geom_aligned_2dstack to argparse

--- examples/tmpltbank/testStoch3.sh
+++ examples/tmpltbank/testStoch3.sh
@@ -1,2 +1,2 @@
 # About 412 templates expected
-pycbc_aligned_stoch_bank -v --pn-order onePN --f0 60 --f-low 30 --delta-f 0.1 --min-match 0.97 --min-mass1 2.5 --max-mass1 3 --min-mass2 2.5 --max-mass2 3 --max-ns-spin-mag 0.05 --max-bh-spin-mag 0.05 --nsbh-flag --verbose --asd-file ZERO_DET_high_P.txt --num-seeds 10000 --output-file "testStoch.xml" --f-upper 2000
+pycbc_aligned_stoch_bank --verbose --pn-order onePN --f0 60 --f-low 30 --delta-f 0.1 --min-match 0.97 --min-mass1 2.5 --max-mass1 3 --min-mass2 2.5 --max-mass2 3 --max-ns-spin-mag 0.05 --max-bh-spin-mag 0.05 --nsbh-flag --verbose --asd-file ZERO_DET_high_P.txt --num-seeds 10000 --output-file "testStoch.xml" --f-upper 2000
