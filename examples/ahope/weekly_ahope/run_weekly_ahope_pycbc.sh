1067293ba9c45c82949c51476a71035326fafc67
Alexander Harvey Nitz <alex.nitz@ligo.org>
2014-04-02 17:46:23 -0400

    re-add the steps to generate the dax within the weekly_ahope examples

--- examples/ahope/weekly_ahope/run_weekly_ahope_pycbc.sh
+++ examples/ahope/weekly_ahope/run_weekly_ahope_pycbc.sh
@@ -4,4 +4,24 @@ export LOGPATH=/usr1/${USER}/log
 export PIPEDOWNLOG=/usr1/${USER}
 export HTMLDIR=/home/${USER}/public_html/ahope/development/weekly_ahope/test
 mkdir -p $LOGPATH
-python weekly_ahope.py --config-files weekly_ahope_pycbc.ini pipedown.ini inj_pycbc.ini --config-overrides ahope:start-time:${GPS_START_TIME} ahope:end-time:${GPS_END_TIME} ahope:ahope-html-basedir:${HTMLDIR} ahope:pipedown-tmp-space:${PIPEDOWNLOG} ahope:pipedown-log-path:${LOGPATH}
+
+# Generate the workflow
+python weekly_ahope.py --config-files weekly_ahope_pycbc.ini pipedown.ini inj_pycbc.ini \
+--config-overrides ahope:start-time:${GPS_START_TIME} \
+ahope:end-time:${GPS_END_TIME} \
+ahope:ahope-html-basedir:${HTMLDIR} \
+ahope:pipedown-tmp-space:${PIPEDOWNLOG} \
+ahope:pipedown-log-path:${LOGPATH}
+
+# Move some files needed to do pegasus planning to the workspace folder
+cp plan.sh ${GPS_START_TIME}-${GPS_END_TIME}/
+cp pegasus.conf ${GPS_START_TIME}-${GPS_END_TIME}/
+
+echo 'cat <<END_OF_TEXT' >  temp.sh
+cat "site-local.xml"                 >> temp.sh
+echo 'END_OF_TEXT'       >> temp.sh
+bash temp.sh > "${GPS_START_TIME}-${GPS_END_TIME}/site-local.xml"
+
+# Plan the workflow
+cd ${GPS_START_TIME}-${GPS_END_TIME}/
+sh plan.sh
