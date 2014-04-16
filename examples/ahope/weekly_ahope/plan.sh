54ad788d714d0a7d60ffaa237aaa96fda3e8f1b5
Alexander Harvey Nitz <ahnitz@syr.edu>
2014-04-16 15:54:18 -0400

    update plan.sh and pegasus.conf to not overwrite site-local.xml

--- examples/ahope/weekly_ahope/plan.sh
+++ examples/ahope/weekly_ahope/plan.sh
@@ -4,7 +4,7 @@
 echo 'cat <<END_OF_TEXT' >  temp.sh
 cat "site-local.xml"                 >> temp.sh
 echo 'END_OF_TEXT'       >> temp.sh
-bash temp.sh > "${GPS_START_TIME}-${GPS_END_TIME}/site-local.xml"
+bash temp.sh > site-local-parsed.xml
 
 # Plan the workflow
 pegasus-plan --conf pegasus.conf -d $1 --sites local -o local --dir ${LOGPATH}
