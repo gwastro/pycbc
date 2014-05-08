d620f08ac2c97634290ea673110be409c5d01cba
Alexander Harvey Nitz <ahnitz@syr.edu>
2014-05-08 18:28:15 -0400

    update workflow.py documentation

--- pycbc/workflow/workflow.py
+++ pycbc/workflow/workflow.py
@@ -1,19 +1,50 @@
-import copy, urlparse
+""" This module provides thin wrappers around Pegasus.DAX3 functionality that
+provides additional abstraction and argument handling.
+"""
 import Pegasus.DAX3 as dax
 
-class Executable(object):
-    id = 0
+class ProfileShortcuts(object):
+    """ Container of common methods for setting pegasus profile information
+    on Executables and nodes. This class expects to be inherited from
+    and for a add_profile method to be implemented.
+    """
+    def set_memory(self, size):
+        """ Set the amount of memory that is required in megabytes
+        """
+        self.add_profile('condor', 'request_memory', '%sM' % size)
+         
+    def set_storage(self, size):
+        """ Set the amount of storage required in megabytes
+        """
+        self.add_profile('condor', 'request_disk', '%sM' % size)
+        
+    def set_num_cpus(self, number):
+        self.add_profile('condor', 'request_cpus', number)
+        
+    def set_universe(self, universe):
+        self.add_profile("condor", "universe", universe)
+        
+    def set_category(self, category):
+        self.add_profile('dagman', 'category', category)
+        
+    def set_priority(self, priority):
+        self.add_profile('dagman', 'priority', priority)
+        
+    def set_num_retries(self, number):
+        self.add_profile("dagman", "retry", number)
 
+class Executable(ProfileShortcuts):
+    """ The workflow representation of an Executable 
+    """
+    id = 0
     def __init__(self, name):
         self.name = name
         self.logical_name = self.name + str(Executable.id)
         Executable.id += 1
         self.namespace = self.logical_name
         self._dax_executable = dax.Executable(self.logical_name, 
-                                              namespace=self.namespace,
-                                              version="1.0",
-                                              os="linux", arch="x86_64",
-                                              installed=True) 
+                   namespace=self.namespace, version="1.0", os="linux", 
+                   arch="x86_64", installed=True) 
         self.in_workflow = False
         self.pfns = {}
         
@@ -31,37 +62,16 @@ class Executable(object):
         """ Add profile information to this executable
         """
         entry = dax.Profile(namespace, key, value)
-        self._dax_executable.addProfile(entry)
-        
-    def set_memory(self, size):
-        self.add_profile('condor', 'request_memory', '%sM' % size)
-         
-    def set_storage(self, size):
-        self.add_profile('condor', 'request_disk', '%sM' % size)
-        
-    def set_num_cpus(self, number):
-        self.add_profile('condor', 'request_cpus', number)
-        
-    def set_universe(self, universe):
-        self.add_profile("condor", "universe", universe)
-        
-    def set_category(self, category):
-        self.add_profile('dagman', 'category', category)
-        
-    def set_priority(self, priority):
-        self.add_profile('dagman', 'priority', priority)
-        
-    def set_num_retries(self, number):
-        self.add_profile("dagman", "retry", number)    
+        self._dax_executable.addProfile(entry)  
  
-class Node(object):    
+class Node(ProfileShortcuts):    
     def __init__(self, executable):
         self.in_workflow = False   
         self.executable=executable            
         self._inputs = []
         self._outputs = []        
         self._dax_node = dax.Job(name=executable.logical_name,
-                                 version = "1.0",
+                                 version = "1.0", 
                                  namespace=executable.namespace)
         self._args = []
         self._options = []
@@ -100,38 +110,48 @@ class Node(object):
         
     # public functions to add options, arguments with or without data sources
     def add_input_opt(self, opt, inp):
-        """ Add an option that contains an input 
+        """ Add an option that determines an input
         """
         self.add_opt(opt, inp._dax_repr())
         self._add_input(inp)
         
     def add_output_opt(self, opt, out):
-        """ Add an option that contains an output
+        """ Add an option that determines an output
         """
         self.add_opt(opt, out._dax_repr())
         self._add_output(out)
         
     def add_output_list_opt(self, opt, outputs):
+        """ Add an option that determines a list of outputs
+        """
         self.add_opt(opt)
         for out in outputs:
             self.add_opt(out)
             self._add_output(out)
             
     def add_input_list_opt(self, opt, inputs):
+        """ Add an option that determines a list of inputs
+        """
         self.add_opt(opt)
         for inp in inputs:
             self.add_opt(inp)
             self._add_input(inp)
         
     def add_input_arg(self, inp):
+        """ Add an input as an argument
+        """
         self.add_arg(inp._dax_repr())
         self._add_input(inp)
         
     def add_output_arg(self, out):
+        """ Add an output as an argument
+        """
         self.add_arg(out._dax_repr())
         self._add_output(out)
         
     def new_output_file_opt(self, opt, name):
+        """ Add an option and return a new file handle
+        """
         fil = File(name)
         self.add_output_opt(opt, fil)
         return fil
@@ -142,33 +162,14 @@ class Node(object):
         """
         entry = dax.Profile(namespace, key, value)
         self._dax_node.addProfile(entry)
-    
-    def set_memory(self, size):
-        self.add_profile('condor', 'request_memory', '%sM' % size)
-         
-    def set_storage(self, size):
-        self.add_profile('condor', 'request_disk', '%sM' % size)
-        
-    def set_num_cpus(self, number):
-        self.add_profile('condor', 'request_cpus', number)
-        
-    def set_universe(self, universe):
-        self.add_profile("condor", "universe", universe)
-        
-    def set_category(self, category):
-        self.add_profile('dagman', 'category', category)
-        
-    def set_priority(self, priority):
-        self.add_profile('dagman', 'priority', priority)
-        
-    def set_num_retries(self, number):
-        self.add_profile("dagman", "retry", number)
         
     def _finalize(self):
         args = self._args + self._options
         self._dax_node.addArguments(*args)
         
 class Workflow(object):
+    """ 
+    """
     def __init__(self, name='my_workflow'):
         self.name = name
         self._adag = dax.ADAG(name)
@@ -178,10 +179,22 @@ class Workflow(object):
         self._executables = []
         
     def add_node(self, node):
+        """ Add a node to this workflow
+        
+        This function adds nodes to the workflow. It also determines
+        parent/child relations from the DataStorage inputs to this job. 
+        
+        Parameters
+        ----------
+        node : Node
+            A node that should be exectuded as part of this workflow.
+        """
         node._finalize()
         node.in_workflow = True
         self._adag.addJob(node._dax_node)
  
+        # Determine the parent child relationships based on the inputs that
+        # this node requires.
         for inp in node._inputs:
             if inp.node is not None and inp.node.in_workflow:
                 parent = inp.node._dax_node
@@ -196,10 +209,12 @@ class Workflow(object):
             elif inp.node is None and inp.workflow_input is False:
                 self._inputs += [inp]
                 inp.workflow_input = True
-                                               
+            
+        # Record the outputs that this node generates                                   
         for out in node._outputs:
             self._outputs += node._outputs  
             
+        # Recored the executable that this node uses
         if not node.executable.in_workflow:
             node.executable.in_workflow = True
             self._executables += [node.executable]
@@ -215,6 +230,13 @@ class Workflow(object):
         self._adag.writeXML(f)
 
 class DataStorage(object):
+    """ A workflow representation of a place to store and read data from. 
+    
+    The abstract representation of a place to store and read data from. This 
+    can include files, database, or remote connections. This object is
+    used as a handle to pass between functions, and is used a way to logically
+    represent the order operation on the physical data. 
+    """
     def __init__(self, name):
         self.name = name      
         self.node = None
@@ -230,11 +252,17 @@ class DataStorage(object):
         return self.name
         
 class File(DataStorage, dax.File):
+    """ The workflow representation of a physical file
+    
+    An object that represents a file from the perspective of setting up a 
+    workflow. The file may or may not exist at the time of workflow generation.
+    If it does, this is represented by containing a physical file name (PFN).
+    A storage path is also available to indicate the desired final 
+    destination of this file.
+    """
     def __init__(self, name):
         DataStorage.__init__(self, name)
         dax.File.__init__(self, name)
-        
-        self._dax_file = dax.File(self.name)
         self.storage_path = None
 
     def _dax_repr(self):
