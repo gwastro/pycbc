a91bf3b811f7d90e8e3a5886ac7382534f2bbc6a
Alexander Harvey Nitz <alex.nitz@ligo.org>
2014-06-25 23:38:19 -0400

    minor change to namespace handling

--- pycbc/workflow/workflow.py
+++ pycbc/workflow/workflow.py
@@ -1,3 +1,28 @@
+# Copyright (C) 2014  Alex Nitz
+#
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 3 of the License, or (at your
+# option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+# Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+
+#
+# =============================================================================
+#
+#                                   Preamble
+#
+# =============================================================================
+#
 """ This module provides thin wrappers around Pegasus.DAX3 functionality that
 provides additional abstraction and argument handling.
 """
@@ -37,14 +62,15 @@ class Executable(ProfileShortcuts):
     """ The workflow representation of an Executable 
     """
     id = 0
-    def __init__(self, name):
+    def __init__(self, name, namespace=None, os="linux", 
+                       arch="x86_64", installed=True, version="1.0"):
         self.name = name
         self.logical_name = self.name + "_ID%s" % str(Executable.id)
         Executable.id += 1
-        self.namespace = self.logical_name
+        self.namespace = namespace
         self._dax_executable = dax.Executable(self.logical_name, 
-                   namespace=self.namespace, version="1.0", os="linux", 
-                   arch="x86_64", installed=True) 
+                   namespace=self.namespace, version=version, os=os, 
+                   arch=arch, installed=installed) 
         self.in_workflow = False
         self.pfns = {}
         
