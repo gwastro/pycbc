e77e42f1bccd08e04b9147ffb5be793b1380408b
Alexander Harvey Nitz <alex.nitz@ligo.org>
2014-07-15 15:24:59 -0400

    attempt to fix double precision support in point chisq

--- pycbc/vetoes/chisq_cpu.py
+++ pycbc/vetoes/chisq_cpu.py
@@ -22,7 +22,7 @@
 # =============================================================================
 #
 import numpy, pycbc
-from pycbc.types import Array
+from pycbc.types import Array, real_same_precision_as, complex_same_precision_as
 from scipy.weave import inline
 
 if pycbc.HAVE_OMP:
@@ -53,86 +53,103 @@ def chisq_accum_bin_inline(chisq, q):
           
 chisq_accum_bin = chisq_accum_bin_inline
 
+point_chisq_code = """
+    int num_parallel_regions = 16;
+    
+    #pragma omp parallel for
+    for (unsigned int k=0; k<num_parallel_regions; k++){
+        unsigned int start = vlen * k / num_parallel_regions;
+        unsigned int end = vlen * (k + 1) / num_parallel_regions;
+    
+        //start the cumulative rotations at the offset point
+        TYPE* pr = (TYPE*) malloc(sizeof(TYPE)*n);
+        TYPE* pi = (TYPE*) malloc(sizeof(TYPE)*n);
+        TYPE* vsr = (TYPE*) malloc(sizeof(TYPE)*n);
+        TYPE* vsi = (TYPE*) malloc(sizeof(TYPE)*n);
+        TYPE* outr_tmp = (TYPE*) malloc(sizeof(TYPE)*n);
+        TYPE* outi_tmp = (TYPE*) malloc(sizeof(TYPE)*n);
+        
+        for (int i=0; i<n; i++){
+            pr[i] = cos(2 * 3.141592653 * shifts[i] * (start + offset) / slen);
+            pi[i] = sin(2 * 3.141592653 * shifts[i] * (start + offset) / slen);
+            vsr[i] = cos(2 * 3.141592653 * shifts[i] / slen);
+            vsi[i] = sin(2 * 3.141592653 * shifts[i] / slen);
+            outr_tmp[i] = 0;
+            outi_tmp[i] = 0;
+        }
+        
+        TYPE t1, t2, k1, k2, k3, vs, va;
+        
+        for (unsigned int j=start; j<end; j++){
+            std::complex<TYPE> v = v1[j];
+            TYPE vr = v.real();
+            TYPE vi = v.imag();  
+            vs = vr + vi;
+            va = vi - vr;
+            
+            for (int i=0; i<n; i++){
+                t1 = pr[i];
+                t2 = pi[i];
+                
+                // Complex multiply pr[i] * v
+                k1 = vr * (t1 + t2);
+                k2 = t1 * va;
+                k3 = t2 * vs;
+                            
+                outr_tmp[i] += k1 - k3;
+                outi_tmp[i] += k1 + k2;
+                
+                // phase shift for the next time point
+                pr[i] = t1 * vsr[i] - t2 * vsi[i];
+                pi[i] = t1 * vsi[i] + t2 * vsr[i]; 
+            }                                              
+        } 
+        
+        #pragma omp critical
+        {
+            for (unsigned int i=0; i<n; i++){
+                outr[i] += outr_tmp[i];
+                outi[i] += outi_tmp[i];
+            }
+        }
+        
+        free(pr);
+        free(pi);  
+        free(outr_tmp);
+        free(outi_tmp); 
+        free(vsr);
+        free(vsi);
+        
+    }        
+"""
+
+point_chisq_code_single = point_chisq_code.replace('TYPE', 'float')
+point_chisq_code_double = point_chisq_code.replace('TYPE', 'double')
+
 def shift_sum(v1, shifts, slen=None, offset=0):
+    real_type = real_same_precision_as(v1)
+    shifts = numpy.array(shifts, dtype=real_type)
+    
     v1 = numpy.array(v1.data, copy=False)
-    shifts = numpy.array(shifts, dtype=numpy.float32)
+
     vlen = len(v1)
     if slen is None:
         slen = vlen
 
-    code = """
-        int num_parallel_regions = 16;
-        
-        #pragma omp parallel for
-        for (unsigned int k=0; k<num_parallel_regions; k++){
-            unsigned int start = vlen * k / num_parallel_regions;
-            unsigned int end = vlen * (k + 1) / num_parallel_regions;
-        
-            //start the cumulative rotations at the offset point
-            float* pr = (float*) malloc(sizeof(float)*n);
-            float* pi = (float*) malloc(sizeof(float)*n);
-            float* vsr = (float*) malloc(sizeof(float)*n);
-            float* vsi = (float*) malloc(sizeof(float)*n);
-            float* outr_tmp = (float*) malloc(sizeof(float)*n);
-            float* outi_tmp = (float*) malloc(sizeof(float)*n);
-            for (int i=0; i<n; i++){
-                pr[i] = cos(2 * 3.141592653 * shifts[i] * (start + offset) / slen);
-                pi[i] = sin(2 * 3.141592653 * shifts[i] * (start + offset) / slen);
-                vsr[i] = cos(2 * 3.141592653 * shifts[i] / slen);
-                vsi[i] = sin(2 * 3.141592653 * shifts[i] / slen);
-                outr_tmp[i] = 0;
-                outi_tmp[i] = 0;
-            }
-            float t1, t2, k1, k2, k3, vs, va;
-            
-            for (unsigned int j=start; j<end; j++){
-                std::complex<float> v = v1[j];
-                float vr = v.real();
-                float vi = v.imag();  
-                vs = vr + vi;
-                va = vi - vr;
-                
-                for (int i=0; i<n; i++){
-                    t1 = pr[i];
-                    t2 = pi[i];
-                    
-                    // Complex multiply pr[i] * v
-                    k1 = vr * (t1 + t2);
-                    k2 = t1 * va;
-                    k3 = t2 * vs;
-                                
-                    outr_tmp[i] += k1 - k3;
-                    outi_tmp[i] += k1 + k2;
-                    
-                    // phase shift for the next time point
-                    pr[i] = t1 * vsr[i] - t2 * vsi[i];
-                    pi[i] = t1 * vsi[i] + t2 * vsr[i]; 
-                }                                              
-            } 
-            
-            #pragma omp critical
-            {
-                for (unsigned int i=0; i<n; i++){
-                    outr[i] += outr_tmp[i];
-                    outi[i] += outi_tmp[i];
-                }
-            }
-            free(pr);
-            free(pi);  
-            free(outr_tmp);
-            free(outi_tmp); 
-            free(vsr);
-            free(vsi);
-        }        
-    """
+    if v1.dtype.name == 'complex64':
+        code = point_chisq_code_single
+    else:
+        code = point_chisq_code_double
+    
     n = int(len(shifts))
     
     # Create some output memory
-    outr =  numpy.zeros(n, dtype=numpy.float32)
-    outi =  numpy.zeros(n, dtype=numpy.float32)
+    outr =  numpy.zeros(n, dtype=real_type)
+    outi =  numpy.zeros(n, dtype=real_type)
     
     inline(code, ['v1', 'n', 'vlen', 'outi', 'outr', 'slen', 'shifts', 'offset'],
                     extra_compile_args=['-march=native -O3 -w'] + omp_flags,
                     libraries=omp_libs
           )
-    return  Array(outr + 1.0j * outi, dtype=numpy.complex64)
+          
+    return  Array(outr + 1.0j * outi, dtype=v1.dtype)
