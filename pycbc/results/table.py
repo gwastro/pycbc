43405e4f95e41efa7e0b2f299a748bc72b1578d4
Alexander Harvey Nitz <alex.nitz@ligo.org>
2014-07-29 19:16:06 -0400

    update the google table generator

--- pycbc/results/table.py
+++ pycbc/results/table.py
@@ -7,8 +7,9 @@ google_table_template = mako.template.Template("""
       google.setOnLoadCallback(drawTable);
       function drawTable() {
         var data = new google.visualization.DataTable();
-        data.addColumn('string', 'Name');
-        data.addColumn('number', 'Salary');
+        % for type, name in column_descriptions:
+            data.addColumn(${str(type)}, ${str(name)});
+        % endfor
         data.addRows(${data});
         var table = new google.visualization.Table(document.getElementById('${div_id}'));
         table.draw(data, {showRowNumber: true, 
@@ -20,14 +21,17 @@ google_table_template = mako.template.Template("""
     <div id='%{div_id}'></div>
 """)
 
-def table(columns, page_size=None):
+def table(columns, names, page_size=None):
     """ Return an html table of this data
     
     Parameters
     ----------
     columns : list of numpy arrays 
+    names : list of strings 
+        Theh list of columns names  
     page_size : {int, None}, optionals
         The number of items to show on each page of the table
+        
     Returns
     -------
     html_table : str 
@@ -40,11 +44,21 @@ def table(columns, page_size=None):
         
     div_id = uuid.uuid4()
     
+    column_descriptions = []
+    for column, name in zip(columns, names):
+        if column.dtype.kind == 'S'
+            ctype = 'string':
+        else:
+            ctype = 'number'
+        column_descriptions.append((ctype, name))
+    
     data = []
-    for 
+    for item in zip(*columns):
+        data.append(list(item))
         
     return google_table_template.render(div_id=div_id,
                                         page=page,
+                                        column_descriptions = column_descriptions,
                                         page_size=page_size,
                                         data=data,
                                        )
