f0401c670e91c21af80ff6e4f86d6a4ac8671426
Alexander Harvey Nitz <ahnitz@syr.edu>
2014-08-10 20:07:32 -0400

    add followup testing plot

---
+++ pycbc/results/followup.py
@@ -0,0 +1,100 @@
+# Copyright (C) 2014 Alex Nitz
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 3 of the License, or (at your
+# option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+# Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+#
+# =============================================================================
+#
+#                                   Preamble
+#
+# =============================================================================
+#
+""" This module provides functions to generate followup plots and trigger
+time series.
+""" 
+import h5py, numpy, matplotlib
+matplotlib.use('Agg')
+import pylab, mpld3
+from pycbc.ahope import AhopeFileList
+from glue.segments import segment
+
+def columns_from_file_list(file_list_file, columns, ifo, start, end):
+    """ Return columns of information stored in single detector trigger
+    files.
+    
+    Parameters
+    ----------
+    file_list_file : string
+        pickle file containing the list of single detector
+    triggers.
+    ifo : string
+        The ifo to return triggers for.
+    columns : list of strings
+        The list of columns to read from the trigger files.
+    start : int
+        The start time to get triggers from
+    end : int
+        The end time to get triggers from
+    
+    Returns
+    -------
+    trigger_dict : dict
+        A dictionary of column vectors with column names as keys.
+    """
+    file_list = AhopeFileList.load(file_list_file)
+    file_list = file_list.find_output_with_ifo(ifo)
+    file_list = file_list.find_all_output_in_range(ifo, segment(start, end))
+    
+    trig_dict = {}
+    for trig_file in file_list:
+        f = h5py.File(trig_file.storage_path, 'r')
+        
+        time = f['end_time'][:]
+        pick = numpy.logical_and(time < end, time > start)
+        pick_loc = numpy.where(pick)[0]
+        
+        
+        for col in columns:
+            if col not in trig_dict:
+                trig_dict[col] = []
+            trig_dict[col] = numpy.concatenate([trig_dict[col], f[col][:][pick_loc]])         
+            
+    return trig_dict
+    
+def trigger_timeseries_plot(trigger_file_list, ifos, start, end):
+
+    fig = pylab.figure()
+    for ifo in ifos:
+        trigs = columns_from_file_list(trigger_file_list,
+                                       ['snr', 'end_time'],
+                                       ifo, start, end)
+        pylab.scatter(trigs['end_time'], trigs['snr'], label=ifo)
+    pylab.legend()
+    pylab.grid() 
+    return mpld3.fig_to_html(fig)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
