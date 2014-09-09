ce5fbefebdb70e9bb225769862b436cbfadaca17
Thomas Dent <thomas.dent@ligo.org>
2014-09-09 14:33:17 -0400

    pnutils: add SEOBNRv2 to named freq cutoffs

--- pycbc/pnutils.py
+++ pycbc/pnutils.py
@@ -391,6 +391,8 @@ def named_frequency_cutoffs():
                                               p["s1z"], p["s2z"]),
       "SEOBNRv1"   : lambda p: get_final_freq("SEOBNRv1", p["m1"], p["m2"],
                                               p["s1z"], p["s2z"])
+      "SEOBNRv2"   : lambda p: get_final_freq("SEOBNRv2", p["m1"], p["m2"],
+                                              p["s1z"], p["s2z"])
     }
     return cutoffFunctions
 
