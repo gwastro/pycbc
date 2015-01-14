739be5bf1136dd7db19b7c5c7226901f49d47178
Alexander Harvey Nitz <ahnitz@syr.edu>
2015-01-14 14:14:42 -0500

    add block diagonal add to future

--- pycbc/future.py
+++ pycbc/future.py
@@ -120,3 +120,23 @@ def in1d(ar1, ar2, assume_unique=False, invert=False):
 
 numpy.in1d = in1d
 
+def block_diag(*arrs):
+    import numpy as np
+    if arrs == ():
+        arrs = ([],)
+    arrs = [np.atleast_2d(a) for a in arrs]
+
+    bad_args = [k for k in range(len(arrs)) if arrs[k].ndim > 2]
+    if bad_args:
+        raise ValueError("arguments in the following positions have dimension "
+                            "greater than 2: %s" % bad_args)
+
+    shapes = np.array([a.shape for a in arrs])
+    out = np.zeros(np.sum(shapes, axis=0), dtype=arrs[0].dtype)
+
+    r, c = 0, 0
+    for i, (rr, cc) in enumerate(shapes):
+        out[r:r + rr, c:c + cc] = arrs[i]
+        r += rr
+        c += cc
+    return out
