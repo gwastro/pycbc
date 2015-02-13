ae142627db24e2f4c2b57d1b31e4a3a4cd808904
Christopher M. Biwer <christopher.biwer@ligo.org>
2015-02-13 00:51:10 -0500

    Added single-detector workflow generator for new plotting.

--- examples/make_static_html/run_pycbc_make_html_page.sh
+++ examples/make_static_html/run_pycbc_make_html_page.sh
@@ -2,5 +2,5 @@
 
 rm -rf /home/cbiwer/public_html/pycbc_plotting/970012743-970099143/*
 
-./pycbc_make_html_page --analysis-title "\"PyCBC Coincidence Analysis\"" --analysis-subtitle "\"Christopher M. Biwer\"" --template-dir "./templates/" --template-name "base.html" --output-path "/home/cbiwer/public_html/pycbc_plotting/970012743-970099143/" --plots-dir "./rundir/970012743-970099143/results_plots/" 
+../../bin/pycbc_make_html_page --analysis-title "\"PyCBC Coincidence Analysis\"" --analysis-subtitle "\"Christopher M. Biwer\"" --template-dir "./templates/" --template-name "base.html" --output-path "/home/cbiwer/public_html/pycbc_plotting/970012743-970099143/" --plots-dir "./rundir/970012743-970099143/results_plots/" 
 
