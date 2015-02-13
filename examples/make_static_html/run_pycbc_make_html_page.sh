d031cb6b5bd74eaad3913043876779a282000c43
Christopher M. Biwer <christopher.biwer@ligo.org>
2015-02-12 22:55:17 -0500

    Added Home button to html template.

--- examples/make_static_html/run_pycbc_make_html_page.sh
+++ examples/make_static_html/run_pycbc_make_html_page.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
 
-./pycbc_make_html_page --analysis-title "\"PyCBC Coincidence Analysis\"" --analysis-subtitle "\"Christopher M. Biwer\"" --template-dir "/home/cbiwer/projects/pycbc_plotting/make_static_html/templates/" --template-name "base.html" --output-path "/home/cbiwer/public_html/pycbc_plotting/970012743-970099143/" --plots-dir "./rundir/970012743-970099143/results_plots/" 
+rm -rf /home/cbiwer/public_html/pycbc_plotting/970012743-970099143/*
+
+./pycbc_make_html_page --analysis-title "\"PyCBC Coincidence Analysis\"" --analysis-subtitle "\"Christopher M. Biwer\"" --template-dir "./templates/" --template-name "base.html" --output-path "/home/cbiwer/public_html/pycbc_plotting/970012743-970099143/" --plots-dir "./rundir/970012743-970099143/results_plots/" 
 
