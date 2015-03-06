7943e068812ea8cdead4af8270ad0d16c02cd5af
Christopher M. Biwer <christopher.biwer@ligo.org>
2015-03-06 03:21:15 -0500

    Change pyccb_run_sngl_workflow example settings.

--- examples/workflow/sngl/pycbc_run_sngl_workflow.sh
+++ examples/workflow/sngl/pycbc_run_sngl_workflow.sh
@@ -7,16 +7,13 @@ export GPS_START_TIME=1102089616
 export GPS_END_TIME=1102100616
 export WORKFLOW_NAME=${GPS_START_TIME}-${GPS_END_TIME}
 
-export HTMLDIR=/home/${USER}/public_html/pycbc_test_plotting
-export OUTPUT=/~${USER}/pycbc_plotting/sngl_test
+export HTMLDIR=/home/${USER}/public_html/daily_cbc_offline/${WORKFLOW_NAME}
+export OUTPUT=/~${USER}/daily_cbc_offline/${WORKFLOW_NAME}
 
 export INSTALLED_CONFIG_FILES="sngl/example_sngl.ini"
-#export REUSE_CACHE=/bin/true
 
 export LOGPATH=/usr1/${USER}/log
 mkdir -p $LOGPATH
-
-rm -rf ${WORKFLOW_NAME}
 
 python /home/cbiwer/src/pycbc/bin/pycbc_make_sngl_workflow --name ${WORKFLOW_NAME} \
                           --installed-config-files ${INSTALLED_CONFIG_FILES} \
@@ -27,5 +24,4 @@ python /home/cbiwer/src/pycbc/bin/pycbc_make_sngl_workflow --name ${WORKFLOW_NAM
                                              html:analysis-subtitle:${GPS_START_TIME}-${GPS_END_TIME}
 
 cd ${WORKFLOW_NAME}
-cp ../input.map .
-pycbc_basic_pegasus_plan ${WORKFLOW_NAME}.dax ${LOGPATH} --cache input.map
+pycbc_basic_pegasus_plan ${WORKFLOW_NAME}.dax ${LOGPATH}
