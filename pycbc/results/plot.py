2ac5c41bbdab9e13b16f4c6bd9402cc84298d70e
Alexander Harvey Nitz <ahnitz@syr.edu>
2015-05-01 19:37:12 -0400

    add metdata load/store from png files

---
+++ pycbc/results/plot.py
@@ -0,0 +1,66 @@
+"""
+This Module contains generic utility functions for creating plots within 
+PyCBC. 
+"""
+import os.path
+from PIL import Image, PngImagePlugin
+import ConfigParser
+
+def save_png_with_metadata(fig, filename, fig_kwds, kwds):
+    fig.savefig(filename, **fig_kwds)
+     
+    im = Image.open(filename)
+    meta = PngImagePlugin.PngInfo()
+    
+    for key in kwds:
+        meta.add_text(str(key), str(kwds[key]))       
+         
+    im.save(filename, "png", pnginfo=meta)
+
+def load_png_metadata(filename):
+    cp = ConfigParser.ConfigParser(Image.open(filename).info)
+    cp.add_section(filename)
+    return cp
+    
+_metadata_saver = {'.png':save_png_with_metadata,
+                  }
+_metadata_loader = {'.png':load_png_metadata,
+                   }
+
+def save_fig_with_metadata(fig, filename, fig_kwds={}, **kwds):
+    """ Save plot to file with metadata included. Kewords translate to metadata
+    that is stored directly in the plot file. Limited format types available.
+    
+    Parameters
+    ----------
+    fig: matplotlib figure
+        The matplotlib figure to save to the file
+    filename: str
+        Name of file to store the plot.
+    """
+    try:
+        extension = os.path.splitext(filename)[1]
+        _metadata_saver[extension](fig, filename, fig_kwds, kwds)
+    except KeyError:
+        raise TypeError('Cannot save file %s with metadata, extension %s not ' 
+                        'supported at this time' % (filename, extension))
+
+def load_metadata_from_file(filename):
+    """ Load the plot related metadata saved in a file
+    
+    Parameters
+    ----------
+    filename: str
+        Name of file load metadata from.
+        
+    Returns
+    -------
+    cp: ConfigParser
+        A configparser object containing the metadata
+    """
+    try:
+        extension = os.path.splitext(filename)[1]
+        return _metadata_loader[extension](filename)
+    except KeyError:
+        raise TypeError('Cannot read metadata from file %s, extension %s not ' 
+                        'supported at this time' % (filename, extension))    
