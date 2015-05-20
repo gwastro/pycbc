cfcbaacc9ff5d77d17cb9ae0a50fb95bff65f30e
Alexander Harvey Nitz <ahnitz@syr.edu>
2015-05-20 16:22:25 -0400

    modify example static build script to include symbol table strip, reduces files size

--- tools/static/build_static_pycbc_inspiral.sh
+++ tools/static/build_static_pycbc_inspiral.sh
@@ -2,6 +2,6 @@
 pyinstaller `which pycbc_inspiral` \
 --additional-hooks-dir ./hooks/ \
 --runtime-hook runtime-scipy.py \
---name pycbc_inspiral_static  \
+--name pycbc_inspiral_static \
+--strip \
 --onefile
-
