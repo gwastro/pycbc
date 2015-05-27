089e1ceb659be2ad8f697f2c1d3e1db8d71a1c13
Christopher M. Biwer <christopher.biwer@ligo.org>
2015-05-27 16:35:02 -0400

    Fix typo in zpk_filter docstring.

--- pycbc/filter/zpk.py
+++ pycbc/filter/zpk.py
@@ -70,7 +70,7 @@ def filter_zpk(timeseries, z, p, k):
 
     Examples
     --------
-    To apply a 5 zeroes at 1Hz, 5 poles at 1Hz, and a gain of 1e-10 filter
+    To apply a 5 zeroes at 100Hz, 5 poles at 1Hz, and a gain of 1e-10 filter
     to a TimeSeries instance, do:
     >>> filtered_data = zpk_filter(timeseries, [100]*5, [1]*5, 1e-10)
     """
