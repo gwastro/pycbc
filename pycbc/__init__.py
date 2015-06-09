8a25779d8263c1437a211e7de432d6de0bc5cd80
Tito Dal Canton <tito.canton@ligo.org>
2015-06-09 16:34:13 +0200

    Enable toggling verbose mode via SIGUSR1

--- pycbc/__init__.py
+++ pycbc/__init__.py
@@ -26,6 +26,35 @@
 
 """
 import subprocess, os, sys, tempfile
+import logging
+import signal
+
+
+def init_logging(verbose=False):
+    """
+    Common utility for setting up logging in PyCBC. Installs a signal handler
+    such that verbosity can be activated at run-time by sending a SIGUSR1 to
+    the process.
+    """
+    def sig_handler(signum, frame):
+        logger = logging.getLogger()
+        log_level = logger.level
+        if log_level == logging.DEBUG:
+            log_level = logging.WARN
+        else:
+            log_level = logging.DEBUG
+        logging.warn('Got signal %d, setting log level to %d',
+                     signum, log_level)
+        logger.setLevel(log_level)
+
+    signal.signal(signal.SIGUSR1, sig_handler)
+
+    if verbose:
+        initial_level = logging.DEBUG
+    else:
+        initial_level = logging.WARN
+    logging.basicConfig(format='%(asctime)s %(message)s', level=initial_level)
+
 
 # Check for optional components of the PyCBC Package
 try:
