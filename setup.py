30e3e3078b72e75979f9f65e4242405107c92634
Alexander Harvey Nitz <alex.nitz@gmail.com>
2015-06-09 18:18:27 -0400

    update version

--- setup.py
+++ setup.py
@@ -228,7 +228,7 @@ def get_version_info():
             
     # If this is a release or another kind of source distribution of PyCBC
     except:
-        version = '1.0.0'
+        version = '0.1.1'
         release = 'True'
         date = hash = branch = tag = author = committer = status = builder = build_date = ''
     
