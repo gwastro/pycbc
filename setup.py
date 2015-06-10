9ac5fe482e549d7516dbb7098f42dd53d5c424c5
Alexander Harvey Nitz <alex.nitz@gmail.com>
2015-06-10 14:00:26 -0400

    remove numpy from setup_requires

--- setup.py
+++ setup.py
@@ -36,12 +36,13 @@ from pycbc.setuputils import pkg_config
 from distutils.file_util import write_file
 
 requires = ['lal.lal', 'lalsimulation.lalsimulation', 'glue', 'pylal']
-setup_requires = ['numpy>=1.6.4']
+setup_requires = []
 install_requires =  setup_requires + ['Mako>=1.0.1',
                       'argparse>=1.3.0',
                       'decorator>=3.4.2',
                       'scipy>=0.13.0',
                       'matplotlib>=1.3.1',
+                      'numpy>=1.6.4',
                       'pillow',
                       'jinja2',
                       ]
