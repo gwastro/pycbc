3d36c49c25e2646c1f9b3cba1a76704da25249ed
Tito Dal Canton <tito.canton@ligo.org>
2015-06-15 16:23:30 +0200

    Relax versions of dependencies

--- setup.py
+++ setup.py
@@ -52,13 +52,13 @@ except ImportError:
 requires = ['lal.lal', 'lalsimulation.lalsimulation', 'glue', 'pylal']
 setup_requires = []
 install_requires =  setup_requires + ['Mako>=1.0.1',
-                      'argparse>=1.3.0',
-                      'decorator>=3.4.2',
+                      'argparse>=1.2.1',
+                      'decorator>=3.3.3',
                       'scipy>=0.13.0',
                       'matplotlib>=1.3.1',
                       'numpy>=1.6.4',
                       'pillow',
-                      'h5py>=2.5',
+                      'h5py>=2.0.1',
                       'jinja2',
                       ]
 links = []
