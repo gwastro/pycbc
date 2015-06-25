76333751327d639cd828eacfcf05372ef243d249
Christopher M. Biwer <christopher.biwer@ligo.org>
2015-06-25 16:05:36 -0400

    Added import logging to versioning module.

--- pycbc/results/versioning.py
+++ pycbc/results/versioning.py
@@ -16,6 +16,7 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
+import logging
 import os
 import subprocess
 from ConfigParser import ConfigParser
