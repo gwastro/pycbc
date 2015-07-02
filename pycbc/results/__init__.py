127851e724b4d68073cddc246b2e27755724b7eb
Alexander Harvey Nitz <ahnitz@syr.edu>
2015-07-02 16:48:35 -0400

    add overflow and ifo based coloring to hist

--- pycbc/results/__init__.py
+++ pycbc/results/__init__.py
@@ -1,4 +1,5 @@
 from pycbc.results.table import *
 from pycbc.results.metadata import *
 from pycbc.results.versioning import *
-from pycbc.restuls.color import *
+from pycbc.results.color import *
+from pycbc.results.plot import *
