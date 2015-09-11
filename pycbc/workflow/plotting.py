3085c0ccd4dd7309ce281392f232e709728fc3f0
Larne Pekowsky <larne.pekowsky@ligo.org>
2015-09-11 16:04:33 -0400

    Allow the location of the top-level template to be relative

--- pycbc/workflow/plotting.py
+++ pycbc/workflow/plotting.py
@@ -259,7 +259,7 @@ def make_snrifar_plot(workflow, bg_file, out_dir, closed_box=False, tags=[]):
     
 def make_results_web_page(workflow, results_dir):
     import pycbc.results
-    template_path =  pycbc.results.__path__[0] + '/templates/orange.html'
+    template_path = 'templates/orange.html'
 
     out_dir = workflow.cp.get('results_page', 'output-path')
     makedir(out_dir)
