611f3a70bc0ceff3106d138a0dec242f3a8ac76c
Larne Pekowsky <larne.pekowsky@ligo.org>
2015-09-28 19:05:47 -0400

    Force rebuild of numpy then exit

--- tools/install_travis.sh
+++ tools/install_travis.sh
@@ -15,12 +15,16 @@ export LD_LIBRARY_PATH=${INST}/lib:${INST}/lib64
 export PKG_CONFIG_PATH=${INST}/lib/pkgconfig
 export PATH=/usr/lib/ccache:${PATH}:${INST}/bin
 
+rm -f ${INST}/dep_install.done
+
 if [ -f ${INST}/dep_install.done ]
 then
     echo "Found cache of installed dependencies, using it"
 else
     # Install needed version of numpy
     pip install 'numpy==1.9.3' --upgrade 
+
+    exit 0
 
     # install the version of swig that for some reason we have to use
 
