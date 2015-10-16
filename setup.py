94df8dbd49fe38f607399c49c2ccea7e4912f607
Larne Pekowsky <larne.pekowsky@ligo.org>
2015-10-16 17:20:34 -0400

    Set to next development version

--- setup.py
+++ setup.py
@@ -256,8 +256,8 @@ def get_version_info():
             
     # If this is a release or another kind of source distribution of PyCBC
     except:
-        version = '1.2.2'
-        release = 'True'
+        version = '1.3.dev0'
+        release = 'False'
         date = hash = branch = tag = author = committer = status = builder = build_date = ''
     
         with open('pycbc/version.py', 'w') as f:
