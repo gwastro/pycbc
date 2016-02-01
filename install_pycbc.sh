85d5f7bceb616fc47046265ec112d77b51647e9d
Christopher M. Biwer <christopher.biwer@ligo.org>
2016-02-01 14:01:53 -0500

    Fix LAL_DATA_PATH if you choose to install it.

--- install_pycbc.sh
+++ install_pycbc.sh
@@ -515,7 +515,11 @@ fi
 for j in "${rom_hash[@]}" ; do
   if [[ ${rom_hash[*]} == ${md5sum_hash[*]} ]] ; then
     echo "All files are in ${LAL_DATA_PATH}." 
-    echo "export LAL_DATA_PATH=${LAL_DATA_PATH}" >>  ${VIRTUAL_ENV}/bin/activate
+    if [[ ${install_rom} == "yes" ]] ; then
+        echo "export LAL_DATA_PATH=${LAL_DATA_PATH}" >>  ${VIRTUAL_ENV}/bin/activate
+    elif [[ ${install_rom} == "no" ]] ; then
+        echo "export LAL_DATA_PATH=${LAL_DATA_PATH}"/share/lalsimulation >>  ${VIRTUAL_ENV}/bin/activate
+    fi
     break
   fi
 
