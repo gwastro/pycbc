642a7e9d3ee999b558519f42f38c207a16702814
Bernd Machenschalk <bernd.machenschalk@ligo.org>
2016-02-04 10:13:24 +0100

    E@H Etch /usr/local compile: update git and gcc versions

--- tools/einsteinathome/pycbc_etch64_compile-usr-local.sh
+++ tools/einsteinathome/pycbc_etch64_compile-usr-local.sh
@@ -10,7 +10,7 @@ test ".$PREFIX" = "." &&
 export PATH="$PREFIX:$PATH" LD_LIBRARY_PATH="$PREFIX/lib:$LD_LIBRARY_PATH"
 
 test ".$GCC" = "." &&
-  GCC=5.3.0
+  GCC=4.8.5
 
 if fgrep -q -x "$PREFIX/lib" /etc/ld.so.conf; then
   echo "$PREFIX/lib already in /etc/ld.so.conf"
@@ -72,11 +72,11 @@ for url in `echo "
   $gnu/mpfr/mpfr-2.4.2.tar.gz
   $gnu/binutils/binutils-2.24.tar.gz
   $gnu/gcc/gcc-$GCC/gcc-$GCC.tar.bz2
-  http://atlas.atlas.aei.uni-hannover.de/~bema/tarballs/git-1.6.0.2.tar.gz
   http://atlas.atlas.aei.uni-hannover.de/~bema/tarballs/zlib-1.2.6.tar.gz
   http://www.multiprecision.org/mpc/download/mpc-0.9.tar.gz
   http://pkgconfig.freedesktop.org/releases/pkg-config-0.23.tar.gz
   http://www.python.org/ftp/python/2.7.10/Python-2.7.10.tgz
+  https://www.kernel.org/pub/software/scm/git/git-1.9.5.tar.gz
 "` ; do
   test -r `echo $url | sed 's%.*/%%'` ||
   wget --no-check-certificate --passive-ftp "$url" || exit
@@ -88,22 +88,21 @@ compile_tar_gz automake-1.11
 compile_tar_gz libtool-1.5.8
 compile_tar_gz pkg-config-0.23
 
-tar -xzvf git-1.6.0.2.tar.gz
-cd git-1.6.0.2
+tar -xzvf git-1.9.5.tar.gz
+cd git-1.9.5
 make configure
 ./configure --prefix=$PREFIX --with-tcltk
 make && make install
 cd ..
-rm -rf git-1.6.0.2
+rm -rf git-1.9.5
 
 compile_tar_gz gmp-4.3.2 "--build=`gcc -dumpmachine`"
 compile_tar_gz mpfr-2.4.2 "--with-gmp=$PREFIX"
 compile_tar_gz mpc-0.9 "--with-gmp=$PREFIX" "--with-mpfr=$PREFIX"
+compile_tar_gz binutils-2.24
 compile_gcc $GCC "--with-gmp=$PREFIX" "--with-mpfr=$PREFIX" "--with-mpc=$PREFIX" --disable-multilib
 # "--target=`gcc -dumpmachine`" --disable-multilib --with-multilib-list=m64
 ( cd $PREFIX/bin && for i in gcc g++ gfortran; do rm -f $i && ln -s $i-$GCC $i; done )
-
-compile_tar_gz binutils-2.24
 
 rm -f Python-2.7.10.tar.gz
 ln -s Python-2.7.10.tgz Python-2.7.10.tar.gz
