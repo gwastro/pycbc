5fc0232b3a8e6c1e35f7adda7915327dc601f283
Bernd Machenschalk <bernd.machenschalk@ligo.org>
2016-04-04 13:35:27 +0200

    E@H: tools for validation
    - the validate_test_CBC1 binary is currently built from a private git repo (project-daemons),
      and even withing that the build is somewhat tricky, requiring a couple of files and a patch
      to the Makefile for building on Cygwin. validator_test_CBC1.zip contains binaries for Linux
      and Cygwin.

---
+++ tools/einsteinathome/CBC1H5tool.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+import sys
+import h5py
+
+datasets = ['template_hash', 'chisq', 'chisq_dof', 'coa_phase', 'end_time', 'sigmasq', 'snr', 'template_duration']
+
+# open file
+fin = h5py.File(sys.argv[1],'r')
+# print fin.keys()[0]
+
+'''
+for group in fin.values():
+    print group.keys()
+    for dataset in group.values():
+        print dataset.name, dataset.len()
+print "groups printed"
+print fin.items()['/H1/template_hash']
+#gin = fin.group()
+'''
+
+# should be "/H1/", but also work for e.g. "/L1/"
+group = "/" + fin.keys()[0] + "/"
+
+# check that all datasets exist and have same length
+len = None
+for dataset in datasets:
+    if not fin.get(group + dataset):
+        raise ValueError("dataset " + group + dataset + " not found")
+    elif not len:
+        len = fin.get(group + dataset).len()
+    elif len != fin.get(group + dataset).len():
+        raise ValueError("datasets have different length")
+
+print("Dataset Length:", len, file=sys.stderr)
+
+# print data to stdout in correct order
+for i in range(len):
+    for dataset in datasets:
+        print (" ", fin.get(group + dataset)[i], end="")
+    print ("")
+
+fin.close()
+
+sys.exit(0)
