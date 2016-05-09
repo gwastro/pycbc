d10d590dde6455816cafe28469813a705e3992e7
Christopher M. Biwer <christopher.biwer@ligo.org>
2016-05-09 11:27:42 -0400

    Add prior class example.

--- pycbc/inference/prior.py
+++ pycbc/inference/prior.py
@@ -26,10 +26,68 @@ This modules provides classes and functions for evaluating the prior
 for parameter estimation.
 """
 
+import numpy
+
 def no_prior(params):
     """ Function that returns default value if user does not specify a prior.
     """
     return 0
 
+class PriorEvaluator(object):
+    """
+    Callable class that calculates the prior.
 
+    Parameters
+    ----------
+    variable_names : list
+        A list of str that contain the names of the variable parameters.
+    params_dist : {'flat'}
+        A list of str that contain the names of the PriorEvaluator class
+        function to use to calculate the prior for that variable.
+    params_min : {None, list}
+        The lowest acceptable value for parameter.
+    params_max : {None, list}
+        The largest acceptable value for parameter.
+    """
+
+    def __init__(self, variable_params, params_dist, params_min=None,
+                 params_max=None):
+
+        # store the names of the variable params
+        self.variable_params = variable_params
+
+        # store the distribution for the variable params
+        self.params_dist = [getattr(PriorEvaluator, dist) for dist in params_dist]
+
+        # store the minimum and maximum acceptable values for a variable param
+        # if nothing is specified then use -inf to inf
+        if params_min is None:
+            self.params_min = numpy.ones(len(self.variable_params)) * -numpy.inf
+        else:
+            self.params_min = numpy.array(params_min)
+        if params_max is None:
+            self.params_max = numpy.ones(len(self.variable_params)) * numpy.inf
+        else:
+            self.params_max = numpy.array(params_max)
+
+    def flat(self, param):
+        """ Simple flat prior for a single parameter.
+        """
+        return 0.0
+
+    def __call__(self, params):
+        """ Evalualate prior for parameters.
+        """
+
+        # check if values are valid and if params are outside of min and max
+        # acceptable values then return -inf
+        params = numpy.array(params)
+        result = sum((params < self.params_min) | (params > self.params_max))
+        if result:
+            return -numpy.inf
+
+        # evaluate prior for each parameter
+        val = sum([self.params_dist[i](self, param) for i,param in enumerate(params)])
+
+        return val
 
