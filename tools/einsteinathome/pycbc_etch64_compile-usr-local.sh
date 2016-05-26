3649aed4261f916541b7dc4d31b763e220c65950
Bernd Machenschalk <bernd.machenschalk@ligo.org>
2016-05-26 10:58:33 +0200

    E@H Etch /usr/local compile: added libsqlite3-dev to packages

--- tools/einsteinathome/pycbc_etch64_compile-usr-local.sh
+++ tools/einsteinathome/pycbc_etch64_compile-usr-local.sh
@@ -11,7 +11,7 @@ apt-get update
 # tools for pycbc
 apt-get install git-core ant gcc g++ gfortran automake autoconf make libtool pkg-config bzip2
 # libraries for pycbc
-apt-get install libpcre3-dev libfreetype6-dev libjpeg-dev libpng-dev libmysqlclient-dev libpq-dev libssl-dev
+apt-get install libpcre3-dev libfreetype6-dev libjpeg-dev libpng-dev libmysqlclient-dev libpq-dev libssl-dev libsqlite3-dev
 # make sure these libraries aren't on the system
 apt-get remove lapack3-dev atlas3-base atlas3-headers refblas3
 # further necessary / useful tools
