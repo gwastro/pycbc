ba97b76275f6637b7f2fcc1c6efd82df08440d25
Christopher M. Biwer <christopher.biwer@ligo.org>
2016-06-27 10:52:05 -0400

    Use wfparams in read_label_from_config.

--- pycbc/io/inference_hdf.py
+++ pycbc/io/inference_hdf.py
@@ -56,7 +56,12 @@ def read_label_from_config(cp, variable_arg, section="labels", html=False):
     if cp.has_option(section, variable_arg):
         label = cp.get(section, variable_arg)
     else:
-        label = variable_arg
+        # try looking in pycbc.waveform.parameters
+        try:
+            label = getattr(wfparams, param).label
+        except AttributeError:
+            # just use the parameter name
+            label = param
 
     # replace LaTeX with HTML
     if html:
