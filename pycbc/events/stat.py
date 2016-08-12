cf132d0cfe58eb8544749b4e571ee4f814b42cec
tdent <thomas.dent@aei.mpg.de>
2016-08-12 10:53:59 +0200

    adding expfit stat and dictionarizing choice of statistic (#1021)
    * adding expfit stat and dictionarizing choice of statistic
    
    * stat module: adding back trivial string lookup function
    
    * whitespace

--- pycbc/events/stat.py
+++ pycbc/events/stat.py
@@ -28,33 +28,6 @@ statistic values
 import numpy
 from . import events
 
-def get_statistic(option, files):
-    """Return an appropriate coincident statistic class instance.
-
-    Parameters
-    ----------
-    option: str
-        Name of the statistic class to instantiate
-    files: list of strs
-        A list containing the filenames of hdf format files used to help
-    construct the coincident statistics.
-
-    Returns
-    -------
-    stat: instance of `Stat` class
-        The requested class instance
-    """
-    if option == 'newsnr':
-        return NewSNRStatistic(files)
-    elif option == 'newsnr_cut':
-        return NewSNRCutStatistic(files)
-    elif option == 'phasetd_newsnr':
-        return PhaseTDStatistic(files)
-    elif option == 'max_cont_trad_newsnr':
-        return MaxContTradNewSNRStatistic(files)
-    else:
-        raise ValueError('%s is not an available detection statistic' % option)
-
 class Stat(object):
 
     """ Base class which should be extended to provide a coincident statistic"""
@@ -80,7 +53,7 @@ class Stat(object):
 class NewSNRStatistic(Stat):
 
     """ Calculate the NewSNR coincident detection statistic """
-    
+
     def single(self, trigs):
         """Calculate the single detector statistic.
 
@@ -173,11 +146,11 @@ class PhaseTDStatistic(NewSNRStatistic):
     def __init__(self, files):
         NewSNRStatistic.__init__(self, files)
         self.hist = self.files['phasetd_newsnr']['map'][:]
-        top = float(self.hist.max())     
+        top = float(self.hist.max())
 
         #normalize so that peak has no effect on newsnr
         self.hist = self.hist / top
-        self.hist = numpy.log(self.hist) 
+        self.hist = numpy.log(self.hist)
 
     def single(self, trigs):
         """Calculate the single detector statistic.
@@ -207,7 +180,7 @@ class PhaseTDStatistic(NewSNRStatistic):
             Single detector ranking statistic for the first detector.
         s2: numpy.ndarray
             Single detector ranking statistic for the second detector.
-        slide: numpy.ndarray 
+        slide: numpy.ndarray
             Array of ints. These represent the multiple of the timeslide
         interval to bring a pair of single detector triggers into coincidence.
         step: float
@@ -229,12 +202,12 @@ class PhaseTDStatistic(NewSNRStatistic):
         sbins = self.files['phasetd_newsnr']['sbins'][:]
         rbins = self.files['phasetd_newsnr']['rbins'][:]
 
-        # Find which bin each coinc falls into        
-        tv = numpy.searchsorted(tbins, td) - 1 
+        # Find which bin each coinc falls into
+        tv = numpy.searchsorted(tbins, td) - 1
         pv = numpy.searchsorted(pbins, pd) - 1
         s1v = numpy.searchsorted(sbins, s1[:,4]) - 1
-        s2v = numpy.searchsorted(sbins, s2[:,4]) - 1    
-        rv = numpy.searchsorted(rbins, rd) - 1  
+        s2v = numpy.searchsorted(sbins, s2[:,4]) - 1
+        rv = numpy.searchsorted(rbins, rd) - 1
 
         # The following just enforces that the point fits into
         # the bin boundaries. If a point lies outside the boundaries it is
@@ -249,11 +222,78 @@ class PhaseTDStatistic(NewSNRStatistic):
         s2v[s2v >= len(sbins) - 1] = len(sbins) - 2
         rv[rv < 0] = 0
         rv[rv >= len(rbins) - 1] = len(rbins) - 2
-        
+
         rstat = s1[:,0]**2.0 + s2[:,0]**2.0
         cstat = rstat + 2.0 * self.hist[tv, pv, s1v, s2v, rv]
-        cstat[cstat < 0] = 0        
+        cstat[cstat < 0] = 0
         return cstat ** 0.5
+
+class ExpFitStatistic(NewSNRStatistic):
+    def __init__(self, files):
+        NewSNRStatistic.__init__(self, files)
+        if not len(files):
+            raise RuntimeError("Can't find any statistic files !")
+        # the stat file attributes are hard-coded as '%{ifo}-fit_coeffs'
+        self.ifos = [f.split('-')[0] for f in self.statfiles.keys() if \
+                     f.split('-')[1] == 'fit_coeffs']
+        if (not len(files)) or (not len(self.ifos)):
+            raise RuntimeError("None of the statistic files has the required "
+                               "attribute called {ifo}-fit_coeffs !")
+        self.fits_by_tid = {}
+        for i in self.ifos:
+           self.fits_by_tid[i] = self.assign_fits(i)
+
+    def assign_fits(self, ifo):
+        coeff_file = self.statfiles[ifo+'-fit_coeffs']
+        template_id = coeff_file['template_id'][:]
+        alphas = coeff_file['fit_coeff'][:]
+        lambdas = coeff_file['count_above_thresh'][:]
+        # the template_ids and fit coeffs are stored in an arbitrary order
+        # create new arrays in template_id order for easier recall
+        tid_sort = numpy.argsort(template_id)
+        return {'alpha':alphas[tid_sort], 'lambda':lambdas[tid_sort],
+                'thresh':coeff_file.attrs['stat_threshold']}
+
+    def find_fits(self, trigs):
+        """Get fit coeffs for a specific ifo and template id"""
+        ifo = trigs.ifo
+        tnum = trigs.template_num
+        # fits_by_template is a dictionary of dictionaries of arrays
+        # indexed by ifo / coefficient name / template_id
+        alphai = self.fits_by_tid[ifo]['alpha'][tnum][0]
+        lambdai = self.fits_by_tid[ifo]['lambda'][tnum][0]
+        thresh = self.fits_by_tid[ifo]['thresh']
+        return alphai, lambdai, thresh
+
+    def single(self, trigs):
+        """
+        Calculate the parts of the coinc statistic depending on sngl parameters
+
+        Read in single trigger information, make the newsnr statistic
+        and rescale by the fitted coefficients alpha and lambda
+        """
+        alphai, lambdai, thresh = self.find_fits(trigs)
+        dof = 2 * trigs['chisq_dof'] - 2
+        newsnr = events.newsnr(trigs['snr'], trigs['chisq'] / dof)
+        # alphai is constant of proportionality between single-ifo newsnr and
+        #  negative log noise likelihood in given template
+        # lambdai is rate of trigs in given template compared to average
+        # thresh is stat threshold used in given ifo
+        lognoisel = - alphai * (newsnr - thresh) + numpy.log(alphai) + \
+                      numpy.log(lambdai)
+        return numpy.array(lognoisel, ndmin=1, dtype=numpy.float32)
+
+    def coinc(self, s0, s1, slide, step):
+        """Calculate the final coinc ranking statistic"""
+        # Approximate log likelihood ratio by summing single-ifo negative
+        # log noise likelihoods
+        loglr = - s0 - s1
+        # add squares of threshold stat values with notional Gaussian formula
+        threshes = [self.fits_by_tid[i]['thresh'] for i in self.ifos]
+        loglr += sum([t**2 / 2. for t in threshes])
+        # convert back to a coinc-SNR-like statistic
+        # notionally, log likelihood ratio \propto rho_c^2 / 2
+        return (2 * loglr) ** 0.5
 
 class MaxContTradNewSNRStatistic(NewSNRStatistic):
 
@@ -281,3 +321,36 @@ class MaxContTradNewSNRStatistic(NewSNRStatistic):
                                          trigs['cont_chisq'] / autochisq_dof)
         return numpy.array(numpy.minimum(chisq_newsnr, autochisq_newsnr,
                              dtype=numpy.float32), ndmin=1, copy=False)
+
+statistic_dict = {
+    'newsnr': NewSNRStatistic,
+    'newsnr_cut': NewSNRCutStatistic,
+    'phasetd_newsnr': PhaseTDStatistic,
+    'exp_fit_stat': ExpFitStatistic,
+    'max_cont_trad_newsnr': MaxContTradNewSNRStatistic,
+}
+
+def get_statistic(stat):
+    """
+    Error-handling sugar around dict lookup
+
+    Parameters
+    ----------
+    stat : string
+        Name of the statistic
+
+    Returns
+    -------
+    class
+        Subclass of Stat base class
+
+    Raises
+    ------
+    RuntimeError
+        If the string is not recognized as corresponding to a Stat subclass
+    """
+    try:
+        return statistic_dict[stat]
+    except KeyError:
+        raise RuntimeError('%s is not an available detection statistic' % stat)
+
