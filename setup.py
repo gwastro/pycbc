dea770bdfaeea834681c79d05074fc585984013e
Thomas Dent <thomas.dent@ligo.org>
2016-08-23 17:42:26 +0200

    now remove scikit-learn dependency, allow fitting with alternative smoothing function

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ install_requires =  setup_requires + ['Mako>=1.0.1',
                       'pycbc-glue>=0.9.8',
                       'kombine',
                       'emcee>=2.2.0',
-                      'scikit-learn>=0.17.0',
+                      #'scikit-learn>=0.17.0',  # travis does not like scikit-learn
                       ]
 links = ['https://github.com/ligo-cbc/mpld3/tarball/master#egg=mpld3-0.3git']
 
