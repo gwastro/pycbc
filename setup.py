d2600eeffb52b712095bb1a2e500495aad48a44e
Duncan Brown <dabrown@syr.edu>
2016-08-31 20:07:19 -0400

    Set back to 1.6.dev0 (#1065)

--- setup.py
+++ setup.py
@@ -262,8 +262,8 @@ def get_version_info():
             
     # If this is a release or another kind of source distribution of PyCBC
     except:
-        version = '1.5.3'
-        release = 'True'
+        version = '1.6.dev0'
+        release = 'False'
         date = hash = branch = tag = author = committer = status = builder = build_date = ''
     
         with open('pycbc/version.py', 'w') as f:
