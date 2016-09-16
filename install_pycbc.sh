4c2d346505420f644b43f5db2a1c37f5ebb510b6
Duncan Brown <duncan.brown@ligo.org>
2016-09-16 08:59:07 -0400

    fixed lalapps build

--- install_pycbc.sh
+++ install_pycbc.sh
@@ -587,7 +587,7 @@ source ${VIRTUAL_ENV}/opt/lalsuite/etc/lalsuiterc
 
 #Build a static lalapps_inspinj and install it
 cd $VIRTUAL_ENV/src/lalsuite/lalsuite/lalapps
-./configure --prefix=${VIRTUAL_ENV}/opt/lalsuite --enable-static-binaries --disable-mpi --disable-bambi --disable-cfitsio --disable-pss --disable-gds --disable-lalstochastic --disable-lalxml --disable-lalinference --disable-laldetchar --disable-lalpulsar
+./configure --prefix=${VIRTUAL_ENV}/opt/lalsuite --enable-static-binaries 
 cd $VIRTUAL_ENV/src/lalsuite/lalsuite/lalapps/src/lalapps
 make -j $nproc
 cd $VIRTUAL_ENV/src/lalsuite/lalsuite/lalapps/src/inspiral
