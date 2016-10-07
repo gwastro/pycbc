529d558830b01ad3fee4ff2e3e32db021e4d8c61
Alexander Harvey Nitz <alex.nitz@ligo.org>
2016-10-07 15:21:59 +0200

    bump version to 1.5.4 for release

--- setup.py
+++ setup.py
@@ -261,8 +261,8 @@ def get_version_info():
             
     # If this is a release or another kind of source distribution of PyCBC
     except:
-        version = '1.6.dev0'
-        release = 'False'
+        version = '1.5.4'
+        release = 'True'
         date = hash = branch = tag = author = committer = status = builder = build_date = ''
     
         with open('pycbc/version.py', 'w') as f:
