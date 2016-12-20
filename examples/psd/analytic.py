401ad8347a745a554991aafada6f1c11845919f7
Alex Nitz <alex.nitz@gmail.com>
2016-12-20 19:16:42 +0100

    Update analytic.py

--- examples/psd/analytic.py
+++ examples/psd/analytic.py
@@ -8,7 +8,7 @@ delta_f = 1.0 / 4
 flen = int(1024 / delta_f)
 low_frequency_cutoff = 30.0
 
-# One can either call the psd generator by name 
+# One can either call the psd generator by name
 p1 = pycbc.psd.aLIGOZeroDetHighPower(flen, delta_f, low_frequency_cutoff)
 
 # or by using the name as a string.
