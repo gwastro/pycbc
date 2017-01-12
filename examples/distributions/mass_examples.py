5b13d9c00a63304f48991de9a019c1a8fd28a795
Steven Reyes <steven.reyes@ligo.org>
2017-01-12 09:54:28 -0500

    Change some comments around.

--- examples/distributions/mass_examples.py
+++ examples/distributions/mass_examples.py
@@ -7,26 +7,26 @@ mass1_distribution = distributions.Uniform(mass1=(0.5, 1.5))
 # Take 100000 random variable samples from this uniform mass distribution.
 mass1_samples = mass1_distribution.rvs(size=1000000)
 
-# Create another mass distribution object that is Gaussian between 0.5 and
-# 1.5 solar masses with a mean of 1.2 solar masses and a standard deviation
-# of 0.15 solar masses. Gaussian takes the variance as an output so square
-# the standard deviation.
+# Draw another distribution that is Gaussian between 0.5 and 1.5 solar masses
+# with a mean of 1.2 solar masses and a standard deviation of 0.15 solar
+# masses. Gaussian takes the variance as an input so square the standard
+# deviation.
 variance = 0.15*0.15
-mass2_distribution = distributions.Gaussian(mass2=(0.5, 1.5), mass2_mean=1.2,
-                                            mass2_var=variance)
+mass2_gaussian = distributions.Gaussian(mass2=(0.5, 1.5), mass2_mean=1.2,
+                                        mass2_var=variance)
 
 # Take 100000 random variable samples from this gaussian mass distribution.
-mass2_samples = mass2_distribution.rvs(size=1000000)
+mass2_samples = mass2_gaussian.rvs(size=1000000)
 
 # We can make pairs of distributions together, instead of apart.
 two_mass_distributions = distributions.Uniform(mass3=(1.6, 3.0),
                                                mass4=(1.6, 3.0))
 two_mass_samples = two_mass_distributions.rvs(size=1000000)
 
-# Choose 20 mass bins for the histogram subplots.
+# Choose 20 bins for the histogram subplots.
 n_bins = 20
 
-# Make some subplots
+# Plot histograms of samples in subplots
 fig, axes = plt.subplots(nrows=2, ncols=2)
 ax0, ax1, ax2, ax3, = axes.flat
 
