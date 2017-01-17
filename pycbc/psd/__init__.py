6b28a54bd700e5f82c532bfa75de755a03556e2f
Andrew R. Williamson <a-r-williamson@users.noreply.github.com>
2017-01-17 04:59:31 -0500

    Getting pycbc_multi_inspiral to run to completion (#1339)
    * Getting pycbc_multi_inspiral to run to completion
    
    * Changing the writing out of multi IFO triggers
    
    * Adding exception types per code quality check

--- pycbc/psd/__init__.py
+++ pycbc/psd/__init__.py
@@ -438,3 +438,37 @@ def associate_psds_to_segments(opt, fd_segments, gwstrain, flen, delta_f, flow,
             err_msg = "No PSDs found intersecting segment!"
             raise ValueError(err_msg)
         fd_segment.psd = best_psd
+
+def associate_psds_to_single_ifo_segments(opt, fd_segments, gwstrain, flen,
+                                          delta_f, flow, ifo,
+                                          dyn_range_factor=1., precision=None):
+    """
+    Associate PSDs to segments for a single ifo when using the multi-detector
+    CLI
+    """
+    single_det_opt = copy_opts_for_single_ifo(opt, ifo)
+    associate_psds_to_segments(single_det_opt, fd_segments, gwstrain, flen,
+                               delta_f, flow, dyn_range_factor=dyn_range_factor,
+                               precision=precision)
+
+def associate_psds_to_multi_ifo_segments(opt, fd_segments, gwstrain, flen,
+                                         delta_f, flow, ifos,
+                                         dyn_range_factor=1., precision=None):
+    """
+    Associate PSDs to segments for all ifos when using the multi-detector CLI
+    """
+    for ifo in ifos:
+        if gwstrain is not None:
+            strain = gwstrain[ifo]
+        else:
+            strain = None
+
+        if fd_segments is not None:
+            segments = fd_segments[ifo]
+        else:
+            segments = None
+
+        associate_psds_to_single_ifo_segments(opt, segments, strain, flen,
+                delta_f, flow, ifo, dyn_range_factor=dyn_range_factor,
+                precision=precision)
+
