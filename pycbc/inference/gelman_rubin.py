71612d76c90310243c822f67bc67f72508f9e309
Christopher M. Biwer <christopher.biwer@ligo.org>
2017-01-18 14:24:30 -0500

    Add gelman_rubin module.

---
+++ pycbc/inference/gelman_rubin.py
@@ -0,0 +1,38 @@
+# Copyright (C) 2017  Christopher M. Biwer
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 3 of the License, or (at your
+# option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+# Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+""" This modules provides functions for evaluating the Gelman-Rubin convergence
+diagnostic statistic.
+"""
+
+def gelman_rubin(x):
+    """ Calculates the Gelman-Rubin convergence statistic.
+    """
+
+    # calculate mean of each chain
+
+    # calculate overall mean
+
+    # calculate variance of each chain
+
+    # calculate the between-chain variance
+
+    # calculate the within-chain variance
+
+    # calculate the pooled variance
+
+    # calculate the potential scale reduction factor
+    psrf = 0.0
+
+    return psrf
