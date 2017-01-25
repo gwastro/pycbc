24bdb376d9f9df48916bd38d7dfa38db083d5ae0
Christopher M. Biwer <cmbiwer@gmail.com>
2017-01-25 07:45:00 -0500

    Add Geweke convergence diagnostic statistic (#1376)
    * Add geweke module.
    
    * Comments and docstring for geweke.
    
    * Docstring.
    
    * Add start index for first segment and end index for reference segment.
    
    * Docstring.
    
    * Plot each walker seperately.
    
    * Add logging statement.
    
    * Update docstring.
    
    * Add walkers option.
    
    * Add options on command line for Geweke calculation.
    
    * Save plot with meta-data.
    
    * More verbosity.
    
    * Add to setup.py.
    
    * Use pycbc logging init.
    
    * Turn load_samples to False.

--- setup.py
+++ setup.py
@@ -436,6 +436,7 @@ setup (
                'bin/inference/pycbc_inference_plot_acf',
                'bin/inference/pycbc_inference_plot_acl',
                'bin/inference/pycbc_inference_plot_corner',
+               'bin/inference/pycbc_inference_plot_geweke',
                'bin/inference/pycbc_inference_plot_movie',
                'bin/inference/pycbc_inference_plot_posterior',
                'bin/inference/pycbc_inference_plot_prior',
