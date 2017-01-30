d54abe5b0889b9399dde1f2ffe4e80a00b923745
Bernd Machenschalk <bernd.machenschalk@ligo.org>
2017-01-30 08:14:31 +0100

    E@H: fstab: removed obsolete code & added a header

--- tools/einsteinathome/fstab.c
+++ tools/einsteinathome/fstab.c
@@ -1,3 +1,25 @@
+/*
+  Helper tool for running Cygwin Apps under BOINC 
+
+  This tool is meant to be called with an absolute (Windows) path to the
+  project directory as the first/only argument
+
+  It will create a file etc/fstab with a single entry pointing to the project directory
+  such that "boinc-resolved" paths will point to the correct locations
+
+  example:
+
+    fstab C:\Program Data\BOINC Data\projects\albert.phys.uwm.edu
+
+    => etc/fstab:
+    C:/Program\040Data/BOINC\040Data/projects/albert.phys.uwm.edu /projects/albert.phys.uwm.edu dummy binary 0 0
+
+    boinc-softlinks will resolve to
+    ../../projects/albert.phys.uwm.edu/file
+
+  (C) Bernd Machenschalk 2016,2017
+*/
+
 #include <stdio.h>
 #include <string.h>
 #include <limits.h>
@@ -18,12 +40,13 @@
 char path[PATH_MAX];
 
 int main(int argc, char*argv[]) {
+
+#if defined(_WIN32) || defined(TEST_WIN32)
+
   char *c = argv[1];
   char *n, *proj;
   unsigned int i=0;
   FILE* fp;
-
-#if defined(_WIN32) || defined(TEST_WIN32)
 
   // replace all "\" with "/" in argv[1]
   while (*c) {
@@ -63,54 +86,6 @@ int main(int argc, char*argv[]) {
     return(1);
   fprintf(fp, "%s %s dummy binary 0 0\n", path, proj);
   fclose(fp);
-
-#else // (TEST)_WIN32
-
-  int ret;
-  char buf[1024];
-
-  n = strrchr(argv[1],'/') + 1;
-#define LINK "/tmp/BOINC.project."
-  i = strlen(LINK) + strlen(n) + 1;
-  proj = (char*)malloc(i * sizeof(char));
-  strcpy(proj, LINK);
-  strcat(proj, n);
-  ret = symlink(argv[1], proj);
-  if ((ret != 0) && (errno != EEXIST)) {
-    fprintf(stderr, "ERROR: Can't link %s %s: %d\n", argv[1], proj, errno);
-    exit(1);
-  }
-  // replace "../../projects<proj>" by "<LINK>" in all files named on the
-  // command-line after the project directory (argv[1])
-  // such that boinc_resolves to these paths without blanks
-  for (i=2; i<argc; i++) {
-    fp = fopen(argv[i], "r");
-    if (!fp) {
-      fprintf(stderr, "ERROR: Can't open file %s for reading\n", argv[i]);
-      exit(1);
-    }
-    if (!fgets(buf, sizeof(buf), fp) && ferror(fp)) {
-      fprintf(stderr, "ERROR: Can't read from file %s\n", argv[i]);
-      exit(1);
-    }
-    fclose(fp);
-#define XML "<soft_link>../../projects/"
-    if (strncmp(buf, XML, strlen(XML))) {
-      fprintf(stderr, "WARNING: file %s is not a XML soft-link to project dir\n", argv[i]);
-      fclose(fp);
-      continue;
-    }
-    fp = fopen(argv[i], "w");
-    if (!fp) {
-      fprintf(stderr, "ERROR: Can't open file %s for writing\n", argv[i]);
-      exit(1);
-    }
-    if (fprintf(fp, "<soft_link>" LINK "%s", buf+strlen(XML)) <= 0) {
-      fprintf(stderr, "ERROR: Can't write to file %s\n", argv[i]);
-      exit(1);
-    }
-    fclose(fp);
-  }
 
 #endif // (TEST)_WIN32
 
