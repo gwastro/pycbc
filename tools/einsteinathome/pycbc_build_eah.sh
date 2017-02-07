gwastro/pycbc

commit 273e766cde03c7743e2f2c12c6400eb343fdbfca

allow user to specify a remote and branch

tools/einsteinathome/pycbc_build_eah.sh
@@ -199,6 +199,8 @@ for i in $*; do
         --bema-testing)
             pycbc_branch=einsteinathome_testing
             pycbc_remote=bema-ligo;;
+        --pycbc-remote=*) pycbc_remote="`echo $i|sed 's/^--pycbc-remote=//'`";;
+        --pycbc-branch=*) pycbc_branch="`echo $i|sed 's/^--pycbc-branch=//'`";;
         --no-cleanup) cleanup=false;;
         --no-analysis) run_analysis=false;;
         --verbose-python) verbose_pyinstalled_python=true;;
@@ -751,9 +753,15 @@ if $scratch_pycbc || ! test -d pycbc/.git ; then
     cd ..
 fi
 cd pycbc
-if test ".$pycbc_branch" = ".HEAD"; then
+if test ".$pycbc_remote" = "ligo-cbc" || test ".$pycbc_remote" = "bema-ligo" ; then
     :
-elif test ".$pycbc_branch" = ".master"; then
+else
+    git remote add $pycbc_remote https://github.com/${pycbc_remote}/pycbc.git
+    git remote update
+fi
+if test ".$pycbc_branch" = ".HEAD" ; then
+    :
+elif test ".$pycbc_branch" = ".master" ; then
     git checkout master
     git pull
     test ".$pycbc_commit" != "." &&

commit db3b9a6c7f0c6ad404040afe59c192cd02a0c7b4

E@H build script: remove special treatment of bema-ligo remote
- also fix bug introduced in commit:273e766c that makes Cygwin's git crash

tools/einsteinathome/pycbc_build_eah.sh
@@ -757,16 +757,13 @@ echo -e "\\n\\n>> [`date`] building pycbc"
 if $scratch_pycbc || ! test -d pycbc/.git ; then
     # clone
     rm -rf pycbc
-    git clone git://github.com/ligo-cbc/pycbc
+    git clone -o ligo-cbc -n git://github.com/ligo-cbc/pycbc
     cd pycbc
-    git remote rename origin ligo-cbc
-    git remote add bema-ligo https://github.com/bema-ligo/pycbc.git
-    git remote update
-    git branch einsteinathome_testing bema-ligo/einsteinathome_testing
+    git checkout -b master ligo-cbc/master
     cd ..
 fi
 cd pycbc
-if test ".$pycbc_remote" = "ligo-cbc" || test ".$pycbc_remote" = "bema-ligo" ; then
+if test ".$pycbc_remote" = ".ligo-cbc" ; then
     :
 else
     git remote rm $pycbc_remote || true
@@ -782,6 +779,7 @@ elif test ".$pycbc_branch" = ".master" ; then
         git checkout $pycbc_commit
 else
     # checkout branch from scratch, master must and should exist
+    git reset --hard HEAD
     git checkout master
     git branch -D $pycbc_branch || true
     git remote update