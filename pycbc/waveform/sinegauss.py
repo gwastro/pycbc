88c60b7a9648ba1dde245cd629650d30b1254b7a
Ian Harry <iwharry@googlemail.com>
2017-02-17 14:16:31 +0100

    Fix the whitespace issues.

--- pycbc/waveform/sinegauss.py
+++ pycbc/waveform/sinegauss.py
@@ -6,7 +6,7 @@ import numpy
 
 def fd_sine_gaussian(amp, quality, central_frequency, fmin, fmax, delta_f):
     """ Generate a Fourier domain sine-Gaussian
-    
+
     Parameters
     ----------
     amp: float
@@ -32,7 +32,7 @@ def fd_sine_gaussian(amp, quality, central_frequency, fmin, fmax, delta_f):
     kmax = int(fmax / delta_f)
     kmin = int(fmin / delta_f)
     tau = quality / 2 / numpy.pi / central_frequency
-    d = amp * numpy.pi ** 0.5 / 2 * tau 
+    d = amp * numpy.pi ** 0.5 / 2 * tau
     d *= numpy.exp(-(numpy.pi  * tau  * (f - central_frequency))**2.0)
     d *= (1 + numpy.exp(-quality ** 2.0 * f / central_frequency))
     v = numpy.zeros(kmax, dtype=numpy.complex128)
