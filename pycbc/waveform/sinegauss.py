8109668b154725edcc87ac5bb51732ccef5ae998
Alex Nitz <alex.nitz@gmail.com>
2017-02-17 14:36:45 +0100

    Update sinegauss.py

--- pycbc/waveform/sinegauss.py
+++ pycbc/waveform/sinegauss.py
@@ -32,8 +32,8 @@ def fd_sine_gaussian(amp, quality, central_frequency, fmin, fmax, delta_f):
     kmax = int(fmax / delta_f)
     kmin = int(fmin / delta_f)
     tau = quality / 2 / numpy.pi / central_frequency
-    d = amp * numpy.pi ** 0.5 / 2 * tau
-    d *= numpy.exp(-(numpy.pi  * tau  * (f - central_frequency))**2.0)
+    A = amp * numpy.pi ** 0.5 / 2 * tau
+    d = A * numpy.exp(-(numpy.pi  * tau  * (f - central_frequency))**2.0)
     d *= (1 + numpy.exp(-quality ** 2.0 * f / central_frequency))
     v = numpy.zeros(kmax, dtype=numpy.complex128)
     v[kmin:kmax] = d[:]
