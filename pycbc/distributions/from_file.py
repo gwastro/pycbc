6330ea7cada7145d01c75fbd4785dc6633254018
Christopher M. Biwer <christopher.biwer@ligo.org>
2017-02-18 11:05:22 -0500

    Remove unused imports.

--- pycbc/distributions/from_file.py
+++ pycbc/distributions/from_file.py
@@ -1,10 +1,26 @@
+# Copyright (C) 2016 Miriam Cabero Mueller
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 3 of the License, or (at your
+# option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+# Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+"""
+This modules provides classes for evaluating arbitrary distributions from
+a file.
+"""
+
+import h5py
 import numpy
 import scipy.stats
-import h5py
-from ConfigParser import Error
-import warnings
 from pycbc.distributions import bounded
-from pycbc.inference import boundaries
 
 class FromFile(bounded._BoundedDist):
     """A distribution that reads the values of the parameter(s) from an hdf
--- pycbc/distributions/power_law.py
+++ pycbc/distributions/power_law.py
@@ -1,10 +1,24 @@
+# Copyright (C) 2016 Christopher M. Biwer
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 3 of the License, or (at your
+# option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+# Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+"""
+This modules provides classes for evaluating distributions where the
+probability density function is a power law.
+"""
+
 import numpy
-import scipy.stats
-import h5py
-from ConfigParser import Error
-import warnings
 from pycbc.distributions import bounded
-from pycbc.inference import boundaries
 
 class UniformRadius(bounded._BoundedDist):
     r"""
