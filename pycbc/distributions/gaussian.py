f833b3aa81476389f41089ef18cd5e17249a16c1
Christopher M. Biwer <christopher.biwer@ligo.org>
2017-02-18 11:05:22 -0500

    Remove unused imports.

--- pycbc/distributions/gaussian.py
+++ pycbc/distributions/gaussian.py
@@ -1,10 +1,24 @@
+# Copyright (C) 2016  Christopher M. Biwer, Collin Capano
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 3 of the License, or (at your
+# option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+# Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+"""
+This modules provides classes for evaluating Gaussian distributions.
+"""
+
 import numpy
 import scipy.stats
-import h5py
-from ConfigParser import Error
-import warnings
 from pycbc.distributions import bounded
-from pycbc.inference import boundaries
 
 class Gaussian(bounded._BoundedDist):
     r"""A Gaussian distribution on the given parameters; the parameters are
