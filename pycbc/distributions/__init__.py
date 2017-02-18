e9a1f4b11d0ff7bcd7ca492ae7ce8da2fb3b6841
Christopher M. Biwer <christopher.biwer@ligo.org>
2017-02-18 11:14:37 -0500

    Fix import in example.

--- pycbc/distributions/__init__.py
+++ pycbc/distributions/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2016  Collin Capano, Christopher M. Biwer
+# Copyright (C)  2016 Collin Capano, Christopher M. Biwer
 # This program is free software; you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by the
 # Free Software Foundation; either version 3 of the License, or (at your
