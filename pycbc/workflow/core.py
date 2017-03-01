87ea5475d233b8044a2a2e113a0117ed5b456351
Duncan Brown <duncan.brown@ligo.org>
2017-02-28 19:32:36 -0500

    poke travis

--- pycbc/workflow/core.py
+++ pycbc/workflow/core.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2013  Ian Harry, Alex Nitz
+# Copyright (C) 2013, 2017  Ian Harry, Alex Nitz, Duncan Brown
 #
 # This program is free software; you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by the
