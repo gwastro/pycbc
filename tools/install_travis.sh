63427efd0348fd1455d47223a5a49090de2ea6fb
Duncan Brown <duncan.brown@ligo.org>
2017-03-08 13:37:55 -0500

    use the eah environment for the virtual build

--- tools/install_travis.sh
+++ tools/install_travis.sh
@@ -15,10 +15,18 @@ mkdir -p ${BUILD}
 
 # run the einstein at home build and test script
 pushd ${BUILD}
-${LOCAL}/tools/einsteinathome/pycbc_build_eah.sh --lalsuite-commit=a2a5a476d33f169b8749e2840c306a48df63c936 --clean-pycbc --silent-build
+${LOCAL}/tools/einsteinathome/pycbc_build_eah.sh --lalsuite-commit=a2a5a476d33f169b8749e2840c306a48df63c936 --clean-pycbc --silent-build --no-analysis
 popd
 
 # setup the pycbc environment to run the additional travis tests
+BUILDDIRNAME="pycbc-build"
+PYCBC="$BUILD/$BUILDDIRNAME"
+PYTHON_PREFIX="$PYCBC"
+ENVIRONMENT="$PYCBC/environment"
+PREFIX="$ENVIRONMENT"
+PATH="$PREFIX/bin:$PYTHON_PREFIX/bin:$PATH"
+export LD_LIBRARY_PATH="$PREFIX/lib:$PREFIX/bin:$PYTHON_PREFIX/lib:$libgfortran_dir:/usr/local/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="$PREFIX/lib/pkgconfig:$PYTHON_PREFIX/lib/pkgconfig:/usr/local/lib/pkgconfig:$PKG_CONFIG_PATH"
 source ${BUILD}/pycbc-build/environment/etc/lalsuite-user-env.sh
 source ${BUILD}/pycbc-build/environment/bin/activate
 export PYTHONUSERBASE=${BUILD}/.local
@@ -31,7 +39,7 @@ pip install --upgrade pip setuptools
 pip install 'setuptools==18.2' --upgrade
 
 # install pegasus
-pip install http://download.pegasus.isi.edu/pegasus/4.7.2/pegasus-python-source-4.7.4.tar.gz
+pip install http://download.pegasus.isi.edu/pegasus/4.7.4/pegasus-python-source-4.7.4.tar.gz
 
 # re-install pycbc
 python setup.py install
