39a347cd72afe01bf5226594d2c4ca6fe0cc422a
Duncan Brown <duncan.brown@ligo.org>
2017-03-08 16:06:32 -0500

    turn on gw150914 analysis in travis

--- tools/install_travis.sh
+++ tools/install_travis.sh
@@ -15,7 +15,7 @@ mkdir -p ${BUILD}
 
 # run the einstein at home build and test script
 pushd ${BUILD}
-${LOCAL}/tools/einsteinathome/pycbc_build_eah.sh --lalsuite-commit=a2a5a476d33f169b8749e2840c306a48df63c936 --pycbc-fetch-ref=${PYCBC_MERGE_REF} --clean-pycbc --silent-build --no-analysis
+${LOCAL}/tools/einsteinathome/pycbc_build_eah.sh --lalsuite-commit=a2a5a476d33f169b8749e2840c306a48df63c936 --pycbc-fetch-ref=${PYCBC_MERGE_REF} --clean-pycbc --silent-build
 popd
 
 # setup the pycbc environment to run the additional travis tests
