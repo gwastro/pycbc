fb2609903536004567806dd2c802db617e69d923
Duncan Brown <duncan.brown@ligo.org>
2017-03-09 16:00:07 -0500

    get library needed to build fft module docs

--- tools/install_travis.sh
+++ tools/install_travis.sh
@@ -61,5 +61,15 @@ pip install numpydoc
 pip install sphinx-rtd-theme
 pip install git+https://github.com/ligo-cbc/sphinxcontrib-programoutput.git#egg=sphinxcontrib-programoutput
 
+# get library needed to build documentation
+wget_opts="-c --passive-ftp --no-check-certificate --tries=5 --timeout=30"
+url="https://code.pycbc.phy.syr.edu/ligo-cbc/pycbc-software/download/b3680bfb627a7350f29d31c7d91c4e09ff8a9fdc/x86_64/composer_xe_2015.0.090"
+p="libmkl_rt.so"
+pushd ${BUILD}/pycbc-sources
+test -r $p || wget $wget_opts ${url}/${p}
+cp -v $p $PREFIX/lib/$p
+chmod +x $PREFIX/lib/$p
+popd
+
 # re-install pycbc
 python setup.py install
