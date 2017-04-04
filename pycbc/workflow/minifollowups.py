7e1a011d2da9109a604dfedd08fcf1917e78218d
Thomas Massinger <tjma12@users.noreply.github.com>
2017-04-04 14:42:19 -0700

    Added code to print loudest events to a wiki table (#1554)
    * Added code to print loudest events to a wiki table
    
    * Moved add_wiki_row into script

--- pycbc/workflow/minifollowups.py
+++ pycbc/workflow/minifollowups.py
@@ -167,6 +167,8 @@ def setup_single_det_minifollowups(workflow, single_trig_file, tmpltbank_file,
     exe = Executable(workflow.cp, 'singles_minifollowup',
                      ifos=curr_ifo, out_dir=dax_output, tags=tags)
 
+    wikifile = curr_ifo + '_'.join(tags) + 'loudest_table.txt'
+
     node = exe.create_node()
     node.add_input_opt('--config-files', config_file)
     node.add_input_opt('--bank-file', tmpltbank_file)
@@ -175,6 +177,7 @@ def setup_single_det_minifollowups(workflow, single_trig_file, tmpltbank_file,
     node.add_opt('--inspiral-data-read-name', insp_data_name)
     node.add_opt('--inspiral-data-analyzed-name', insp_anal_name)
     node.add_opt('--instrument', curr_ifo)
+    node.add_opt('--wiki-file', wikifile)
     if veto_file is not None:
         assert(veto_segment_name is not None)
         node.add_input_opt('--veto-file', veto_file)
