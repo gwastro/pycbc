8814882cb720b71ee8b177f255e94a145bae6f0d
Duncan Brown <duncan.brown@ligo.org>
2017-04-07 16:52:42 -0400

    build virtualenv in centos 7 container

--- tools/test_inside_docker.sh
+++ tools/test_inside_docker.sh
@@ -54,6 +54,8 @@ if [ "x${OS_VERSION}" == "x7" ] ; then
   yum -y install git2u-all
   yum -y install lscsoft-all
 
+  rpm --nodeps -e lal-python-6.18.0-1.el7.x86_64 lalstochastic-octave-1.1.20-1.el7.x86_64 laldetchar-octave-0.3.5-1.el7.x86_64 lalinference-devel-1.9.0-1.el7.x86_64 lalxml-devel-1.2.4-1.el7.x86_64 gstlal-calibration-1.1.4-2.el7.x86_64 lal-octave-6.18.0-1.el7.x86_64 lalburst-octave-1.4.4-1.el7.x86_64 lalinspiral-octave-1.7.7-1.el7.x86_64 lalstochastic-1.1.20-1.el7.x86_64 gstlal-ugly-1.2.0-2.el7.x86_64 lalsimulation-1.7.0-1.el7.x86_64 lalsimulation-devel-1.7.0-1.el7.x86_64 lalpulsar-octave-1.16.0-1.el7.x86_64 laldetchar-devel-0.3.5-1.el7.x86_64 lalinspiral-debuginfo-1.7.7-1.el7.x86_64 lalmetaio-devel-1.3.1-1.el7.x86_64 lalinference-python-1.9.0-1.el7.x86_64 lalapps-6.21.0-1.el7.x86_64 lalmetaio-debuginfo-1.3.1-1.el7.x86_64 lalframe-devel-1.4.3-1.el7.x86_64 python-pylal-0.13.1-1.el7.x86_64 gstlal-ugly-devel-1.2.0-2.el7.x86_64 lalapps-debuginfo-6.21.0-1.el7.x86_64 lalburst-1.4.4-1.el7.x86_64 lalxml-python-1.2.4-1.el7.x86_64 lalinference-octave-1.9.0-1.el7.x86_64 lalframe-debuginfo-1.4.3-1.el7.x86_64 lalinspiral-1.7.7-1.el7.x86_64 lalburst-devel-1.4.4-1.el7.x86_64 lalsimulation-python-1.7.0-1.el7.x86_64 gstlal-inspiral-1.3.0-2.el7.x86_64 lscsoft-pylal-1.1-1.el7.noarch lscsoft-lalsuite-2.9-1.el7.noarch lalinference-1.9.0-1.el7.x86_64 lalstochastic-devel-1.1.20-1.el7.x86_64 lal-debuginfo-6.18.0-1.el7.x86_64 lal-6.18.0-1.el7.x86_64 lalstochastic-python-1.1.20-1.el7.x86_64 laldetchar-python-0.3.5-1.el7.x86_64 lalxml-debuginfo-1.2.4-1.el7.x86_64 lalxml-1.2.4-1.el7.x86_64 lalinspiral-devel-1.7.7-1.el7.x86_64 lalpulsar-python-1.16.0-1.el7.x86_64 laldetchar-debuginfo-0.3.5-1.el7.x86_64 lscsoft-gstlal-1.9-1.el7.noarch lalpulsar-1.16.0-1.el7.x86_64 lalxml-octave-1.2.4-1.el7.x86_64 lalframe-python-1.4.3-1.el7.x86_64 gstlal-inspiral-devel-1.3.0-2.el7.x86_64 lalsimulation-debuginfo-1.7.0-1.el7.x86_64 lalmetaio-1.3.1-1.el7.x86_64 laldetchar-0.3.5-1.el7.x86_64 lalburst-python-1.4.4-1.el7.x86_64 lalframe-octave-1.4.3-1.el7.x86_64 lscsoft-gstlal-dev-1.0-1.el7.noarch lalframe-1.4.3-1.el7.x86_64 lalsimulation-octave-1.7.0-1.el7.x86_64 lalmetaio-octave-1.3.1-1.el7.x86_64 lalpulsar-devel-1.16.0-1.el7.x86_64 lalburst-debuginfo-1.4.4-1.el7.x86_64 gstlal-devel-1.1.0-2.el7.x86_64 lalinference-debuginfo-1.9.0-1.el7.x86_64 lal-devel-6.18.0-1.el7.x86_64 lalsuite-extra-1.1.0-1.el7.noarch lalinspiral-python-1.7.7-1.el7.x86_64 lalpulsar-debuginfo-1.16.0-1.el7.x86_64 gstlal-1.1.0-2.el7.x86_64 lalstochastic-debuginfo-1.1.20-1.el7.x86_64 lalmetaio-python-1.3.1-1.el7.x86_64 lscsoft-lalsuite-dev-1.7-1.el7.noarch skyarea-0.2.1-1.el7.noarch
+
   TRAVIS_TAG="vX.Y.Z"
   CVMFS_PATH=/cvmfs/oasis.opensciencegrid.org/ligo/sw/pycbc/x86_64_rhel_7/virtualenv
   mkdir -p ${CVMFS_PATH}
@@ -66,6 +68,57 @@ if [ "x${OS_VERSION}" == "x7" ] ; then
   pip install --upgrade pip
   pip install six packaging appdirs
   pip install --upgrade setuptools
+
+  pip install "numpy>=1.6.4" unittest2 python-cjson Cython decorator
+  SWIG_FEATURES="-cpperraswarn -includeall -I/usr/include/openssl" pip install M2Crypto
+
+  mkdir -p ${VIRTUAL_ENV}/src
+  cd ${VIRTUAL_ENV}/src
+  git clone https://github.com/lscsoft/lalsuite.git
+  cd ${VIRTUAL_ENV}/src/lalsuite
+  git checkout ${LALSUITE_CODE}
+  ./00boot
+  ./configure --prefix=${VIRTUAL_ENV}/opt/lalsuite --enable-swig-python --disable-lalstochastic --disable-lalxml --disable-lalinference --disable-laldetchar --disable-lalapps
+  make -j 32 install
+  echo 'source ${VIRTUAL_ENV}/opt/lalsuite/etc/lalsuite-user-env.sh' >> ${VIRTUAL_ENV}/bin/activate
+  deactivate
+
+  source ${VENV_PATH}/bin/activate
+  cd $VIRTUAL_ENV/src/lalsuite/lalapps
+  LIBS="-lhdf5_hl -lhdf5 -ldl -lz" ./configure --prefix=${VIRTUAL_ENV}/opt/lalsuite --enable-static-binaries --disable-lalinference --disable-lalburst --disable-lalpulsar --disable-lalstochastic
+  cd $VIRTUAL_ENV/src/lalsuite/lalapps/src/lalapps
+  make
+  cd $VIRTUAL_ENV/src/lalsuite/lalapps/src/inspiral
+  make lalapps_inspinj
+  cp lalapps_inspinj $VIRTUAL_ENV/bin
+  cd $VIRTUAL_ENV/src/lalsuite/lalapps/src/ring
+  make lalapps_coh_PTF_inspiral
+  cp lalapps_coh_PTF_inspiral $VIRTUAL_ENV/bin
+
+  pip install http://download.pegasus.isi.edu/pegasus/4.7.4/pegasus-python-source-4.7.4.tar.gz
+  pip install git+https://github.com/ligovirgo/dqsegdb@clean_pip_install_1_4_1#egg=dqsegdb
+
+  cd /pycbc
+  python setup.py install
+
+  pip install pycbc-pylal
+
+  pip install 'matplotlib==1.5.3'
+  pip install ipython
+  pip install jupyter
+
+cat << EOF >> $VIRTUAL_ENV/bin/activate
+
+# If a suitable MKL exists, set it up
+if [ -f /opt/intel/composer_xe_2015/mkl/bin/mklvars.sh ] ; then
+  . /opt/intel/composer_xe_2015/mkl/bin/mklvars.sh intel64
+elif [ -f /ldcg/intel/2017u0/compilers_and_libraries_2017.0.098/linux/mkl/bin/mklvars.sh ] ; then
+  . /ldcg/intel/2017u0/compilers_and_libraries_2017.0.098/linux/mkl/bin/mklvars.sh intel64
+fi
+
+# Use the revison 11 ROM data from CVMFS
+export LAL_DATA_PATH=/cvmfs/oasis.opensciencegrid.org/ligo/sw/pycbc/lalsuite-extra/11/share/lalsimulation
+EOF
 
   deactivate
 fi 
