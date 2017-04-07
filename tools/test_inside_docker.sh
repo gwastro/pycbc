3150887ccca19b54497a49424d767d922697f3d5
Duncan Brown <duncan.brown@ligo.org>
2017-04-07 16:21:04 -0400

    install sl6 required software

--- tools/test_inside_docker.sh
+++ tools/test_inside_docker.sh
@@ -17,6 +17,14 @@ cat /etc/issue
 if [ "x${OS_VERSION}" == "x6" ] ; then
   echo -e "\\n>> [`date`] Building pycbc_inspiral bundle for CentOS 6"
 
+  # install requirements into docker container
+  yum install -y gcc gcc-c++ gcc-gfortran python-devel pcre-devel autoconf automake make zlib-devel libpng-devel libjpeg-devel libsqlite3-dev sqlite-devel
+  ln -s /usr/bin/g++ /usr/bin/g++-4.4.7
+  ln -s /usr/bin/gfortran /usr/bin/g++-4.4.7
+  ln -s /usr/bin/gfortran /usr/bin/gfortran-4.4.7
+  yum -y install epel-release
+  yum -y install ant asciidoc fop docbook-style-xsl.noarch R-devel
+
   # create working dir for build script
   BUILD=/pycbc/build
   mkdir -p ${BUILD}
