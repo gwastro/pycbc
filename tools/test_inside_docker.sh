bdf465059f4cf7991083fed639960401c519a825
Duncan Brown <duncan.brown@ligo.org>
2017-04-07 17:10:27 -0400

    remove lal from centos 7 and install wget in sl6

--- tools/test_inside_docker.sh
+++ tools/test_inside_docker.sh
@@ -18,7 +18,7 @@ if [ "x${OS_VERSION}" == "x6" ] ; then
   echo -e "\\n>> [`date`] Building pycbc_inspiral bundle for CentOS 6"
 
   # install requirements into docker container
-  yum install -y gcc gcc-c++ gcc-gfortran python-devel pcre-devel autoconf automake make zlib-devel libpng-devel libjpeg-devel libsqlite3-dev sqlite-devel
+  yum install -y gcc gcc-c++ gcc-gfortran python-devel pcre-devel autoconf automake make zlib-devel libpng-devel libjpeg-devel libsqlite3-dev sqlite-devel wget curl
   ln -s /usr/bin/g++ /usr/bin/g++-4.4.7
   ln -s /usr/bin/gfortran /usr/bin/gfortran-4.4.7
   yum -y install ant asciidoc fop docbook-style-xsl.noarch R-devel
@@ -54,7 +54,7 @@ if [ "x${OS_VERSION}" == "x7" ] ; then
   yum -y install git2u-all
   yum -y install lscsoft-all
 
-  rpm --nodeps -e lal-python-6.18.0-1.el7.x86_64 lalstochastic-octave-1.1.20-1.el7.x86_64 laldetchar-octave-0.3.5-1.el7.x86_64 lalinference-devel-1.9.0-1.el7.x86_64 lalxml-devel-1.2.4-1.el7.x86_64 gstlal-calibration-1.1.4-2.el7.x86_64 lal-octave-6.18.0-1.el7.x86_64 lalburst-octave-1.4.4-1.el7.x86_64 lalinspiral-octave-1.7.7-1.el7.x86_64 lalstochastic-1.1.20-1.el7.x86_64 gstlal-ugly-1.2.0-2.el7.x86_64 lalsimulation-1.7.0-1.el7.x86_64 lalsimulation-devel-1.7.0-1.el7.x86_64 lalpulsar-octave-1.16.0-1.el7.x86_64 laldetchar-devel-0.3.5-1.el7.x86_64 lalinspiral-debuginfo-1.7.7-1.el7.x86_64 lalmetaio-devel-1.3.1-1.el7.x86_64 lalinference-python-1.9.0-1.el7.x86_64 lalapps-6.21.0-1.el7.x86_64 lalmetaio-debuginfo-1.3.1-1.el7.x86_64 lalframe-devel-1.4.3-1.el7.x86_64 python-pylal-0.13.1-1.el7.x86_64 gstlal-ugly-devel-1.2.0-2.el7.x86_64 lalapps-debuginfo-6.21.0-1.el7.x86_64 lalburst-1.4.4-1.el7.x86_64 lalxml-python-1.2.4-1.el7.x86_64 lalinference-octave-1.9.0-1.el7.x86_64 lalframe-debuginfo-1.4.3-1.el7.x86_64 lalinspiral-1.7.7-1.el7.x86_64 lalburst-devel-1.4.4-1.el7.x86_64 lalsimulation-python-1.7.0-1.el7.x86_64 gstlal-inspiral-1.3.0-2.el7.x86_64 lscsoft-pylal-1.1-1.el7.noarch lscsoft-lalsuite-2.9-1.el7.noarch lalinference-1.9.0-1.el7.x86_64 lalstochastic-devel-1.1.20-1.el7.x86_64 lal-debuginfo-6.18.0-1.el7.x86_64 lal-6.18.0-1.el7.x86_64 lalstochastic-python-1.1.20-1.el7.x86_64 laldetchar-python-0.3.5-1.el7.x86_64 lalxml-debuginfo-1.2.4-1.el7.x86_64 lalxml-1.2.4-1.el7.x86_64 lalinspiral-devel-1.7.7-1.el7.x86_64 lalpulsar-python-1.16.0-1.el7.x86_64 laldetchar-debuginfo-0.3.5-1.el7.x86_64 lscsoft-gstlal-1.9-1.el7.noarch lalpulsar-1.16.0-1.el7.x86_64 lalxml-octave-1.2.4-1.el7.x86_64 lalframe-python-1.4.3-1.el7.x86_64 gstlal-inspiral-devel-1.3.0-2.el7.x86_64 lalsimulation-debuginfo-1.7.0-1.el7.x86_64 lalmetaio-1.3.1-1.el7.x86_64 laldetchar-0.3.5-1.el7.x86_64 lalburst-python-1.4.4-1.el7.x86_64 lalframe-octave-1.4.3-1.el7.x86_64 lscsoft-gstlal-dev-1.0-1.el7.noarch lalframe-1.4.3-1.el7.x86_64 lalsimulation-octave-1.7.0-1.el7.x86_64 lalmetaio-octave-1.3.1-1.el7.x86_64 lalpulsar-devel-1.16.0-1.el7.x86_64 lalburst-debuginfo-1.4.4-1.el7.x86_64 gstlal-devel-1.1.0-2.el7.x86_64 lalinference-debuginfo-1.9.0-1.el7.x86_64 lal-devel-6.18.0-1.el7.x86_64 lalsuite-extra-1.1.0-1.el7.noarch lalinspiral-python-1.7.7-1.el7.x86_64 lalpulsar-debuginfo-1.16.0-1.el7.x86_64 gstlal-1.1.0-2.el7.x86_64 lalstochastic-debuginfo-1.1.20-1.el7.x86_64 lalmetaio-python-1.3.1-1.el7.x86_64 lscsoft-lalsuite-dev-1.7-1.el7.noarch skyarea-0.2.1-1.el7.noarch
+  rpm --nodeps -e `rpm -qa | grep lal | tr \\n " "`
 
   TRAVIS_TAG="vX.Y.Z"
   CVMFS_PATH=/cvmfs/oasis.opensciencegrid.org/ligo/sw/pycbc/x86_64_rhel_7/virtualenv
