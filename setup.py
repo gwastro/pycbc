308792bcd298eda688a99e4600050658fb68c7e3
Duncan Brown <dabrown@syr.edu>
2017-04-08 22:48:01 -0400

    Set to 1.6.10 to test release deployment (#1577)

--- setup.py
+++ setup.py
@@ -262,7 +262,7 @@ def get_version_info():
 
     # If this is a release or another kind of source distribution of PyCBC
     except:
-        version = '1.6.9'
+        version = '1.6.10'
         release = 'True'
         date = hash = branch = tag = author = committer = status = builder = build_date = ''
 
