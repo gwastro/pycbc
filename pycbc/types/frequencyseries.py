fb5007b34c31568010b654f40da7295dcd93d1f3
Alexander Harvey Nitz <alex.nitz@ligo.org>
2017-04-11 16:29:43 +0200

    also for frequencyseries

--- pycbc/types/frequencyseries.py
+++ pycbc/types/frequencyseries.py
@@ -115,6 +115,17 @@ class FrequencySeries(Array):
     sample_frequencies = property(get_sample_frequencies,
                                   doc="Array of the sample frequencies.")
 
+    def __getitem__(self, index):
+        if index.step is not None:
+            new_delta_f = self._delta_f * index.step
+        else:
+            new_delta_f = self._delta_f
+        
+        return FrequencySeries(Array.__getitem__(self, index),
+                               delta_f=new_delta_f,
+                               epoch=self._epoch,
+                               copy=False)
+
     def __eq__(self,other):
         """
         This is the Python special method invoked whenever the '=='
