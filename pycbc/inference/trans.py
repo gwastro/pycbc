400d185abbc363de3e2c7ef97571e4a30f6f0675
Christopher M. Biwer <christopher.biwer@ligo.org>
2017-04-13 10:26:28 -0400

    Rename var.

--- pycbc/inference/trans.py
+++ pycbc/inference/trans.py
@@ -39,20 +39,20 @@ class BaseConversion(object):
         raise NotImplementedError("Not added.")
 
     @staticmethod
-    def format_output(inputs, maps):
+    def format_output(old_maps, new_maps):
         """ Convert inverse.
         """
 
         # if input is WaveformArray then return WaveformArray
-        if isinstance(inputs, record.WaveformArray):
+        if isinstance(old_maps, record.WaveformArray):
             keys = maps.keys()
-            values = [maps[key] for key in maps]
-            inputs = inputs.add_fields(values, keys)
-            return inputs
+            values = [new_maps[key] for key in keys]
+            old_maps = old_maps.add_fields(values, keys)
+            return old_maps
 
         # if input is dict then return dict
-        elif isinstance(inputs, dict):
-            return inputs.update(maps)
+        elif isinstance(old_maps, dict):
+            return old_maps.update(new_maps)
 
         # else error
         else:
@@ -72,24 +72,27 @@ class MchirpQToMass1Mass2(BaseConversion):
 
     @classmethod
     def convert(cls, maps):
-        mass1 = conversions.mass1_from_mchirp_q(maps[parameters.mchirp],
+        out = {}
+        out[parameters.mass1] = conversions.mass1_from_mchirp_q(
+                                                maps[parameters.mchirp],
                                                 maps[parameters.q])
-        mass2 = conversions.mass2_from_mchirp_q(maps[parameters.mchirp],
+        out[parameters.mass2] = conversions.mass2_from_mchirp_q(
+                                                maps[parameters.mchirp],
                                                 maps[parameters.q])
-        return cls.format_output(
-                    maps, {parameters.mass1 : mass1, parameters.mass2 : mass2})
+        return cls.format_output(maps, out)
 
     @classmethod
     def convert_inverse(cls, maps):
-        mchirp = conversions.mchirp_from_mchirp_q(maps[parameters.mchirp],
+        out = {}
+        out[parameters.mchirp] = \
+                 conversions.mchirp_from_mchirp_q(maps[parameters.mchirp],
                                                   maps[parameters.q])
         m_p = conversions.primary_mass(maps[parameters.mass1],
                                        maps[parameters.mass2])
         m_s = conversions.secondary_mass(maps[parameters.mass1],
                                          maps[parameters.mass2])
-        q = m_p / m_s
-        return cls.format_output(
-                    maps, {parameters.mchirp : mchirp, parameters.q : q})
+        out[parmeters.q] = m_p / m_s
+        return cls.format_output(maps, out)
 
 class SphericalSpin1ToCartesianSpin1(BaseConversion):
     """ Converts spin1x, spin1y, and spin1z to spin1_a, spin1_azimuthal,
@@ -130,25 +133,27 @@ class MassSpinToCartesianSpin(BaseConversion):
 
     @classmethod
     def convert(cls, maps):
-        spin1x = conversions.spin1x_from_xi1_phi_a_phi_s(
+        out = {}
+        out[parameters.spin1x] = conversions.spin1x_from_xi1_phi_a_phi_s(
                                maps["xi1"], maps["phi_a"], maps["phi_s"])
-        spin1y = conversions.spin1y_from_xi1_phi_a_phi_s(
+        out[parameters.spin1y] = conversions.spin1y_from_xi1_phi_a_phi_s(
                                maps["xi1"], maps["phi_a"], maps["phi_s"])
-        spin1z = conversions.spin1z_from_mass1_mass2_chi_eff_chi_a(
+        out[parameters.spin1z] = \
+                         conversions.spin1z_from_mass1_mass2_chi_eff_chi_a(
                                maps[parameters.mass1], maps[parameters.mass2],
                                maps["chi_eff"], maps["chi_a"])
-        spin2x = conversions.spin2x_from_mass1_mass2_xi2_phi_a_phi_s(
+        out[parameters.spin2x] = \
+                         conversions.spin2x_from_mass1_mass2_xi2_phi_a_phi_s(
                                maps[parameters.mass1], maps[parameters.mass2],
                                maps["xi2"], maps["phi_a"], maps["phi_s"])
-        spin2y = conversions.spin2y_from_mass1_mass2_xi2_phi_a_phi_s(
+        out[parameters.spin2y] = \
+                         conversions.spin2y_from_mass1_mass2_xi2_phi_a_phi_s(
                                maps[parameters.mass1], maps[parameters.mass2],
                                maps["xi2"], maps["phi_a"], maps["phi_s"])
-        spin2z = conversions.spin2z_from_mass1_mass2_chi_eff_chi_a(
+        out[parameters.spin2z] = \
+                         conversions.spin2z_from_mass1_mass2_chi_eff_chi_a(
                                maps[parameters.mass1], maps[parameters.mass2],
                                maps["chi_eff"], maps["chi_a"])
-        out = {parameters.spin1x : spin1x, parameters.spin1y : spin1y,
-               parameters.spin1z : spin1z, parameters.spin2x : spin2x,
-               parameters.spin2y : spin2y, parameters.spin2z : spin2z}
         return cls.format_output(maps, out)
 
 class DistanceToRedshift(BaseConversion):
