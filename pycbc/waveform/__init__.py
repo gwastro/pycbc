05ad6a9bbb81c55fab5c89e24e77fe71e1ca6864
Collin Capano <collin.capano@ligo.org>
2017-04-15 16:51:14 +0200

    remove taper from waveform __init__

--- pycbc/waveform/__init__.py
+++ pycbc/waveform/__init__.py
@@ -1,6 +1,5 @@
 from pycbc.waveform.waveform import *
 from pycbc.waveform.utils import *
-from pycbc.waveform.taper import *
 from pycbc.waveform.bank import *
 from pycbc.waveform.generator import *
 from pycbc.waveform.ringdown import *
