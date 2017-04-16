d716a0796e8df36d07534666676bc9f4da2fce77
Christopher M. Biwer <christopher.biwer@ligo.org>
2017-04-16 11:39:32 -0400

    Add kwargs.

--- pycbc/distributions/bounded.py
+++ pycbc/distributions/bounded.py
@@ -114,7 +114,7 @@ def get_param_bounds_from_config(cp, section, tag, param):
 
 
 def bounded_from_config(cls, cp, section, variable_args,
-                        bounds_required=False):
+                        bounds_required=False, , **kwargs):
     """Returns a bounded distribution based on a configuration file. The
     parameters for the distribution are retrieved from the section titled
     "[`section`-`variable_args`]" in the config file.
