gwastro/pycbc

commit 8b047601a534062b676bac3c0a0d339f93d515aa

pycbc.libutils: use subprocess instead of command
command is deprecated

pycbc/libutils.py
@@ -21,7 +21,7 @@ according to the paths that pkg-config specifies.
 """
 
 from __future__ import print_function
-import os, fnmatch, ctypes, commands, sys, subprocess
+import os, fnmatch, ctypes, subprocess, sys, subprocess
 from ctypes.util import find_library
 from collections import deque
 
@@ -51,7 +51,7 @@ def pkg_config(pkg_libraries):
     if len(pkg_libraries)>0 :
         # PKG_CONFIG_ALLOW_SYSTEM_CFLAGS explicitly lists system paths.
         # On system-wide LAL installs, this is needed for swig to find lalswig.i
-        for token in commands.getoutput("PKG_CONFIG_ALLOW_SYSTEM_CFLAGS=1 pkg-config --libs --cflags %s" % ' '.join(pkg_libraries)).split():
+        for token in subprocess.getoutput("PKG_CONFIG_ALLOW_SYSTEM_CFLAGS=1 pkg-config --libs --cflags %s" % ' '.join(pkg_libraries)).split():
             if token.startswith("-l"):
                 libraries.append(token[2:])
             elif token.startswith("-L"):
@@ -104,7 +104,7 @@ def pkg_config_libdirs(packages):
             raise ValueError("Package {0} cannot be found on the pkg-config search path".format(pkg))
 
     libdirs = []
-    for token in commands.getoutput("PKG_CONFIG_ALLOW_SYSTEM_LIBS=1 pkg-config --libs-only-L {0}".format(' '.join(packages))).split():
+    for token in subprocess.getoutput("PKG_CONFIG_ALLOW_SYSTEM_LIBS=1 pkg-config --libs-only-L {0}".format(' '.join(packages))).split():
         if token.startswith("-L"):
             libdirs.append(token[2:])
     return libdirs

commit bd6b0362e87a9e9eec02cb78f0a32d022b394a54

Fixed a number of python3-compatibility issues (#1643)
* updated print statements to print function

required for python 3
this only updates the build script and package modules, no executable scripts

* updated exception syntax to be python3-compatible

* pycbc._version_helper: don't import exceptions

module doesn't exist for python3 and LookupError is available without it anyway

* pycbc.libutils: use subprocess instead of command

command is deprecated

* use relative imports

python3 defaults to absolute imports, so these wouldn't work

* pycbc.waveform.parameters: updated import

UserList module was removed for python3

* pycbc.types.array: catch long NameError [py3]

* libutils: fixed python2 bug introduced by 8b04760

turns out subprocess doesn't include getoutput for py2

pycbc/__init__.py
@@ -24,7 +24,7 @@
 #
 """PyCBC contains a toolkit for CBC gravitational wave analysis
 """
-from __future__ import absolute_import
+from __future__ import (absolute_import, print_function)
 import subprocess, os, sys, tempfile
 import logging
 import signal
@@ -130,12 +130,14 @@ _cache_dir_path = os.path.join(_cache_dir_path, pycbc_version)
 _cache_dir_path = os.path.join(_cache_dir_path, git_hash)
 if os.environ.get("NO_TMPDIR", None):
     if os.environ.get("INITIAL_LOG_LEVEL", 0) >= 10:
-        print >>sys.stderr, "__init__: Skipped creating %s as NO_TEMPDIR is set" % _cache_dir_path
+        print("__init__: Skipped creating %s as NO_TEMPDIR is set"
+              % _cache_dir_path, file=sys.stderr)
 else:
     try: os.makedirs(_cache_dir_path)
     except OSError: pass
     if os.environ.get("INITIAL_LOG_LEVEL", 0) >= 10:
-        print >>sys.stderr, "__init__: Setting weave cache to %s" % _cache_dir_path
+        print("__init__: Setting weave cache to %s" % _cache_dir_path,
+              file=sys.stderr)
 os.environ['PYTHONCOMPILED'] = _cache_dir_path
 
 # Check for MKL capability
@@ -143,7 +145,7 @@ try:
     import pycbc.fft.mkl
     HAVE_MKL=True
 except ImportError as e:
-    print e
+    print(e)
     HAVE_MKL=False
     
 

pycbc/_version.py
@@ -78,5 +78,5 @@ class Version(argparse.Action):
         except ImportError:
             version_str += "\nLALSimulation not installed in environment\n"
 
-        print version_str
+        print(version_str)
         sys.exit(0)

pycbc/_version_helper.py
@@ -20,7 +20,6 @@
 
 __author__ = 'Adam Mercer <adam.mercer@ligo.org>'
 
-import exceptions
 import os
 import time
 import subprocess
@@ -41,7 +40,7 @@ class GitInfo(object):
         build_date = None
 
 
-class GitInvocationError(exceptions.LookupError):
+class GitInvocationError(LookupError):
     pass
 
 

pycbc/events/__init__.py
@@ -2,7 +2,7 @@
 This packages contains modules for clustering events
 """
 
-from events import *
-from veto import *
-from coinc import *
+from .events import *
+from .veto import *
+from .coinc import *
 

pycbc/fft/fft_callback.py
@@ -185,12 +185,12 @@ def compile(source, name):
            "--compiler-options", "'-fPIC'", 
            "-o", obj_file, 
            "-c", src_file] 
-    print " ".join(cmd)
+    print(" ".join(cmd))
     subprocess.check_call(cmd)
     
     cmd = ["nvcc", "-shared", "-ccbin", "g++", "-m64", 
        "-o", lib_file, obj_file, "-lcufft_static", "-lculibos"]  
-    print " ".join(cmd)
+    print(" ".join(cmd))
     
     subprocess.check_call(cmd)
     

pycbc/filter/__init__.py
@@ -1,2 +1,2 @@
-from matchedfilter import *
-from resample import *
+from .matchedfilter import *
+from .resample import *

pycbc/libutils.py
@@ -19,9 +19,15 @@ This module provides a simple interface for loading a shared library via ctypes,
 allowing it to be specified in an OS-independent way and searched for preferentially
 according to the paths that pkg-config specifies.
 """
-import os, fnmatch, ctypes, commands, sys, subprocess
+
+from __future__ import print_function
+import os, fnmatch, ctypes, sys, subprocess
 from ctypes.util import find_library
 from collections import deque
+try:
+    from subprocess import getoutput
+except ImportError:
+    from commands import getoutput
 
 def pkg_config(pkg_libraries):
     """Use pkg-config to query for the location of libraries, library directories,
@@ -42,14 +48,14 @@ def pkg_config(pkg_libraries):
         if os.system('pkg-config --exists %s 2>/dev/null' % pkg) == 0:
             pass
         else:
-            print "Could not find library {0}".format(pkg)
+            print("Could not find library {0}".format(pkg))
             sys.exit(1)
 
     # Get the pck-config flags
     if len(pkg_libraries)>0 :
         # PKG_CONFIG_ALLOW_SYSTEM_CFLAGS explicitly lists system paths.
         # On system-wide LAL installs, this is needed for swig to find lalswig.i
-        for token in commands.getoutput("PKG_CONFIG_ALLOW_SYSTEM_CFLAGS=1 pkg-config --libs --cflags %s" % ' '.join(pkg_libraries)).split():
+        for token in getoutput("PKG_CONFIG_ALLOW_SYSTEM_CFLAGS=1 pkg-config --libs --cflags %s" % ' '.join(pkg_libraries)).split():
             if token.startswith("-l"):
                 libraries.append(token[2:])
             elif token.startswith("-L"):
@@ -91,7 +97,8 @@ def pkg_config_libdirs(packages):
         FNULL = open(os.devnull, 'w')
         subprocess.check_call(["pkg-config", "--version"], stdout=FNULL, close_fds=True)
     except:
-        print >>sys.stderr, "PyCBC.libutils: pkg-config call failed, setting NO_PKGCONFIG=1"
+        print("PyCBC.libutils: pkg-config call failed, setting NO_PKGCONFIG=1",
+              file=sys.stderr)
         os.environ['NO_PKGCONFIG'] = "1"
         return []
 
@@ -101,7 +108,7 @@ def pkg_config_libdirs(packages):
             raise ValueError("Package {0} cannot be found on the pkg-config search path".format(pkg))
 
     libdirs = []
-    for token in commands.getoutput("PKG_CONFIG_ALLOW_SYSTEM_LIBS=1 pkg-config --libs-only-L {0}".format(' '.join(packages))).split():
+    for token in getoutput("PKG_CONFIG_ALLOW_SYSTEM_LIBS=1 pkg-config --libs-only-L {0}".format(' '.join(packages))).split():
         if token.startswith("-L"):
             libdirs.append(token[2:])
     return libdirs

pycbc/results/followup.py
@@ -108,7 +108,7 @@ def trigger_timeseries_plot(file_list, ifos, start, end):
         trigs = columns_from_file_list(file_list,
                                        ['snr', 'end_time'],
                                        ifo, start, end)
-        print trigs
+        print(trigs)
         pylab.scatter(trigs['end_time'], trigs['snr'], label=ifo,     
                       color=ifo_color[ifo])
                             

pycbc/results/metadata.py
@@ -60,7 +60,7 @@ def load_html_metadata(filename):
     data = open(filename, 'r').read()
 
     if 'pycbc-meta' in data:
-        print "LOADING HTML FILE %s" % filename
+        print("LOADING HTML FILE %s" % filename)
     parser.feed(data)
     cp = ConfigParser.ConfigParser(parser.metadata)
     cp.add_section(os.path.basename(filename))

pycbc/results/render.py
@@ -133,7 +133,7 @@ def render_default(path, cp):
                 #        for now I just coalesce.
                 seg_dict = wf_file.return_union_seglist()
             except Exception as e:
-                print 'No segment table found in', path, ':', e
+                print('No segment table found in %s : %s' % (path, e))
 
     # render template
     template_dir = pycbc.results.__path__[0] + '/templates/files'

pycbc/tmpltbank/em_progenitors.py
@@ -306,9 +306,9 @@ def load_ns_sequence(eos_name):
         #ns_sequence_path = os.path.join(NS_SEQUENCE_FILE_DIRECTORY, 'equil_2H.dat')
         ns_sequence = np.loadtxt(ns_sequence_path)
     else:
-        print 'Only the 2H EOS is currently supported!'
-        print 'If you plan to use a different NS EOS, be sure not to filter'
-        print 'too many templates!\n'
+        print('Only the 2H EOS is currently supported!')
+        print('If you plan to use a different NS EOS, be sure not to filter')
+        print('too many templates!\n')
         raise Exception('Unsupported EOS!')
 
     max_ns_g_mass = max(ns_sequence[:,0])
@@ -442,8 +442,8 @@ def remnant_mass(eta, ns_g_mass, ns_sequence, chi, incl, shift):
     """
     # Sanity checks
     if not (eta>0. and eta<=0.25 and abs(chi)<=1):
-        print 'The BH spin magnitude must be <=1 and eta must be between 0 and 0.25'
-        print 'This script was launched with ns_mass={0}, eta={1}, chi={2}, inclination={3}\n'.format(ns_b_mass, eta, chi, incl)
+        print('The BH spin magnitude must be <=1 and eta must be between 0 and 0.25')
+        print('This script was launched with ns_mass={0}, eta={1}, chi={2}, inclination={3}\n'.format(ns_b_mass, eta, chi, incl))
         raise Exception('Unphysical parameters!')
 
     # Binary mass ratio define to be > 1
@@ -455,10 +455,10 @@ def remnant_mass(eta, ns_g_mass, ns_sequence, chi, incl, shift):
 
     # Sanity checks
     if not (ns_compactness>0 and q>=1):
-        print 'A positive NS compactness and a mass ratio that is >1 must be obtained.'
-        print 'This script was launched with ns_mass={0}, eta={1}, chi={2}, inclination={3}'.format(ns_b_mass, eta, chi, incl)
-        print 'and obtained ns_compactness={0} and q={1}.'.format(ns_compactness, q)
-        print 'SOMETHING WENT WRONG!!\n'
+        print('A positive NS compactness and a mass ratio that is >1 must be obtained.')
+        print('This script was launched with ns_mass={0}, eta={1}, chi={2}, inclination={3}'.format(ns_b_mass, eta, chi, incl))
+        print('and obtained ns_compactness={0} and q={1}.'.format(ns_compactness, q))
+        print('SOMETHING WENT WRONG!!\n')
         raise Exception('Unphysical parameters!')
 
     # Calculate the dimensionless parameter kappa
@@ -469,10 +469,10 @@ def remnant_mass(eta, ns_g_mass, ns_sequence, chi, incl, shift):
 
     #Sanity checks
     if not abs(chi_eff)<=1:
-        print 'The effective BH spin magnitude must be <=1'
-        print 'This script was launched with ns_mass={0}, eta={1}, chi={2}, inclination={3}'.format(ns_b_mass, eta, chi, incl)
-        print 'and obtained chi_eff={0}.'.format(chi_eff)
-        print 'SOMETHING WENT WRONG!!\n'
+        print('The effective BH spin magnitude must be <=1')
+        print('This script was launched with ns_mass={0}, eta={1}, chi={2}, inclination={3}'.format(ns_b_mass, eta, chi, incl))
+        print('and obtained chi_eff={0}.'.format(chi_eff))
+        print('SOMETHING WENT WRONG!!\n')
         raise Exception('Unphysical parameters!')
 
     # Taking the 1st element with full_output=1 avoids some annoying messages on stdout

pycbc/tmpltbank/option_utils.py
@@ -117,7 +117,7 @@ def insert_base_bank_options(parser):
         err_msg = "must be a number between 0 and 1 excluded, not %r" % s
         try:
             value = float(s)
-        except ValueError, e:
+        except ValueError:
             raise argparse.ArgumentTypeError(err_msg)
         if value <= 0 or value >= 1:
             raise argparse.ArgumentTypeError(err_msg)
@@ -757,8 +757,8 @@ def verify_mass_range_options(opts, parser, nonSpin=False):
             # And this is the bad case
             err_msg = "The maximum eta provided is not possible given "
             err_msg += "restrictions on component masses."
-            print m1_at_min_m2, m2_at_min_m1, m1_at_max_m2, m2_at_max_m1
-            print opts.min_mass1, opts.max_mass1, opts.min_mass2, opts.max_mass2
+            print(m1_at_min_m2, m2_at_min_m1, m1_at_max_m2, m2_at_max_m1)
+            print(opts.min_mass1, opts.max_mass1, opts.min_mass2, opts.max_mass2)
             raise ValueError(err_msg)
         # Update min_tot_mass if needed
         if min_tot_mass > opts.min_total_mass:

pycbc/types/__init__.py
@@ -1,4 +1,4 @@
-from array import *
-from timeseries import *
-from frequencyseries import *
-from optparse import *
+from .array import *
+from .timeseries import *
+from .frequencyseries import *
+from .optparse import *

pycbc/types/array.py
@@ -47,7 +47,10 @@ from pycbc.types.aligned import ArrayWithAligned
 # array of uint32 integers
 _ALLOWED_DTYPES = [_numpy.float32, _numpy.float64, _numpy.complex64,
                    _numpy.complex128, _numpy.uint32, _numpy.int32, _numpy.int]
-_ALLOWED_SCALARS = [int, long, float, complex] + _ALLOWED_DTYPES
+try:
+    _ALLOWED_SCALARS = [int, long, float, complex] + _ALLOWED_DTYPES
+except NameError:
+    _ALLOWED_SCALARS = [int, float, complex] + _ALLOWED_DTYPES
 
 def _convert_to_scheme(ary):
     if not isinstance(ary._scheme, _scheme.mgr.state.__class__):

pycbc/types/optparse.py
@@ -331,7 +331,7 @@ def positive_float(s):
     err_msg = "must be a positive number, not %r" % s
     try:
         value = float(s)
-    except ValueError, e:
+    except ValueError:
         raise argparse.ArgumentTypeError(err_msg)
     if value <= 0:
         raise argparse.ArgumentTypeError(err_msg)
@@ -346,7 +346,7 @@ def nonnegative_float(s):
     err_msg = "must be either positive or zero, not %r" % s
     try:
         value = float(s)
-    except ValueError, e:
+    except ValueError:
         raise argparse.ArgumentTypeError(err_msg)
     if value < 0:
         raise argparse.ArgumentTypeError(err_msg)

pycbc/waveform/parameters.py
@@ -25,7 +25,10 @@
 """Classes to define common parameters used for waveform generation.
 """
 
-from UserList import UserList
+try:
+    from collections import UserList
+except ImportError:
+    from UserList import UserList
 
 #
 # =============================================================================

pycbc/waveform/waveform.py
@@ -254,23 +254,23 @@ cuda_fd = dict(_lalsim_fd_approximants.items() + _cuda_fd_approximants.items())
 def print_td_approximants():
     print("LalSimulation Approximants")
     for approx in _lalsim_td_approximants.keys():
-        print "  " + approx
+        print("  " + approx)
     print("CUDA Approximants")
     for approx in _cuda_td_approximants.keys():
-        print "  " + approx
+        print("  " + approx)
 
 def print_fd_approximants():
     print("LalSimulation Approximants")
     for approx in _lalsim_fd_approximants.keys():
-        print "  " + approx
+        print("  " + approx)
     print("CUDA Approximants")
     for approx in _cuda_fd_approximants.keys():
-        print "  " + approx
+        print("  " + approx)
 
 def print_sgburst_approximants():
     print("LalSimulation Approximants")
     for approx in _lalsim_sgburst_approximants.keys():
-        print "  " + approx
+        print("  " + approx)
 
 def td_approximants(scheme=_scheme.mgr.state):
     """Return a list containing the available time domain approximants for

pycbc/workflow/configuration.py
@@ -522,14 +522,14 @@ class WorkflowConfigParser(pycbc_glue.pipeline.DeepCopyableConfigParser):
                     testString = testString.replace('${'+repString+'}',\
                                             self.get(section,splitString[0]))
                 except ConfigParser.NoOptionError:
-                    print "Substitution failed"
+                    print("Substitution failed")
                     raise
             if len(splitString) == 2:
                 try:
                     testString = testString.replace('${'+repString+'}',\
                                        self.get(splitString[0],splitString[1]))
                 except ConfigParser.NoOptionError:
-                    print "Substitution failed"
+                    print("Substitution failed")
                     raise
             reObj = re.search(r"\$\{.*?\}", testString)
 

pycbc/workflow/datafind.py
@@ -29,6 +29,7 @@ documentation for this function can be found here:
 https://ldas-jobs.ligo.caltech.edu/~cbc/docs/pycbc/ahope/datafind.html
 """
 
+from __future__ import print_function
 import os, copy
 import urlparse
 import logging
@@ -950,8 +951,8 @@ def run_datafind_instance(cp, outputDir, connection, observatory, frameType,
     for entry in dfCache:
         start = str(int(entry.segment[0]))
         duration = str(int(abs(entry.segment)))
-        print >> fP, "%s %s %s %s %s" \
-            %(entry.observatory, entry.description, start, duration, entry.url)
+        print("%s %s %s %s %s" \
+              % (entry.observatory, entry.description, start, duration, entry.url), file=fP)
         entry.segment = segments.segment(int(entry.segment[0]), int(entry.segment[1]))
 
     fP.close()

pycbc/workflow/grb_utils.py
@@ -27,6 +27,8 @@ This library code contains functions and classes that are used in the
 generation of pygrb workflows. For details about pycbc.workflow see here:
 http://ligo-cbc.github.io/pycbc/latest/html/workflow.html
 """
+
+from __future__ import print_function
 import sys
 import os
 import shutil
@@ -185,7 +187,7 @@ def make_exttrig_file(cp, ifos, sci_seg, out_dir):
             elif entry == 'event_id':
                 row.event_id = ilwd.ilwdchar("external_trigger:event_id:0")
             else:
-                print >> sys.stderr, "Column %s not recognized" %(entry)
+                print("Column %s not recognized" %(entry), file=sys.stderr)
                 raise ValueError
 
     # Save file

pycbc/workflow/jobsetup.py
@@ -569,7 +569,7 @@ class JobSegmenter(object):
         if compatibility_mode and (self.valid_length != abs(self.valid_chunk)):
             errMsg = "In compatibility mode the template bank and matched-"
             errMsg += "filter jobs must read in the same amount of data."
-            print self.valid_length, self.valid_chunk
+            print(self.valid_length, self.valid_chunk)
             raise ValueError(errMsg)
         elif compatibility_mode and len(data_lengths) > 1:
             raise ValueError("Cannot enable compatibility mode tiling with "

setup.py
@@ -18,6 +18,9 @@
 """
 setup.py file for PyCBC package
 """
+
+from __future__ import print_function
+
 import os, fnmatch, sys, subprocess, shutil
 
 from trace import fullmodname
@@ -39,7 +42,7 @@ from distutils.version import LooseVersion
 try:
     import numpy.version
     if LooseVersion(numpy.version.version) < LooseVersion("1.6.4"):
-        print (" Numpy >= 1.6.4 is required for pycbc dependencies. \n"
+        print(" Numpy >= 1.6.4 is required for pycbc dependencies. \n"
               " We found version %s already installed. Please update \n"
               " to a more recent version and then retry PyCBC  \n"
               " installation. \n"
@@ -107,13 +110,13 @@ class clean(_clean):
         for f in self.clean_files:
             try:
                 os.unlink(f)
-                print 'removed {0}'.format(f)
+                print('removed {0}'.format(f))
             except:
                 pass
 
         for fol in self.clean_folders:
             shutil.rmtree(fol, ignore_errors=True)
-            print 'removed {0}'.format(fol)
+            print('removed {0}'.format(fol))
 
 class install(_install):
     def run(self):
@@ -127,11 +130,12 @@ class install(_install):
                      "creating %s" % filename)
 
         env_file = open(filename, 'w')
-        print >> env_file, "# Source this file to access PyCBC"
-        print >> env_file, "PATH=" + self.install_scripts + ":$PATH"
-        print >> env_file, "PYTHONPATH=" + self.install_libbase + ":$PYTHONPATH"
-        print >> env_file, "export PYTHONPATH"
-        print >> env_file, "export PATH"
+        print("# Source this file to access PyCBC", file=env_file)
+        print("PATH=" + self.install_scripts + ":$PATH", file=env_file)
+        print("PYTHONPATH=" + self.install_libbase + ":$PYTHONPATH",
+              file=env_file)
+        print("export PYTHONPATH", file=env_file)
+        print("export PATH", file=env_file)
         env_file.close()
 
         _install.run(self)
@@ -186,7 +190,7 @@ class TestBase(Command):
             test_results.append(result_str)
 
         for test in test_results:
-            print test
+            print(test)
 
 class test(Command):
     def has_cuda(self):

pycbc/workflow/configparser_test.py
@@ -22,7 +22,7 @@ def parse_workflow_ini_file(cpFile,parsed_filepath=None):
     """    
     # First read the .ini file
     cp = read_ini_file(cpFile)
-    print cp.sections()
+    print(cp.sections())
 
     # Check for any substitutions that can be made
     # FIXME: The python 3 version of ConfigParser can do this automatically
@@ -34,11 +34,11 @@ def parse_workflow_ini_file(cpFile,parsed_filepath=None):
 
     # Split sections like [inspiral&tmplt] into [inspiral] and [tmplt]
     cp = split_multi_sections(cp)
-    print cp.sections()  
+    print(cp.sections())
 
     # Check for duplicate options in sub-sections
     sanity_check_subsections(cp)
-    print cp.sections()
+    print(cp.sections())
 
     # Dump parsed .ini file if needed
     if parsed_filepath: