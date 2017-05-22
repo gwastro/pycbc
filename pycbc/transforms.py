d93c5fe9900ab69b49ae4800f28940e027ae1f79
Collin Capano <collin.capano@ligo.org>
2017-05-22 23:01:04 +0200

    Read transforms from config file (#1689)
    * add from_config method to transforms
    
    * read multiple transforms from config file and order
    
    * read sampling parameters from config file
    
    * allow for inverse transforms
    
    * fix codacy warnings
    
    * QuantifiedCode fixes

--- pycbc/transforms.py
+++ pycbc/transforms.py
@@ -25,6 +25,7 @@ from pycbc import cosmology
 from pycbc.io import record
 from pycbc.waveform import parameters
 from pycbc.distributions.boundaries import Bounds
+from pycbc.distributions.bounded import VARARGS_DELIM
 
 class BaseTransform(object):
     """A base class for transforming between two sets of parameters.
@@ -97,6 +98,63 @@ class BaseTransform(object):
         # else error
         else:
             raise TypeError("Input type must be FieldArray or dict.")
+
+    @classmethod
+    def from_config(cls, cp, section, outputs, skip_opts=None,
+                    additional_opts=None):
+        """Initializes a transform from the given section.
+
+        Parameters
+        ----------
+        cp : pycbc.workflow.WorkflowConfigParser
+            A parsed configuration file that contains the transform options.
+        section : str
+            Name of the section in the configuration file.
+        outputs : str
+            The names of the parameters that are output by this transformation,
+            separated by `VARARGS_DELIM`. These must appear in the "tag" part
+            of the section header.
+        skip_opts : list, optional
+            Do not read options in the given list.
+        additional_opts : dict, optional
+            Any additional arguments to pass to the class. If an option is
+            provided that also exists in the config file, the value provided
+            will be used instead of being read from the file.
+
+        Returns
+        -------
+        cls
+            An instance of the class.
+        """
+        tag = outputs
+        if skip_opts is None:
+            skip_opts = []
+        if additional_opts is None:
+            additional_opts = {}
+        else:
+            additional_opts = additional_opts.copy()
+        outputs = set(outputs.split(VARARGS_DELIM))
+        special_args = ['name'] + skip_opts + additional_opts.keys()
+        # get any extra arguments to pass to init
+        extra_args = {}
+        for opt in cp.options("-".join([section, tag])):
+            if opt in special_args:
+                continue
+            # check if option can be cast as a float
+            val = cp.get_opt_tag(section, opt, tag)
+            try:
+                val = float(val)
+            except ValueError:
+                pass
+            # add option
+            extra_args.update({opt:val})
+        extra_args.update(additional_opts)
+        out = cls(**extra_args)
+        # check that the outputs matches
+        if outputs-out.outputs != set() or out.outputs-outputs != set():
+            raise ValueError("outputs of class do not match outputs specified "
+                             "in section")
+        return out
 
 
 #
@@ -470,7 +528,7 @@ class ChiPToCartesianSpin(BaseTransform):
 
 
 class Logit(BaseTransform):
-    """Applies a logit transform from an `input` parameter to an `output`
+    """Applies a logit transform from an `inputvar` parameter to an `outputvar`
     parameter. This is the inverse of the logistic transform.
 
     Typically, the input of the logit function is assumed to have domain
@@ -479,9 +537,9 @@ class Logit(BaseTransform):
 
     Parameters
     ----------
-    input : str
+    inputvar : str
         The name of the parameter to transform.
-    output : str
+    outputvar : str
         The name of the transformed parameter.
     domain : tuple or distributions.bounds.Bounds, optional
         The domain of the input parameter. Can be any finite
@@ -489,11 +547,11 @@ class Logit(BaseTransform):
     """
     name = 'logit'
 
-    def __init__(self, input, output, domain=(0., 1.)):
-        self._input = input
-        self._output = output
-        self._inputs = [input]
-        self._outputs = [output]
+    def __init__(self, inputvar, outputvar, domain=(0., 1.)):
+        self._inputvar = inputvar
+        self._outputvar = outputvar
+        self._inputs = [inputvar]
+        self._outputs = [outputvar]
         self._bounds = Bounds(domain[0], domain[1],
                               btype_min='open', btype_max='open')
         # shortcuts for quick access later
@@ -502,14 +560,14 @@ class Logit(BaseTransform):
         super(Logit, self).__init__()
 
     @property
-    def input(self):
+    def inputvar(self):
         """Returns the input parameter."""
-        return self._input
+        return self._inputvar
 
     @property
-    def output(self):
+    def outputvar(self):
         """Returns the output parameter."""
-        return self._output
+        return self._outputvar
 
     @property
     def bounds(self):
@@ -580,7 +638,7 @@ class Logit(BaseTransform):
 
     def transform(self, maps):
         r"""Computes :math:`\mathrm{logit}(x; a, b)`.
-        
+
         The domain :math:`a, b` of :math:`x` are given by the class's bounds.
 
         Parameters
@@ -595,14 +653,14 @@ class Logit(BaseTransform):
             A map between the transformed variable name and value(s), along
             with the original variable name and value(s).
         """
-        x = maps[self._input]
+        x = maps[self._inputvar]
         # check that x is in bounds
         isin = self._bounds.__contains__(x)
         if isinstance(isin, numpy.ndarray) and not isin.all():
             raise ValueError("one or more values are not in bounds")
         elif not isin:
             raise ValueError("{} is not in bounds".format(x))
-        out = {self._output : self.logit(x, self._a, self._b)}
+        out = {self._outputvar : self.logit(x, self._a, self._b)}
         return self.format_output(maps, out)
 
     def inverse_transform(self, maps):
@@ -622,8 +680,8 @@ class Logit(BaseTransform):
             A map between the transformed variable name and value(s), along
             with the original variable name and value(s).
         """
-        y = maps[self._output]
-        out = {self._input : self.logistic(y, self._a, self._b)}
+        y = maps[self._outputvar]
+        out = {self._inputvar : self.logistic(y, self._a, self._b)}
         return self.format_output(maps, out)
 
     def jacobian(self, maps):
@@ -648,7 +706,7 @@ class Logit(BaseTransform):
         float
             The value of the jacobian at the given point(s).
         """
-        x = maps[self._input]
+        x = maps[self._inputvar]
         # check that x is in bounds
         isin = self._bounds.__contains__(x)
         if isinstance(isin, numpy.ndarray) and not isin.all():
@@ -679,10 +737,78 @@ class Logit(BaseTransform):
         float
             The value of the jacobian at the given point(s).
         """
-        x = maps[self._output]
+        x = maps[self._outputvar]
         expx = numpy.exp(x)
         return expx * (self._b - self._a) / (1. + expx)**2.
 
+    @classmethod
+    def from_config(cls, cp, section, outputs, skip_opts=None,
+                    additional_opts=None):
+        """Initializes a Logit transform from the given section.
+
+        The section must specify an input and output variable name. The domain
+        of the input may be specified using `min-{input}`, `max-{input}`.
+        Example:
+
+        .. code-block:: ini
+
+            [{section}-logitq]
+            name = logit
+            inputvar = q
+            outputvar = logitq
+            min-q = 1
+            max-q = 8
+
+        Parameters
+        ----------
+        cp : pycbc.workflow.WorkflowConfigParser
+            A parsed configuration file that contains the transform options.
+        section : str
+            Name of the section in the configuration file.
+        outputs : str
+            The names of the parameters that are output by this transformation,
+            separated by `VARARGS_DELIM`. These must appear in the "tag" part
+            of the section header.
+        skip_opts : list, optional
+            Do not read options in the given list.
+        additional_opts : dict, optional
+            Any additional arguments to pass to the class. If an option is
+            provided that also exists in the config file, the value provided
+            will be used instead of being read from the file.
+
+        Returns
+        -------
+        cls
+            An instance of the class.
+        """
+        # pull out the minimum, maximum values of the input variable
+        inputvar = cp.get_opt_tag(section, 'inputvar', outputs)
+        s = '-'.join([section, outputs])
+        opt = 'min-{}'.format(inputvar)
+        if skip_opts is None:
+            skip_opts = []
+        if additional_opts is None:
+            additional_opts = {}
+        else:
+            additional_opts = additional_opts.copy()
+        if cp.has_option(s, opt):
+            a = cp.get_opt_tag(section, opt, outputs)
+            skip_opts.append(opt)
+        else:
+            a = None
+        opt = 'max-{}'.format(inputvar)
+        if cp.has_option(s, opt):
+            b = cp.get_opt_tag(section, opt, outputs)
+            skip_opts.append(opt)
+        else:
+            b = None
+        if a is None and b is not None or b is None and a is not None:
+            raise ValueError("if providing a min(max)-{}, must also provide "
+                             "a max(min)-{}".format(inputvar, inputvar))
+        elif a is not None:
+            additional_opts.update({'domain': (float(a), float(b))})
+        return super(Logit, cls).from_config(cp, section, outputs, skip_opts,
+                                             additional_opts)
 
 #
 # =============================================================================
@@ -778,9 +904,9 @@ class Logistic(Logit):
 
     Parameters
     ----------
-    input : str
+    inputvar : str
         The name of the parameter to transform.
-    output : str
+    outputvar : str
         The name of the transformed parameter.
     frange : tuple or distributions.bounds.Bounds, optional
         The range of the output parameter. Can be any finite
@@ -793,14 +919,83 @@ class Logistic(Logit):
     jacobian = inverse.inverse_jacobian
     inverse_jacobian = inverse.jacobian
 
-    def __init__(self, input, output, codomain=(0.,1.)):
-        super(Logistic, self).__init__(output, input, domain=codomain)
+    def __init__(self, inputvar, outputvar, codomain=(0.,1.)):
+        super(Logistic, self).__init__(outputvar, inputvar, domain=codomain)
 
     @property
     def bounds(self):
         """Returns the range of the output parameter.
         """
         return self._bounds
+
+    @classmethod
+    def from_config(cls, cp, section, outputs, skip_opts=None,
+                    additional_opts=None):
+        """Initializes a Logistic transform from the given section.
+
+        The section must specify an input and output variable name. The
+        codomain of the output may be specified using `min-{output}`,
+        `max-{output}`. Example:
+
+        .. code-block:: ini
+
+            [{section}-q]
+            name = logistic
+            inputvar = logitq
+            outputvar = q
+            min-q = 1
+            max-q = 8
+
+        Parameters
+        ----------
+        cp : pycbc.workflow.WorkflowConfigParser
+            A parsed configuration file that contains the transform options.
+        section : str
+            Name of the section in the configuration file.
+        outputs : str
+            The names of the parameters that are output by this transformation,
+            separated by `VARARGS_DELIM`. These must appear in the "tag" part
+            of the section header.
+        skip_opts : list, optional
+            Do not read options in the given list.
+        additional_opts : dict, optional
+            Any additional arguments to pass to the class. If an option is
+            provided that also exists in the config file, the value provided
+            will be used instead of being read from the file.
+
+        Returns
+        -------
+        cls
+            An instance of the class.
+        """
+        # pull out the minimum, maximum values of the output variable
+        outputvar = cp.get_opt_tag(section, 'output', outputs)
+        if skip_opts is None:
+            skip_opts = []
+        if additional_opts is None:
+            additional_opts = {}
+        else:
+            additional_opts = additional_opts.copy()
+        s = '-'.join([section, outputs])
+        opt = 'min-{}'.format(outputvar)
+        if cp.has_option(s, opt):
+            a = cp.get_opt_tag(section, opt, outputs)
+            skip_opts.append(opt)
+        else:
+            a = None
+        opt = 'max-{}'.format(outputvar)
+        if cp.has_option(s, opt):
+            b = cp.get_opt_tag(section, opt, outputs)
+            skip_opts.append(opt)
+        else:
+            b = None
+        if a is None and b is not None or b is None and a is not None:
+            raise ValueError("if providing a min(max)-{}, must also provide "
+                             "a max(min)-{}".format(outputvar, outputvar))
+        elif a is not None:
+            additional_opts.update({'codomain': (float(a), float(b))})
+        return super(Logistic, cls).from_config(cp, section, outputs,
+                                                skip_opts, additional_opts)
 
 
 # set the inverse of the forward transforms to the inverse transforms
@@ -927,7 +1122,7 @@ def get_common_cbc_transforms(requested_params, variable_args,
     return list(requested_params), all_c
 
 
-def apply_transforms(samples, transforms):
+def apply_transforms(samples, transforms, inverse=False):
     """Applies a list of BaseTransform instances on a mapping object.
 
     Parameters
@@ -935,16 +1130,85 @@ def apply_transforms(samples, transforms):
     samples : {FieldArray, dict}
         Mapping object to apply transforms to.
     transforms : list
-        List of BaseTransform instances to apply.
+        List of BaseTransform instances to apply. Nested transforms are assumed
+        to be in order for forward transforms.
+    inverse : bool, optional
+        Apply inverse transforms. In this case transforms will be applied in
+        the opposite order. Default is False.
 
     Returns
     -------
     samples : {FieldArray, dict}
-        Mapping object with conversions applied. Same type as input.
+        Mapping object with transforms applied. Same type as input.
     """
+    if inverse:
+        transforms = transforms[::-1]
     for t in transforms:
         try:
-            samples = t.transform(samples)
+            if inverse:
+                samples = t.inverse_transform(samples)
+            else:
+                samples = t.transform(samples)
         except NotImplementedError:
             continue
     return samples
+
+def order_transforms(transforms):
+    """Orders transforms to ensure proper chaining.
+
+    For example, if `transforms = [B, A, C]`, and `A` produces outputs needed
+    by `B`, the transforms will be re-rorderd to `[A, B, C]`.
+
+    Parameters
+    ----------
+    transforms : list
+        List of transform instances to order.
+
+    Outputs
+    -------
+    list :
+        List of transformed ordered such that forward transforms can be carried
+        out without error.
+    """
+    # get a set of all inputs and all outputs
+    outputs = set().union(*[t.outputs for t in transforms])
+    out = []
+    remaining = [t for t in transforms]
+    while remaining:
+        # pull out transforms that have no inputs in the set of outputs
+        leftover = []
+        for t in remaining:
+            if t.inputs.isdisjoint(outputs):
+                out.append(t)
+                outputs -= t.outputs
+            else:
+                leftover.append(t)
+        remaining = leftover
+    return out
+
+def read_transforms_from_config(cp, section="transforms"):
+    """Returns a list of PyCBC transform instances for a section in the
+    given configuration file.
+
+    If the transforms are nested (i.e., the output of one transform is the
+    input of another), the returned list will be sorted by the order of the
+    nests.
+
+    Parameters
+    ----------
+    cp : WorflowConfigParser
+        An open config file to read.
+    section : {"transforms", string}
+        Prefix on section names from which to retrieve the transforms.
+
+    Returns
+    -------
+    list
+        A list of the parsed transforms.
+    """
+    trans = []
+    for subsection in cp.get_subsections(section):
+        name = cp.get_opt_tag(section, "name", subsection)
+        t = transforms[name].from_config(cp, section, subsection)
+        trans.append(t)
+    return order_transforms(trans)
