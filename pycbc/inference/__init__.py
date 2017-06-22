f4bfc7efcd01d2ca49b788d1c51c81c64357eb14
dfinstad <daniel.finstad@ligo.org>
2017-06-22 09:07:32 -0400

    Remove unnecessary import

--- pycbc/inference/__init__.py
+++ pycbc/inference/__init__.py
@@ -2,3 +2,4 @@ from pycbc.inference.likelihood import *
 from pycbc.inference.sampler import *
 from pycbc.inference.prior import *
 from pycbc.inference.burn_in import *
+from pycbc.inference.recal import *
