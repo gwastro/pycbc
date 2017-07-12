e8a5532d20be9fff6e840a032a73b40227c71760
Duncan Brown <dabrown@syr.edu>
2017-07-12 10:36:27 -0400

    Set for 1.7.4 release

--- setup.py
+++ setup.py
@@ -270,7 +270,7 @@ def get_version_info():
 
     # If this is a release or another kind of source distribution of PyCBC
     except:
-        version = '1.7.3'
+        version = '1.7.4'
         release = 'True'
         date = hash = branch = tag = author = committer = status = builder = build_date = ''
 
