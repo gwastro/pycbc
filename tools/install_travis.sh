3d468757743311b8d28ab1c3230ffda1769b652f
Duncan Brown <duncan.brown@ligo.org>
2017-09-06 16:24:16 -0400

    apply https://github.com/travis-ci/travis-ci/issues/7940 workaround

--- tools/install_travis.sh
+++ tools/install_travis.sh
@@ -47,6 +47,10 @@ pip install --upgrade pip setuptools
 # needed by mock 
 pip install 'setuptools==18.2' --upgrade
 
+# XXX this is a fix for https://github.com/travis-ci/travis-ci/issues/7940
+# as Pegasus pulls in boto which hits this issue
+export BOTO_CONFIG=/dev/null
+
 # install pegasus
 pip install http://download.pegasus.isi.edu/pegasus/4.7.5/pegasus-python-source-4.7.5.tar.gz
 
