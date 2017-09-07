4496456834bb4935f2593d99162b11844bc5209f
Ian Harry <ian.harry@ligo.org>
2017-09-07 10:01:55 +0200

    Adding another get_chirp_params function

--- pycbc/tmpltbank/lambda_mapping.py
+++ pycbc/tmpltbank/lambda_mapping.py
@@ -16,7 +16,7 @@
 from __future__ import division
 import re
 import numpy
-from lal import MTSUN_SI, GAMMA, PI, CreateDict
+from lal import MTSUN_SI, GAMMA, PI, CreateDict, CreateREAL8Vector
 import lalsimulation
 from pycbc import pnutils
 
@@ -151,6 +151,144 @@ def ethinca_order_from_string(order):
     else: raise ValueError("Order "+str(order)+" is not valid for ethinca"
                            "calculation! Valid orders: "+
                            str(get_ethinca_orders().keys()))
+
+def get_chirp_params_newer(mass1, mass2, spin1z, spin2z, f0, order,
+                           quadparam1=None, quadparam2=None, lambda1=None,
+                           lambda2=None):
+    """
+    Take a set of masses and spins and convert to the various lambda
+    coordinates that describe the orbital phase. Accepted PN orders are:
+    {}
+ 
+    Parameters
+    ----------
+    mass1 : float or array
+        Mass1 of input(s).
+    mass2 : float or array
+        Mass2 of input(s).
+    spin1z : float or array
+        Parallel spin component(s) of body 1.
+    spin2z : float or array
+        Parallel spin component(s) of body 2.
+    f0 : float
+        This is an arbitrary scaling factor introduced to avoid the potential
+        for numerical overflow when calculating this. Generally the default
+        value (70) is safe here. **IMPORTANT, if you want to calculate the
+        ethinca metric components later this MUST be set equal to f_low.**
+        This value must also be used consistently (ie. don't change its value
+        when calling different functions!).
+    order : string
+        The Post-Newtonian order that is used to translate from masses and
+        spins to the lambda_i coordinate system. Valid orders given above.
+
+    Returns
+    --------
+    lambdas : list of floats or numpy.arrays
+        The lambda coordinates for the input system(s)
+    """
+
+    # Determine whether array or single value input
+    sngl_inp = False
+    try:
+        num_points = len(mass1)
+    except TypeError:
+        sngl_inp = True
+        # If you care about speed, you aren't calling this function one entry
+        # at a time.
+        mass1 = numpy.array([mass1])
+        mass2 = numpy.array([mass2])
+        spin1z = numpy.array([spin1z])
+        spin2z = numpy.array([spin2z])
+        if quadparam1 is not None:
+            quadparam1 = numpy.array([quadparam1])
+        if quadparam2 is not None:
+            quadparam2 = numpy.array([quadparam2])
+        if lambda1 is not None:
+            lambda1 = numpy.array([lambda1])
+        if lambda2 is not None:
+            lambda2 = numpy.array([lambda2])
+        num_points = 1
+
+    if quadparam1 is None:
+        quadparam1 = numpy.zeros(len(mass1), dtype=float)
+    if quadparam2 is None:
+        quadparam2 = numpy.zeros(len(mass1), dtype=float)
+    if lambda1 is None:
+        lambda1 = numpy.zeros(len(mass1), dtype=float)
+    if lambda2 is None:
+        lambda2 = numpy.zeros(len(mass1), dtype=float)
+
+    mass1_v = CreateREAL8Vector(len(mass1))
+    mass1_v.data[:] = mass1[:]
+    mass2_v = CreateREAL8Vector(len(mass1))
+    mass2_v.data[:] = mass2[:]
+    spin1z_v = CreateREAL8Vector(len(mass1))
+    spin1z_v.data[:] = spin1z[:]
+    spin2z_v = CreateREAL8Vector(len(mass1))
+    spin2z_v.data[:] = spin2z[:]
+    lambda1_v = CreateREAL8Vector(len(mass1))
+    lambda1_v.data[:] = lambda1[:]
+    lambda2_v = CreateREAL8Vector(len(mass1))
+    lambda2_v.data[:] = lambda2[:]
+    dquadparam1_v = CreateREAL8Vector(len(mass1))
+    dquadparam1_v.data[:] = quadparam1[:] - 1.
+    dquadparam2_v = CreateREAL8Vector(len(mass1))
+    dquadparam2_v.data[:] = quadparam2[:] - 1.
+
+    phasing_arr = lalsimulation.SimInspiralTaylorF2AlignedPhasingArray\
+        (mass1_v, mass2_v, spin1z_v, spin2z_v, lambda1_v, lambda2_v,
+         dquadparam1_v, dquadparam2_v)
+ 
+    vec_len = lalsimulation.PN_PHASING_SERIES_MAX_ORDER + 1;
+    phasing_vs = numpy.zeros([num_points, vec_len])
+    phasing_vlogvs = numpy.zeros([num_points, vec_len])
+    phasing_vlogvsqs = numpy.zeros([num_points, vec_len])
+
+    lng = len(mass1)
+    jmp = lng * vec_len
+    for idx in range(vec_len):
+        phasing_vs[:,idx] = phasing_arr.data[lng*idx : lng*(idx+1)]
+        phasing_vlogvs[:,idx] = \
+            phasing_arr.data[jmp + lng*idx : jmp + lng*(idx+1)]
+        phasing_vlogvsqs[:,idx] = \
+            phasing_arr.data[2*jmp + lng*idx : 2*jmp + lng*(idx+1)]
+
+    pmf = PI * (mass1 + mass2)*MTSUN_SI * f0
+    pmf13 = pmf**(1./3.)
+
+    mapping = generate_inverse_mapping(order)
+    lambdas = []
+    lambda_str = '^Lambda([0-9]+)'
+    loglambda_str = '^LogLambda([0-9]+)'
+    logloglambda_str = '^LogLogLambda([0-9]+'
+    for idx in xrange(len(mapping.keys())):
+        # RE magic engage!
+        rematch = re.match(lambda_str, mapping[idx])
+        if rematch:
+            pn_order = int(rematch.groups()[0])
+            lambdas.append(phasing_vs[:,pn_order] * pmf13**(-5+pn_order))
+            continue
+        rematch = re.match(loglambda_str, mapping[idx])
+        if rematch:
+            pn_order = int(rematch.groups()[0])
+            lambdas.append(phasing_vlogvs[:,pn_order] * pmf13**(-5+pn_order))
+            continue
+        rematch = re.match(logloglambda_str, mapping[idx])
+        if rematch:
+            pn_order = int(rematch.groups()[0])
+            lambdas.append(phasing_vlogvsqs[:,pn_order] * pmf13**(-5+pn_order))
+            continue
+        err_msg = "Failed to parse " +  mapping[idx]
+        raise ValueError(err_msg)
+
+    if sngl_inp:
+        return [l[0] for l in lambdas]
+    else:
+        return lambdas
+
+get_chirp_params_newer.__doc__ = \
+    get_chirp_params_newer.__doc__.format(pycbcValidOrdersHelpDescriptions)
+
 
 def get_chirp_params_new(mass1, mass2, spin1z, spin2z, f0, order,
                          quadparam1=None, quadparam2=None, lambda1=None,
@@ -358,4 +496,4 @@ def get_chirp_params_old(mass1, mass2, spin1z, spin2z, f0, order):
 get_chirp_params_old.__doc__ = \
     get_chirp_params_old.__doc__.format(pycbcValidOrdersHelpDescriptions)
 
-get_chirp_params = get_chirp_params_new
+get_chirp_params = get_chirp_params_newer
