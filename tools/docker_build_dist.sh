d314338b84e54608f151feffec75704e55d31303
Duncan Brown <duncan.brown@ligo.org>
2017-09-09 14:35:42 -0400

    enable strace

--- tools/docker_build_dist.sh
+++ tools/docker_build_dist.sh
@@ -48,6 +48,8 @@ fi
 if [ "x${PYCBC_CONTAINER}" == "xpycbc_inspiral_bundle" ] ; then
   echo -e "\\n>> [`date`] Building pycbc_inspiral bundle for CentOS 6"
 
+  yum -y install strace
+
   # create working dir for build script
   BUILD=/pycbc/build
   mkdir -p ${BUILD}
