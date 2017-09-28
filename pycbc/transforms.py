7d57063f37db04cdfbb37f7686596ce94b5b12d2
Soumi De <soumi.de@ligo.org>
2017-09-28 19:34:33 -0400

    Add luminosity dist to chirp dist transform

--- pycbc/transforms.py
+++ pycbc/transforms.py
@@ -332,7 +332,7 @@ class MchirpQToMass1Mass2(BaseTransform):
         return self.format_output(maps, out)
 
     def jacobian(self, maps):
-        """Returns the Jacobian for the transforming mchirp and q to mass1 and
+        """Returns the Jacobian for transforming mchirp and q to mass1 and
         mass2.
         """
         mchirp = maps['mchirp']
@@ -340,12 +340,92 @@ class MchirpQToMass1Mass2(BaseTransform):
         return mchirp * ((1.+q)/q**3.)**(2./5)
 
     def inverse_jacobian(self, maps):
-        """Returns the Jacobian for the transforming mass1 and mass2 to
+        """Returns the Jacobian for transforming mass1 and mass2 to
         mchirp and q.
         """
         m1 = conversions.primary_mass(maps['mass1'], maps['mass2'])
         m2 = conversions.secondary_mass(maps['mass1'], maps['mass2'])
         return conversions.mchirp_from_mass1_mass2(m1, m2)/m2**2.
+
+
+class ChirpDistanceToDistance(BaseTransform):
+    """ Converts chirp distance to luminosity distance, given the chirp mass.
+    """
+    name = "chirp_distance_to_distance"
+    _inputs = [parameters.chirp_distance]
+    _outputs = [parameters.distance, parameters.mchirp]
+
+
+    def transform(self, maps):
+        """This function transforms from luminosity distance to chirp distance,
+        given the chirp mass.
+        Parameters
+        ----------
+        maps : a mapping object
+        Examples
+        --------
+        Convert a dict of numpy.array:
+        >>> import numpy as np
+        >>> from pycbc import transforms
+        >>> t = transforms.ChirpDistanceToDistance()
+        >>> t.transform({'chirp_distance': np.array([40.]), 'mchirp': np.array([1.2])})
+        {'mchirp': array([ 1.2]), 'chirp_distance': array([ 40.]), 'distance': array([ 39.48595679])}
+        Returns
+        -------
+        out : dict
+            A dict with key as parameter name and value as numpy.array or float
+            of transformed values.
+        """
+        out = {}
+        out[parameters.distance] = \
+                 conversions.distance_from_chirp_distance_mchirp(maps[parameters.chirp_distance],
+                                                     maps[parameters.mchirp])
+        return self.format_output(maps, out)
+
+
+    def inverse_transform(self, maps):
+        """This function transforms from luminosity distance to chirp distance,
+        given the chirp mass.
+        Parameters
+        ----------
+        maps : a mapping object
+        Examples
+        --------
+        Convert a dict of numpy.array:
+        >>> import numpy as np
+        >>> from pycbc import transforms
+        >>> t = transforms.ChirpDistanceToDistance()
+        >>> t.inverse_transform({'distance': np.array([40.]), 'mchirp': np.array([1.2])})
+        {'distance': array([ 40.]), 'chirp_distance': array([ 40.52073522]), 'mchirp': array([ 1.2])}
+        Returns
+        -------
+        out : dict
+            A dict with key as parameter name and value as numpy.array or float
+            of transformed values.
+        """
+        out = {}
+        out[parameters.chirp_distance] = \
+                 conversions.chirp_distance(maps[parameters.distance],
+                                                     maps[parameters.mchirp])
+        return self.format_output(maps, out)
+    
+
+    def jacobian(self, maps):
+        """Returns the Jacobian for transforming luminosity distance to
+        chirp distance, given the chirp mass.
+        """
+        ref_mass=1.4
+        mchirp = maps['mchirp']
+        return (2.**(-1./5) * ref_mass / mchirp)**(-5./6)
+
+
+    def inverse_jacobian(self, maps):
+        """Returns the Jacobian for transforming luminosity distance to
+        chirp distance, given the chirp mass.
+        """
+        ref_mass=1.4
+        mchirp = maps['mchirp']
+        return (2.**(-1./5) * ref_mass / mchirp)**(5./6)
 
 
 class SphericalSpin1ToCartesianSpin1(BaseTransform):
@@ -408,7 +488,7 @@ class SphericalSpin1ToCartesianSpin1(BaseTransform):
 
 class SphericalSpin2ToCartesianSpin2(SphericalSpin1ToCartesianSpin1):
     """ Converts spherical spin parameters (magnitude and two angles) to
-    catesian spin parameters. This class only transforms spsins for the second
+    cartesian spin parameters. This class only transforms spins for the second
     component mass.
     """
     name = "spherical_spin_2_to_cartesian_spin_2"
@@ -969,6 +1049,19 @@ class Mass1Mass2ToMchirpQ(MchirpQToMass1Mass2):
     inverse_jacobian = inverse.jacobian
 
 
+class DistanceToChirpDistance(ChirpDistanceToDistance):
+    """The inverse of ChirpDistanceToDistance.
+    """
+    name = "distance_to_chirp_distance"
+    inverse = ChirpDistanceToDistance
+    _inputs = inverse._outputs
+    _outputs = inverse._inputs
+    transform = inverse.inverse_transform
+    inverse_transform = inverse.transform
+    jacobian = inverse.inverse_jacobian
+    inverse_jacobian = inverse.jacobian
+
+
 class CartesianSpin1ToSphericalSpin1(SphericalSpin1ToCartesianSpin1):
     """The inverse of SphericalSpin1ToCartesianSpin1.
     """
@@ -1139,6 +1232,7 @@ class Logistic(Logit):
 
 # set the inverse of the forward transforms to the inverse transforms
 MchirpQToMass1Mass2.inverse = Mass1Mass2ToMchirpQ
+ChirpDistanceToDistance.inverse = DistanceToChirpDistance
 SphericalSpin1ToCartesianSpin1.inverse = CartesianSpin1ToSphericalSpin1
 SphericalSpin2ToCartesianSpin2.inverse = CartesianSpin2ToSphericalSpin2
 AlignedMassSpinToCartesianSpin.inverse = CartesianSpinToAlignedMassSpin
@@ -1160,6 +1254,8 @@ transforms = {
     CustomTransform.name : CustomTransform,
     MchirpQToMass1Mass2.name : MchirpQToMass1Mass2,
     Mass1Mass2ToMchirpQ.name : Mass1Mass2ToMchirpQ,
+    ChirpDistanceToDistance.name : ChirpDistanceToDistance,
+    DistanceToChirpDistance.name : DistanceToChirpDistance,
     SphericalSpin1ToCartesianSpin1.name : SphericalSpin1ToCartesianSpin1,
     CartesianSpin1ToSphericalSpin1.name : CartesianSpin1ToSphericalSpin1,
     SphericalSpin2ToCartesianSpin2.name : SphericalSpin2ToCartesianSpin2,
@@ -1182,7 +1278,7 @@ common_cbc_forward_transforms = [
     MchirpQToMass1Mass2(), DistanceToRedshift(),
     SphericalSpin1ToCartesianSpin1(), SphericalSpin2ToCartesianSpin2(),
     AlignedMassSpinToCartesianSpin(), PrecessionMassSpinToCartesianSpin(),
-    ChiPToCartesianSpin(),
+    ChiPToCartesianSpin(), ChirpDistanceToDistance()
 ]
 common_cbc_inverse_transforms = [_t.inverse()
                                    for _t in common_cbc_forward_transforms
