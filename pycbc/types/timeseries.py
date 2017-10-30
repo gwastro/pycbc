ebc8e86d7c588818d38c000126fa705a4d4b90d0
Alex Nitz <alex.nitz@gmail.com>
2017-10-30 12:01:22 +0100

    Update timeseries.py

--- pycbc/types/timeseries.py
+++ pycbc/types/timeseries.py
@@ -562,6 +562,11 @@ class TimeSeries(Array):
         """ notch filter the time series using an FIR filtered generated from
         the ideal response passed through a kaiser window (beta = 5.0)
 
+        The suppression of the notch filter is related to the bandwidth and
+        the number of samples in the filter length. For a few Hz bandwidth,
+        typically severall thousand or ten's of thousands of samples are
+        required to create significant downweigting.
+
         Parameters
         ----------
         Time Series: TimeSeries
