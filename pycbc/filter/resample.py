562b1cc64f02209bdb78eafed432e3b17f4e7f9c
Alexander Harvey Nitz <alex.nitz@ligo.org>
2017-11-08 13:47:38 +0100

    ws

--- pycbc/filter/resample.py
+++ pycbc/filter/resample.py
@@ -185,8 +185,8 @@ def notch_fir(timeseries, f1, f2, order, beta=5.0):
     The suppression of the notch filter is related to the bandwidth and
     the number of samples in the filter length. For a few Hz bandwidth,
     a length corresponding to a few seconds is typically
-    required to create significant suppression in the notched band. 
-    To achieve frequency resolution df at sampling frequency fs, 
+    required to create significant suppression in the notched band.
+    To achieve frequency resolution df at sampling frequency fs,
     order should be at least fs/df.
 
     Parameters
@@ -198,7 +198,7 @@ def notch_fir(timeseries, f1, f2, order, beta=5.0):
     f2: float
         The end of the frequency suppression.
     order: int
-        Number of corrupted samples on each side of the time series 
+        Number of corrupted samples on each side of the time series
         (Extent of the filter on either side of zero)
     beta: float
         Beta parameter of the kaiser window that sets the side lobe attenuation.
