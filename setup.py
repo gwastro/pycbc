660f9d7723d40ef7125218c19e5ccca297c5f971
Christopher M. Biwer <christopher.biwer@ligo.org>
2017-11-19 22:07:02 -0500

    Add plotting script.

--- setup.py
+++ setup.py
@@ -458,6 +458,7 @@ setup (
                'bin/inference/pycbc_inference_plot_acl',
                'bin/inference/pycbc_inference_plot_geweke',
                'bin/inference/pycbc_inference_plot_gelman_rubin',
+               'bin/inference/pycbc_inference_plot_inj_recovery',
                'bin/inference/pycbc_inference_plot_movie',
                'bin/inference/pycbc_inference_plot_posterior',
                'bin/inference/pycbc_inference_plot_prior',
