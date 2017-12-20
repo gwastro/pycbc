4e8eca0162d3044c19e7db77d840e5bfee18d8dc
Tito Dal Canton <tito@dalcanton.it>
2017-12-20 10:57:10 -0500

    Set for 1.9.0 release

--- setup.py
+++ setup.py
@@ -276,8 +276,8 @@ def get_version_info():
 
     # If this is a release or another kind of source distribution of PyCBC
     except:
-        version = '1.9.0dev'
-        release = 'False'
+        version = '1.9.0'
+        release = 'True'
 
         date = hash = branch = tag = author = committer = status = builder = build_date = ''
 
