3300088752a11860661f18c18e1d3d21af74b234
Ian Harry <ian.harry@ligo.org>
2018-02-08 11:19:46 +0100

    Wrong

--- setup.py
+++ setup.py
@@ -275,7 +275,7 @@ def get_version_info():
 
     # If this is a release or another kind of source distribution of PyCBC
     except:
-        version = '1.9.1dev'
+        version = '1.9.2dev'
         release = 'False'
 
         date = hash = branch = tag = author = committer = status = builder = build_date = ''
