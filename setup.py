ccac17ee31be43a43b1d0c54cf383e72c385ebfd
Alexander Harvey Nitz <alex.nitz@ligo.org>
2018-02-23 10:04:18 +0100

    Set version for 1.9.2 release

--- setup.py
+++ setup.py
@@ -275,8 +275,8 @@ def get_version_info():
 
     # If this is a release or another kind of source distribution of PyCBC
     except:
-        version = '1.9.2dev'
-        release = 'False'
+        version = '1.9.2'
+        release = 'True'
 
         date = hash = branch = tag = author = committer = status = builder = build_date = ''
 
