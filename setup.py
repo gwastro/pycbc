f310288af89843e77542daf3c6b60ea942a9d16a
Duncan Macleod <duncanmmacleod@gmail.com>
2018-04-11 19:16:39 +0100

    six: use string_types for string type checking (#2129)

--- setup.py
+++ setup.py
@@ -80,6 +80,7 @@ install_requires =  setup_requires + ['Mako>=1.0.1',
                       'corner>=2.0.1',
                       'requests>=1.2.1',
                       'beautifulsoup4>=4.6.0',
+                      'six',
                       ]
 
 #FIXME Remove me when we bump to h5py > 2.5
