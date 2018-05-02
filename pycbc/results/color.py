a844d4d76b11ad994ac8079b78b418a32e058a3a
Alex Nitz <alex.nitz@gmail.com>
2018-05-02 16:00:21 +0200

    don't import gwpy for colors (#2157)

--- pycbc/results/color.py
+++ pycbc/results/color.py
@@ -1,19 +1,14 @@
 """ Utilities for managing matplotlib colors and mapping ifos to color
 """
 
-# try and import color scheme from GWpy, see
-# https://gwpy.github.io/docs/stable/plotter/colors.html for details
-try:
-    from gwpy.plotter.colors import GW_OBSERVATORY_COLORS as _ifo_color_map
-except ImportError:
-    _ifo_color_map = {
-        'G1': '#222222',  # dark gray
-        'K1': '#ffb200',  # yellow/orange
-        'H1': '#ee0000',  # red
-        'I1': '#b0dd8b',  # light green
-        'L1': '#4ba6ff',  # blue
-        'V1': '#9b59b6',  # magenta/purple
-    }
+_ifo_color_map = {
+    'G1': '#222222',  # dark gray
+    'K1': '#ffb200',  # yellow/orange
+    'H1': '#ee0000',  # red
+    'I1': '#b0dd8b',  # light green
+    'L1': '#4ba6ff',  # blue
+    'V1': '#9b59b6',  # magenta/purple
+}
 
 
 def ifo_color(ifo):
