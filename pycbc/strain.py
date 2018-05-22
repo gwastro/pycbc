090b5bcfbcf61c9ff6237e47980bed3ee29659a7
Miriam Cabero <miriam.cabero@ligo.org>
2018-05-22 17:54:46 +0200

    Fix codeclimate issue? I don't know what it wants!

--- pycbc/strain.py
+++ pycbc/strain.py
@@ -378,7 +378,7 @@ def from_cli(opt, dyn_range_fac=1, precision='single',
             strain = resample_to_delta_t(strain, 1.0/opt.sample_rate)
 
         if opt.injection_file or opt.sgburst_injection_file \
-            or opt.ringdown_injection_file and not opt.channel_name:
+          or opt.ringdown_injection_file and not opt.channel_name:
             raise ValueError('Please provide channel names with the format '
                              'ifo:channel (e.g. H1:CALIB-STRAIN) to inject '
                              'simulated signals into fake strain')
