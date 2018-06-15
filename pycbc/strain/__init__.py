eb997e03b53f87468237f138fceb01ce10098f11
Alexander Harvey Nitz <alex.nitz@gmail.com>
2018-06-15 17:39:53 +0200

    fix gate links

--- pycbc/strain/__init__.py
+++ pycbc/strain/__init__.py
@@ -1,9 +1,13 @@
 from . import recalibrate
+
 from .strain import detect_loud_glitches
 from .strain import from_cli, from_cli_single_ifo, from_cli_multi_ifos
 from .strain import insert_strain_option_group, insert_strain_option_group_multi_ifo
 from .strain import verify_strain_options, verify_strain_options_multi_ifo
 from .strain import gate_data, StrainSegments, StrainBuffer
+
+from .gate import add_gate_options_group, gates_from_cli
+from .gate import apply_gates_to_td, apply_gates_to_fd, psd_gates_from_cli
 
 models = {
     recalibrate.Recalibrate.name : Recalibrate
