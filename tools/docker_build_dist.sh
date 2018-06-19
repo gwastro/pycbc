ecb4a886ee585e18eb181e4e9f3b72ae551b6455
Ian Harry <iwharry@googlemail.com>
2018-06-19 10:48:48 +0200

    DEBUG (#2199)
    * DEBUG
    
    * Warning: Minotaurs hidden in tools directory
    
    * Try this???
    
    * This must be it!!
    
    * Print *all* the file
    
    * Worth a try
    
    * Shouldn't need that
    
    * Into the rabbit hole
    
    * Remove cat statements
    
    * Remove this

--- tools/docker_build_dist.sh
+++ tools/docker_build_dist.sh
@@ -53,6 +53,23 @@ if [ "x${PYCBC_CONTAINER}" == "xpycbc_inspiral_bundle" ] ; then
   export PYTHONUSERBASE=${BUILD}/.local
   export XDG_CACHE_HOME=${BUILD}/.cache
 
+  # Autoconf needs m4
+  wget -O m4-1.4.9.tar.gz http://ftp.gnu.org/gnu/m4/m4-1.4.9.tar.gz
+  tar -zvxf m4-1.4.9.tar.gz
+  cd m4-1.4.9
+  ./configure
+  make
+  make install
+  cd ..
+
+  # Build new autoconf
+  curl -L -O http://ftp.gnu.org/gnu/autoconf/autoconf-2.69.tar.gz
+  tar zxf autoconf-2.69.tar.gz
+  cd autoconf-2.69
+  ./configure
+  make && make install
+  cd ..
+
   # get library to build optimized pycbc_inspiral bundle
   wget_opts="-c --passive-ftp --no-check-certificate --tries=5 --timeout=30 --no-verbose"
   primary_url="https://git.ligo.org/ligo-cbc/pycbc-software/raw/"
