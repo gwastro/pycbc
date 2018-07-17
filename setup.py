56e9b69fa55afe413f90fb010b296c8ae32c315f
Ian Harry <ian.harry@ligo.org>
2018-07-17 09:30:22 +0200

    Prepare for new v1.11.X release

--- setup.py
+++ setup.py
@@ -277,7 +277,7 @@ def get_version_info():
 
     # If this is a release or another kind of source distribution of PyCBC
     except:
-        version = '1.11.1'
+        version = '1.11.2'
         release = 'True'
 
         date = hash = branch = tag = author = committer = status = builder = build_date = ''
