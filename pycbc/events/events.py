74fc94b2ebf7e1ad9ec21cd02f791688371d757d
Francesco Pannarale <francesco.pannarale@ligo.org>
2018-07-17 22:51:54 +0100

    Avoiding failure when calling len() on None

--- pycbc/events/events.py
+++ pycbc/events/events.py
@@ -555,7 +555,7 @@ class EventManagerCoherent(EventManager):
         # initialize with zeros - since vectors can be None, look for the
         # first one that isn't
         new_events = None
-        new_events = numpy.zeros(max([len(v) for v in vectors]), dtype=self.network_event_dtype)
+        new_events = numpy.zeros(max([len(v) for v in vectors if v is not None]), dtype=self.network_event_dtype)
         # they shouldn't all be None
         assert new_events is not None
         new_events['template_id'] = self.template_index
