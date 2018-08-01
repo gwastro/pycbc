c3ddfbc40b5f2e0ab75a07172502ba84f80bde12
Charlie Hoy <33521252+hoyc1@users.noreply.github.com>
2018-08-01 16:07:09 +0100

    adding time marginalisation and distance marginalisation
    * adding time and distance marginalisation
    
    * fixing pep8 issues
    
    * fixing further pep8 issues
    
    * further pep8 issues
    
    * adding new marginalized Gaussian noise function which takes keyword ararguments
    
    * fixing pep8 issues
    
    * fixing pep8
    
    * fixing pep8 issues
    
    * fixing Duncans issues
    
    * deliberating as to whether we should use a look up table or do distance marginalisation on the fly
    
    * removing lookup table. I think it is better to do integration on the fly. Saves us having to carry the big array in memory
    
    * changing the init function in marginalized class
    
    * removing individual marginalized classes and adding documentation to mamain marginalized class
    
    * fixing typo in time marginalization
    
    * removing space around keyword argument
    
    * fixing the error on the marginalization calculations
    
    * fixing the error on the marginalization calculations
    
    * editing documentation and setting attributes
    
    * removing lines which are repeated
    
    * putting if/elif statements in __init__ and using matched_filter_core instead of numpy
    
    * putting marginalized gaussian noise class in a seperate file
    
    * changing __init__ to account for new marginalized gaussian file
    
    * trying to reduce cognitive complexity of __init__ function
    
    * reducing cognitive complexity of __init__ function
    
    * converting to pep8
    
    * reducing cognitive complexity of _loglr function
    
    * trying to further reduce cognitive complexity in _loglr function
    
    * fixing silly typo
    
    * overriding the from_config function to add code to load priors
    
    * reducing cognitive complexity of from_config function
    
    * adding waveform generator to from_config function
    
    * adding waveform generator to from_config function
    
    * adding name=marginalized_gaussian_noise and fixing test_sampler.py
    
    * fixing pep8 problems
    
    * removing marg_prior_eval
    
    * removing marg_prior_eval again
    
    * adding test for MarginalizedGaussianNoise class
    
    * adding zdhp_psd to args
    
    * adding missing comma
    
    * trying to fix travis
    
    * writing config file within function rather than temperorarily saving it and loading it up
    
    * adding from pycbc import distributions and adding distance prior to calculation
    
    * removing test
    
    * fixing stupid mistakes
    
    * fixing bug in test_loglr
    
    * fixing travis
    
    * fixing pep8
    
    Signed-off-by: Duncan Brown <dabrown@syr.edu>

--- pycbc/inference/models/__init__.py
+++ pycbc/inference/models/__init__.py
@@ -21,8 +21,8 @@ assuming various noise models.
 
 
 from .analytic import (TestEggbox, TestNormal, TestRosenbrock, TestVolcano)
-from .gaussian_noise import (GaussianNoise,
-                             MarginalizedPhaseGaussianNoise)
+from .gaussian_noise import GaussianNoise
+from .marginalized_gaussian_noise import MarginalizedGaussianNoise
 
 
 # Used to manage a model instance across multiple cores or MPI
@@ -167,5 +167,5 @@ models = {_cls.name: _cls for _cls in (
     TestRosenbrock,
     TestVolcano,
     GaussianNoise,
-    MarginalizedPhaseGaussianNoise,
+    MarginalizedGaussianNoise,
 )}
