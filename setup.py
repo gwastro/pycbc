5f73b442ea20d771a2d5fc5ddc7b6d1908532de2
Alex Nitz <alex.nitz@ligo.org>
2018-08-16 12:15:26 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -155,8 +155,8 @@ def get_version_info():
 
     # If this is a release or another kind of source distribution of PyCBC
     except:
-        version = '1.12.2'
-        release = 'True'
+        version = '1.12.dev3'
+        release = 'False'
 
         date = hash = branch = tag = author = committer = status = builder = build_date = ''
 
