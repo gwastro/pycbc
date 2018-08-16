8923f1be52abd8779d32db6fd163e317f1dfc34f
Josh Willis <joshua.willis@ligo.org>
2018-08-15 20:43:05 -0700

    Bump version for release

--- setup.py
+++ setup.py
@@ -278,7 +278,7 @@ def get_version_info():
 
     # If this is a release or another kind of source distribution of PyCBC
     except:
-        version = '1.11.7'
+        version = '1.11.8'
         release = 'True'
 
         date = hash = branch = tag = author = committer = status = builder = build_date = ''
