1c4ebd81d74765f6a5c8bb13f95d8247a00d7641
Alexander Harvey Nitz <alex.nitz@gmail.com>
2018-08-20 21:45:23 +0200

    no need for name

--- pycbc/scheme.py
+++ pycbc/scheme.py
@@ -176,7 +176,7 @@ def schemed(prefix):
                 try:
                     backend = __import__(prefix + scheme_prefix[sch], fromlist=[fn.__name__])
                     schemed_fn = getattr(backend, fn.__name__)
-                except (ImportError, AttributeError) as e:
+                except (ImportError, AttributeError):
                     continue
 
                 if mgr.state not in _import_cache:
