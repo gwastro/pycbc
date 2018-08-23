8847dbf55393ef27f5cea977701219c4ada2c4a5
Alexander Harvey Nitz <ahnitz@syr.edu>
2018-08-23 08:36:03 -0700

    add function to set fftw plan time limit

--- pycbc/fft/fftw.py
+++ pycbc/fft/fftw.py
@@ -217,6 +217,18 @@ def export_double_wisdom_to_filename(filename):
     if retval == 0:
         raise RuntimeError("Could not export wisdom to file {0}".format(filename))
 
+def set_planning_limit(time):
+    if not _fftw_threaded_set:
+        set_threads_backend()
+
+    f = double_lib.fftw_set_timelimit
+    f.argtypes= [ctypes.c_double]
+    f(time)
+
+    f = float_lib.fftwf_set_timelimit
+    f.argtypes= [ctypes.c_double]
+    f(time)
+
 # Create function maps for the dtypes
 plan_function = {'float32': {'complex64': float_lib.fftwf_plan_dft_r2c_1d},
                  'float64': {'complex128': double_lib.fftw_plan_dft_r2c_1d},
