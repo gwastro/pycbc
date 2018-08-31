b9c007220d4cd08698e05d8b812b9c800005736d
Josh Willis <josh-willis@users.noreply.github.com>
2018-08-31 17:42:46 -0500

    Beaut soup fix (#2308)
    * Don't BeautifulSoup binary! (#2307)
    
    * Increment version to 1.11.14

--- setup.py
+++ setup.py
@@ -278,7 +278,7 @@ def get_version_info():
 
     # If this is a release or another kind of source distribution of PyCBC
     except:
-        version = '1.11.13'
+        version = '1.11.14'
         release = 'True'
 
         date = hash = branch = tag = author = committer = status = builder = build_date = ''
