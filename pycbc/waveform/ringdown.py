b1843667623412a032b1b9f47286c3cf7c18ec3a
Collin Capano <collin.capano@ligo.org>
2018-09-05 11:17:21 +0200

    use get_lm_f0tau_allmodes from conversions

--- pycbc/waveform/ringdown.py
+++ pycbc/waveform/ringdown.py
@@ -29,7 +29,7 @@ import numpy, lal
 import lalsimulation as lalsim
 from pycbc.types import TimeSeries, FrequencySeries, float64, complex128, zeros
 from pycbc.waveform.waveform import get_obj_attrs
-from pycbc.conversions import get_lm_f0tau
+from pycbc.conversions import get_lm_f0tau_allmodes
 
 default_qnm_args = {'t_0':0}
 qnm_required_args = ['f_0', 'tau', 'amp', 'phi']
@@ -114,22 +114,6 @@ def lm_freqs_taus(**kwargs):
                 raise ValueError('tau_%d%d%d is required' %(l,m,n))
 
     return freqs, taus
-
-# Functions to obtain f_0 and tau from mass and spin #########################
-def get_lm_f0tau_allmodes(mass, spin, modes):
-    """Return a dictionary with the f_0 and tau for all the modes
-    in the list lmns (list of strings)
-    """
-
-    f_0, tau = {}, {}
-    for lmn in modes:
-        l, m, nmodes = int(lmn[0]), int(lmn[1]), int(lmn[2])
-        tmp_f0, tmp_tau = get_lm_f0tau(mass, spin, l, m, nmodes)
-        for n in range(nmodes):
-            f_0['%d%d%d' %(l,m,n)] = tmp_f0[n]
-            tau['%d%d%d' %(l,m,n)] = tmp_tau[n]
-
-    return f_0, tau
 
 # Functions to obtain t_final and f_final #####################################
 
