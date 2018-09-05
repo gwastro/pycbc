921ea39765ba97d704b520165efde7cbba8f3a26
Collin Capano <collin.capano@ligo.org>
2018-09-05 11:16:45 +0200

    add reference, get_lm_f0tau_allmodes to conversions

--- pycbc/conversions.py
+++ pycbc/conversions.py
@@ -828,6 +828,8 @@ def get_lm_f0tau(mass, spin, l, m, nmodes):
     # we'll ravel the arrays so we can evaluate each parameter combination
     # one at a a time
     origshape = mass.shape
+    if nmodes < 1:
+        raise ValueError("nmodes must be >= 1")
     if nmodes > 1:
         newshape = tuple(list(origshape)+[nmodes])
     else:
@@ -849,6 +851,52 @@ def get_lm_f0tau(mass, spin, l, m, nmodes):
     taus = taus.reshape(newshape)
     return (formatreturn(f0s, input_is_array),
             formatreturn(taus, input_is_array))
+
+
+def get_lm_f0tau_allmodes(mass, spin, modes):
+    """Returns a dictionary of all of the frequencies and damping times for the
+    requested modes.
+
+    Parameters
+    ----------
+    mass : float or array
+        Mass of the black hole (in solar masses).
+    spin : float or array
+        Dimensionless spin of the final black hole.
+    modes : list of str
+        The modes to get. Each string in the list should be formatted 'lmN',
+        where l (m) is the l (m) index of the harmonic and N is the number of
+        overtones to generate (note, N is not the index of the overtone). For
+        example, '221' will generate the 0th overtone of the l = m = 2 mode.
+
+    Returns
+    -------
+    f0 : dict
+        Dictionary mapping the modes to the frequencies. The dictionary keys
+        are 'lmn' string, where l (m) is the l (m) index of the harmonic and
+        n is the index of the overtone. For example, '220' is the l = m = 2
+        mode and the 0th overtone.
+    tau : dict
+        Dictionary mapping the modes to the damping times. The keys are the
+        same as ``f0``.
+    """
+    f0, tau = {}, {}
+    key = '{}{}{}'
+    for lmn in modes:
+        l, m, nmodes = int(lmn[0]), int(lmn[1]), int(lmn[2])
+        tmp_f0, tmp_tau = get_lm_f0tau(mass, spin, l, m, nmodes)
+        if nmodes == 1:
+            # in this case, tmp_f0 and tmp_tau will just be floats
+            f0[key.format(l, m, '0')] = tmp_f0
+            tau[key.format(l, m, '0')] = tmp_tau
+        else:
+            for n in range(nmodes):
+                # we need to wrap tmp_f0 with formatreturn to ensure that if
+                # only a mass, spin pair was requested, the value stored to
+                # the dict is a float
+                f0[key.format(l, m, n)] = formatreturn(tmp_f0[..., n])
+                tau[key.format(l, m, n)] = formatreturn(tmp_tau[..., n])
+    return f0, tau
 
 
 def freq_from_final_mass_spin(final_mass, final_spin, l=2, m=2, nmodes=1):
@@ -907,9 +955,10 @@ def tau_from_final_mass_spin(final_mass, final_spin, l=2, m=2, nmodes=1):
     return get_lm_f0tau(final_mass, final_spin, l, m, nmodes)[1]
 
 
-# following are from Berti et al. 2006.
-# keys are l,m. Constants are for converting from
-# frequency and damping time to mass and spin
+# The following are from Table VIII of Berti et al., PRD 73 064030,
+# arXiv:gr-qc/0512160 (2006).
+# Keys are l,m. Constants are for converting from
+# frequency and damping time to mass and spin.
 _berti_spin_constants = {
     (2,2): (0.7, 1.4187, -0.4990),
     }
