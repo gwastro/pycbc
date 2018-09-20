df4739e8c7fe171cf414acf3c7ba49336c50d4f8
Alexander Harvey Nitz <alex.nitz@gmail.com>
2018-09-20 16:08:28 +0000

    move caches to a fixed size

--- pycbc/vetoes/chisq.py
+++ pycbc/vetoes/chisq.py
@@ -318,12 +318,14 @@ class SingleDetPowerChisq(object):
         return eval(arg, {"__builtins__":None}, safe_dict)
 
     def cached_chisq_bins(self, template, psd):
+        from pycbc.opt import LimitedSizeDict
+
         key = id(psd)
         if not hasattr(psd, '_chisq_cached_key'):
-            psd._chisq_cached_key = {}
+            psd._chisq_cached_key = LimitedSizeDict(size_limit=2**15)
 
         if not hasattr(template, '_bin_cache'):
-            template._bin_cache = {}
+            template._bin_cache = LimitedSizeDict(size_limite=2**10)
 
         if key not in template._bin_cache or id(template.params) not in psd._chisq_cached_key:
             psd._chisq_cached_key[id(template.params)] = True
