6235be2d992a8811d4e4f2cf6eb429b16048f7e5
Alexander Harvey Nitz <alex.nitz@ligo.org>
2018-09-24 09:14:38 +0200

    fix ws

--- pycbc/opt.py
+++ pycbc/opt.py
@@ -195,16 +195,16 @@ def verify_optimization_options(opt, parser):
         logging.info("Pinned to CPUs %s " % requested_cpus)
 
 class LimitedSizeDict(OrderedDict):
-  def __init__(self, *args, **kwds):
-    self.size_limit = kwds.pop("size_limit", None)
-    OrderedDict.__init__(self, *args, **kwds)
-    self._check_size_limit()
+    def __init__(self, *args, **kwds):
+        self.size_limit = kwds.pop("size_limit", None)
+        OrderedDict.__init__(self, *args, **kwds)
+        self._check_size_limit()
 
-  def __setitem__(self, key, value):
-    OrderedDict.__setitem__(self, key, value)
-    self._check_size_limit()
+    def __setitem__(self, key, value):
+        OrderedDict.__setitem__(self, key, value)
+        self._check_size_limit()
 
-  def _check_size_limit(self):
-    if self.size_limit is not None:
-      while len(self) > self.size_limit:
-        self.popitem(last=False)
+    def _check_size_limit(self):
+        if self.size_limit is not None:
+        while len(self) > self.size_limit:
+            self.popitem(last=False)
