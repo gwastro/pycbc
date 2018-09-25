d6ca0cbf4147314140ef777afca0b09fdaf6cdff
Steven Reyes <steven.reyes@ligo.org>
2018-09-25 12:21:56 -0400

    try to fix bounds

--- pycbc/distributions/uniform_discrete_intervals.py
+++ pycbc/distributions/uniform_discrete_intervals.py
@@ -9,6 +9,9 @@ def __init__(self, **params):
 
      # temporarily suppress numpy divide by 0 warning
     numpy.seterr(divide='ignore')
+    self._bounds = {}
+    self._stride = {}
+    self._interval = {}
 
     stride = [p for p in params if p.endswith('_stride')]
     interval = [p for p in params if p.endswith('_interval')]
@@ -18,6 +21,21 @@ def __init__(self, **params):
     self._lognorm = -sum([numpy.log(abs(bnd[1]-bnd[0]))
                           for bnd in self._bounds.values()])
     self._norm = numpy.exp(self._lognorm)
+
+    missing = set(self._mean.keys()) - set(params.keys())
+    if any(missing):
+        raise ValueError("means provided for unknow params {}".format(
+            ', '.join(missing)))
+    missing = set(self._var.keys()) - set(params.keys())
+    if any(missing):
+        raise ValueError("vars provided for unknow params {}".format(
+            ', '.join(missing)))
+    # set default mean/var for params not specified
+    self._stride.update(dict([[p, 0.]
+        for p in params if p not in self._mean]))
+    self._interval.update(dict([[p, 1.]
+        for p in params if p not in self._var]))
+
     numpy.seterr(divide='warn')
 
     @property
