82530a32ab2e09d6928a5075cd4903edd34d7a6a
Steven Reyes <steven.reyes@ligo.org>
2018-09-25 10:58:33 -0400

    debugging

--- pycbc/distributions/uniform_discrete_intervals.py
+++ pycbc/distributions/uniform_discrete_intervals.py
@@ -9,15 +9,37 @@ class UniformDiscreteIntervals(uniform.Uniform):
     name = "uniform_discrete_intervals"
 
 def __init__(self, **params):
-    super(UniformDiscreteIntervals, self).__init__(**params)
-    self._norm = (1.0 - bnd[0]) / (bnd[1] - bnd[0] + 1.0)
-    self._log_norm = numpy.log(self._norm)
+    self._bounds = {}
+    self._stride = {}
+    self._interval = {}
+#    self._norm = (1.0 - bnd[0]) / (bnd[1] - bnd[0] + 1.0)
+#    self._log_norm = numpy.log(self._norm)
+    self._stride = [p for p in params if p.endswith('_stride')]
+    self._interval = [p for p in params if p.endswith('_interval')]
 
-def norm(self):
-    return self._norm
+#    super(UniformDiscreteIntervals, self).__init__(**params)
+    print "Hello World"
+    print self._stride
+    print self._interval
+    print self._bounds[params][0]
+    print self._bounds[params][1]
+    for p,bnds in self._bounds.items():
+        a, b = bnds
 
-def log_norm(self):
-    return self._log_norm
+#    super(UniformDiscreteIntervals, self).__init__(**params)
+#def norm(self):
+#    return self._norm
+
+#def log_norm(self):
+#    return self._log_norm
+
+@property
+def stride(self):
+    return self._stride
+
+@property
+def interval(self):
+    return self._interval
 
 def rvs(self, size=1, param=None):
     """
@@ -29,9 +51,9 @@ def rvs(self, size=1, param=None):
 
     arr = numpy.zeros(size, dtype=dtype)
     for (p,_) in dtype:
-        n = self._bounds[p][1] - self.bounds[p][0] + 1.0
-        arr[p] = numpy.floor(numpy.random.uniform(size=size) * n + self._bounds[p][0] - 1.0) 
-
+        x = numpy.arange(self._bounds[p][0], self._bounds[p][1],
+                         self._stride + self._interval)
+        arr[p] = numpy.random.uniform(x, x + self._stride, size=size)
     return arr
 
 __all__ = ['UniformDiscreteIntervals']
