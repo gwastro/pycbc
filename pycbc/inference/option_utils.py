cc285ef7aa916fb4fed74b528943b3f7c93dbdd4
Duncan Brown <dabrown@syr.edu>
2018-09-25 12:24:35 -0400

    fixes https://github.com/gwastro/pycbc/issues/2346

--- pycbc/inference/option_utils.py
+++ pycbc/inference/option_utils.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2016 Collin Capano
+# Copyright (C) 2016 Collin Capano, Duncan Brown
 #
 # This program is free software; you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by the
