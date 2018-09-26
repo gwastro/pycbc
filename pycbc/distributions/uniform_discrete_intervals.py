abf2f30d026e629b6add665463579389f569c2f6
Steven Reyes <steven.reyes@ligo.org>
2018-09-26 05:01:55 -0400

    Try more debugging

--- pycbc/distributions/uniform_discrete_intervals.py
+++ pycbc/distributions/uniform_discrete_intervals.py
@@ -14,13 +14,8 @@ class UniformIntervals(bounded.BoundedDist):
         stride_args = [p for p in params if p.endswith('_stride')]
         interval_args = [p for p in params if p.endswith('_interval')]
 
-        print stride_args
-        print interval_args
         self._stride = dict([[p, params.pop(p)] for p in stride_args])
         self._interval = dict([[p, params.pop(p)] for p in interval_args])
-
-        print self._stride
-        print self._interval
 
         super(UniformIntervals, self).__init__(**params)
 
