b7f8932c80d2e50ac256b89f8840003d9180e51d
Alexander Harvey Nitz <alex.nitz@ligo.org>
2018-09-27 15:37:01 +0200

    initial jumps

---
+++ pycbc/inference/sampler/jump.py
@@ -0,0 +1,58 @@
+# Copyright (C) 2018 Alex Nitz Sebastian Khan
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 3 of the License, or (at your
+# option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+# Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+
+#
+# =============================================================================
+#
+#                                   Preamble
+#
+# =============================================================================
+#
+""" This module contains jump proposals for samplers to pick new parameters.
+The format will follow the emcee format.
+"""
+from __future__ import absolute_import
+from emcee.moves import MHMove, StretchMove, WalkMove, KDEMove
+
+_jump_proposals = {'stretch': StretchMove,
+                   'walk': WalkMove,
+                   'kde': KDEMove}
+
+def get_jump_from_config(section, cp):
+    name = cp.get(section, 'jump')
+    options = cp.items('jump-{}'.format(name))
+    kwds = {}
+    for key, value in options:
+        try:
+            kwds[key] = float(value)
+        except:
+            kwds[key] = value
+    return _jump_proposals[name](**kwds)
+
+class Jump(object):
+    def __init__(self):
+        pass
+
+    def propose(self, old_pos):
+        return new_pos
+
+class EmceeJump(MHMove):
+    def __init__(self, jump):
+        self.jump = jump
+
+    def get_p
+
+
