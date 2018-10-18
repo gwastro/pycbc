42bdf228cdfd31bf7ec8314441ebb983eb2eba53
Collin Capano <cdcapano@gmail.com>
2018-10-18 19:00:21 +0200

    Add documentation for developers about the new sampler API (#2385)
    * fix some errors in the doc strings
    
    * add sphinx extension for inheritance diagrams to conf
    
    * add sampler_api and inference io rst files
    
    * add more to the detailed example
    
    * add steps for adding a new sampler
    
    * use substitutions
    
    * fix some warnings
    
    * write sampler IO API documentation
    
    * add h5py to intersphinx; fix pep8 issue
    
    * added _include directory to docs for dynamically generated rst files
    
    * add code to run scripts in _include to conf
    
    * use automatically generated inheritance diagrams
    
    * clean up scripts
    
    * use sampler/io names for labelling inheritance diagrams
    
    * remove redundant anchor
    
    * list abstract methods in BaseMCMC doc string
    
    * small update to base sampler doc

---
+++ docs/_include/sampler_inheritance_diagrams.py
@@ -0,0 +1,24 @@
+# Creates RST for the sampler inheritance diagrams
+from __future__ import print_function
+from pycbc.inference.sampler import samplers
+
+fname = 'sampler_inheritance_diagrams.rst'
+
+tmplt = """.. _inheritance-{name}:
+
+* ``{name}``:
+
+.. inheritance-diagram:: {module}.{clsname}
+   :parts: 3
+
+|
+
+"""
+fp = open(fname, 'w')
+
+for sampler, cls in sorted(samplers.items()):
+    out = tmplt.format(name=sampler, clsname=cls.__name__,
+                       module=cls.__module__)
+    print(out, file=fp)
+
+fp.close()
