669d48ec4a03d5162a68d1453b4033a26afbda08
Alexander Harvey Nitz <alex.nitz@gmail.com>
2018-10-19 20:14:52 +0200

    move calibration test from pytest to unittest

--- test/test_calibration.py
+++ test/test_calibration.py
@@ -1,41 +1,53 @@
-import pytest
+import unittest
 
 from pycbc import strain
 from pycbc.types import FrequencySeries
 import numpy as np
 from pycbc.workflow.configuration import WorkflowConfigParser
 from pycbc.strain.recalibrate import Recalibrate
+from utils import simple_exit
 
-
-@pytest.fixture
 def strain_array():
     frequency_array = np.linspace(0, 2048, 100)
     delta_f = frequency_array[1] - frequency_array[0]
     strain_array = frequency_array**2
     return FrequencySeries(strain_array, delta_f)
 
-
-def test_cannot_instantiate_base_class():
-    with pytest.raises(TypeError):
-        Recalibrate('test')
-
-
-@pytest.mark.parametrize("name, params", [
-    ('cubic_spline', dict(ifo_name='test',
+class CalibrateTestBase(unittest.TestCase):
+    def test_instantiate(self):
+        self.assertRaises(TypeError, Recalibrate, 'test')  
+        
+    def test_instantiation(self):
+        params = dict(ifo_name='test',
                           minimum_frequency=10,
                           maximum_frequency=1024,
-                          n_points=5)),
-])
-def test_instantiation(name, params):
-    model = strain.models[name](**params)
-    assert model.name == name
+                          n_points=5)
+        model = strain.models['cubic_spline'](**params)
+        self.assertTrue(model.name == 'cubic_spline')
+        
+    def test_instantiation_from_config(self):
+        parameters = dict(minimum_frequency='10', maximum_frequency='1024',
+                          n_points='5')
+    
+        cp = WorkflowConfigParser()
+        cp.add_section('test')
+        ifo_name = 'ifo'
+        cp.set('test', '{}_model'.format(ifo_name), 'cubic_spline')
+        for key in parameters:
+            cp.set('test', '{}_{}'.format(ifo_name, key), parameters[key])
+        from_config = strain.read_model_from_config(cp, ifo_name, 'test')
+        self.assertTrue(all([from_config.name == 'cubic_spline',
+                             from_config.ifo_name == ifo_name]))
 
-
-@pytest.mark.parametrize("name, init_params, dict_params", [
-    ('cubic_spline',
-     dict(ifo_name='test', minimum_frequency=10,
-          maximum_frequency=1024, n_points=5),
-     dict(recalib_amplitude_test_0=0.1,
+    def test_too_few_spline_points_fails(self):
+        self.assertRaises(ValueError, strain.CubicSpline, 
+                         ifo_name='test', minimum_frequency=10,
+                         maximum_frequency=1024, n_points=3)
+   
+    def test_update_parameters(self):
+        init_params = dict(ifo_name='test', minimum_frequency=10,
+          maximum_frequency=1024, n_points=5)
+        dict_params = dict(recalib_amplitude_test_0=0.1,
           recalib_amplitude_test_1=0.1,
           recalib_amplitude_test_2=0.1,
           recalib_amplitude_test_3=0.1,
@@ -45,36 +57,19 @@ def test_instantiation(name, params):
           recalib_phase_test_2=0.1,
           recalib_phase_test_3=0.1,
           recalib_phase_test_4=0.1,
-          )
-     )
-])
-def test_update_parameters(name, init_params, dict_params):
-    model = strain.models[name](**init_params)
-    dict_params.update(dict(foo='bar'))
-    prefix = 'recalib_'
-    model.map_to_adjust(strain_array(), prefix=prefix, **dict_params)
-    model_keys = [key[len(prefix):] for key in dict_params if prefix in key]
-    assert all([model.params[key] == dict_params[prefix+key]
-                for key in model_keys])
+          )    
+        model = strain.models['cubic_spline'](**init_params)
+        dict_params.update(dict(foo='bar'))
+        prefix = 'recalib_'
+        model.map_to_adjust(strain_array(), prefix=prefix, **dict_params)
+        model_keys = [key[len(prefix):] for key in dict_params if prefix in key]
+        self.assertTrue(all([model.params[key] == dict_params[prefix+key]
+                    for key in model_keys]))
+ 
 
+if __name__ == '__main__':
+    suite = unittest.TestSuite()
+    suite.addTest(unittest.TestLoader().loadTestsFromTestCase(CalibrateTestBase))
+    results = unittest.TextTestRunner(verbosity=2).run(suite)
+    simple_exit(results)
 
-@pytest.mark.parametrize("name, parameters", [
-    ('cubic_spline', dict(minimum_frequency='10', maximum_frequency='1024',
-                          n_points='5'))
-])
-def test_instantiation_from_config(name, parameters):
-    cp = WorkflowConfigParser()
-    cp.add_section('test')
-    ifo_name = 'ifo'
-    cp.set('test', '{}_model'.format(ifo_name), name)
-    for key in parameters:
-        cp.set('test', '{}_{}'.format(ifo_name, key), parameters[key])
-    from_config = strain.read_model_from_config(cp, ifo_name, 'test')
-    assert all([from_config.name == name, from_config.ifo_name == ifo_name])
-
-
-def test_too_few_spline_points_fails():
-    with pytest.raises(ValueError):
-        strain.CubicSpline(
-            ifo_name='test', minimum_frequency=10,
-            maximum_frequency=1024, n_points=3)
