3c26ed11462a2754e7cb226784c024c39c22365f
Collin Capano <cdcapano@gmail.com>
2018-10-19 12:25:36 +0200

    Update the inference doc page and examples (#2388)
    * add some useful script Duncan Macleod had created for gwin
    
    * don't excute scripts in the _include directory that start with _
    
    * sort rows in dict to rst
    
    * add header to distribution table; remove calib params from waveform parameters table
    
    * add models, samplers, and transforms rst tables
    
    * small change in conf
    
    * update inference configuration documentation
    
    * add analytic 2D normal example to examples/inference
    
    * point to the analytic example scripts and add a posterior plot
    
    * add bbh examples
    
    * update the bbh example
    
    * add gw150914 example files
    
    * update the gw150914 example in the docs
    
    * update doc on output files and checkpointing
    
    * fix varible_args -> variable_params

---
+++ examples/inference/bbh-injection/make_injection.sh
@@ -0,0 +1,8 @@
+pycbc_create_injections --verbose \
+        --config-files inference.ini \
+        --ninjections 1 \
+        --seed 10 \
+        --output-file injection.hdf \
+        --variable-params-section variable_params \
+        --static-args-section static_params \
+        --dist-section prior
