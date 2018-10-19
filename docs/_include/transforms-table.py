924268d2867e689b2f7d10f18f91f32cddedcc18
Collin Capano <cdcapano@gmail.com>
2018-10-19 12:25:36 +0200

    Update the inference doc page and examples (#2388)
    * add some useful script Duncan Macleod had created for gwin
    
    * don't excute scripts in the _include directory that start with _
    
    * sort rows in dict to rst
    
    * add header to distribution table; remove calib params from waveform parameters table
    
    * add models, samplers, and transforms rst tables
    
    * small change in conf
    
    * update inference configuration documentation
    
    * add analytic 2D normal example to examples/inference
    
    * point to the analytic example scripts and add a posterior plot
    
    * add bbh examples
    
    * update the bbh example
    
    * add gw150914 example files
    
    * update the gw150914 example in the docs
    
    * update doc on output files and checkpointing
    
    * fix varible_args -> variable_params

---
+++ docs/_include/transforms-table.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+# Copyright (C) 2018 Duncan Macleod, Collin Capano
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 3 of the License, or (at your
+# option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+# Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+"""Prints an RST table of available distributions from the distributions
+module.
+"""
+from __future__ import (print_function, absolute_import)
+from pycbc.transforms import transforms
+from _dict_to_rst import (rst_dict_table, format_class)
+
+tbl = rst_dict_table(transforms, key_format='``\'{0}\'``'.format,
+                     header=('Name', 'Class'),
+                     val_format=format_class)
+
+filename = 'transforms-table.rst'
+with open(filename, 'w') as fp:
+    print(tbl, file=fp)
