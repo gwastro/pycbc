cb06109394671d0d6e0388d297fa1e89211b9993
Collin Capano <cdcapano@gmail.com>
2018-10-24 09:21:23 +0200

    Separate MCMCIO classes into MetadataIO and samples IO (#2400)
    * rearrange mcmcio inheritance to avoid signature change in read_raw_samples
    
    * make the default temps in read_raw_samples 'all', while the default temps in parser 0
    
    * always make SingleTempMCMCIO.read_raw_samples return 2D array when flatten is False
    
    * make plot_acf work both with and without temps

--- pycbc/inference/io/base_multitemper.py
+++ pycbc/inference/io/base_multitemper.py
@@ -26,7 +26,7 @@
 
 from __future__ import absolute_import
 import argparse
-from .base_mcmc import MCMCIO
+from .base_mcmc import MCMCMetadataIO
 import numpy
 
 class ParseTempsArg(argparse.Action):
@@ -65,21 +65,44 @@ class ParseTempsArg(argparse.Action):
         setattr(namespace, self.dest, temps)
 
 
-class MultiTemperedMCMCIO(MCMCIO):
-    """Abstract base class for multi-tempered MCMC sampler IO.
+class MultiTemperedMetadataIO(MCMCMetadataIO):
+    """Adds support for reading/writing multi-tempered metadata to
+    MCMCMetadatIO.
     """
-
     @property
     def ntemps(self):
         """Returns the number of temperatures used by the sampler."""
         return self[self.sampler_group].attrs['ntemps']
 
     def write_sampler_metadata(self, sampler):
-        """Adds writing ntemps to MCMCIO.
+        """Adds writing ntemps to file.
         """
-        super(MultiTemperedMCMCIO, self).write_sampler_metadata(sampler)
+        super(MultiTemperedMetadataIO, self).write_sampler_metadata(sampler)
         self[self.sampler_group].attrs["ntemps"] = sampler.ntemps
 
+    @staticmethod
+    def extra_args_parser(parser=None, skip_args=None, **kwargs):
+        """Adds --temps to MCMCIO parser.
+        """
+        if skip_args is None:
+            skip_args = []
+        parser, actions = MCMCMetadataIO.extra_args_parser(
+            parser=parser, skip_args=skip_args, **kwargs)
+        if 'temps' not in skip_args:
+            act = parser.add_argument(
+                "--temps", nargs="+", default=0, action=ParseTempsArg,
+                help="Get the given temperatures. May provide either a "
+                     "sequence of integers specifying the temperatures to "
+                     "plot, or 'all' for all temperatures. Default is to only "
+                     "plot the coldest (= 0) temperature chain.")
+            actions.append(act)
+        return parser, actions
+
+
+class MultiTemperedMCMCIO(object):
+    """Provides functions for reading/writing samples from a parallel-tempered
+    MCMC sampler.
+    """
     def write_samples(self, samples, parameters=None,
                       start_iteration=None, max_iterations=None):
         """Writes samples to the given file.
@@ -144,7 +167,7 @@ class MultiTemperedMCMCIO(MCMCIO):
 
     def read_raw_samples(self, fields,
                          thin_start=None, thin_interval=None, thin_end=None,
-                         iteration=None, temps=None, walkers=None,
+                         iteration=None, temps='all', walkers=None,
                          flatten=True):
         """Base function for reading samples.
 
@@ -165,11 +188,9 @@ class MultiTemperedMCMCIO(MCMCIO):
             Only read the given iteration. If this provided, it overrides
             the ``thin_(start|interval|end)`` options.
         temps : 'all' or (list of) int, optional
-            The temperature index (or list of indices) to retrieve. If None,
-            only samples from the coldest (= 0) temperature chain will be
-            retrieved. To retrieve all temperates pass 'all', or a list of
-            all of the temperatures. Default is to only load the coldest
-            temperature.
+            The temperature index (or list of indices) to retrieve. To retrieve
+            all temperates pass 'all', or a list of all of the temperatures.
+            Default is 'all'.
         walkers : (list of) int, optional
             Only read from the given walkers. Default is to read all.
         flatten : bool, optional
@@ -195,10 +216,7 @@ class MultiTemperedMCMCIO(MCMCIO):
             nwalkers = self.nwalkers
         # temperatures to load
         selecttemps = False
-        if temps is None:
-            tidx = 0
-            ntemps = 1
-        elif isinstance(temps, int):
+        if isinstance(temps, int):
             tidx = temps
             ntemps = 1
         else:
@@ -238,21 +256,3 @@ class MultiTemperedMCMCIO(MCMCIO):
                 arr = arr.reshape((ntemps, nwalkers, niterations))
             arrays[name] = arr
         return arrays
-
-    @staticmethod
-    def extra_args_parser(parser=None, skip_args=None, **kwargs):
-        """Adds --temps to MCMCIO parser.
-        """
-        if skip_args is None:
-            skip_args = []
-        parser, actions = MCMCIO.extra_args_parser(
-            parser=parser, skip_args=skip_args, **kwargs)
-        if 'temps' not in skip_args:
-            act = parser.add_argument(
-                "--temps", nargs="+", default=None, action=ParseTempsArg,
-                help="Get the given temperatures. May provide either a "
-                     "sequence of integers specifying the temperatures to "
-                     "plot, or 'all' for all temperatures. Default is to only "
-                     "plot the coldest (= 0) temperature chain.")
-            actions.append(act)
-        return parser, actions
