daa8d96b402636b3a7606960da03628046d798e9
Alexander Harvey Nitz <alex.nitz@gmail.com>
2018-11-10 22:24:46 +0100

    more stuff

--- pycbc/inference/models/single_template.py
+++ pycbc/inference/models/single_template.py
@@ -19,15 +19,16 @@
 import numpy
 
 from pycbc import filter as pyfilter
-from pycbc.waveform import NoWaveformError
+from pycbc.waveform import NoWaveformError, get_fd_waveform
 from pycbc.types import Array
+from pycbc.detector import Detector
 
-from .base_data import BaseDataModel
+from .base import BaseModel
 
 # In this model we only calculate terms up to a constant.
 # We are primarily interested in the posterior result
 
-class SingleTemplate(BaseDataModel):
+class SingleTemplate(BaseModel):
     r"""Model that assumes we know all the intrinsic parameters.
 
     This model assumes we know all the intrinsic parameters, and are only
@@ -36,15 +37,38 @@ class SingleTemplate(BaseDataModel):
     """
     name = 'single_template'
 
-    def __init__(self, variable_params, data, waveform_generator,
-                 f_lower, psds=None, f_upper=None, norm=None,
-                 **kwargs):
+    def __init__(self, data, psds, f_lower, f_upper=None, **kwargs):
         # set up the boiler-plate attributes; note: we'll compute the
         # log evidence later
-        super(GaussianNoise, self).__init__(variable_params, data,
-                                            waveform_generator, **kwargs)
+        super(SingleTemplate, self).__init__(**kwargs)          
+        sample_rate = 32768
+               
+        # Generate template waveforms
+        df = data[data.keys()[0]].delta_f
+        hp, _ = get_fd_waveform(delta_f=df, **self.static_params)
  
-        pass
+        if f_upper is None:
+            f_upper = len(data[data.keys()[0]]-1) * df
+ 
+        # Extend data and template to high sample rate
+        flen = int(sample_rate / df) / 2 + 1
+        hp.resize(flen)
+        for ifo in data:
+            data[ifo].resize(flen)
+ 
+        # Calculate high sample rate SNR time series
+        self.sh = {}
+        self.hh = {}
+        self.det = {}
+        for ifo in data:
+            self.det[ifo] = Detector(ifo)
+            snr, _, norm = pyfilter.matched_filter_core(hp, data[ifo], 
+                                            psd=psds[ifo],                   
+                                            low_frequency_cutoff=f_lower, 
+                                            high_frequency_cutoff=f_upper)
+                                            
+            self.sh[ifo] = 4 * df * snr
+            self.hh[ifo] = - 2.0 * df / norm
 
     def _lognl(self):
         """Computes the log likelihood assuming the data is noise.
@@ -56,7 +80,7 @@ class SingleTemplate(BaseDataModel):
         # constant in this model.
         return 0
 
-    def _loglr(self):
+    def loglr(self):
         r"""Computes the log likelihood ratio,
 
         Returns
@@ -65,10 +89,24 @@ class SingleTemplate(BaseDataModel):
             The value of the log likelihood ratio.
         """
         # calculate <d-h|d-h> = <h|h> - 2<h|d> + <d|d> up to a constant
-        return 5.7
+        p = self.current_params
+        
+        vloglr = 0
+        for ifo in self.sh:
+            fp, fc = self.det[ifo].antenna_pattern(p['ra'], p['dec'],
+                                         p['polarization'], p['tc'])   
+            dt = self.det[ifo].time_delay_from_earth_center(p['ra'],
+                                         p['dec'], p['tc'])                          
+            ip = numpy.cos(p['inclination'])
+            ic = 0.5 * (1.0 + ip * ip)                     
+            htf = (fp * ip + 1.0j * fc * ic) / p['distance']
+            
+            vloglr += (self.sh[ifo].at_time(p['tc'] + dt) * htf * numpy.exp(1.0j*p['coa_phase'])).real
+            vloglr += self.hh[ifo] * abs(htf) ** 2.0     
+        return float(vloglr)
 
     def _loglikelihood(self):
         r"""Computes the log likelihood of the paramaters,
 
         """
-        return self.loglr
+        return self.loglr()
