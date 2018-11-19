d83cdad17a77e3f059affd6a6ba7b07a217e82f4
Alexander Harvey Nitz <alex.nitz@gmail.com>
2018-11-20 00:39:35 +0100

    ws

--- pycbc/inference/models/single_template.py
+++ pycbc/inference/models/single_template.py
@@ -40,38 +40,38 @@ class SingleTemplate(BaseModel):
     def __init__(self, data, psds, f_lower, f_upper=None, **kwargs):
         # set up the boiler-plate attributes; note: we'll compute the
         # log evidence later
-        super(SingleTemplate, self).__init__(**kwargs)          
+        super(SingleTemplate, self).__init__(**kwargs)
         sample_rate = 32768
-               
+
         # Generate template waveforms
         df = data[data.keys()[0]].delta_f
         p = self.static_params.copy()
         if 'distance' in p:
             p.pop('distance')
         hp, _ = get_fd_waveform(delta_f=df, distance=1, **p)
- 
+
         if f_upper is None:
             f_upper = len(data[data.keys()[0]]-1) * df
- 
+
         # Extend data and template to high sample rate
         flen = int(sample_rate / df) / 2 + 1
         hp.resize(flen)
         for ifo in data:
             data[ifo].resize(flen)
- 
+
         # Calculate high sample rate SNR time series
         self.sh = {}
         self.hh = {}
         self.det = {}
         for ifo in data:
             self.det[ifo] = Detector(ifo)
-            snr, _, norm = pyfilter.matched_filter_core(hp, data[ifo], 
-                                            psd=psds[ifo],                   
-                                            low_frequency_cutoff=f_lower, 
+            snr, _, _ = pyfilter.matched_filter_core(hp, data[ifo],
+                                            psd=psds[ifo],
+                                            low_frequency_cutoff=f_lower,
                                             high_frequency_cutoff=f_upper)
-                                            
+
             self.sh[ifo] = 4 * df * snr
-            self.hh[ifo] = - 1./2. * pyfilter.sigmasq(hp, psd=psds[ifo],
+            self.hh[ifo] = -0.5 * pyfilter.sigmasq(hp, psd=psds[ifo],
                                               low_frequency_cutoff=f_lower,
                                               high_frequency_cutoff=f_upper)
 
@@ -81,7 +81,7 @@ class SingleTemplate(BaseModel):
         Since this is a constant for Gaussian noise, this is only computed once
         then stored.
         """
-        # FIXME we won't bother calculating this for now since it is a 
+        # we won't bother calculating this for now since it is a
         # constant in this model.
         return 0
 
@@ -100,17 +100,17 @@ class SingleTemplate(BaseModel):
         vloglr = 0
         for ifo in self.sh:
             fp, fc = self.det[ifo].antenna_pattern(p['ra'], p['dec'],
-                                         p['polarization'], p['tc'])   
+                                         p['polarization'], p['tc'])
             dt = self.det[ifo].time_delay_from_earth_center(p['ra'],
-                                         p['dec'], p['tc'])                          
+                                         p['dec'], p['tc'])
             ip = numpy.cos(p['inclination'])
-            ic = 0.5 * (1.0 + ip * ip)                     
+            ic = 0.5 * (1.0 + ip * ip)
             htf = (fp * ip - 1.0j * fc * ic) / p['distance']
-            
+
             sh = abs((self.sh[ifo].at_time(p['tc'] + dt) * htf))
             vloglr += numpy.log(scipy.special.i0e(sh)) + sh
-            vloglr += self.hh[ifo] * abs(htf) ** 2.0     
-            
+            vloglr += self.hh[ifo] * abs(htf) ** 2.0
+
         return float(vloglr)
 
     def _loglikelihood(self):
