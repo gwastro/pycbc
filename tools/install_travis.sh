1827d917efd5d77efb45a4252df6d2d7e9935e41
Alexander Harvey Nitz <alex.nitz@ligo.org>
2019-01-15 14:55:17 +0100

    pegasus workaround

--- tools/install_travis.sh
+++ tools/install_travis.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 set -e
 
+# FIXME this is a workaround for a bug in psycopg2 2.6 (required by pegasus)
+# see e.g. https://stackoverflow.com/questions/47044854/error-installing-psycopg2-2-6-2
+# We should ask pegasus to update their requirement to psycopg2 2.7 which fixes
+# this bug
+echo -e "Trying to get rid of pg_config"
+sudo apt-get -y purge libpq-dev
+echo -e "Making sure it is really gone..."
+if [ -n "`which pg_config`" ]
+then
+    echo -e "...still here:"
+    which pg_config
+    sudo rm -f `which pg_config`
+else
+    echo -e "...seems gone"
+fi
+
 echo -e ">> [`date`] upgrading setuptools and pip"
 pip install --upgrade setuptools pip
 
