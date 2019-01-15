65948b66f2d7f49994547bfc5838bf214552b5e5
root <alex.nitz@ligo.org>
2019-01-15 17:44:34 +0100

    typo

--- tools/install_travis.sh
+++ tools/install_travis.sh
@@ -31,9 +31,9 @@ pip install .
 # FIXME, should be a way to make reduced package (with subset of data files)
 GIT_LFS_SKIP_SMUDGE=1 git clone https://git.ligo.org/lscsoft/lalsuite-extra
 cd lalsuite-extra
-git lfs pull -I data/lalsimulation/SEOBNRv2ROM_*.dat
-git lfs pull -I data/lalsimulation/*ChirpTime*.dat
-git lfs pull -I data/lalsimulation/SEOBNRv4ROM_v2.0.hdf5
+git lfs pull -I "data/lalsimulation/SEOBNRv2ROM_*.dat"
+git lfs pull -I "data/lalsimulation/*ChirpTime*.dat"
+git lfs pull -I "data/lalsimulation/SEOBNRv4ROM_v2.0.hdf5"
 mv data/lalsimulation/* ../
 cd ../
 
