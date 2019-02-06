8cb7d47246b5f417561ceb3dc4f082ab7c4acee8
Miriam Cabero <miriam.cabero@ligo.org>
2019-02-06 18:28:35 +0100

    Docstrings and a few codeclimate issues

--- pycbc/strain/lines.py
+++ pycbc/strain/lines.py
@@ -19,17 +19,47 @@
 import numpy
 from pycbc.types import TimeSeries, zeros
 
-def imag_median(complex_list):
-    a = numpy.median([complex_number.real \
+def complex_median(complex_list):
+    """ Get the median value of a list of complex numbers.
+
+    Parameters
+    ----------
+    complex_list: list
+        List of complex numbers to calculate the median.
+
+    Returns
+    -------
+    a + 1.j*b: complex number
+        The median of the real and imaginary parts.
+    """
+    median_real = numpy.median([complex_number.real
                      for complex_number in complex_list])
-    b = numpy.median([complex_number.imag \
+    median_imag = numpy.median([complex_number.imag
                      for complex_number in complex_list])
-    return a + 1.j*b
+    return median_real + 1.j*median_imag
 
 def avg_inner_product(data1, data2, bin_size):
-    # Calculate the time-domain inner product in each bin of duration bin_size
-    # and return the median from all bins to avoid outliers due to the presence
-    # of a signal in a particular bin
+    """ Calculate the time-domain inner product averaged over bins.
+
+    Parameters
+    ----------
+    data1: pycbc.types.TimeSeries
+        First data set.
+    data2: pycbc.types.TimeSeries
+        Second data set, with same duration and sample rate as data1.
+    bin_size: float
+        Duration of the bins the data will be divided into to calculate
+        the inner product.
+
+    Returns
+    -------
+    inner_prod: list
+        The (complex) inner product of data1 and data2 obtained in each bin.
+    amp: float
+        The absolute value of the median of the inner product.
+    phi: float
+        The angle of the median of the inner product.
+    """
     assert data1.duration == data2.duration
     assert data1.sample_rate == data2.sample_rate
     seglen = int(bin_size * data1.sample_rate)
@@ -38,17 +68,38 @@ def avg_inner_product(data1, data2, bin_size):
         start, end = idx * seglen, (idx+1) * seglen
         norm = len(data1[start:end])
         bin_prod = 2 * sum(data1.data[start:end].real *
-                    numpy.conjugate(data2.data[start:end])) / norm
+                           numpy.conjugate(data2.data[start:end])) / norm
         inner_prod.append(bin_prod)
 
-    inner_median = imag_median(inner_prod)
+    # Get the median over all bins to avoid outliers due to the presence
+    # of a signal in a particular bin.
+    inner_median = complex_median(inner_prod)
     return inner_prod, numpy.abs(inner_median), numpy.angle(inner_median)
 
 def line_model(freq, data, tref, amp=1, phi=0):
-    # Simple time-domain model for the frequency line,
-    # with the same duration as data.
-    # The returned data are complex to allow measuring the amplitude and phase
-    # of the strain data, for extracting purposes use only the real part
+    """ Simple time-domain model for a frequency line.
+
+    Parameters
+    ----------
+    freq: float
+        Frequency of the line.
+    data: pycbc.types.TimeSeries
+        Reference data, to get delta_t, start_time, duration and sample_times.
+    tref: float
+        Reference time for the line model.
+    amp: {1., float}, optional
+        Amplitude of the frequency line.
+    phi: {0. float}, optional
+        Phase of the frequency line (radians).
+
+    Returns
+    -------
+    freq_line: pycbc.types.TimeSeries
+        A timeseries of the line model with frequency 'freq'. The returned
+        data are complex to allow measuring the amplitude and phase of the
+        corresponding frequency line in the strain data. For extraction, use
+        only the real part of the data.
+    """
     freq_line = TimeSeries(zeros(len(data)), delta_t=data.delta_t,
                            epoch=data.start_time)
 
@@ -59,6 +110,25 @@ def line_model(freq, data, tref, amp=1, phi=0):
     return freq_line
 
 def matching_line(freq, data, tref, bin_size=1):
+    """ Find the parameter of the line with frequency 'freq' in the data.
+
+    Parameters
+    ----------
+    freq: float
+        Frequency of the line to find in the data.
+    data: pycbc.types.TimeSeries
+        Data from which the line wants to be measured.
+    tref: float
+        Reference time for the frequency line.
+    bin_size: {1, float}, optional
+        Duration of the bins the data will be divided into for averaging.
+
+    Returns
+    -------
+    line_model: pycbc.types.TimeSeries
+        A timeseries containing the frequency line with the amplitude
+        and phase measured from the data.
+    """
     template_line = line_model(freq, data, tref=tref)
     # Measure amplitude and phase of the line in the data
     _, amp, phi = avg_inner_product(data, template_line,
@@ -66,8 +136,22 @@ def matching_line(freq, data, tref, bin_size=1):
     return line_model(freq, data, tref=tref, amp=amp, phi=phi)
 
 def calibration_lines(freqs, data, tref=None):
-    # Calibration lines are stable and the frequency is well know, so we use
-    # a different (simpler) method to remove them. Give freqs as a list
+    """ Extract the calibration lines from strain data.
+
+    Parameters
+    ----------
+    freqs: list
+        List containing the frequencies of the calibration lines.
+    data: pycbc.types.TimeSeries
+        Strain data to extract the calibration lines from.
+    tref: {None, float}, optional
+        Reference time for the line. If None, will use data.start_time.
+
+    Returns
+    -------
+    data: pycbc.types.TimeSeries
+        The strain data with the calibration lines removed.
+    """
     if tref is None:
         tref = float(data.start_time)
     for freq in freqs:
@@ -78,10 +162,28 @@ def calibration_lines(freqs, data, tref=None):
     return data
 
 def clean_data(freqs, data, chunk, avg_bin):
-    # In general, noise lines are time-varying (wandering). To account for this
-    # time variation, divide data into chunks of size chunk > bin_size
-    # (bin_size is the segment duration for averaging the inner product)
-    tref = float(data.start_time)
+    """ Extract time-varying (wandering) lines from strain data.
+
+    Parameters
+    ----------
+    freqs: list
+        List containing the frequencies of the wandering lines.
+    data: pycbc.types.TimeSeries
+        Strain data to extract the wandering lines from.
+    chunk: float
+        Duration of the chunks the data will be divided into to account
+        for the time variation of the wandering lines. Should be smaller
+        than data.duration, and allow for at least a few chunks.
+    avg_bin: float
+        Duration of the bins each chunk will be divided into for averaging
+        the inner product when measuring the parameters of the line. Should
+        be smaller than chunk.
+
+    Returns
+    -------
+    data: pycbc.types.TimeSeries
+        The strain data with the wandering lines removed.
+    """
     if avg_bin >= chunk:
         raise ValueError('The bin size for averaging the inner product '
                          'must be less than the chunk size.')
@@ -91,6 +193,7 @@ def clean_data(freqs, data, chunk, avg_bin):
     steps = numpy.arange(0, int(data.duration/chunk)-0.5, 0.5)
     seglen = chunk * data.sample_rate
 
+    tref = float(data.start_time)
     for freq in freqs:
         for step in steps:
             start, end = int(step*seglen), int((step+1)*seglen)
