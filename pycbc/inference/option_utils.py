gwastro/pycbc

commit f2cca127c4bf20db9889b13cf73a8d551ea20c87

Load constraints when reading prior from config for plotting (#2562)
* Load constraints when reading prior from config

* Use functions from distributions module for reading in config

* Add/edit comments

* Code climate

* Made prior the default sections and addressed code climate

pycbc/inference/option_utils.py
@@ -465,7 +465,7 @@ def add_density_option_group(parser):
     return density_group
 
 
-def prior_from_config(cp, sections=None):
+def prior_from_config(cp, sections='prior'):
     """Loads a prior distribution from the given config file.
 
     Parameters
@@ -481,26 +481,15 @@ def prior_from_config(cp, sections=None):
     distributions.JointDistribution
         The prior distribution.
     """
-    # get prior distribution for each variable parameter
-    # parse command line values for section and subsection
-    # if only section then look for subsections
-    # and add distributions to list
-    variable_params = []
-    dists = []
-    if sections is None:
-        sections = ['prior']
-    for sec in sections:
-        section = sec.split("-")[0]
-        subsec = sec.split("-")[1:]
-        if len(subsec) > 0:
-            subsections = ["-".join(subsec)]
-        else:
-            subsections = cp.get_subsections(section)
-        for subsection in subsections:
-            name = cp.get_opt_tag(section, "name", subsection)
-            dist = distributions.distribs[name].from_config(
-                cp, section, subsection)
-            variable_params += dist.params
-            dists.append(dist)
+    # Read variable and static parameters from the config file
+    variable_params, _ = distributions.read_params_from_config(
+        cp, prior_section=sections, vargs_section='variable_params',
+        sargs_section='static_params')
+    # Read constraints to apply to priors from the config file
+    constraints = distributions.read_constraints_from_config(cp)
+    # Get PyCBC distribution instances for each variable parameter in the
+    # config file
+    dists = distributions.read_distributions_from_config(cp, sections)
     # construct class that will return draws from the prior
-    return distributions.JointDistribution(variable_params, *dists)
+    return distributions.JointDistribution(variable_params, *dists,
+                                           **{"constraints": constraints})

commit b08bd461a808aa91347b1ac0074e4fa10069abba

This update corrects an error caused by pull request #2562.  It changes parser.add_argument from default = ["prior"] to default="prior".  It also changes the option name from - (#2586)
-sections to --prior-section

bin/inference/pycbc_inference_plot_prior
@@ -66,7 +66,7 @@ parser.add_argument("--parameters", nargs="+", action=ParseParametersArg,
                          "property of that parameter will be used. If not "
                          "provided, will plot all of the parameters in the "
                          "[variable_params] section of the config file.")
-parser.add_argument("--sections", type=str, nargs="+", default=["prior"],
+parser.add_argument("--prior-section", type=str, default="prior",
                     help="Name of section with distribution configurations. "
                          "Default is 'prior'.")
 # add output options
@@ -92,7 +92,7 @@ logging.info("Reading configuration files")
 cp = WorkflowConfigParser(opts.config_files)
 
 logging.info("Loading prior")
-prior = prior_from_config(cp, sections=opts.sections)
+prior = prior_from_config(cp, prior_section=opts.prior_section)
 logging.info("Drawing samples from prior")
 samples = prior.rvs(opts.nsamples)
 

pycbc/inference/option_utils.py
@@ -465,7 +465,7 @@ def add_density_option_group(parser):
     return density_group
 
 
-def prior_from_config(cp, sections='prior'):
+def prior_from_config(cp, prior_section='prior'):
     """Loads a prior distribution from the given config file.
 
     Parameters
@@ -483,13 +483,13 @@ def prior_from_config(cp, sections='prior'):
     """
     # Read variable and static parameters from the config file
     variable_params, _ = distributions.read_params_from_config(
-        cp, prior_section=sections, vargs_section='variable_params',
+        cp, prior_section=prior_section, vargs_section='variable_params',
         sargs_section='static_params')
     # Read constraints to apply to priors from the config file
     constraints = distributions.read_constraints_from_config(cp)
     # Get PyCBC distribution instances for each variable parameter in the
     # config file
-    dists = distributions.read_distributions_from_config(cp, sections)
+    dists = distributions.read_distributions_from_config(cp, prior_section)
     # construct class that will return draws from the prior
     return distributions.JointDistribution(variable_params, *dists,
                                            **{"constraints": constraints})