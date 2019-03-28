45a00037aa59eaa97652479b5b51a6518b6aea8b
Alexander Harvey Nitz <alex.nitz@ligo.org>
2019-03-28 14:15:40 +0100

    set back to master

--- tools/test_coinc_search_workflow.sh
+++ tools/test_coinc_search_workflow.sh
@@ -2,8 +2,13 @@
 set -e
 echo -e "\\n>> [`date`] Testing pycbc_make_coinc_search_workflow"
 
-#VENV_PATH=${1}
-TRAVIS_TAG=master
+VENV_PATH=${1}
+TRAVIS_TAG=${2}
+
+if [ "x${VENV_PATH}" == "x" ] ; then
+  echo -e "\\n>> [`date`] Error: VENV_PATH was not passed to script or is empty"
+  exit 1
+fi
 
 if [ "x${TRAVIS_TAG}" == "x" ] ; then
   echo -e "\\n>> [`date`] Error: TRAVIS_TAG was not passed to script or is empty"
@@ -11,7 +16,7 @@ if [ "x${TRAVIS_TAG}" == "x" ] ; then
 fi
 
 echo -e "\\n>> [`date`] Entering virtual environment $VENV_PATH"
-#source ${VENV_PATH}/bin/activate
+source ${VENV_PATH}/bin/activate
 
 CONFIG_PATH="https://raw.githubusercontent.com/ligo-cbc/pycbc-config/${TRAVIS_TAG}/test"
 echo -e "\\n>> [`date`] Using config files from ${CONFIG_PATH}"
