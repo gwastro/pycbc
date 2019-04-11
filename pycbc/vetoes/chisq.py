a6b3ad4c9cd32a0fd23557ab31af0418252c63a2
Ian Harry <ian.harry@ligo.org>
2019-04-11 12:20:51 +0100

    Undo this again

--- pycbc/vetoes/chisq.py
+++ pycbc/vetoes/chisq.py
@@ -133,7 +133,7 @@ def power_chisq_at_points_from_precomputed(corr, snr, snr_norm, bins, indices):
     """
     num_bins = len(bins) - 1
     chisq = shift_sum(corr, indices, bins) # pylint:disable=assignment-from-no-return
-    return (chisq * num_bins - (snr.conj() * snr).real()) * (snr_norm ** 2.0)
+    return (chisq * num_bins - (snr.conj() * snr).real) * (snr_norm ** 2.0)
 
 _q_l = None
 _qtilde_l = None
