a832486d3d263cb15df98497fd7b3f9811e57124
Thomas Dent <thomas.dent@usc.es>
2019-04-17 00:46:43 +0200

    Update coinc_rate.py
    few edits to comments

--- pycbc/events/coinc_rate.py
+++ pycbc/events/coinc_rate.py
@@ -21,31 +21,32 @@ def multiifo_noise_coinc_rate(rates, slop):
     Parameters
     ----------
     rates: dict
-        Dictionary keyed on interferometer
-        Value is a vector of single-detector trigger rates
+        Dictionary keyed on ifo string
+        Value is a sequence of single-detector trigger rates, units assumed 
+        to be Hz
     slop: float
-        time to be added to time-of-flights between detectors to account
+        time added to maximum time-of-flight between detectors to account
         for timing error
 
     Returns
     -------
     expected_coinc_rates: dict
-        A dictionary of ifo combined rates keyed on the combination string
-        value is the expected coincidence rate in the combination given the
-        individual detector trigger rates
+        Dictionary keyed on the ifo combination string
+        Value is expected coincidence rate in the combination, units Hz
     """
     ifos = numpy.array(sorted(rates.keys()))
     rates_raw = list(rates[ifo] for ifo in ifos)
     expected_coinc_rates = {}
+    
     # Calculate coincidence for all-ifo combination
-    # multiply the product of rates and by the overlap time
+    # multiply product of trigger rates by the overlap time
     allowed_area = multiifo_noise_coincident_area(ifos, slop)
-
     rateprod = [numpy.prod(rs) for rs in zip(*rates_raw)]
     ifostring = ' '.join(ifos)
     expected_coinc_rates[ifostring] = allowed_area * numpy.array(rateprod)
+    
     # if more than one possible coincidence type exists,
-    # calculate coincidences for subsets through recursion
+    # calculate coincidence for subsets through recursion
     if len(ifos) > 2:
         # Calculate rate for each 'miss-one-out' detector combination
         subsets = itertools.combinations(ifos, len(ifos) - 1)
@@ -70,21 +71,21 @@ def multiifo_noise_coincident_area(ifos, slop):
     Parameters
     ----------
     ifos: list of strings
-        list of interferometers for area calculation
+        list of interferometers
     slop: float
-        extra time to add on to time-of-flight for timing error
+        extra time to add to maximum time-of-flight for timing error
 
     Returns
     -------
     allowed_area: float
-        area in units of seconds^(n_ifos-1) that the coincident values
-        can fall in
+        area in units of seconds^(n_ifos-1) that coincident values can fall in
     """
     # set up detector objects
     dets = {}
     for ifo in ifos:
         dets[ifo] = pycbc.detector.Detector(ifo)
     n_ifos = len(ifos)
+    
     if n_ifos == 2:
         allowed_area = 2 * (dets[ifos[0]].light_travel_time_to_detector(
                                                         dets[ifos[1]]) + slop)
@@ -117,14 +118,15 @@ def multiifo_signal_coincident_area(ifos):
     Parameters
     ----------
     ifos: list of strings
-        list of interferometers for area calculation
+        list of interferometers
 
     Returns
     -------
     allowed_area: float
-        area in units of seconds^(n_ifos-1) that coincident signals will take
+        area in units of seconds^(n_ifos-1) which coincident signals will occupy
     """
     n_ifos = len(ifos)
+    
     if n_ifos == 2:
         det0 = pycbc.detector.Detector(ifos[0])
         det1 = pycbc.detector.Detector(ifos[1])
@@ -144,7 +146,7 @@ def multiifo_signal_coincident_area(ifos):
             det1 = dets[ifos[ifo2_num[i]]]
             tofs[i] = det0.light_travel_time_to_detector(det1)
 
-        # combine these to calculate allowed area
+        # calculate allowed area
         phi_12 = numpy.arccos((tofs[0]**2 + tofs[1]**2 -
                                tofs[2]**2) / (2 * tofs[0] * tofs[1]))
         allowed_area = numpy.pi * tofs[0] * tofs[1] * numpy.sin(phi_12)
