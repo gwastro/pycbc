07b1214570621daf02ec0b7dd9ca25dca10347c4
Ian Harry <iwharry@googlemail.com>
2019-04-17 16:29:20 +0100

    Set back to dev (#2649)

--- setup.py
+++ setup.py
@@ -118,8 +118,8 @@ def get_version_info():
         vinfo = _version_helper.generate_git_version_info()
     except:
         vinfo = vdummy()
-        vinfo.version = '1.13.6'
-        vinfo.release = 'True'
+        vinfo.version = '1.13.dev7'
+        vinfo.release = 'False'
 
     with open('pycbc/version.py', 'w') as f:
         f.write("# coding: utf-8\n")
