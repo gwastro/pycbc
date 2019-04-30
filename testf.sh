6066e67e1a274c29b674922d3157d4d52b897bae
Alexander Harvey Nitz <alex.nitz@ligo.org>
2019-04-30 20:07:00 +0200

    add approximant heuristic

--- testf.sh
+++ testf.sh
@@ -1,10 +1,16 @@
 python bin/pycbc_optimize_cbc \
---trigger-time 1126259462.44 \
+--trigger-time 1187008882.43 \
 --instruments H1 L1 \
 --frame-type LOSC  \
 --verbose \
 --pad-data 8 \
---template-parameters approximant:IMRPhenomD mass1:29 mass2:36 f_lower:25 \
---channel-name L1:LOSC-STRAIN H1:LOSC-STRAIN \
+--autogating-pad 1.0 \
+--autogating-width 0.25 \
+--autogating-taper 0.25 \
+--autogating-threshold 50 \
+--template-parameters mass1:1.37 mass2:1.37 f_lower:25 \
+--channel-name L1:GWOSC-16KHZ_R1_STRAIN H1:GWOSC-16KHZ_R1_STRAIN \
 --strain-high-pass 15.0 \
 --sample-rate 2048
+
+#--trigger-time 1126259462.44 \
