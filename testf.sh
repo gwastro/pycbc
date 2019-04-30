1ec589bbe7420d1fae3977567721dce3a837aa5d
Alexander Harvey Nitz <alex.nitz@ligo.org>
2019-04-30 19:19:53 +0200

    update script with maximization routine and bounds heuristic

--- testf.sh
+++ testf.sh
@@ -1,10 +1,10 @@
 python bin/pycbc_optimize_cbc \
---trigger-time 1126259462 \
+--trigger-time 1126259462.44 \
 --instruments H1 L1 \
 --frame-type LOSC  \
 --verbose \
 --pad-data 8 \
---template-parameters approximant:IMRPhenomD mass1:10 mass2:10 f_lower:25 \
+--template-parameters approximant:IMRPhenomD mass1:29 mass2:36 f_lower:25 \
 --channel-name L1:LOSC-STRAIN H1:LOSC-STRAIN \
 --strain-high-pass 15.0 \
 --sample-rate 2048
