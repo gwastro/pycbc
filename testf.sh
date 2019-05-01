aa0a38ed562c1b5e5a383660196803c352f37c41
Alexander Harvey Nitz <alex.nitz@gmail.com>
2019-05-01 17:42:40 +0200

    restructure to allow multiple trigger processing and time slides

--- testf.sh
+++ testf.sh
@@ -1,6 +1,9 @@
 python bin/pycbc_optimize_cbc \
+\
 --trigger-time 1187008882.43 \
 --instruments H1 L1 \
+--template-parameters mass1:1.37 mass2:1.37 f_lower:25 \
+\
 --frame-type LOSC  \
 --verbose \
 --pad-data 8 \
@@ -8,7 +11,6 @@ python bin/pycbc_optimize_cbc \
 --autogating-width 0.25 \
 --autogating-taper 0.25 \
 --autogating-threshold 50 \
---template-parameters mass1:1.37 mass2:1.37 f_lower:25 \
 --channel-name L1:GWOSC-16KHZ_R1_STRAIN H1:GWOSC-16KHZ_R1_STRAIN \
 --strain-high-pass 15.0 \
 --sample-rate 2048
