a031a64e42aa268e0171d321b3beb052652d3330
Alexander Harvey Nitz <alex.nitz@ligo.org>
2019-05-02 18:28:04 +0200

    update

--- testf.sh
+++ testf.sh
@@ -4,15 +4,13 @@ python bin/pycbc_optimize_cbc \
 --instruments H1 L1 \
 --template-parameters mass1:1.37 mass2:1.37 f_lower:25 \
 \
---frame-type LOSC  \
---verbose \
---pad-data 8 \
 --autogating-pad 1.0 \
 --autogating-width 0.25 \
 --autogating-taper 0.25 \
 --autogating-threshold 50 \
+\
+--hdf-store L1:L1.hdf H1:H1.hdf  \
 --channel-name L1:GWOSC-16KHZ_R1_STRAIN H1:GWOSC-16KHZ_R1_STRAIN \
+--low-frequency-cutoff 20.0 \
 --strain-high-pass 15.0 \
---sample-rate 2048
-
-#--trigger-time 1126259462.44 \
+--verbose
