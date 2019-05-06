d2218ca4515f416a0083913a51bca8930655c864
Thomas Dent <thomas.dent@usc.es>
2019-05-06 14:37:53 +0200

    simplify logic for approximant default

--- pycbc/events/triggers.py
+++ pycbc/events/triggers.py
@@ -283,11 +283,11 @@ def get_param(par, args, m1, m2, s1z, s2z):
     elif par in ['chi_eff', 'effective_spin']:
         parvals = conversions.chi_eff(m1, m2, s1z, s2z)
     elif par == 'template_duration':
-        if not hasattr(args, 'approximant'):
-            args.approximant = None
         # default to SEOBNRv4 duration function
+        if not hasattr(args, 'approximant') or args.approximant == None:
+            args.approximant = "SEOBNRv4"
         parvals = pnutils.get_imr_duration(m1, m2, s1z, s2z, args.f_lower,
-                                           args.approximant or "SEOBNRv4")
+                                           args.approximant)
         if args.min_duration:
             parvals += args.min_duration
     elif par == 'tau0':
