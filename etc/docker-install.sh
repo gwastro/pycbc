72ea44c6d50d1a676112914e8652ef56b9cf020b
Duncan Brown <dabrown@syr.edu>
2019-06-04 12:03:14 -0700

    fixed typo in install script

--- etc/docker-install.sh
+++ etc/docker-install.sh
@@ -8,4 +8,6 @@ mkdir -p /opt/pycbc/pycbc-software/share/lal-data
 mount /cvmfs/config-osg.opensciencegrid.org
 mount /cvmfs/oasis.opensciencegrid.org
 rsync --exclude='SEOBNRv1ROM*' --exclude='SEOBNRv2ROM_DS_HI_v1.0.hdf5' --exclude='NRSur4d2s_FDROM.hdf5' -ravz /cvmfs/oasis.opensciencegrid.org/ligo/sw/pycbc/lalsuite-extra/current/share/lalsimulation/ /opt/pycbc/pycbc-software/share/lal-data/
+umount /cvmfs/config-osg.opensciencegrid.org
+umount /cvmfs/oasis.opensciencegrid.org
 exit 0
