dcdf37609febd2053e1be63cf7177b7ffff834d8
Ian Harry <ian.harry@ligo.org>
2019-06-04 12:29:53 -0700

    Run trigger "consolidation" every N templates

--- pycbc/events/eventmgr.py
+++ pycbc/events/eventmgr.py
@@ -340,6 +340,10 @@ class EventManager(object):
         self.accumulate.append(self.template_events)
         self.template_events = numpy.array([], dtype=self.event_dtype)
 
+    def consolidate_events(self):
+        self.events = numpy.concatenate(self.accumulate)
+        self.accumulate = [self.events]
+
     def finalize_events(self):
         self.events = numpy.concatenate(self.accumulate)
 
