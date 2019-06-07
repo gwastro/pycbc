ead345a3f1cd9bf4e2367e294ede3c9f95125576
Alexander Harvey Nitz <alex.nitz@gmail.com>
2019-06-07 23:01:57 +0200

    note p3 support

--- setup.py
+++ setup.py
@@ -255,6 +255,7 @@ setup (
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
         'Intended Audience :: Science/Research',
         'Natural Language :: English',
         'Topic :: Scientific/Engineering',
