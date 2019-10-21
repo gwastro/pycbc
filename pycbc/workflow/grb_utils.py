42a2e2430f51310194e212b8e3900b6846bc517f
Andrew Williamson <andrew.williamson@ligo.org>
2019-10-21 05:39:24 -0700

    More detailed description for GRB sky error func
    * Also included a new argument to allow for different probability
    coverage. Default is the same as old hardcoded value of 90%.
    * Updating Fermi-GBM systematics for GRB sky grids
    * updating Changes to grb_utils so get_sky_grid_error is correctly called
    
    Removing precision argument no longer needed in get_sky_grid_scale
    
    remove whitespace in grb_utils
    
    Fixing indent bug caused by GitHub web interface

--- pycbc/workflow/grb_utils.py
+++ pycbc/workflow/grb_utils.py
@@ -280,9 +280,61 @@ def make_gating_node(workflow, datafind_files, outdir=None, tags=None):
     return condition_strain_nodes, condition_strain_outs
 
 
-def get_sky_grid_scale(sky_error, sigma_sys=6.8359):
+def get_sky_grid_scale(sky_error=0.0, Fermi=False, upscale=False, 
+		       core_sigma=3.6, core_frac=0.98, tail_sigma=29.6, 
+		       containment=0.9):
     """
-    Calculate suitable 3-sigma radius of the search patch, incorporating Fermi
-    GBM systematic if necessary.
+    Calculate the angular radius corresponding to a desired localization
+    uncertainty level. This is used to generate the search grid and involves
+    scaling up the standard 1-sigma value provided to the workflow, assuming
+    a normal probability profile. Fermi systematic errors can be included,
+    following https://arxiv.org/abs/1909.03006, with default values valid
+    before 11 September 2019. The default probability coverage is 90%.
+
+    Parameters
+    ----------
+    sky_error : float
+        The reported statistical 1-sigma sky error of the trigger.
+    Fermi : bool
+        Whether to apply Fermi-GBM systematics. Default = False.
+    upscale : bool
+        Whether to apply rescale to convert from 1 sigma -> containment
+        for non-Fermi triggers. Default = True as Swift reports 90% 
+        radius directly.
+    core_sigma : float
+        Size of the GBM systematic core component.
+    core_frac : float
+        Fraction of the systematic uncertainty contained within the core
+        component.
+    tail_sigma : float
+        Size of the GBM systematic tail component.
+    containment : float
+        The desired localization probability to be covered by the sky grid.
+
+    Returns
+    _______
+
+    float
+        Sky error radius in degrees.
     """
-    return 1.65 * (sky_error**2 + sigma_sys**2)**0.5
+    if Fermi:
+        r = np.linspace(sky_error*0.5, sky_error*4.0, precision)
+        sky_error /= np.sqrt(-2 * np.log(0.32))
+        tail_frac = 1.0 - core_frac
+        s1 = np.sqrt(sky_error**2 + core_sigma**2)
+        s2 = np.sqrt(sky_error**2 + tail_sigma**2)
+        part1 = core_frac * (1 - np.exp(-0.5 * (r / s1)**2))
+        part2 = tail_frac * (1 - np.exp(-0.5 * (r / s2)**2))
+        return r[(np.abs(part1 + part2 - containment)).argmin()]
+
+    else:
+        # Use Rayleigh distribution to go from 1 sigma containment to containment
+        # given by function variable. interval method returns bounds of equal
+        # probability about the median, but we want 1-sided bound, hence
+        # use (2 * containment - 1)
+        if upscale:
+            from scipy.stats import rayleigh
+            scale = rayleigh.interval(2 * containment - 1)[-1]
+        else:
+            scale = 1.0
+        return scale * sky_error
