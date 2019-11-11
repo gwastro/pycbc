7f5fe6bc98420da60ab6c266ff38bbda45d94e94
Collin Capano <collin.capano@aei.mpg.de>
2019-11-11 14:39:33 +0000

    delay applying waveform transforms to when the likelihood is actually called

--- pycbc/inference/models/base.py
+++ pycbc/inference/models/base.py
@@ -479,7 +479,7 @@ class BaseModel(object):
         """
         return self._current_stats.getstatsdict(self.default_stats)
 
-    def _trytoget(self, statname, fallback, **kwargs):
+    def _trytoget(self, statname, fallback, apply_transforms=False, **kwargs):
         """Helper function to get a stat from ``_current_stats``.
 
         If the statistic hasn't been calculated, ``_current_stats`` will raise
@@ -493,6 +493,9 @@ class BaseModel(object):
             The stat to get from ``current_stats``.
         fallback : method of self
             The function to call if the property call fails.
+        apply_transforms : bool, optional
+            Apply waveform transforms to the current parameters before calling
+            the fallback function. Default is False.
         \**kwargs :
             Any other keyword arguments are passed through to the function.
 
@@ -504,6 +507,11 @@ class BaseModel(object):
         try:
             return getattr(self._current_stats, statname)
         except AttributeError:
+            # apply waveform transforms if requested
+            if apply_transforms and self.waveform_transforms is not None:
+                self._current_params = transforms.apply_transforms(
+                    self._current_params, self.waveform_transforms,
+                    inverse=False)
             val = fallback(**kwargs)
             setattr(self._current_stats, statname, val)
             return val
@@ -516,7 +524,8 @@ class BaseModel(object):
         If that raises an ``AttributeError``, will call `_loglikelihood`` to
         calculate it and store it to ``current_stats``.
         """
-        return self._trytoget('loglikelihood', self._loglikelihood)
+        return self._trytoget('loglikelihood', self._loglikelihood,
+                              apply_transforms=True)
 
     @abstractmethod
     def _loglikelihood(self):
@@ -612,7 +621,7 @@ class BaseModel(object):
         return p0
 
     def _transform_params(self, **params):
-        """Applies all transforms to the given params.
+        """Applies sampling transforms and boundary conditions to some params.
 
         Parameters
         ----------
@@ -628,11 +637,6 @@ class BaseModel(object):
         # variable args
         if self.sampling_transforms is not None:
             params = self.sampling_transforms.apply(params, inverse=True)
-        # apply waveform transforms
-        if self.waveform_transforms is not None:
-            params = transforms.apply_transforms(params,
-                                                 self.waveform_transforms,
-                                                 inverse=False)
         # apply boundary conditions
         params = self.prior_distribution.apply_boundary_conditions(**params)
         return params
