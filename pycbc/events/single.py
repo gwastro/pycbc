3efc41395401560bb2314b237c2d248a572895b3
Gareth Davies <gareth.davies@ligo.org>
2019-11-15 09:21:52 -0800

    codeclimate fixes

--- pycbc/events/single.py
+++ pycbc/events/single.py
@@ -1,8 +1,8 @@
 """ utilities for assigning FAR to single detector triggers
 """
-from pycbc.events import ranking, trigger_fits as fits
+from pycbc.events import ranking
 from pycbc.types import MultiDetOptionAction
-from pycbc import bin_utils, conversions as conv
+from pycbc import conversions as conv
 
 class LiveSingle(object):
     def __init__(self, ifo,
@@ -55,11 +55,12 @@ class LiveSingle(object):
                           for k in triggers}
             fake_coinc['foreground/stat'] = nsnr
             fake_coinc['foreground/ifar'] = calculate_ifar(triggers, nsnr, ifo,
-                                                fixed_ifar=self.fixed_ifar,
-                                                conservative)
+                                                           fixed_ifar=self.fixed_ifar,
+                                                           conservative=conservative)
             fake_coinc['HWINJ'] = data_reader.near_hwinj()
             return fake_coinc
         return None
+
 
 class LiveSingleFarThreshold(LiveSingle):
     def __init__(self, ifo,
@@ -68,10 +69,11 @@ class LiveSingleFarThreshold(LiveSingle):
                  duration_threshold=0,
                  fixed_ifar=0):
         super(LiveSingleFarThreshold).__init__(self, ifo,
-                 newsnr_threshold=10.0,
-                 reduced_chisq_threshold=5,
-                 duration_threshold=0)
+              newsnr_threshold=10.0,
+              reduced_chisq_threshold=5,
+              duration_threshold=0)
         self.fixed_ifar = fixed_ifar
+
 
 class LiveSingleCalculateIfar(LiveSingle):
     def __init__(self, ifo,
@@ -79,27 +81,29 @@ class LiveSingleCalculateIfar(LiveSingle):
                  reduced_chisq_threshold=5,
                  duration_threshold=0):
         super(LiveSingleFarThreshold).__init__(self, ifo,
-                 newsnr_threshold=10.0,
-                 reduced_chisq_threshold=5,
-                 duration_threshold=0)
+              newsnr_threshold=10.0,
+              reduced_chisq_threshold=5,
+              duration_threshold=0)
         self.fixed_ifar = False
 
+
 single_fits_coeff_count_time = {
-    'H1' : (5.69015, 107893373, 10184192),
-    'L1' : (5.72713,  95714610, 10908800),
-    'V1' : (5.58611, 112008750, 11468656),
-    'conservative' : (5.03662, 28909432, 10184192)
+    'H1': (5.69015, 107893373, 10184192),
+    'L1': (5.72713,  95714610, 10908800),
+    'V1': (5.58611, 112008750, 11468656),
+    'conservative': (5.03662, 28909432, 10184192)
 }
 
 def calculate_ifar(triggers, newsnr, ifo, fit_threshold=6.0,
                    fixed_ifar=False, conservative=False):
-    if fixed_ifar: return fixed_ifar
+    if fixed_ifar:
+        return fixed_ifar
     # Fit coefficients for each ifo based on O3a trigger fits files
-    # 1 / (total counts=1.6e7 * exp(-alpha=5.7 * (SNR=9.776 - thresh=6) / number_templates=200000 / live_time=1.6e6) and convert to years
     if conservative:
         cct_to_use = 'conservative'
     else:
         cct_to_use = ifo
     cct = single_fits_coeff_count_time[cct_to_use]
-    n_louder = cct[1] * fits.cum_fit('exponential', newsnr, cct[0], fit_threshold)
+    n_louder = cct[1] * fits.cum_fit('exponential', newsnr, cct[0],
+                                     fit_threshold)
     return conv.sec_to_year(cct[2] / (n_louder))
