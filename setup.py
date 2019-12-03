081d04061f34e6c27aa00568d852c6768560807e
Ian Harry <ian.harry@ligo.org>
2019-12-03 22:24:04 +0000

    Prepare for release (#3039)

--- setup.py
+++ setup.py
@@ -121,8 +121,8 @@ def get_version_info():
         vinfo = _version_helper.generate_git_version_info()
     except:
         vinfo = vdummy()
-        vinfo.version = '1.14.dev5'
-        vinfo.release = 'False'
+        vinfo.version = '1.15.0'
+        vinfo.release = 'True'
 
     with open('pycbc/version.py', 'w') as f:
         f.write("# coding: utf-8\n")
