bd91e3f2c6ba726b988e11f1b6083284ccbeab2c
jsantoro392 <canevasantoro.1490713@studenti.uniroma1.it>
2019-12-10 14:46:37 +0000

    removed whitespaces and shortened lines

--- pycbc/tmpltbank/coord_utils.py
+++ pycbc/tmpltbank/coord_utils.py
@@ -264,9 +264,9 @@ def get_random_mass(numPoints, massRangeParams):
 
             # Keep a point if:
             # 1) the secondary object is a not a BH (mass2 < boundary mass)
-            #    [Store masses and spins of non BBHs]        
+            #    [Store masses and spins of non BBHs]       
             mask_not_bbh[mass2 < boundary_mass] = True
-            mass1_not_bbh= mass1[mask_not_bbh]
+            mass1_not_bbh = mass1[mask_not_bbh]
             mass2_not_bbh = mass2[mask_not_bbh]
             spin1z_not_bbh = spin1z[mask_not_bbh]
             spin2z_not_bbh = spin2z[mask_not_bbh]
@@ -276,34 +276,40 @@ def get_random_mass(numPoints, massRangeParams):
             mask_nsbh[mass1_not_bbh > boundary_mass] = True
             #    [mask_bns identifies BNS systems]
             mask_bns = ~mask_nsbh
-            #    [Store masses and spins of BNSs] 
+            #    [Store masses and spins of BNSs]
             mass1_bns = mass1_not_bbh[mask_bns]
             mass2_bns = mass2_not_bbh[mask_bns]
             spin1z_bns = spin1z_not_bbh[mask_bns]
             spin2z_bns = spin2z_not_bbh[mask_bns]
             # 3) ...it is an NS-BH with remnant mass greater than the threshold
             #    required to have a counterpart
-            #    [Store masses and spins of all NSBHs] 
+            #    [Store masses and spins of all NSBHs]
             mass1_nsbh = mass1_not_bbh[mask_nsbh]
             mass2_nsbh = mass2_not_bbh[mask_nsbh]
             spin1z_nsbh = spin1z_not_bbh[mask_nsbh]
             spin2z_nsbh = spin2z_not_bbh[mask_nsbh]
-            #    [Store etas of all NSBHs] 
+            #    [Store etas of all NSBHs]
             _, eta_nsbh = pnutils.mass1_mass2_to_mtotal_eta(mass1_nsbh, mass2_nsbh)
             #    [mask_bright_nsbh will identify NSBH systems with high enough
             #     threshold mass]
             mask_bright_nsbh = numpy.zeros(len(mass1_nsbh), dtype=bool)
             if len(eta_nsbh) != 0:
-                remnant = remnant_masses(eta_nsbh, mass2_nsbh, ns_sequence, spin1z_nsbh, 0., massRangeParams.remnant_mass_threshold)
+                remnant = remnant_masses(eta_nsbh, mass2_nsbh, ns_sequence, 
+                                         spin1z_nsbh, 0., 
+                                         massRangeParams.remnant_mass_threshold)
                 mask_bright_nsbh[remnant > massRangeParams.remnant_mass_threshold] = True
 
             # Keep only points that correspond to binaries that can produce an
             # EM counterpart (i.e., BNSs and EM-bright NSBHs) and add their
             # properties to the pile of accpeted points to output
-            mass1_out = numpy.concatenate((mass1_out, mass1_bns, mass1_nsbh[mask_bright_nsbh]))
-            mass2_out = numpy.concatenate((mass2_out, mass2_bns, mass2_nsbh[mask_bright_nsbh]))
-            spin1z_out = numpy.concatenate((spin1z_out, spin1z_bns, spin1z_nsbh[mask_bright_nsbh]))
-            spin2z_out = numpy.concatenate((spin2z_out, spin2z_bns, spin2z_nsbh[mask_bright_nsbh]))
+            mass1_out = numpy.concatenate((mass1_out, mass1_bns, 
+                                           mass1_nsbh[mask_bright_nsbh]))
+            mass2_out = numpy.concatenate((mass2_out, mass2_bns, 
+                                           mass2_nsbh[mask_bright_nsbh]))
+            spin1z_out = numpy.concatenate((spin1z_out, spin1z_bns, 
+                                            spin1z_nsbh[mask_bright_nsbh]))
+            spin2z_out = numpy.concatenate((spin2z_out, spin2z_bns, 
+                                            spin2z_nsbh[mask_bright_nsbh]))
 
             # Number of points that survived all cuts
             numPointsFound = len(mass1_out)
