5057834ffb621bce6685b0428388ada8540f4263
jsantoro392 <canevasantoro.1490713@studenti.uniroma1.it>
2019-12-11 12:53:51 +0000

    fixing indentation

--- pycbc/tmpltbank/em_progenitors.py
+++ pycbc/tmpltbank/em_progenitors.py
@@ -355,8 +355,8 @@ def remnant_mass(eta, ns_g_mass, ns_sequence, chi, incl, shift):
     delta = 1.761
     # The remnant mass over the NS rest mass
     remnant_mass = (max(alpha/eta**(1./3.)*(1 - 2*ns_compactness)
-                   - beta*ns_compactness / eta*PG_ISSO_solver(chi, incl)
-                   + gamma, 0.)) ** delta
+                        - beta*ns_compactness / eta*PG_ISSO_solver(chi, incl)
+                        + gamma, 0.)) ** delta
     # Convert to solar masses
     remnant_mass = remnant_mass*ns_b_mass
 
