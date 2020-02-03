011fec98031cb8de4e4990291d6337306cbe6f56
veronica-villa <51956991+veronica-villa@users.noreply.github.com>
2020-02-03 17:28:14 +0100

    Add calculation of source type probabilities to PyCBC Live (#3077)
    * Introduce source probability calculation
    
    * Mass gap probabilities improved
    
    * Change save function to add probs file
    
    * Change in z_delta and mchirp_delta
    
    * Add argument for mchirp_delta coefficient
    
    * Add mchirp bbh condition
    
    * Change save function
    
    * Add CBC probabilities to upload method
    
    * Small corrections to mchirp_area.py
    
    * Add logging messages
    
    * Fix small errors
    
    * Fix small syntax error
    
    * Fix codeclimate issues
    
    * Fix codeclimate issues
    
    * Fix codeclimate issues
    
    * Fix Travis issue
    
    * Changes in pie plot code
    
    * Remove unnecessary serialization to JSON file
    
    * Move distance and redshift estimations to mchirp_area module
    
    * Fix small errors
    
    * Fix codeclimate issue
    
    * Add color codes for the different CBC sources
    
    * Change option names and help strings
    
    * Assign colors to CBC sources and tag JSON file and plot as EM followup
    
    * Fix codeclimate issues
    
    * Change mc_area_args to be an attribute of LiveEventManager

--- pycbc/mchirp_area.py
+++ pycbc/mchirp_area.py
@@ -1,6 +1,6 @@
-# Integration of the area laying in the different cbc regions
-# By A. Curiel Barroso
-# August 2019
+# Module with utilities for estimating candidate events source probabilities
+# Initial code by A. Curiel Barroso, August 2019
+# Modified by V. Villa-Ortega, January 2020
 
 """Functions to compute the area corresponding to different CBC on the m1 & m2
 plane when given a central mchirp value and uncertainty.
@@ -8,8 +8,65 @@ It also includes a function that calculates the source frame when given the
 detector frame mass and redshift.
 """
 
+import math
 from pycbc.conversions import mass2_from_mchirp_mass1 as m2mcm1
 from scipy.integrate import quad
+from pycbc.cosmology import _redshift
+
+
+def insert_args(parser):
+    mchirp_group = parser.add_argument_group("Arguments for estimating the "
+                                             "source probabilities of a "
+                                             "candidate event using the snr, "
+                                             "mchirp, and effective distance.")
+    mchirp_group.add_argument('--src-class-mass-range', type=float, nargs=2,
+                              metavar=('MIN_M2', 'MAX_M1'),
+                              default=[1.0, 45.0],
+                              help="Minimum and maximum values for the mass "
+                                   "of the binary components, used as limits "
+                                   "of the mass plane when computing the area "
+                                   "corresponding to different CBC sources.")
+    mchirp_group.add_argument('--src-class-mass-gap', type=float, nargs=2,
+                              metavar=('MAX_NS', 'MIN_BH'), default=[3.0, 5.0],
+                              help="Limits of the mass gap, that correspond "
+                                   "to the maximum mass of a neutron star "
+                                   "and the minimum mass of a black hole. "
+                                   "Used as limits of integration of the "
+                                   "different CBC regions.")
+    mchirp_group.add_argument('--src-class-mchirp-to-delta', type=float,
+                              metavar='m0', default=0.01,
+                              help='Coefficient to estimate the value of the '
+                                   'mchirp uncertainty by mchirp_delta = '
+                                   'm0 * mchirp.')
+    mchirp_group.add_argument('--src-class-eff-to-lum-distance', type=float,
+                              metavar='a0', default=0.759,
+                              help='Coefficient to estimate the value of the '
+                                   'luminosity distance from the minimum '
+                                   'eff distance by D_lum = a0 * min(D_eff).')
+    mchirp_group.add_argument('--src-class-lum-distance-to-delta', type=float,
+                              nargs=2, metavar=('b0', 'b1'),
+                              default=[-0.449, -0.342],
+                              help='Coefficients to estimate the value of the '
+                                   'uncertainty on the luminosity distance '
+                                   'from the estimated luminosity distance and'
+                                   ' the coinc snr by delta_lum = D_lum * '
+                                   'exp(b0) * coinc_snr ** b1.')
+    mchirp_group.add_argument('--src-class-mass-gap-separate',
+                              action='store_true',
+                              help='Gives separate probabilities for each kind'
+                                   ' of mass gap CBC sources: GNS, GG, BHG.')
+
+
+def from_cli(args):
+    return {'mass_limits': {'max_m1': args.src_class_mass_range[1],
+                            'min_m2': args.src_class_mass_range[0]},
+            'mass_bdary': {'ns_max': args.src_class_mass_gap[0],
+                           'gap_max': args.src_class_mass_gap[1]},
+            'estimation_coeff': {'a0': args.src_class_eff_to_lum_distance,
+                                 'b0': args.src_class_lum_distance_to_delta[0],
+                                 'b1': args.src_class_lum_distance_to_delta[1],
+                                 'm0': args.src_class_mchirp_to_delta},
+            'mass_gap': args.src_class_mass_gap_separate}
 
 
 def src_mass_from_z_det_mass(z, del_z, mdet, del_mdet):
@@ -22,23 +79,15 @@ def src_mass_from_z_det_mass(z, del_z, mdet, del_mdet):
     return (msrc, del_msrc)
 
 
-# Integration function
-def mchange(x, mc):
-    """Returns a component mass as a function of mchirp and the other
-    component mass.
-    """
-    return m2mcm1(mc, x)
-
-
 def intmc(mc, x_min, x_max):
-    """Returns the integral of mchange between the minimum and maximum values
-    of a component mass taking mchirp as an argument.
+    """Returns the integral of a component mass as a function of the mass of
+       the other component, taking mchirp as an argument.
     """
-    integral = quad(mchange, x_min, x_max, args=mc)
+    integral = quad(lambda x, mc: m2mcm1(mc, x), x_min, x_max, args=mc)
     return integral[0]
 
 
-def calc_areas(trig_mc_det, mass_limits, mass_bdary, z):
+def calc_areas(trig_mc_det, mass_limits, mass_bdary, z, mass_gap):
     """Computes the area inside the lines of the second component mass as a
     function of the first component mass for the two extreme values
     of mchirp: mchirp +/- mchirp_uncertainty, for each region of the source
@@ -227,12 +276,58 @@ def calc_areas(trig_mc_det, mass_limits, mass_bdary, z):
         int_inf_nsbh = ints_nsbh + intline_inf_nsbh
 
         ansbh = int_sup_nsbh - int_inf_nsbh
-
+    if mass_gap:
+        return {
+            "BNS": abns,
+            "GNS": agns,
+            "NSBH": ansbh,
+            "GG": agg,
+            "BHG": abhg,
+            "BBH": abbh
+            }
     return {
-        "bns": abns,
-        "gns": agns,
-        "nsbh": ansbh,
-        "gg": agg,
-        "bhg": abhg,
-        "bbh": abbh
+        "BNS": abns,
+        "NSBH": ansbh,
+        "BBH": abbh,
+        "Mass Gap": agns + agg + abhg
         }
+
+
+def calc_probabilities(mchirp, snr, eff_distance, src_args):
+    """Computes the different probabilities that a candidate event belongs to
+       each CBC source category taking as arguments the chirp mass, the
+       coincident SNR and the effective distance, and estimating the
+       chirp mass uncertainty, the luminosity distance (and its uncertainty)
+       and the redshift (and its uncertainty). Probability estimation is done
+       assuming it is directly proportional to the area laying in the
+       correspondent CBC region.
+    """
+    mass_limits = src_args['mass_limits']
+    mass_bdary = src_args['mass_bdary']
+    coeff = src_args['estimation_coeff']
+    trig_mc_det = {'central': mchirp, 'delta': mchirp * coeff['m0']}
+    dist_estimation = coeff['a0'] * eff_distance
+    dist_std_estimation = (dist_estimation * math.exp(coeff['b0']) *
+                           snr ** coeff['b1'])
+    z_estimation = _redshift(dist_estimation)
+    z_est_max = _redshift(dist_estimation + dist_std_estimation)
+    z_est_min = _redshift(dist_estimation - dist_std_estimation)
+    z_std_estimation = 0.5 * (z_est_max - z_est_min)
+    z = {'central': z_estimation, 'delta': z_std_estimation}
+    mass_gap = src_args['mass_gap']
+
+    # If the mchirp is greater than the mchirp corresponding to two masses
+    # equal to the maximum mass, the probability for BBH is 100%
+    mc_max = mass_limits['max_m1'] / (2 ** 0.2)
+    if trig_mc_det['central'] > mc_max * (1 + z['central']):
+        if mass_gap is not False:
+            probabilities = {"BNS": 0.0, "GNS": 0.0, "NSBH": 0.0, "GG": 0.0,
+                             "BHG": 0.0, "BBH": 1.0}
+        else:
+            probabilities = {"BNS": 0.0, "NSBH": 0.0, "BBH": 1.0,
+                             "Mass Gap": 0.0}
+    else:
+        areas = calc_areas(trig_mc_det, mass_limits, mass_bdary, z, mass_gap)
+        total_area = sum(areas.values())
+        probabilities = {key: areas[key]/total_area for key in areas}
+    return probabilities
