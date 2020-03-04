488552b7112e9577b6237c20953a5f30a33afd43
Alex Nitz <alex.nitz@gmail.com>
2020-03-04 18:55:48 +0100

    population run keywords into dynesty (#3170)
    * population run keywords into dynesty
    
    * cc + docs
    
    * fix typo

--- pycbc/inference/sampler/dynesty.py
+++ pycbc/inference/sampler/dynesty.py
@@ -60,8 +60,6 @@ class DynestySampler(BaseSampler):
         A model from ``pycbc.inference.models``.
     nlive : int
         Number of live points to use in sampler.
-    dlogz: float
-        Tolerance limit to the value of logz (also a convergence criteria)
     pool : function with map, Optional
         A provider of a map function that allows a function call to be run
         over multiple sets of arguments and possibly maps them to
@@ -70,8 +68,9 @@ class DynestySampler(BaseSampler):
     name = "dynesty"
     _io = DynestyFile
 
-    def __init__(self, model, nlive, dlogz, nprocesses=1,
-                 loglikelihood_function=None, use_mpi=False, **kwargs):
+    def __init__(self, model, nlive, nprocesses=1,
+                 loglikelihood_function=None, use_mpi=False, run_kwds=None,
+                 **kwargs):
         self.model = model
         log_likelihood_call, prior_call = setup_calls(
             model,
@@ -82,19 +81,18 @@ class DynestySampler(BaseSampler):
         if pool is not None:
             pool.size = nprocesses
 
+        self.run_kwds = {} if run_kwds is None else run_kwds
         self.nlive = nlive
-        self.dlogz = dlogz
         self.names = model.sampling_params
         self.ndim = len(model.sampling_params)
         self.checkpoint_file = None
         self._sampler = dynesty.NestedSampler(log_likelihood_call,
                                               prior_call, self.ndim,
                                               nlive=self.nlive,
-                                              dlogz=self.dlogz,
                                               pool=pool, **kwargs)
 
     def run(self):
-        self._sampler.run_nested()
+        self._sampler.run_nested(**self.run_kwds)
 
     @property
     def io(self):
@@ -116,9 +114,16 @@ class DynestySampler(BaseSampler):
             "name in section [sampler] must match mine")
         # get the number of live points to use
         nlive = int(cp.get(section, "nlive"))
-        dlogz = float(cp.get(section, "dlogz"))
         loglikelihood_function = \
             get_optional_arg_from_config(cp, section, 'loglikelihood-function')
+
+        # optional run_nested arguments for dynesty
+        rargs = {'maxiter': int,
+                 'maxcall': int,
+                 'dlogz': float,
+                 'logl_max': float,
+                 'n_effective': int,
+                 }
 
         # optional arguments for dynesty
         cargs = {'bound': str,
@@ -127,13 +132,18 @@ class DynestySampler(BaseSampler):
                  'update_interval': float,
                  'sample': str}
         extra = {}
+        run_extra = {}
         for karg in cargs:
             if cp.has_option(section, karg):
                 extra[karg] = cargs[karg](cp.get(section, karg))
 
-        obj = cls(model, nlive=nlive, dlogz=dlogz, nprocesses=nprocesses,
+        for karg in rargs:
+            if cp.has_option(section, karg):
+                run_extra[karg] = rargs[karg](cp.get(section, karg))
+
+        obj = cls(model, nlive=nlive, nprocesses=nprocesses,
                   loglikelihood_function=loglikelihood_function,
-                  use_mpi=use_mpi, **extra)
+                  use_mpi=use_mpi, run_kwds=run_extra, **extra)
         setup_output(obj, output_file)
         if not obj.new_checkpoint:
             obj.resume_from_checkpoint()
