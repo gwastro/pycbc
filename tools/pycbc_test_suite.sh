253a5f5867215bcfda68c14b4ed876faed0d625c
Tito Dal Canton <tito.canton@ligo.org>
2020-05-07 01:58:46 -0700

    Add example to test suite

--- tools/pycbc_test_suite.sh
+++ tools/pycbc_test_suite.sh
@@ -57,6 +57,18 @@ else
 fi
 popd
 
+# run PyCBC Live test
+pushd examples/live
+bash -e run.sh
+if test $? -ne 0 ; then
+    RESULT=1
+    echo -e "    FAILED!"
+    echo -e "---------------------------------------------------------"
+else
+    echo -e "    Pass."
+fi
+popd
+
 # Run Inference Scripts
 ## Run inference on 2D-normal analytic likelihood function
 pushd examples/inference/analytic-normal2d
