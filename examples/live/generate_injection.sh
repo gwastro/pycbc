6846f8c3b620098d3581120b0b81895cab0b9009
maxtrevor <65971534+maxtrevor@users.noreply.github.com>
2020-05-28 14:59:57 -0400

    Update generate_injection.sh

--- examples/live/generate_injection.sh
+++ examples/live/generate_injection.sh
@@ -1,30 +1,18 @@
-# set parameters for injection
-NETWORK_SNR = 20.0
-GEOCENTRIC_END_TIME = 1272790440
-RA = 45.0
-DEC = 45.0
-POLARIZATION = 0.0
-APPROXIMANT = 'SEOBNRv4'
-MASS1 = 25.0
-MASS2 = 25.0
-INCLINATION = 0.0
-TAPER = 'TAPER_START'
-INSTRUMENTS = 'H1 L1 V1'
-LOW_FREQUENCY_CUTOFF = 10.0
 
 
 pycbc_generate_hwinj \
-  -- network_snr NETWORK_SNR \
-  --geocentric_end_time GEOCENTRIC_END_TIME \
-  --ra RA \
-  --dec DEC \
-  --polarization POLARIZATION \
-  --approximant APPROXIMANT \
-  --mass1 MASS1 \
-  --mass2 MASS2 \
-  --inclination INCLINATION \
-  --taper TAPER \
-  --instruments INSTRUMENTS \
-  --low_frequency_cutoff LOW_FREQUENCY_CUTOFF
+  -- network_snr 20.0 \
+  --geocentric_end_time 1272790440 \
+  --ra 45.0 \
+  --dec 45.0 \
+  --polarization 0.0 \
+  --approximant SEOBNRv4 \
+  --mass1 25.0 \
+  --mass2 25.0 \
+  --inclination 0.0 \
+  --taper TAPER_START \
+  --instruments H1 L1 V1 \
+  --low_frequency_cutoff 10 \
+  --waveform_low_frequency_cutoff 10
   
   
