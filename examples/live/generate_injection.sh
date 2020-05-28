213e657ddeb99a388486456aa282699b582ad8c1
maxtrevor <65971534+maxtrevor@users.noreply.github.com>
2020-05-28 15:03:04 -0400

    Update generate_injection.sh

--- examples/live/generate_injection.sh
+++ examples/live/generate_injection.sh
@@ -1,8 +1,5 @@
-
-
 pycbc_generate_hwinj \
-  -- network_snr 20.0 \
-  --geocentric_end_time 1272790440 \
+  --network_snr 20.0 \
   --ra 45.0 \
   --dec 45.0 \
   --polarization 0.0 \
@@ -11,8 +8,12 @@ pycbc_generate_hwinj \
   --mass2 25.0 \
   --inclination 0.0 \
   --taper TAPER_START \
-  --instruments H1 L1 V1 \
+  --waveform_low_frequency_cutoff 10 \
+  --geocentric_end_time 1272790440 \    
   --low_frequency_cutoff 10 \
-  --waveform_low_frequency_cutoff 10
+  --instruments H1 L1 V1
+  
+  
+  
   
   
