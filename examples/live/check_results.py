0a58dbbd02c805470f3c2c48698541d7198c3bfb
maxtrevor <65971534+maxtrevor@users.noreply.github.com>
2020-06-02 13:10:14 -0400

    Update check_results.py

--- examples/live/check_results.py
+++ examples/live/check_results.py
@@ -101,8 +101,8 @@ if detectors_with_trigs != tested_detectors:
     fail = True
 
 if fail:
-    print('Test Failed')
+    log.error('Test Failed')
 else:
-    print('Test Passed')
+    log.info('Test Passed')
  
 sys.exit(1 if fail else 0)
