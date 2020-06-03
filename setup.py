135e94c2b95af4b2c5597edde355fc235e2e809a
Gareth S Davies <gareth.davies@ligo.org>
2020-06-03 10:47:40 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -124,8 +124,8 @@ def get_version_info():
         vinfo = _version_helper.generate_git_version_info()
     except:
         vinfo = vdummy()
-        vinfo.version = '1.16.dev2'
-        vinfo.release = 'False'
+        vinfo.version = '1.16.2'
+        vinfo.release = 'True'
 
     with open('pycbc/version.py', 'w') as f:
         f.write("# coding: utf-8\n")
