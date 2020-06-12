a38a6cefa1cf6728da3c495471a8245c4da55200
Collin Capano <collin.capano@aei.mpg.de>
2020-06-12 14:22:05 +0000

    add write_data method to base_hdf

--- pycbc/inference/io/base_hdf.py
+++ pycbc/inference/io/base_hdf.py
@@ -811,3 +811,38 @@ class BaseInferenceFile(h5py.File):
                 cls.write_kwargs_to_attrs(attrs, **val)
             else:
                 attrs[arg] = val
+
+    def write_data(self, name, data, path=None):
+        """Writes data as either a dataset or an attr.
+
+        If the given data is a numpy array or list, the data will be written
+        as a dataset. Otherwise, the data is written to the attrs using
+        :py:func:`write_kwargs_to_attrs`. When writing the data as a dataset,
+        it is assumed that the data has the same size everytime.
+
+        Parameters
+        ----------
+        name : str
+            The name to associate with the data. This will be the dataset
+            name (if data is array-like) or the key in the attrs.
+        data : object
+            The data to write. Can be of any arbitrary type.
+        path : str, optional
+            Write to the given path. Default (None) will write to the top
+            level.
+        """
+        if path is None:
+            path = '/'
+        group = self[path]
+        if isinstance(data, (numpy.ndarray, list)):
+            # write arrays as datasets
+            # Note: this assumes that the data always has the
+            # same shape.
+            try:
+                group[name][:] = data
+            except KeyError:
+                # dataset doesn't exist yet
+                group[name] = data
+        else:
+            # write all other data as attrs
+            self.write_kwargs_to_attrs(group.attrs, **{name: data})
