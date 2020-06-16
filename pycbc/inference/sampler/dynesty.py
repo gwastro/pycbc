300e57a956246e93947f5403b44984a465660383
Prayush Kumar <prayush.kumar@ligo.org>
2020-06-16 13:14:21 -0400

    Add basic infra to checkpoint Dynesty sampler
    This adds two incremental changes:
    
    - Modify the run method of Dynesty to allow
      for interval-based, duration-based and
      signal-triggered checkpointing
    - Inherit from the nested sampler base class
      that provides methods to do pickle-based
      checkpointing of sampler state and HDF-based
      checkpointing of sampler results c.f. the
      external sampler object.
    
    In the future, when HDF5 based protocols will be used
    for checkpointing Dynesty, only the checkpoint and
    resume_from_checkpoint methods will need to be
    overwritten.

--- pycbc/inference/sampler/dynesty.py
+++ pycbc/inference/sampler/dynesty.py
@@ -29,14 +29,19 @@ packages for parameter estimation.
 
 from __future__ import absolute_import
 
+import os
+import time
+import datetime
+import signal
 import logging
-from pycbc.pool import choose_pool
+from pycbc.pool import (choose_pool, is_main_process)
 import numpy
 import dynesty
 from dynesty.utils import resample_equal
 from pycbc.inference.io import (DynestyFile, validate_checkpoint_files)
 from pycbc.distributions import read_constraints_from_config
 from .base import (BaseSampler, setup_output)
+from .base_nested_sampler import BaseNestedSampler
 from .base_mcmc import get_optional_arg_from_config
 from .base_cube import setup_calls
 from .. import models
@@ -50,7 +55,7 @@ from .. import models
 # =============================================================================
 #
 
-class DynestySampler(BaseSampler):
+class DynestySampler(BaseNestedSampler, BaseSampler):
     """This class is used to construct an Dynesty sampler from the dynesty
     package.
 
@@ -60,23 +65,52 @@ class DynestySampler(BaseSampler):
         A model from ``pycbc.inference.models``.
     nlive : int
         Number of live points to use in sampler.
+
     pool : function with map, Optional
         A provider of a map function that allows a function call to be run
         over multiple sets of arguments and possibly maps them to
         cores/nodes/etc.
+
+    checkpoint : bool
+        Enable checkpointing
+    checkpoint_interval : int
+        Checkpoint after every so many iterations
+    checkpoint_period : int
+        Checkpoint only after these many seconds since last checkpoint
+    checkpoint_on_signal : bool
+        Checkpoint if receiving external signals
+    checkpoint_signal : str
+        Signal to send when checkpointing
+
+    nprocesses : int
+        Number of processes to use in MPI pool
+    use_mpi : bool
+        Enable / disable MPI usage in multiprocessing pool
+    run_kwds : dict
+        Keyword options for dynesty's sampling routine
+    kwargs : dict
+
     """
     name = "dynesty"
     _io = DynestyFile
 
-    def __init__(self, model, nlive, nprocesses=1,
-                 loglikelihood_function=None, use_mpi=False, run_kwds=None,
-                 **kwargs):
+    def __init__(self, model, nlive,
+                 loglikelihood_function=None,
+                 checkpoint=True,
+                 checkpoint_interval=1000,
+                 checkpoint_period=600,
+                 checkpoint_on_signal=True,
+                 checkpoint_signal=None,
+                 nprocesses=1, use_mpi=False,
+                 run_kwds=None, **kwargs):
         self.model = model
         log_likelihood_call, prior_call = setup_calls(
             model,
             nprocesses=nprocesses,
             loglikelihood_function=loglikelihood_function)
         # Set up the pool
+        self.use_mpi = use_mpi
+        self.nprocesses = nprocesses
         pool = choose_pool(mpi=use_mpi, processes=nprocesses)
         if pool is not None:
             pool.size = nprocesses
@@ -85,7 +119,16 @@ class DynestySampler(BaseSampler):
         self.nlive = nlive
         self.names = model.sampling_params
         self.ndim = len(model.sampling_params)
-        self.checkpoint_file = None
+
+        self._checkpoint = checkpoint
+        self._checkpoint_interval = checkpoint_interval
+        self._checkpoint_signal = checkpoint_signal
+        self._checkpoint_on_signal = checkpoint_on_signal
+        self._checkpoint_period = checkpoint_period
+        if self._checkpoint:
+            logging.info("Checkpointing every {0} iters, {1} secs".format(
+                self.checkpoint_interval, self.checkpoint_period))
+
         if self.nlive < 0:
             # Interpret a negative input value for the number of live points
             # (which is clearly an invalid input in all senses)
@@ -98,9 +141,67 @@ class DynestySampler(BaseSampler):
                                                   prior_call, self.ndim,
                                                   nlive=self.nlive,
                                                   pool=pool, **kwargs)
+        self._is_main_process = is_main_process()
+
+    def set_sampler_specific_state_from_file(self, filename):
+        """Set state of sampler object that could not be serialized"""
+        self._sampler.rstate = numpy.random
+        self._sampler.nqueue = -1
+        logging.info("Creating a new multiprocessing pool")
+        pool = choose_pool(mpi=self.use_mpi, processes=self.nprocesses)
+        if pool is not None:
+            pool.size = self.nprocesses
+        self._sampler.pool = pool
+        self._sampler.M = pool.map
+
+    def getstate(self):
+        """Strips unserializable attributes of sampler
+        """
+        self_dict = self._sampler.__dict__.copy()
+        # Remove multiprocessing / mpi pool-related attributes
+        # as they cannot be pickled
+        del self_dict['pool']
+        del self_dict['M']
+        return self_dict
 
     def run(self):
-        self._sampler.run_nested(**self.run_kwds)
+        """Run the sampler."""
+        if self._checkpoint_on_signal:
+            # Enable on-signal checkpointing:
+            # This purely guards against interruptions on compute clusters
+            signal.signal(signal.SIGTERM, self.checkpoint_on_signal)
+            signal.signal(signal.SIGALRM, self.checkpoint_on_signal)
+            signal.signal(signal.SIGQUIT, self.checkpoint_on_signal)
+            signal.signal(signal.SIGTSTP, self.checkpoint_on_signal)
+            signal.signal(signal.SIGINT, self.checkpoint_on_signal)
+            signal.signal(signal.SIGUSR1, self.checkpoint_on_signal)
+            signal.signal(signal.SIGUSR2, self.checkpoint_on_signal)
+
+        # Instead of calling the `run_nested` wrapper, we invoke
+        # the core nested sampling loop for the dynesty sampler
+        t0 = datetime.datetime.now()
+        sampling_time = 0
+        for it, res in enumerate(self._sampler.sample(**self.run_kwds)):
+            i = it - 1
+            dynesty.results.print_fn_fallback(res, i, self._sampler.ncall,
+                                              dlogz=self.run_kwds['dlogz'])
+            if not self._checkpoint or it == 0:
+                continue
+            if self.checkpoint_interval is not None:
+                if it % self.checkpoint_interval != 0:
+                    continue
+            # Checkpoint periodically
+            sampling_time += (datetime.datetime.now() - t0).total_seconds()
+            t0 = datetime.datetime.now()
+            if os.path.isfile(self.checkpoint_file):
+                time_since_last_checkpoint_s =\
+                    time.time() - os.path.getmtime(self.checkpoint_file)
+            else:
+                time_since_last_checkpoint_s = numpy.inf
+            if time_since_last_checkpoint_s > self.checkpoint_period:
+                self._sampler.kwargs['sampling_time'] = sampling_time
+                if self.is_main_process:
+                    self.checkpoint()
 
     @property
     def io(self):
@@ -149,19 +250,34 @@ class DynestySampler(BaseSampler):
             if cp.has_option(section, karg):
                 run_extra[karg] = rargs[karg](cp.get(section, karg))
 
+        # get the checkpoint interval, if it's specified
+        checkpoint_interval = cls.checkpoint_from_config(cp, section)
+        checkpoint_signal = cls.ckpt_signal_from_config(cp, section)
+        checkpoint_period = get_optional_arg_from_config(cp, section,
+                                                         'checkpoint-period',
+                                                         dtype=int)
+        checkpoint_on_signal =\
+            get_optional_arg_from_config(cp, section, 'checkpoint-on-signal',
+                                         dtype=bool)
+        checkpoint = get_optional_arg_from_config(cp, section,
+                                                  'checkpoint', dtype=bool)
+        if checkpoint is None and ((checkpoint_interval is not None) or
+                                   (checkpoint_period is not None) or
+                                   checkpoint_on_signal):
+            checkpoint = True
+
         obj = cls(model, nlive=nlive, nprocesses=nprocesses,
                   loglikelihood_function=loglikelihood_function,
+                  checkpoint=checkpoint,
+                  checkpoint_interval=checkpoint_interval,
+                  checkpoint_period=checkpoint_period,
+                  checkpoint_signal=checkpoint_signal,
+                  checkpoint_on_signal=checkpoint_on_signal,
                   use_mpi=use_mpi, run_kwds=run_extra, **extra)
         setup_output(obj, output_file)
         if not obj.new_checkpoint:
             obj.resume_from_checkpoint()
         return obj
-
-    def checkpoint(self):
-        pass
-
-    def resume_from_checkpoint(self):
-        pass
 
     def finalize(self):
         logz = self._sampler.results.logz[-1:][0]
