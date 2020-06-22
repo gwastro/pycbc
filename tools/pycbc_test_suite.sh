46629aff18598a911461b0ccccb157ecf396a26a
Alexander Harvey Nitz <alex.nitz@gmail.com>
2020-06-22 11:52:52 +0200

    update

--- tools/pycbc_test_suite.sh
+++ tools/pycbc_test_suite.sh
@@ -8,14 +8,7 @@ RESULT=0
 
 echo "MYTEST"
 
-python -c "import numpy
-a = numpy.array([-2.8498888-4.8523064j,
-                          11.672292 -0.39886224j,
-                           -3.907175 -3.9876463j,
-                           3.5721998-4.4356446j,
-                           -0.9019761+6.379277j], dtype=numpy.complex64)
-print (abs(a)) 
-"
+python t.py
 
 #run pycbc inspiral test
 pushd examples/inspiral
