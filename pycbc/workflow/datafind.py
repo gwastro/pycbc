92ff73bf473825f6ae141bf503acccafc64a5b28
Ian Harry <ian.harry@ligo.org>
2020-06-26 04:15:55 -0700

    Allow time-variable frame type

--- pycbc/workflow/datafind.py
+++ pycbc/workflow/datafind.py
@@ -494,15 +494,15 @@ def setup_datafind_runtime_cache_single_call_perifo(cp, scienceSegs, outputDir,
     datafindouts = []
     datafindcaches = []
     logging.info("Querying datafind server for all science segments.")
-    checked_times = segments.segmentlist([])
     for ifo, scienceSegsIfo in scienceSegs.items():
         observatory = ifo[0].upper()
+        checked_times = segments.segmentlist([])
         frame_types = cp.get_opt_tags(
             "workflow-datafind",
             "datafind-%s-frame-type" % (ifo.lower()), tags
         )
         # Check if this is one type, or time varying
-        frame_types = frame_type.replace(' ', '').strip().split(',')
+        frame_types = frame_types.replace(' ', '').strip().split(',')
         for ftype in frame_types:
             # Check the times, default to full time initially
             # This REQUIRES a coalesced segment list to work
@@ -511,12 +511,14 @@ def setup_datafind_runtime_cache_single_call_perifo(cp, scienceSegs, outputDir,
             # Then check for limits
             if '[' in ftype:
                 bopt = ftype.split('[')[1].split(']')[0]
-                start, end = bopt.split(':')
-                start = int(start)
-                end = int(end) 
+                newstart, newend = bopt.split(':')
+                start = max(int(newstart), start)
+                end = min(int(newend), end)
+                if end <= start:
+                    continue
                 ftype = ftype.replace('[' + bopt +']', '')
             curr_times = segments.segment(start, end)
-            if checked_times.intersects_semgent(curr_times):
+            if checked_times.intersects_segment(curr_times):
                 err_msg = "Different frame types cannot overlap in time."
                 raise ValueError(err_msg)
             checked_times.append(curr_times)
