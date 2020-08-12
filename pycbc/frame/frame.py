b8522a472665e89f6c0ef270a7c73227cb1d8a91
Alexander Harvey Nitz <alex.nitz@gmail.com>
2020-08-12 16:11:26 +0200

    make it easier to just get the losc strain channel

--- pycbc/frame/frame.py
+++ pycbc/frame/frame.py
@@ -341,6 +341,10 @@ def query_and_read_frame(frame_type, channels, start_time, end_time,
     """
     # Allows compatibility with our standard tools
     # We may want to place this into a higher level frame getting tool
+    if frame_type == 'LOSC_STRAIN':
+        from pycbc.frame.losc import read_strain_losc
+        data = [read_strain_losc(c[:2], start_time, end_time) for c in channels]
+        return data
     if frame_type == 'LOSC':
         from pycbc.frame.losc import read_frame_losc
         return read_frame_losc(channels, start_time, end_time)
