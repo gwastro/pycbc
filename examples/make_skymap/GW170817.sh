67256970ce1b3fa9630a77209f53b2e32c15d1bb
Tito Dal Canton <tito.canton@ligo.org>
2020-08-21 13:27:49 -0700

    Add example for pycbc_make_skymap

---
+++ examples/make_skymap/GW170817.sh
@@ -0,0 +1,8 @@
+pycbc_make_skymap \
+    --trig-time 1187008882.4457 \
+    --mass1 1.457423 \
+    --mass2 1.299302 \
+    --spin1z "-0.018811" \
+    --spin2z "0.011777" \
+    --ifos H1 L1 V1 \
+    --ligolw-event-output coinc_GW170817.xml
