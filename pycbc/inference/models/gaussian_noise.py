a3eadb52ba2b2e3b55dbb2afc3b091db2136c19a
Alexander Harvey Nitz <alex.nitz@gmail.com>
2020-09-03 11:48:57 +0200

    add polarization marginalized model

--- pycbc/inference/models/gaussian_noise.py
+++ pycbc/inference/models/gaussian_noise.py
@@ -31,6 +31,9 @@ from pycbc.strain.calibration import Recalibrate
 from pycbc.inject import InjectionSet
 from pycbc.io import FieldArray
 from pycbc.types.optparse import MultiDetOptionAction
+from pycbc.detector import Detector
+
+from scipy.special import logsumexp
 
 from .base import ModelStats
 from .base_data import BaseDataModel
@@ -928,6 +931,105 @@ class GaussianNoise(BaseGaussianNoise):
             # now try returning again
             return getattr(self._current_stats, '{}_optimal_snrsq'.format(det))
 
+class MarginalizedPolarization(BaseGaussianNoise):
+    r"""
+    """
+    name = 'marginalized_polarization'
+
+    def __init__(self, variable_params, data, low_frequency_cutoff, psds=None,
+                 high_frequency_cutoff=None, normalize=False,
+                 polarization_sampes=1000,
+                 static_params=None, **kwargs):
+        # set up the boiler-plate attributes
+        super(GaussianNoise, self).__init__(
+            variable_params, data, low_frequency_cutoff, psds=psds,
+            high_frequency_cutoff=high_frequency_cutoff, normalize=normalize,
+            static_params=static_params, **kwargs)
+        # create the waveform generator
+        self.waveform_generator = create_waveform_generator(
+            self.variable_params, self.data,
+            waveform_transforms=self.waveform_transforms,
+            recalibration=self.recalibration,
+            gates=self.gates, **self.static_params)
+
+        self.polarization_samples = polarization_samples
+        self.pol = numpy.linspace(0, 2*numpy.pi, self.polarization_samples)
+        self.dets = {}
+
+    def _nowaveform_loglr(self):
+        """Convenience function to set loglr values if no waveform generated.
+        """
+        return -numpy.inf
+
+    def _loglr(self):
+        r"""Computes the log likelihood ratio,
+
+        .. math::
+
+            \log \mathcal{L}(\Theta) = \sum_i
+                \left<h_i(\Theta)|d_i\right> -
+                \frac{1}{2}\left<h_i(\Theta)|h_i(\Theta)\right>,
+
+        at the current parameter values :math:`\Theta`.
+
+        Returns
+        -------
+        float
+            The value of the log likelihood ratio.
+        """
+        params = self.current_params
+        try:
+            wfs = self.waveform_generator.generate(**params)
+        except NoWaveformError:
+            return self._nowaveform_loglr()
+        except FailedWaveformError as e:
+            if self.ignore_failed_waveforms:
+                return self._nowaveform_loglr()
+            else:
+                raise e
+
+        lr = 0.
+        for det, (hp, hc) in wfs.items():
+            if det not in self.dets:
+                self.dets[det] = Detector(det)
+            det = self.dets[det]
+            fp, fc = det.antenna_pattern(self.current_params['ra'],
+                                         self.current_params['dec'],
+                                         self.pol,
+                                         self.current_params['tc'])
+
+            # the kmax of the waveforms may be different than internal kmax
+            kmax = min(len(h), self._kmax[det])
+            slc = slice(self._kmin[det], kmax)
+
+            # whiten both polarizations
+            hp[self._kmin[det]:kmax] *= self._weight[det][slc]
+            hc[self._kmin[det]:kmax] *= self._weight[det][slc]
+
+            # h = fp * hp + hc * hc
+            # <h, d> = fp * <hp,d> + fc * <hc,d>
+            # the inner products
+            cplx_hpd = self._whitened_data[det][slc].inner(hp[slc])  # <hp, d>
+            cplx_hcd = self._whitened_data[det][slc].inner(hc[slc])  # <hc, d>
+
+            cplx_hd = fp * cplx_hpd + fc * cplx_hcd
+
+            # <h, h> = <fp * hp + fc * hc, fp * hp + fc * hc>
+            # = Real(fpfp * <hp,hp> + fcfc * <hc,hc> + fphc * (<hp, hc> + <hc, hp>))
+            hphp = hp[slc].inner(hp[slc]).real  # < hp, hp>
+            hchc = hc[slc].inner(hc[slc]).real  # <hc, hc>
+
+            # Below could be combined, but too tired to figure out
+            # if there should be a sign applied if so
+            hphc = h[slc].inner(h[slc]).real  # <hp, hc>
+            hchp = h[slc].inner(h[slc]).real  # <hc, hp>
+
+            hh = fp * fp * hphp + fc * fc * hchc + fp * fc * (hphc + hchp)
+
+            cplx_loglr = cplx_hd - 0.5 * hh
+            lr += logsumexp(cplx_loglr.real)
+
+        return float(lr)
 
 #
 # =============================================================================
