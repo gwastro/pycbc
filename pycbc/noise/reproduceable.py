a24ba683fc304932567051b19cae9376d79b57d1
SSingh087 <66710757+SSingh087@users.noreply.github.com>
2020-09-04 13:01:00 +0530

    updated noise for LISA

--- pycbc/noise/reproduceable.py
+++ pycbc/noise/reproduceable.py
@@ -32,7 +32,7 @@ SAMPLE_RATE = 16384
 BLOCK_SIZE = 100
 FILTER_LENGTH = 128
 
-def block(seed):
+def block(seed, SAMPLE_RATE = 16384, BLOCK_SIZE=128):
     """ Return block of normal random numbers
 
     Parameters
@@ -50,7 +50,7 @@ def block(seed):
     variance = SAMPLE_RATE / 2
     return rng.normal(size=num, scale=variance**0.5)
 
-def normal(start, end, seed=0):
+def normal(start, end, seed=0, SAMPLE_RATE=16384, BLOCK_SIZE=128):
     """ Generate data with a white Gaussian (normal) distribution
 
     Parameters
@@ -80,7 +80,8 @@ def normal(start, end, seed=0):
     ts = TimeSeries(data, delta_t=1.0 / SAMPLE_RATE, epoch=start)
     return ts.time_slice(start, end)
 
-def colored_noise(psd, start_time, end_time, seed=0, low_frequency_cutoff=1.0):
+def colored_noise(psd, start_time, end_time, seed=0, low_frequency_cutoff=1.0,
+                  SAMPLE_RATE=16384, BLOCK_SIZE=100, FILTER_LENGTH=128):
     """ Create noise from a PSD
 
     Return noise from the chosen PSD. Note that if unique noise is desired
@@ -161,6 +162,32 @@ def colored_noise(psd, start_time, end_time, seed=0, low_frequency_cutoff=1.0):
     colored = white_noise.to_timeseries()
     del white_noise
     return colored.time_slice(start_time, end_time)
+
+def LISA_colored_noise(psd, start_time, end_time, seed=0, low_frequency_cutoff=.01):
+    """ Create LISA noise from a PSD
+    Return noise from the chosen PSD. Note that if unique noise is desired
+    a unique seed should be provided.
+    Parameters
+    ----------
+    psd : pycbc.types.FrequencySeries
+        PSD to color the noise
+    start_time : int
+        Start time in GPS seconds to generate noise
+    end_time : int
+        End time in GPS seconds to generate nosie
+    seed : {None, int}
+        The seed to generate the noise.
+    low_frequency_cutof : {1.0, float}
+        The low frequency cutoff to pass to the PSD generation.
+
+    Returns
+    --------
+    noise : TimeSeries
+        A TimeSeries containing gaussian noise colored by the given psd for LISA.
+    """
+    return colored_noise(psd, start_time, end_time, seed=seed,
+                         low_frequency_cutoff=low_frequency_cutoff,
+                         SAMPLE_RATE=16, BLOCK_SIZE=1000000, FILTER_LENGTH=131072)
 
 def noise_from_string(psd_name, start_time, end_time, seed=0, low_frequency_cutoff=1.0):
     """ Create noise from an analytic PSD
