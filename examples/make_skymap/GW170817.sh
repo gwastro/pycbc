ba491e38b68c80b89bf7493e6d51fa00705cb247
Tito Dal Canton <tito.canton@ligo.org>
2020-09-30 21:51:50 +0200

    pycbc_make_skymap: fix a bug, some improvements, and code cleanup (#3431)
    * pycbc_make_skymap: fix a bug, some improvements, and code cleanup
    
    Restore the original use case where the frame files are automatically guessed,
    which was broken by the addition of the --custom-frame-files option.
    
    Improve the logging in case of failure of pycbc_single_template.
    
    Remove an unused option.
    
    Massive code cleanup.
    
    * More cleanup
    
    * Add example for pycbc_make_skymap
    
    * Add zoomed plot for GW170817 and example for GW150914

---
+++ examples/make_skymap/GW170817.sh
@@ -0,0 +1,20 @@
+pycbc_make_skymap \
+    --trig-time 1187008882.4457 \
+    --thresh-SNR 5.5 \
+    --f-low 27 \
+    --mass1 1.457423 \
+    --mass2 1.299302 \
+    --spin1z "-0.018811" \
+    --spin2z "0.011777" \
+    --ifos H1 L1 V1 \
+    --ligolw-event-output coinc_GW170817.xml
+
+# make a zoomed-in plot to see the details
+ligo-skymap-plot \
+    --output 1187008882_skymap_zoom.png \
+    --projection zoom \
+    --projection-center '13h8m -21d' \
+    --zoom-radius 10deg \
+    --contour 50 90 \
+    --annotate \
+    1187008882.fits
