5f46ab596098e5afd3b378944cd8d803e35255a2
veronica-villa <veronica.villa@ligo.org>
2020-10-22 08:02:00 -0700

    Fix error when no gating veto argument is given

--- pycbc/io/hdf.py
+++ pycbc/io/hdf.py
@@ -988,7 +988,7 @@ class ForegroundTriggers(object):
 
 class ReadByTemplate(object):
     def __init__(self, filename, bank=None, segment_name=None, veto_files=None,
-                 gating_veto_windows=None):
+                 gating_veto_windows={}):
         self.filename = filename
         self.file = h5py.File(filename, 'r')
         self.ifo = tuple(self.file.keys())[0]
@@ -1008,7 +1008,7 @@ class ReadByTemplate(object):
             veto_segs = veto.select_segments_by_definer(vfile, ifo=self.ifo,
                                                         segment_name=name)
             self.segs = (self.segs - veto_segs).coalesce()
-        if gating_veto_windows:
+        if self.ifo in gating_veto_windows:
             gating_veto = gating_veto_windows[self.ifo].split(',')
             gveto_before = float(gating_veto[0])
             gveto_after = float(gating_veto[1])
