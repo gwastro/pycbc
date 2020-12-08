213570913d56ca93c37ae3438ef31e3a237ecde9
Ian Harry <ian.harry@ligo.org>
2020-12-08 14:44:21 +0000

    Try to implement a getitem in SingleDetTriggers

--- pycbc/io/hdf.py
+++ pycbc/io/hdf.py
@@ -448,6 +448,23 @@ class SingleDetTriggers(object):
             logging.info('%i triggers remain after cut on %s',
                          sum(self.mask), filter_func)
 
+    def __getitem__(self, key):
+        # Is key in the TRIGGER_MERGE file?
+        try:
+            vals = self.get_column(key)
+            return vals
+        except KeyError:
+            pass
+
+        # Is key in the bank file?
+        try:
+            self.checkbank(key)
+            return self.bank[key][:][self.template_id]
+        except RuntimeError:
+            err_msg = "Cannot find {} in input files".format(key)
+            raise ValueError(err_msg)
+        
+
     def checkbank(self, param):
         if self.bank == {}:
             return RuntimeError("Can't get %s values without a bank file"
@@ -652,6 +669,9 @@ class SingleDetTriggers(object):
         return ranking.newsnr_sgveto_psdvar_threshold(self.snr, self.rchisq,
                                            self.sgchisq, self.psd_var_val)
 
+    def get_ranking(self, rank_name, **kwargs):
+        return ranking.get_sngls_ranking_from_trigs(self, rank_name, **kwargs)
+
     def get_column(self, cname):
         # Fiducial value that seems to work, not extensively tuned.
         MFRAC = 0.3
@@ -669,6 +689,7 @@ class SingleDetTriggers(object):
             return self.trigs[cname][:][self.mask]
         else:
             return self.trigs[cname][:]
+
 
 class ForegroundTriggers(object):
     # FIXME: A lot of this is hardcoded to expect two ifos
@@ -977,6 +998,7 @@ class ForegroundTriggers(object):
         outdoc.childNodes[0].appendChild(sngl_inspiral_table)
 
         ligolw_utils.write_filename(outdoc, file_name)
+
 
 class ReadByTemplate(object):
     # default assignment to {} is OK for a variable used only in __init__
