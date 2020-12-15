c05d45c06a8787f43fb2f916918631f45f516c75
Ian Harry <ian.harry@ligo.org>
2020-12-15 11:14:02 -0800

    Trying again for the tag

--- docker/etc/push_image.sh
+++ docker/etc/push_image.sh
@@ -1,4 +1,5 @@
-docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
+SOURCE_TAG=`git show-ref | grep "8d86765d0b3d059dd6e5c6195f00f2cf252e661f" | grep -E -o "refs/tags.{0,100}" | cut -c11-`
 if [ "x${SOURCE_TAG}"  == "x" ] ; then
     docker tag ${DOCKER_IMG} ${DOCKER_IMG}:latest || exit 1 ;
     docker push ${DOCKER_IMG}:latest || exit 1 ;
