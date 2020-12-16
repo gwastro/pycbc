d66e69ccc54b0946b973e7984cfdd2f58f6c0e8e
Ian Harry <ian.harry@ligo.org>
2020-12-16 15:52:34 +0000

    Update push_image.sh

--- docker/etc/push_image.sh
+++ docker/etc/push_image.sh
@@ -1,8 +1,6 @@
 echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
 SOURCE_TAG=`git show-ref | grep ${GITHUB_SHA} | grep -E -o "refs/tags.{0,100}" | cut -c11-`
 MASTER_HASH=`git rev-parse origin/master`
-# REMOVE THIS LINE BEFORE MERGING
-MASTER_HASH=${GITHUB_SHA}
 if [ "x${SOURCE_TAG}"  == "x" ] ; then
     if [ "x${MASTER_HASH}"  == "x${GITHUB_SHA}" ] ; then
         docker tag ${DOCKER_IMG} ${DOCKER_IMG}:latest || exit 1 ;
