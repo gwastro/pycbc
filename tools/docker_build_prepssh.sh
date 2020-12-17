f64b73d45e627c431888de8a1edf3296e1f2106d
Ian Harry <ian.harry@ligo.org>
2020-12-17 07:55:30 -0800

    Revert "Try this?"
    This reverts commit 344c3f2722f529ce7d039e8b48603c2e7a576361.

--- tools/docker_build_prepssh.sh
+++ tools/docker_build_prepssh.sh
@@ -1,10 +1,7 @@
 mkdir -p ~/.ssh
 touch ~/.ssh/id_rsa ~/.ssh/ldg_user ~/.ssh/ldg_token
-
-ssh-keyscan github.com >> ~/.ssh/known_hosts
-ssh-agent -a $SSH_AUTH_SOCK > /dev/null
-ssh-add - <<< "${OSG_ACCESS}"
-
+chmod 600 ~/.ssh/id_rsa ~/.ssh/ldg_user ~/.ssh/ldg_token
+echo ${OSG_ACCESS} > ~/.ssh/id_rsa
 echo -e "Host sugwg-test1.phy.syr.edu\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config ;
 echo -e "Host sugwg-condor.phy.syr.edu\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config ;
 echo -e "Host oasis-login.opensciencegrid.org\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config ;
