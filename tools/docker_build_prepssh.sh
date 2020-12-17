efcac772cc854aa98ec4a1416ca0f3a96e8dfa84
Ian Harry <ian.harry@ligo.org>
2020-12-17 07:55:50 -0800

    Is this it??

--- tools/docker_build_prepssh.sh
+++ tools/docker_build_prepssh.sh
@@ -1,7 +1,7 @@
 mkdir -p ~/.ssh
 touch ~/.ssh/id_rsa ~/.ssh/ldg_user ~/.ssh/ldg_token
 chmod 600 ~/.ssh/id_rsa ~/.ssh/ldg_user ~/.ssh/ldg_token
-echo ${OSG_ACCESS} > ~/.ssh/id_rsa
+echo "${OSG_ACCESS}" > ~/.ssh/id_rsa
 echo -e "Host sugwg-test1.phy.syr.edu\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config ;
 echo -e "Host sugwg-condor.phy.syr.edu\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config ;
 echo -e "Host oasis-login.opensciencegrid.org\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config ;
