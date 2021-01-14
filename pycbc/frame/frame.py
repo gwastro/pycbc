6230eaa59cf2ffc0e5f1addea8ad7213740b1c83
Tito Dal Canton <tito.canton@ligo.org>
2021-01-14 19:13:15 +0100

    PyCBC Live: use Astropy to get a more precise current GPS time (#3583)
    lal.GPSTimeNow() does not return the fractional part, which is a bit annoying
    when monitoring the O(1 s) lag of the early-warning configuration.
    
    I also simplified the logging a bit by pre-inserting the MPI rank in the format
    string.

--- pycbc/frame/frame.py
+++ pycbc/frame/frame.py
@@ -23,6 +23,7 @@ import numpy
 import math
 import os.path, glob, time
 import gwdatafind
+import pycbc
 from six.moves.urllib.parse import urlparse
 from pycbc.types import TimeSeries, zeros
 
@@ -635,7 +636,7 @@ class DataBuffer(object):
             return DataBuffer.advance(self, blocksize)
 
         except RuntimeError:
-            if lal.GPSTimeNow() > timeout + self.raw_buffer.end_time:
+            if pycbc.gps_now() > timeout + self.raw_buffer.end_time:
                 # The frame is not there and it should be by now, so we give up
                 # and treat it as zeros
                 DataBuffer.null_advance(self, blocksize)
