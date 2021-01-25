383bf7e24d55d1bfcf618831255316f2d2a78c75
ArthurTolley <arthur.tolley@port.ac.uk>
2021-01-25 15:00:56 +0000

    Removing the set_measure_level entirely

--- pycbc/strain/strain.py
+++ pycbc/strain/strain.py
@@ -92,12 +92,6 @@ def detect_loud_glitches(strain, psd_duration=4., psd_stride=2.,
     output_intermediates : {bool, False}
         Save intermediate time series for debugging.
     """
-    # don't waste time trying to optimize a single FFT
-    try:
-        pycbc.fft.fftw.set_measure_level(0)
-    except AttributeError:
-        # No FFTW, then we'd be using MKL instead
-        pass
 
     if high_freq_cutoff:
         strain = resample_to_delta_t(strain, 0.5 / high_freq_cutoff,
@@ -174,12 +168,6 @@ def detect_loud_glitches(strain, psd_duration=4., psd_stride=2.,
             int(cluster_window*strain.sample_rate))
     times = [idx * strain.delta_t + strain.start_time \
              for idx in indices[cluster_idx]]
-
-    try:
-        pycbc.fft.fftw.set_measure_level(pycbc.fft.fftw._default_measurelvl)
-    except AttributeError:
-        # Again, no FFTW, so would have been using MKL
-        pass
 
     return times
 
