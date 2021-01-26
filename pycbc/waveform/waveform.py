7d6763f19e474fd7aee754a1f986aadf96945009
Sudarshan Ghonge <sudu.ghonge@gmail.com>
2021-01-26 13:43:59 -0500

    Merge SEOBNRv4HM and IMRPhenomHM filter length calculation to avoid code duplication

--- pycbc/waveform/waveform.py
+++ pycbc/waveform/waveform.py
@@ -839,34 +839,29 @@ def imrphenomd_length_in_time(**kwds):
 def imrphenomhm_length_in_time(**kwargs):
     """Estimates the duration of IMRPhenom waveforms that include higher modes.
     """
-    if 'mode_array' in kwargs and kwargs['mode_array'] is not None:
-        maxm = max(m for _, m in kwargs['mode_array'])
-    else:
-        # the highest m for all of these is 4 (from the 4,4 mode)
-        maxm = 4
-    # we'll use the PhenomD length, with the frequency scaled by 2/m
-    try:
-        flow = kwargs['f_lower']
-    except KeyError:
-        raise ValueError("must provide a f_lower")
-    kwargs['f_lower'] = flow * 2./maxm
-    return get_imr_length("IMRPhenomD", **kwargs)
+    # Default maximum node number for IMRPhenomHM is 4
+    # The relevant lower order approximant here is IMRPhenomD
+    return get_hm_length_in_time("IMRPhenomD", 4, **kwargs)
 
 def seobnrv4hm_length_in_time(**kwargs):
     """ Estimates the duration of SEOBNRv4HM waveforms that include higher modes.
     """
+    # Default maximum node number for SEOBNRv4HM is 5 
+    # The relevant lower order approximant here is SEOBNRv4
+    return get_hm_length_in_time('SEOBNRv4', 5, **kwargs)
+
+def get_hm_length_in_time(lor_approx, maxm_default, **kwargs):
     if 'mode_array' in kwargs and kwargs['mode_array'] is not None:
         maxm = max(m for _, m in kwargs['mode_array'])
     else:
-        # the highest m for all of these is 4 (from the 4,4 mode)
-        maxm = 5
-    # we'll use the SEOBNRv4 length, with the frequency scaled by 2/m
+        maxm = maxm_default       
     try:
         flow = kwargs['f_lower']
     except KeyError:
         raise ValueError("must provide a f_lower")
     kwargs['f_lower'] = flow * 2./maxm
-    return get_imr_length("SEOBNRv4", **kwargs)
+    return get_imr_length(lor_approx, **kwargs)
+    
 
 _filter_norms["SPAtmplt"] = spa_tmplt_norm
 _filter_preconditions["SPAtmplt"] = spa_tmplt_precondition
