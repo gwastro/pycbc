232910b08b22b103b7ddaace9157b8af495c3118
Alexander Harvey Nitz <alex.nitz@gmail.com>
2021-02-04 11:42:24 +0100

    set for release

--- setup.py
+++ setup.py
@@ -124,8 +124,8 @@ def get_version_info():
         vinfo = _version_helper.generate_git_version_info()
     except:
         vinfo = vdummy()
-        vinfo.version = '1.17.dev1'
-        vinfo.release = 'False'
+        vinfo.version = '1.17.1'
+        vinfo.release = 'True'
 
     with open('pycbc/version.py', 'w') as f:
         f.write("# coding: utf-8\n")
