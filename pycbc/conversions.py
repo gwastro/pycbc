dd6968a7cba0938087304c3afa834516bf7f846b
Miriam Cabero <miriam.cabero@condor1.atlas.local>
2021-03-01 18:22:39 +0000

    spin floats

--- pycbc/conversions.py
+++ pycbc/conversions.py
@@ -1217,8 +1217,8 @@ def get_final_from_initial(mass1, mass2, spin1x=0., spin1y=0., spin1z=0.,
     for ii in range(final_mass.size):
         m1 = numpy.float(mass1[ii])
         m2 = numpy.float(mass2[ii])
-        spin1 = [spin1x[ii], spin1y[ii], spin1z[ii]]
-        spin2 = [spin2x[ii], spin2y[ii], spin2z[ii]]
+        spin1 = list(map(float, [spin1x[ii], spin1y[ii], spin1z[ii]]))
+        spin2 = list(map(float, [spin2x[ii], spin2y[ii], spin2z[ii]]))
         _, fm, fs = lalsim.SimIMREOBFinalMassSpin(m1, m2, spin1, spin2,
                                                   getattr(lalsim, approximant))
         final_mass[ii] = fm * (m1 + m2)
