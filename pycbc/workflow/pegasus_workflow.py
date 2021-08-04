82ed833f524bff772e40d68bb525cc599a347cc7
Ian Harry <ian.harry@ligo.org>
2021-08-04 03:29:42 -0700

    Missed a bit

--- pycbc/workflow/pegasus_workflow.py
+++ pycbc/workflow/pegasus_workflow.py
@@ -512,11 +512,11 @@ class Workflow(object):
             self._inputs += red_inputs
             # FIXME: If I'm now putting output_map here, all output_map stuff
             #        should move here.
-            sub.output_map_file.insert_into_dax(self._rc)
+            sub.output_map_file.insert_into_dax(self._rc, self._tc)
             sub_workflow_file = File(sub.filename)
             pfn = os.path.join(os.getcwd(), sub.filename)
             sub_workflow_file.add_pfn(pfn, site='local')
-            sub_workflow_file.insert_into_dax(self._rc)
+            sub_workflow_file.insert_into_dax(self._rc, self._tc)
 
 
         # add workflow input files pfns for local site to dax
