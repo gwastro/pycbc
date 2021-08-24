66a120a4cb510b288f02076827a3a34eb1697ca0
Gareth S Cabourn Davies <gareth.davies@ligo.org>
2021-08-24 15:54:17 +0100

    prep for release (#3768)

--- setup.py
+++ setup.py
@@ -125,8 +125,8 @@ def get_version_info():
         vinfo = _version_helper.generate_git_version_info()
     except:
         vinfo = vdummy()
-        vinfo.version = '1.18.dev2'
-        vinfo.release = 'False'
+        vinfo.version = '1.18.2'
+        vinfo.release = 'True'
 
     with open('pycbc/version.py', 'wb') as f:
         f.write("# coding: utf-8\n".encode('utf-8'))
