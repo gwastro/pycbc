60ad60efb7d48b42634bd7ef90cf8026cbf0cff0
Alexander Harvey Nitz <alex.nitz@gmail.com>
2021-08-31 16:53:52 +0200

    minor simpl

--- pycbc/events/eventmgr.py
+++ pycbc/events/eventmgr.py
@@ -348,7 +348,7 @@ class EventManager(object):
         self.accumulate.append(self.template_events)
         self.template_events = numpy.array([], dtype=self.event_dtype)
 
-    def consolidate_events(self, opt, gwstrain=None):
+    def consolidate_events(self, opt, injections=None):
         self.events = numpy.concatenate(self.accumulate)
         logging.info("We currently have %d triggers", len(self.events))
         if opt.chisq_threshold and opt.chisq_bins:
@@ -373,11 +373,10 @@ class EventManager(object):
                  log_chirp_width=opt.keep_loudest_log_chirp_window)
             logging.info("%d remaining triggers", len(self.events))
 
-        if opt.injection_window and hasattr(gwstrain, 'injections'):
+        if opt.injection_window and injections is not None:
             logging.info("Keeping triggers within %s seconds of injection",
                          opt.injection_window)
-            self.keep_near_injection(opt.injection_window,
-                                     gwstrain.injections)
+            self.keep_near_injection(opt.injection_window, injections)
             logging.info("%d remaining triggers", len(self.events))
 
         self.accumulate = [self.events]
