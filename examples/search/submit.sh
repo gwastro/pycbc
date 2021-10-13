3a9ac53f0ad9a77683152a875f7219806a6d5de3
Ian Harry <ian.harry@ligo.org>
2021-10-13 04:11:07 -0700

    Fix script

--- examples/search/submit.sh
+++ examples/search/submit.sh
@@ -1,6 +1,6 @@
 pycbc_submit_dax --no-grid --no-create-proxy \
 --local-dir ./ \
 --no-query-db \
---executation-sites condorpool_symlink \
+--execution-sites condorpool_symlink \
 --staging-sites condorpool_symlink=local \
 --dax gw.dax
