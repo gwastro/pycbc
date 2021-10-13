0707e859777861f7006151e95941eeb7a039a6e3
Ian Harry <ian.harry@ligo.org>
2021-10-13 07:57:45 -0700

    Add proper options

--- examples/search/submit.sh
+++ examples/search/submit.sh
@@ -1,6 +1,5 @@
-pycbc_submit_dax --no-grid --no-create-proxy \
+pycbc_submit_dax --dax gw.dax --no-grid --no-create-proxy \
 --local-dir ./ \
 --no-query-db \
---execution-sites condorpool_symlink \
---staging-sites condorpool_symlink=local \
---dax gw.dax
+--execution-sites condorpool_shared,condorpool_symlink,condorpool_copy \
+--staging-sites condorpool_symlink=local,condorpool_copy=local
