1e6a00e8d95851f4b75eeaf07d346c16a005c8d2
Ian Harry <ian.harry@ligo.org>
2021-10-14 06:15:59 -0700

    Remove options now not needed

--- examples/search/submit.sh
+++ examples/search/submit.sh
@@ -1,5 +1,3 @@
 pycbc_submit_dax --dax gw.dax --no-grid --no-create-proxy \
 --local-dir ./ \
---no-query-db \
---execution-sites condorpool_shared,condorpool_symlink,condorpool_copy \
---staging-sites condorpool_symlink=local,condorpool_copy=local
+--no-query-db
