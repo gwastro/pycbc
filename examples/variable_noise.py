3e6c6f5ec43620c43f2fdfebc140c06958c99511
Sebastian M. Gaebel <s-gaebel@live.de>
2021-10-22 16:47:13 +0200

    Refactored the variable noise generator to align better with existing pycbc functionality, and moved the example to a seperate script in the example folder.

---
+++ examples/variable_noise.py
@@ -0,0 +1,152 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2021 Sebastian Gaebel
+#
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 3 of the License, or (at your
+# option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+# Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+# =============================================================================
+#
+#                                   Preamble
+#
+# =============================================================================
+#
+"""
+Example to illustrate the use of the pycbc.noise.reproduceable.variable_noise
+function.
+
+@author: Sebastian Gaebel
+@email: gaebel.sebastian@gmail.com
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+from numpy import log
+from scipy.interpolate import interp1d
+from pycbc.types import FrequencySeries
+from pycbc.noise.reproduceable import \
+    generate_psd_fn_linear_in_log_transition, variable_noise
+
+
+if __name__ == '__main__':
+    # read from some plot very roughly by hand
+    rough_LIGO_noise = (np.array([10., 50., 400., 10000.]),
+                        np.array([1e-22, 4e-24, 2e-24, 1e-22]))
+    rough_ET_noise = (np.array([1.3, 60., 300., 10000.]),
+                      np.array([7e-22, 2e-25, 2e-25, 6e-24]))
+    # turn into FrequencySeries
+    freqs, amps = rough_LIGO_noise
+    _interp = interp1d(x=log(freqs), y=log(amps), kind='linear',
+                       bounds_error=True)
+    def interpolator(f):
+        return np.exp(_interp(log(f)))
+    ligo_freqs = np.linspace(rough_LIGO_noise[0][0],  # f_start
+                             rough_LIGO_noise[0][1],  # f_end
+                             1000)  # resolution, somewhat arbitrary
+    delta_f = ligo_freqs[1] - ligo_freqs[0]
+    ligo_amps = interpolator(ligo_freqs)
+    LIGO_noise = FrequencySeries(initial_array=ligo_amps, delta_f=delta_f)
+    del interpolator
+
+    freqs, amps = rough_ET_noise
+    _interp = interp1d(x=log(freqs), y=log(amps), kind='linear',
+                       bounds_error=True)
+    def interpolator(f):
+        return np.exp(_interp(log(f)))
+    et_freqs = np.linspace(rough_ET_noise[0][0],  # f_start
+                           rough_ET_noise[0][1],  # f_end
+                           1000)  # resolution, somewhat arbitrary
+    delta_f = et_freqs[1] - et_freqs[0]
+    et_amps = interpolator(et_freqs)
+    ET_noise = FrequencySeries(initial_array=et_amps, delta_f=delta_f)
+    del interpolator
+
+    psd_fn = generate_psd_fn_linear_in_log_transition(LIGO_noise, ET_noise)
+    sample_rate = 16834.
+    start_time = 12345678
+    duration = 10000
+    psd_resolution = 100000
+    filter_duration = 32.
+    time_resolution = -1000
+
+    noise = variable_noise(psd_fn=psd_fn,
+                           start_time=start_time,
+                           end_time=start_time+duration,
+                           psd_resolution=psd_resolution,
+                           filter_duration=filter_duration,
+                           seed=0,
+                           sample_rate=sample_rate,
+                           time_resolution=time_resolution,
+                           progress=True)
+    times, values = noise.sample_times, np.array(noise)
+
+    # plot the result
+
+    # Note:
+    #   while the slopes all look fine, the overall levels in my plots are off.
+    #   however since the relative noise levels are consistent, I'll chalk
+    #   that up to messing up the units or something similar in the FFT plots.
+
+    # 1st: overall noise
+    freqs = np.fft.rfftfreq(times.size, 1./sample_rate)
+    psd_0 = psd_fn(freqs, 0.)
+    psd_1 = psd_fn(freqs, 1.)
+    spectrum = np.abs(np.fft.rfft(values)) / 10**5.5
+    spectrum = np.convolve(spectrum, np.ones(100), mode='same')  # apply a bit of smoothing
+
+    plt.figure(figsize=(12, 8))
+    plt.loglog(freqs, spectrum, label='Gen. Noise')
+    plt.loglog(freqs, psd_0, label='Initial PSD')
+    plt.loglog(freqs, psd_1, label='Final PSD')
+    plt.legend()
+    plt.grid(True, which='both')
+    plt.xlim(7., 1e4)
+    plt.ylim(1e-25, 1e-21)
+    plt.tight_layout()
+    plt.savefig('test-noise-gen.png', dpi=120)
+
+    # 2nd: noise shapes at start, middle, and end in blocks
+    block_size = int(duration * sample_rate / 10)  # 10% of total
+    mid_start_idx = int(values.size//2-block_size)
+
+
+    freqs = np.fft.rfftfreq(block_size, 1./sample_rate)
+    psd_0 = psd_fn(freqs, 0.)
+    psd_0p5 = psd_fn(freqs, 0.5)
+    psd_1 = psd_fn(freqs, 1.)
+    ts_0 = values[:block_size]
+    ts_0p5 = values[mid_start_idx:mid_start_idx+block_size]
+    ts_1 = values[-block_size:]
+    spec_0 = np.abs(np.fft.rfft(ts_0)) / 10**5.
+    spec_0 = np.convolve(spec_0, np.ones(100), mode='same')  # apply a bit of smoothing
+    spec_0p5 = np.abs(np.fft.rfft(ts_0p5)) / 10**5.
+    spec_0p5 = np.convolve(spec_0p5, np.ones(100), mode='same')  # apply a bit of smoothing
+    spec_1 = np.abs(np.fft.rfft(ts_1)) / 10**5.
+    spec_1 = np.convolve(spec_1, np.ones(100), mode='same')  # apply a bit of smoothing
+
+    plt.figure(figsize=(12, 8))
+    plt.plot(freqs, spec_0, 'b', label='Start Noise')
+    plt.plot(freqs, psd_0, 'b--', label='Start PSD')
+    plt.plot(freqs, spec_0p5, 'r', label='Mid Noise')
+    plt.plot(freqs, psd_0p5, 'r--', label='Mid PSD')
+    plt.plot(freqs, spec_1, 'g', label='End Noise')
+    plt.plot(freqs, psd_1, 'g--', label='End PSD')
+    plt.xscale('log')
+    plt.yscale('log')
+    plt.legend()
+    plt.grid(True, which='both')
+    plt.xlim(7., 1e4)
+    plt.ylim(1e-26, 1e-22)
+    plt.tight_layout()
+    plt.savefig('test-noise-gen-blocks.png', dpi=120)
--- pycbc/noise/reproduceable.py
+++ pycbc/noise/reproduceable.py
@@ -24,6 +24,10 @@
 import numpy, pycbc.psd
 from pycbc.types import TimeSeries, FrequencySeries, complex_same_precision_as
 from numpy.random import RandomState
+np = numpy
+from numpy import log
+from scipy.interpolate import interp1d
+from scipy.signal import firwin2
 
 # This constant need to be constant to be able to recover identical results.
 BLOCK_SAMPLES = 1638400
@@ -219,3 +223,238 @@ def noise_from_string(psd_name, start_time, end_time,
                          sample_rate=sample_rate,
                          low_frequency_cutoff=low_frequency_cutoff,
                          filter_duration=filter_duration)
+
+
+def _compute_FIR_coefficients(freqs, amps, nyquist, n_taps):
+    """
+    Compute the FIR filter coefficients given a filter via frequency series.
+
+    Parameters
+    ----------
+    freqs : (N,) array
+        Frequencies at which the filter is defined.
+    amps : (N,) array
+        PSD amplitudes of the filter.
+    nyquist : float
+        Nyquist frequency of the time series to be filtered,
+        `nyquist = sampling_frequency / 2`.
+    n_taps : int
+        Number of "taps", i.e. FIR filter coefficients. A higher value
+        leads to a more precise approximation of the desired filter.
+
+    Returns
+    -------
+    coefficients : (n_taps,) array
+        FIR filter coefficients as computed via `scipy.signal.firwin2`.
+    """
+    # not an expert on the details, but firwin2 requires the frequencies to
+    #   go from 0 to 1 (nyquist scale) and the gain to be 0 at the endpoints
+    f_scaled = freqs / nyquist  # firwin2 works in units of nyquist
+    f_scaled[0] = 0. # similarly, frequencies need to start at 0
+    f_scaled[-1] = 1.  # brute force the frequency value, floating point math sucks
+    amps[0] = 0.
+    amps[-1] = 0.
+    assert f_scaled[-1] == 1., repr(f_scaled[-1])  # last element should be 1
+    coefficients = firwin2(numtaps=n_taps, freq=f_scaled, gain=amps)
+    return coefficients
+
+
+def variable_noise(
+        psd_fn, start_time, end_time, psd_resolution, filter_duration=128.,
+        seed=0, sample_rate=16384., time_resolution=-1000, progress=False):
+    """ Create noise from a time dependent PSD
+
+    Parameters
+    ----------
+    psd_fn : callable(array, float) -> FrequencySeries
+        Function which provides the time varying PSD. Must accept a call
+        signature of `psd_fn(freqs, tau)`, where `freqs` is a numpy array
+        of frequencies in [Hz], and `tau` is the normalised time, i.e.
+        `current_time / duration` if the start time were at 0. Must return
+        a FrequencySeries with sample_frequencies=`freqs`. Ideally, the
+        Frequency series covers the interval (0, nyquist), the endpoints
+        will be set to 0 due to constraints in the filter construction.
+    start_time : int
+        Start time in GPS seconds to generate noise
+    end_time : int
+        End time in GPS seconds to generate nosie
+    psd_resolution : int
+        Number of points for which the psd_fn in evaluated before being
+        passed to the filter generation. Using a large number should be
+        generally harmless and without side effects. Recommended to be
+        a few time the filter resolution `n_taps`.
+    filter_duration : {128, float}
+        Length of the filter in [sec]. From this and ´sample_rate´ the
+        number of FIR filter coefficients is calculated as
+        ´int(filter_duration * sample_rate)´.
+    seed : {None, int}
+        Seed used to generate the noise.
+    sample_rate : {16384, float}
+        Sample rate of the generate time series in units of [sec]. Required
+        as the filter design depends on the Nyquist frequency.
+    time_resolution : {-1000, float}
+        The FIR filter is recomputed every `time_resolution` seconds to
+        smoothly vary the noise PSD over the length of the time series.
+        This directly impacts performance as filter application is much
+        cheaper than the recomputing of filter coefficients.
+        If the value is larger than the number of samples, a warning is
+        issued if `quiet` is not set to `True`. If the value is negative,
+        the filter coefficient will be recomputed at ´int(time_resolution)´
+        evenly spaced break points.
+    progress : {False, bool}
+        If `True`, display a simple progress counter.
+
+    Returns
+    -------
+    noisy_time_series : TimeSeries
+        A TimeSeries containing noise with a time variant PSD.
+
+    Notes
+    -----
+    Currently, the overall noise level may be off by some constant factor.
+    Check this and remove the warning before serious use.
+
+    Reference for the FIR filter application:
+        https://en.wikipedia.org/wiki/Finite_impulse_response#Definition
+
+    """
+    def zero_pad_left(arr, n):
+        """
+        Zero pad an array to a given size. Zeros are added on the left,
+        i.e. at the beginning.
+
+        Parameters
+        ----------
+        arr : (N,) array
+            Some array of floats.
+        n : int
+            Size to which the array is to be padded.
+
+        Returns
+        -------
+        padded : (n,) array
+            Left padded array.
+
+        """
+        if arr.size >= n:
+            return arr
+        padded = np.concatenate([np.zeros(n - arr.size), arr])
+        return padded
+
+    assert time_resolution != 0
+
+    # first: compute some essential constants
+    duration = end_time - start_time
+    n_taps = int(sample_rate * filter_duration)
+    nyquist = sample_rate / 2.  # [Hz]
+    n_samples = int(duration * sample_rate)
+    #   use some very small number here, f[0] will be set to 0 later anyways
+    freqs = np.geomspace(1e-100, nyquist, psd_resolution)
+
+    # generate white noise, which will then be filtered to
+    #   obtain the desired PSD.
+    white_time_series = normal(start_time, end_time, sample_rate, seed)
+    RNG = np.random.default_rng(seed)
+    white_time_series = RNG.standard_normal(size=n_samples)
+    assert white_time_series.size == n_samples
+
+    # prepare TimeSeries for filter output
+    noisy_time_series = white_time_series.copy()
+
+    # precompute the indices for re-computation of the FIR coefficients
+    # first: turn the time between recomputations into a number of samples
+    #   note to treat the negative time_resolution case seperately
+    if time_resolution < 0:
+        idx_step = int(n_samples / abs(int(time_resolution)))
+    else:
+        idx_step = int(duration / time_resolution)
+    assert idx_step > 0
+    break_indices = range(0, n_samples, idx_step)
+
+    # This loop is a prime candidate for optimisation,
+    #   e.g. pre-computing FIR coefficients and then dropping into cython
+    #   for the convolution.
+    for idx in range(n_samples):
+        # ^ no enumerate since the samples are only used in chunks
+        tau = float(idx) / n_samples
+        if idx in break_indices:
+            if progress:
+                print('Recompute at sample idx {} of {} ({:.1%})'
+                      ''.format(idx, n_samples, idx/n_samples), flush=True)
+            # re-compute the coefficients
+            amps = psd_fn(freqs, tau)
+            assert amps.size == psd_resolution
+            coeffs = _compute_FIR_coefficients(freqs, amps, nyquist, n_taps)
+            # as a quirk of definition, the coefficients are time reversed
+            coeffs = coeffs[::-1]
+            assert coeffs.size == n_taps
+        # select the chunk of the time series which is used by the filter
+        chunk = zero_pad_left(white_time_series[max(idx-n_taps, 0)+1 : idx+1],
+                              n_taps)
+        #   max-term : avoid selecting before element 0
+        #   idx+1 : the current sample needs to be included
+        # convolution
+        noisy_time_series[idx] = np.sum(chunk * coeffs)
+
+    return noisy_time_series
+
+
+# example for creating the input `psd_fn`
+
+def generate_psd_fn_linear_in_log_transition(psd_0, psd_1,
+                                             psd_fill_value=1e-100,
+                                             frequency_gap=1e-3):
+    """
+    Generate a psd_fn according to the requirements of ´variable_noise(...)´
+
+    psd_0 and psd_1 should start & end at the same frequencies, otherwise
+    their values are transitioned to/from the psd_fill_value. The
+    transition between psd_0 and psd_1 is linear in log-log space, i.e.
+    psd(f) = exp(tau * psd_0(f) + (1 - tau) * psd_1(f)).
+
+    Parameters
+    ----------
+    psd_0 : FrequencySeries
+        PSD at the start of the noise segment.
+    psd_1 : FrequencySeries
+        PSD at the end of the noise segment.
+    psd_fill_value : {1e-100, float}
+        Filler value outside the defined frequency range.
+    frequency_gap : {1e-3, float}
+        Interval in [Hz] over which the PSDs are transitioned to the
+        filler value at the boundaries.
+
+    Returns
+    -------
+    psd_fn : FrequencySeries
+        psd_fn(freqs, tau) as consumed by ´variable_noise(...)´.
+
+    """
+    freqs_0 = psd_0.sample_frequencies
+    gains_0 = np.array(psd_0)
+    freqs_1 = psd_1.sample_frequencies
+    gains_1 = np.array(psd_1)
+    # add the transition gaps
+    freqs_0 = np.concatenate([[freqs_0[0] - frequency_gap], freqs_0,
+                              [freqs_0[-1] + frequency_gap]])
+    gains_0 = np.concatenate([[psd_fill_value], gains_0, [psd_fill_value]])
+    freqs_1 = np.concatenate([[freqs_1[0] - frequency_gap], freqs_1,
+                              [freqs_1[-1] + frequency_gap]])
+    gains_1 = np.concatenate([[psd_fill_value], gains_1, [psd_fill_value]])
+    # insert a transition from f_low and
+    interpolant_0 = interp1d(x=log(psd_0.sample_frequencies),
+                             y=log(np.array(psd_0)), kind='linear',
+                             bounds_error=False,
+                             fill_value=log(psd_fill_value))
+    interpolant_1 = interp1d(x=log(psd_1.sample_frequencies),
+                             y=log(np.array(psd_1)), kind='linear',
+                             bounds_error=False,
+                             fill_value=log(psd_fill_value))
+
+    def psd_fn(freqs, tau):
+        assert 0. <= tau, repr(tau)
+        assert tau <= 1., repr(tau)
+        return np.exp(tau * interpolant_0(log(freqs))
+                      + (1. - tau) * interpolant_1(log(freqs)))
+
+    return psd_fn
