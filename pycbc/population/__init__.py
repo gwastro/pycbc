4cb8432b075e07f126ea0bd1be5a8a35d25fe809
WuShichao <wushichao@mail.bnu.edu.cn>
2021-11-02 12:16:30 +0100

    add population_models module into __init__.py

--- pycbc/population/__init__.py
+++ pycbc/population/__init__.py
@@ -2,3 +2,4 @@ from pycbc.population.rates_functions import *
 from pycbc.population.scale_injections import *
 from pycbc.population.fgmc_functions import *
 from pycbc.population.fgmc_laguerre import *
+from pycbc.population.population_models import *
