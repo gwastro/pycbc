a91e896d983c33d6c19288b6badcff5b450e4d71
Tito Dal Canton <tito.dalcanton@ijclab.in2p3.fr>
2021-11-26 15:04:29 +0000

    Fix typo

--- examples/dataquality/on.py
+++ examples/dataquality/on.py
@@ -1,7 +1,8 @@
 """This example shows how to determine when a detector is active."""
 
-import matplotlib.pp as pp
+import matplotlib.pyplot as pp
 from pycbc import dq
+from pycbc.results import ifo_color
 
 
 start_time = 1126051217
@@ -16,11 +17,11 @@ lsegs = dq.query_flag('L1', 'DATA', start_time, end_time)
 pp.figure(figsize=[10,2])
 for seg in lsegs:
     start, end = seg
-    pp.axvspan(start, end, color='green', ymin=0.1, ymax=0.4)
+    pp.axvspan(start, end, color=ifo_color('L1'), ymin=0.1, ymax=0.4)
 
 for seg in hsegs:
     start, end = seg
-    pp.axvspan(start, end, color='red', ymin=0.6, ymax=0.9)
+    pp.axvspan(start, end, color=ifo_color('H1'), ymin=0.6, ymax=0.9)
 
 pp.xlabel('Time (s)')
 pp.show()
