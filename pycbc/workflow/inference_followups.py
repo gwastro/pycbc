cbef6754af724a416d9f14a2e3e4c0bdad362738
Shilpa Kastha <shilpa.kastha@condor1.atlas.local>
2021-12-22 12:01:16 +0000

    fixed the codeclimate issues

--- pycbc/workflow/inference_followups.py
+++ pycbc/workflow/inference_followups.py
@@ -999,8 +999,8 @@ def make_posterior_workflow(workflow, samples_files, config_file, label,
     if make_prior:
         base = "priors/{}".format(label)
         prior_plots += make_inference_prior_plot(
-		workflow, config_file, rdir[base],
-		analysis_seg=workflow.analysis_time, tags=tags+[label])
+        workflow, config_file, rdir[base],
+        analysis_seg=workflow.analysis_time, tags=tags+[label])
         layout.single_layout(rdir[base], prior_plots)
     return posterior_file, summary_files, prior_plots, posterior_plots
 
