5808d4e17e98b64d475c24f68a9ea6bd12dafa6f
Ian Harry <iwharry@googlemail.com>
2022-02-23 11:26:11 +0000

    Some fixes to test suite - focused on MACOS usage (#3899)
    * Some warning fixes
    
    * Update tox.ini to work on MacOS properly
    
    * Use with numpy.errstate ... Thanks Duncan!
    
    * Missed two lines to remove
    
    * Seems I now need to explicitly add conda-forge(???)
    
    * Set directory permission
    
    * Update basic-tests.yml
    
    * Testing now
    
    * Still testing
    
    * Update basic-tests.yml
    
    * Testing again
    
    * Update basic-tests.yml
    
    * Make cache dir global writeable
    
    * Update mac-test.yml
    
    * Doesn't work with openssl 3
    
    * Update tox.ini
    
    * Update conversions.py
    
    * Update test_skymax.py

--- pycbc/distributions/uniform.py
+++ pycbc/distributions/uniform.py
@@ -96,11 +96,10 @@ class Uniform(bounded.BoundedDist):
         super(Uniform, self).__init__(**params)
         # compute the norm and save
         # temporarily suppress numpy divide by 0 warning
-        numpy.seterr(divide='ignore')
-        self._lognorm = -sum([numpy.log(abs(bnd[1]-bnd[0]))
-                                    for bnd in self._bounds.values()])
-        self._norm = numpy.exp(self._lognorm)
-        numpy.seterr(divide='warn')
+        with numpy.errstate(divide="ignore"):
+            self._lognorm = -sum([numpy.log(abs(bnd[1]-bnd[0]))
+                                  for bnd in self._bounds.values()])
+            self._norm = numpy.exp(self._lognorm)
 
     @property
     def norm(self):
