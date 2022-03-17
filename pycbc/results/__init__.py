dc80fb73bd94732f7ddecc5a7dbd80800e5c3a92
Francesco Pannarale <francesco.pannarale@ligo.org>
2022-03-17 05:46:26 -0700

    Cleaning up old PyGRB results webpage material

--- pycbc/results/__init__.py
+++ pycbc/results/__init__.py
@@ -3,7 +3,6 @@ from pycbc.results.metadata import *
 from pycbc.results.versioning import *
 from pycbc.results.color import *
 from pycbc.results.plot import *
-from pycbc.results.legacy_grb import *
 from pycbc.results.layout import *
 from pycbc.results.dq import *
 from pycbc.results.str_utils import *
