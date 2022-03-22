0ad2b67762b574894524174a86455cd4bc4b6c79
GarethCabournDavies <gareth.cabourndavies@ligo.org>
2022-03-22 05:52:56 -0700

    revert testing change

--- pycbc/events/stat.py
+++ pycbc/events/stat.py
@@ -1527,7 +1527,7 @@ class ExpFitFgBgNormStatistic(PhaseTDStatistic,
         # coincs
         # Extent of time-difference space occupied
         noise_twindow = coinc_rate.multiifo_noise_coincident_area(
-                            self.ifos, kwargs['time_addition'])
+                            self.hist_ifos, kwargs['time_addition'])
         # Volume is the allowed time difference window, multiplied by 2pi for
         # each phase difference dimension and by allowed range of SNR ratio
         # for each SNR ratio dimension : there are (n_ifos - 1) dimensions
