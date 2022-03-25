d0241c618516ac3026e1e691e38a26b04d3365d8
GarethCabournDavies <gareth.cabourndavies@ligo.org>
2022-03-25 03:50:45 -0700

    html folder location

--- examples/search/gen.sh
+++ examples/search/gen.sh
@@ -4,4 +4,5 @@ set -e
 pycbc_make_coinc_search_workflow \
 --workflow-name gw \
 --output-dir output \
---config-files analysis.ini plotting.ini executables.ini injections_minimal.ini
+--config-files analysis.ini plotting.ini executables.ini injections_minimal.ini \
+--config-overrides results_page:output-path:$(pwd)/html
