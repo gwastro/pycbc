09a81969796e17aa655a32930ab4d6085164725e
Alexander Harvey Nitz <alex.nitz@gmail.com>
2022-03-29 15:42:45 +0200

    cc

--- pycbc/inference/jump/angular.py
+++ pycbc/inference/jump/angular.py
@@ -15,8 +15,6 @@
 
 """Jump proposals that use cyclic boundaries on [0, 2pi)."""
 
-
-
 from epsie import proposals as epsie_proposals
 
 from .normal import (epsie_from_config, epsie_adaptive_from_config,
