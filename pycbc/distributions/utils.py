8f0698581c6a1266f8348a79d3206af1f00e6ca4
soumide1102 <soumide@lanl.gov>
2022-03-31 15:01:14 -0700

    Adding back 1 blank line to address codeclimate issue

--- pycbc/distributions/utils.py
+++ pycbc/distributions/utils.py
@@ -31,6 +31,7 @@ from pycbc.types.config import InterpolatingConfigParser
 from pycbc import transforms
 from pycbc import distributions
 
+
 def draw_samples_from_config(path, num=1, seed=150914):
     r""" Generate sampling points from a standalone .ini file.
 
