71a27f2769af26cc6b0bf3ee0e6372e674338b25
Alexander Harvey Nitz <alex.nitz@gmail.com>
2022-05-11 19:44:53 +0200

    modularize which will help later

--- pycbc/inference/models/relbin_cpu.pyx
+++ pycbc/inference/models/relbin_cpu.pyx
@@ -9,72 +9,70 @@ cdef extern from "complex.h":
 cimport cython
 
 cpdef likelihood_parts_multi(double [::1] freqs,
+                     double fp,
+                     double fc,
+                     double dtc,
+                     double complex[::1] hp,
+                     double complex[::1] hc,
+                     double complex[::1] h00,
+                     double fp2,
+                     double fc2,
+                     double dtc2,
+                     double complex[::1] hp2,
+                     double complex[::1] hc2,
+                     double complex[::1] h002,
+                     double complex[::1] a0,
+                     double complex[::1] a1,
+                     ) :
+    cdef size_t i
+    cdef double complex hd=0, r0, r0n, r1
+
+    N = freqs.shape[0]
+    for i in range(N):
+        r0n = (exp(-2.0j * 3.141592653 * dtc * freqs[i])
+               * (fp * hp[i] + fc * hc[i])) / h00[i]
+        r0n *= conj((exp(-2.0j * 3.141592653 * dtc2 * freqs[i])
+               * (fp2 * hp2[i] + fc2 * hc2[i])) / h002[i])
+        r1 = r0n - r0
+        if i > 0:
+            hd += a0[i-1] * r0 + a1[i-1] * r1
+
+        r0 = r0n
+    return hd
+
+cpdef likelihood_parts_multi_v(double [::1] freqs,
                      double[::1] fp,
                      double[::1] fc,
                      double[::1] dtc,
-                     double complex[:, ::1] hp,
-                     double complex[:, ::1] hc,
+                     double complex[::1] hp,
+                     double complex[::1] hc,
                      double complex[::1] h00,
+                     double[::1] fp2,
+                     double[::1] fc2,
+                     double[::1] dtc2,
+                     double complex[::1] hp2,
+                     double complex[::1] hc2,
+                     double complex[::1] h002,
                      double complex[::1] a0,
                      double complex[::1] a1,
-                     double [::1] b0,
-                     double [::1] b1,
+
                      ) :
     cdef size_t i
     cdef double complex hd=0, r0, r0n, r1
-    cdef double hh=0
 
     N = freqs.shape[0]
-    HNUM = hp.shape[0]
     for i in range(N):
-        r0n = 0
-        for j in range(HNUM):
-            r0n += (exp(-2.0j * 3.141592653 * dtc[j] * freqs[i])
-                   * (fp[j] * hp[j][i] + fc[j] * hc[j][i]))
-
-        r0n /= h00[i]
+        r0n = (exp(-2.0j * 3.141592653 * dtc[i] * freqs[i])
+               * (fp[i] * hp[i] + fc[i] * hc[i])) / h00[i]
+        r0n *= conj((exp(-2.0j * 3.141592653 * dtc2[i] * freqs[i])
+               * (fp2[i] * hp2[i] + fc2[i] * hc2[i])) / h002[i])
         r1 = r0n - r0
 
         if i > 0:
             hd += a0[i-1] * r0 + a1[i-1] * r1
-            hh += b0[i-1] * norm(r0) + 2.0 * b1[i-1] * real(r1 * conj(r0))
 
         r0 = r0n
-    return hd, hh
-
-cpdef likelihood_parts_multi_v(double [::1] freqs,
-                     double[:, ::1] fp,
-                     double[:, ::1] fc,
-                     double[:, ::1] dtc,
-                     double complex[:, ::1] hp,
-                     double complex[:, ::1] hc,
-                     double complex[::1] h00,
-                     double complex[::1] a0,
-                     double complex[::1] a1,
-                     double [::1] b0,
-                     double [::1] b1,
-                     ) :
-    cdef size_t i
-    cdef double complex hd=0, r0, r0n, r1
-    cdef double hh=0
-
-    N = freqs.shape[0]
-    HNUM = hp.shape[0]
-    for i in range(N):
-        r0n = 0
-        for j in range(HNUM):
-            r0n += (exp(-2.0j * 3.141592653 * dtc[j][i] * freqs[i])
-                   * (fp[j][i] * hp[j][i] + fc[j][i] * hc[j][i]))
-
-        r0n /= h00[i]
-        r1 = r0n - r0
-
-        if i > 0:
-            hd += a0[i-1] * r0 + a1[i-1] * r1
-            hh += b0[i-1] * norm(r0) + 2.0 * b1[i-1] * real(r1 * conj(r0))
-
-        r0 = r0n
-    return hd, hh
+    return hd
 
 cpdef likelihood_parts(double [::1] freqs,
                      double fp,
@@ -89,7 +87,7 @@ cpdef likelihood_parts(double [::1] freqs,
                      double [::1] b1,
                      ) :
     cdef size_t i
-    cdef double complex hd=0, r0, r0n, r1
+    cdef double complex hd=0, r0, r0n, r1, x0, x1, x0n;
     cdef double hh=0
 
     N = freqs.shape[0]
@@ -98,11 +96,15 @@ cpdef likelihood_parts(double [::1] freqs,
                * (fp * hp[i] + fc * hc[i])) / h00[i]
         r1 = r0n - r0
 
+        x0n = norm(r0n)
+        x1 = x0n - x0
+
         if i > 0:
             hd += a0[i-1] * r0 + a1[i-1] * r1
-            hh += b0[i-1] * norm(r0) + 2.0 * b1[i-1] * real(r1 * conj(r0))
+            hh += real(b0[i-1] * x0 + b1[i-1] * x1)
 
         r0 = r0n
+        x0 = x0n
     return hd, hh
 
 cpdef likelihood_parts_v(double [::1] freqs,
@@ -118,7 +120,7 @@ cpdef likelihood_parts_v(double [::1] freqs,
                      double [::1] b1,
                      ) :
     cdef size_t i
-    cdef double complex hd=0, r0, r0n, r1
+    cdef double complex hd=0, r0, r0n, r1, x0, x0n, x1
     cdef double hh=0
 
     N = freqs.shape[0]
@@ -127,9 +129,13 @@ cpdef likelihood_parts_v(double [::1] freqs,
                * (fp[i] * hp[i] + fc[i] * hc[i])) / h00[i]
         r1 = r0n - r0
 
+        x0n = norm(r0n)
+        x1 = x0n - x0
+
         if i > 0:
             hd += a0[i-1] * r0 + a1[i-1] * r1
-            hh += b0[i-1] * norm(r0) + 2.0 * b1[i-1] * real(r1 * conj(r0))
+            hh += real(b0[i-1] * x0 + b1[i-1] * x1)
 
         r0 = r0n
+        x0 = x0n
     return hd, hh
