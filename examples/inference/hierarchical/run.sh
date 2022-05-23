32896106b8626edc5151e909bc6efb21aa4c3eb9
Collin Capano <collin.capano@aei.mpg.de>
2022-05-23 19:05:01 +0000

    tweak example

--- examples/inference/hierarchical/run.sh
+++ examples/inference/hierarchical/run.sh
@@ -1,6 +1,6 @@
 pycbc_inference \
     --config-files model.ini model-event1_relbin.ini model-event2_relbin.ini prior.ini data.ini dynesty.ini \
-    --nprocesses 4 \
+    --nprocesses 8 \
     --output-file hierarchical.hdf \
     --seed 10 \
     --force \
