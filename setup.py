8ab8abc409fa7800643b1ef45ab19fca98cd3229
Alex Nitz <alex.nitz@gmail.com>
2022-05-26 14:05:59 +0200

    Update setup.py (#4036)

--- setup.py
+++ setup.py
@@ -122,8 +122,8 @@ def get_version_info():
         vinfo = _version_helper.generate_git_version_info()
     except:
         vinfo = vdummy()
-        vinfo.version = '2.0.dev3'
-        vinfo.release = 'False'
+        vinfo.version = '2.0.3'
+        vinfo.release = 'True'
 
     with open('pycbc/version.py', 'wb') as f:
         f.write("# coding: utf-8\n".encode('utf-8'))
