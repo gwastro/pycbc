aa2a9391167cf7ceb8179f1e6bd1e5398f940379
Jacob Buchanan <jacob.buchanan.19@cnu.edu>
2022-07-26 14:14:38 -0400

    Improvements on previous commit

--- pycbc/workflow/grb_utils.py
+++ pycbc/workflow/grb_utils.py
@@ -42,6 +42,23 @@ from pycbc.workflow.jobsetup import select_generic_executable
 
 
 def select_grb_pp_class(curr_exe):
+    """
+    This function returns the executable class for PyGRB post-processing scripts.
+
+    Parameters
+    ----------
+    curr_exe : string
+        The name of the executable
+
+    Returns
+    -------
+    exe_class : Sub-class of pycbc.workflow.core.Executable that holds utility
+        functions appropriate for the given executable.  Instances of the class
+        ('jobs') **must** have methods
+        * job.create_node()
+        and
+        * job.get_valid_times(ifo, )
+    """
     exe_to_class_map = {
         'pycbc_grb_trig_combiner'  : PycbcGrbTrigCombinerExecutable,
         'pycbc_grb_trig_cluster'   : PycbcGrbTrigClusterExecutable,
@@ -508,33 +525,6 @@ class PycbcGrbTrigClusterExecutable(Executable):
         node.add_output(out_file)
 
         return node, out_file
-
-
-class PycbcHDFSplitInjExecutable(Executable):
-    """ The class responsible for creating jobs for ``pycbc_hdf_splitinj``.
-    """
-    current_retention_level = Executable.ALL_TRIGGERS
-
-    def __init__(self, cp, exe_name, num_splits, ifo=None, out_dir=None):
-        super().__init__(cp, exe_name, ifo, out_dir, tags=[])
-        self.num_splits = int(num_splits)
-
-    def create_node(self, parent, tags=None):
-        if tags is None:
-            tags = []
-        node = Node(self)
-        node.add_input_opt('--input-file', parent)
-        out_files = FileList([])
-        for i in range(self.num_splits):
-            curr_tag = 'split%d' % i
-            curr_tags = parent.tags + [curr_tag]
-            job_tag = parent.description + "_" + self.name.upper()
-            out_file = File(parent.ifo_list, job_tag, parent.segment,
-                            extension='.hdf', directory=self.out_dir,
-                            tags=curr_tags, store_file=self.retain_files)
-            out_files.append(out_file)
-        node.add_output_list_opt('--output-files', out_files)
-        return node
 
 
 class PycbcGrbInjFinderExecutable(Executable):
