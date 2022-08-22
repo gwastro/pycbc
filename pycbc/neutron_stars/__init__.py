3f26b524135d7b51a8b293994ef5ec9feb87eae2
Samuel Higginbotham <samuel.higginbotham@ligo.org>
2022-08-22 19:03:50 +0100

    moved module level imports to top of init file

--- pycbc/neutron_stars/__init__.py
+++ pycbc/neutron_stars/__init__.py
@@ -1,3 +1,5 @@
+from pycbc.neutron_stars.eos_utils import *
+from pycbc.neutron_stars.pg_isso_solver import *
 import os.path
 # Setup the directory with the NS equilibrium sequence(s)
 NS_DATA_DIRECTORY = os.path.join(
@@ -5,5 +7,3 @@ NS_DATA_DIRECTORY = os.path.join(
 NS_SEQUENCES = [
     f.replace('equil_', '').replace('.dat', '')
     for f in os.listdir(NS_DATA_DIRECTORY) if f.endswith('.dat')]
-from pycbc.neutron_stars.eos_utils import *
-from pycbc.neutron_stars.pg_isso_solver import *
