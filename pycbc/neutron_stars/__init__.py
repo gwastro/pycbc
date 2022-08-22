d44c6dacec21f11b94b21dcc0c493d31287cab27
Samuel Higginbotham <samuel.higginbotham@ligo.org>
2022-08-22 19:32:52 +0100

    moved module level imports back to bottom of init file as moving to top seemed to break code

--- pycbc/neutron_stars/__init__.py
+++ pycbc/neutron_stars/__init__.py
@@ -1,5 +1,3 @@
-from pycbc.neutron_stars.eos_utils import *
-from pycbc.neutron_stars.pg_isso_solver import *
 import os.path
 # Setup the directory with the NS equilibrium sequence(s)
 NS_DATA_DIRECTORY = os.path.join(
@@ -7,3 +5,5 @@ NS_DATA_DIRECTORY = os.path.join(
 NS_SEQUENCES = [
     f.replace('equil_', '').replace('.dat', '')
     for f in os.listdir(NS_DATA_DIRECTORY) if f.endswith('.dat')]
+from pycbc.neutron_stars.eos_utils import *
+from pycbc.neutron_stars.pg_isso_solver import *
