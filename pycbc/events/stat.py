01b70db6ec58a2be21002595b756bfac8d995c7e
GarethCabournDavies <gareth.cabourndavies@ligo.org>
2022-09-08 08:57:00 -0700

    Revert bad commit

--- pycbc/events/stat.py
+++ pycbc/events/stat.py
@@ -375,8 +375,14 @@ class PhaseTDStatistic(QuadratureSumStatistic):
         param = {}
 
         with h5py.File(self.files[selected], 'r') as histfile:
-            self.hist_ifos = [i.decode('UTF-8')
-                              for i in histfile.attrs['ifos']]
+            self.hist_ifos = histfile.attrs['ifos']
+
+            # Patch for pre-hdf5=3.0 histogram files
+            try:
+                logging.info("Decoding hist ifos ..")
+                self.hist_ifos = [i.decode('UTF-8') for i in self.hist_ifos]
+            except (UnicodeDecodeError, AttributeError):
+                pass
 
             # Histogram bin attributes
             self.twidth = histfile.attrs['twidth']
