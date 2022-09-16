5147f39edbf73f1d439efb240cc08a93513df0b1
Alex Nitz <alex.nitz@gmail.com>
2022-09-16 22:13:45 +0200

    time / sky marginalization  + rework / simplify marginalization scripts (#4093)
    * speed up reconstruction, add in ra / dec / time marg to single_template model
    
    * fix
    
    * fix
    
    * use old config for workflow
    
    * update

--- examples/inference/single/run.sh
+++ examples/inference/single/run.sh
@@ -1,7 +1,25 @@
 pycbc_inference \
 --config-file `dirname "$0"`/single.ini \
---nprocesses=1 \
+--nprocesses=2 \
 --output-file single.hdf \
 --seed 0 \
 --force \
 --verbose
+
+# This reconstructs any marginalized parameters
+# and would be optional if you don't need them or 
+# have sampled over all parameters directly (see single.ini)
+pycbc_inference_model_stats \
+--input-file single.hdf \
+--output-file single2.hdf \
+--nprocesses 2 \
+--reconstruct-parameters \
+--force \
+--verbose
+
+pycbc_inference_plot_posterior \
+--input-file single2.hdf \
+--output-file single.png \
+--parameters distance inclination polarization coa_phase tc ra dec \
+--z-arg snr --vmin 31.85 --vmax 32.15 \
+
