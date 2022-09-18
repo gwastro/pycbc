5e369cbbb94e30f7629904754c03ba557aefd35d
Gareth S Cabourn Davies <gareth.cabourndavies@ligo.org>
2022-09-18 18:54:10 +0100

    add statmap files for use with single-detector triggers (#4050)
    * add statmap files for use with single-detector triggers
    
    * Fix a CC issue for local variables
    
    * CC

--- pycbc/events/significance.py
+++ pycbc/events/significance.py
@@ -91,7 +91,7 @@ def count_n_louder(bstat, fstat, dec, skip_background=False,
 
 
 def n_louder_from_fit(back_stat, fore_stat, dec_facs,
-                      fit_func='exponential', fit_thresh=0):
+                      fit_function='exponential', fit_threshold=0):
     """
     Use a fit to events in back_stat in order to estimate the
     distribution for use in recovering the estimate count of louder
@@ -104,10 +104,10 @@ def n_louder_from_fit(back_stat, fore_stat, dec_facs,
         Array of the foreground statistic values or single value
     dec_facs: numpy.ndarray
         Array of the decimation factors for the background statistics
-    fit_func: str
+    fit_function: str
         Name of the function to be used for the fit to background
         statistic values
-    fit_thresh: float
+    fit_threshold: float
         Threshold above which triggers use the fitted value, below this
         the counted number of louder events will be used
 
@@ -122,15 +122,15 @@ def n_louder_from_fit(back_stat, fore_stat, dec_facs,
     """
 
     # Calculate the fitting factor of the ranking statistic distribution
-    alpha, _ = trstats.fit_above_thresh(fit_func, back_stat,
-                                        thresh=fit_thresh,
+    alpha, _ = trstats.fit_above_thresh(fit_function, back_stat,
+                                        thresh=fit_threshold,
                                         weights=dec_facs)
 
     # Count background events above threshold as the cum_fit is
     # normalised to 1
-    bg_above = back_stat > fit_thresh
-    bg_above_thresh = np.sum(dec_facs[bg_above])
-    fg_above = fore_stat > fit_thresh
+    bg_above = back_stat > fit_threshold
+    n_above = np.sum(dec_facs[bg_above])
+    fg_above = fore_stat > fit_threshold
 
     # These will be overwritten, but just to silence a warning
     # in the case where trstats.cum_fit returns zero
@@ -138,10 +138,14 @@ def n_louder_from_fit(back_stat, fore_stat, dec_facs,
     fnlouder = np.zeros_like(fore_stat)
 
     # Ue the fit above the threshold
-    back_cnum[bg_above] = trstats.cum_fit(fit_func, back_stat[bg_above],
-                                          alpha, fit_thresh) * bg_above_thresh
-    fnlouder[fg_above] = trstats.cum_fit(fit_func, fore_stat[fg_above],
-                                         alpha, fit_thresh) * bg_above_thresh
+    back_cnum[bg_above] = n_above * trstats.cum_fit(fit_function,
+                                                    back_stat[bg_above],
+                                                    alpha,
+                                                    fit_threshold)
+    fnlouder[fg_above] = n_above * trstats.cum_fit(fit_function,
+                                                   fore_stat[fg_above],
+                                                   alpha,
+                                                   fit_threshold)
 
     # Below the fit threshold, we expect there to be sufficient events
     # to use the count_n_louder method, and the distribution may deviate
@@ -149,8 +153,16 @@ def n_louder_from_fit(back_stat, fore_stat, dec_facs,
     fg_below = np.logical_not(fg_above)
     bg_below = np.logical_not(bg_above)
 
+    # Count the number of below-threshold background events louder than the
+    # bg and foreground
     back_cnum[bg_below], fnlouder[fg_below] = \
-        count_n_louder(back_stat, fore_stat, dec_facs)
+        count_n_louder(back_stat[bg_below], fore_stat[fg_below], dec_facs)
+
+    # As we have only counted the louder below-threshold events, need to
+    # add the above threshold events, whcih by definition are louder than
+    # all the below-threshold events
+    back_cnum += n_above
+    fnlouder += n_above
 
     return back_cnum, fnlouder
 
