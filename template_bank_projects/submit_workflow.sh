5342d2aa27990b45f638fe8f787e6c50a3eccf46
Marion Pillas <marion.pillas@ldas-pcdev1.ligo.caltech.edu>
2022-10-20 06:18:38 -0700

    changes in submit file

--- template_bank_projects/submit_workflow.sh
+++ template_bank_projects/submit_workflow.sh
@@ -1,11 +1,10 @@
 cd output
 
 pycbc_submit_dax \
---append-pegasus-property \
-    "pegasus_profile:condor|request_disk:10000000" \
-    "pegasus_profile:condor|request_gpus:1" \
-    "pegasus_profile:condor|request_memory:4096" \
-    "pegasus_profile:condor|when_to_transfer_output:ON_SUCCESS" \
-    "pegasus_profile:condor|success_exit_code:0" \
-    "pegasus_profile:condor|accounting_group:ligo.dev.o4.pycbc.offline" \
-    
+    --append-pegasus-property 'pegasus_profile:condor|accounting_group:ligo.dev.o4.cbc.uber.pycbcoffline' \
+    --append-pegasus-property 'pegasus.transfer.bypass.input.staging=true' \
+    --append-pegasus-property "pegasus_profile:condor|request_disk:10000000" \
+    --append-pegasus-property "pegasus_profile:condor|request_gpus:1" \
+    --append-pegasus-property "pegasus_profile:condor|request_memory:4096" \
+    --append-pegasus-property "pegasus_profile:condor|when_to_transfer_output:ON_SUCCESS" \
+    --append-pegasus-property "pegasus_profile:condor|success_exit_code:0" \
