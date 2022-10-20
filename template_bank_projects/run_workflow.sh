6ea63722b30c1614b985ec9c0ecc3d52e62a575b
Marion Pillas <marion.pillas@ldas-pcdev1.ligo.caltech.edu>
2022-10-20 06:54:50 -0700

    fix the run_workflow.sh script, the kickstar job seems to work now

--- template_bank_projects/run_workflow.sh
+++ template_bank_projects/run_workflow.sh
@@ -1,6 +1,14 @@
 WORKFLOW_NAME=pycbc_make_faithsim_workflow
 
-./pycbc_make_faithsim_pegasus \
---workflow-name ${WORKFLOW_NAME} \
---config-files ./configuration_pycbc_faithsim.ini \
---output-dir output
+../pycbc_make_faithsim_pegasus \
+    --workflow-name ${WORKFLOW_NAME} \
+    --config-files ../configuration_pycbc_faithsim.ini \
+    --output-dir output \
+    --config-overrides \
+        workflow:file-retention-level:all_triggers \
+        'pegasus_profile:condor|accounting_group:ligo.dev.o4.cbc.uber.pycbcoffline' \
+        "pegasus_profile:condor|request_disk:10000000" \
+        "pegasus_profile:condor|request_gpus:1" \
+        "pegasus_profile:condor|request_memory:4096" \
+        "pegasus_profile:condor|when_to_transfer_output:ON_SUCCESS" \
+        "pegasus_profile:condor|success_exit_code:0" \
