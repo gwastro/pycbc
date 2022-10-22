1ed4eaccbe5611701439433d57cf1ae016b60620
Marion Pillas <marion.pillas@ldas-pcdev1.ligo.caltech.edu>
2022-10-22 03:25:49 -0700

    all scripts for the workflow, final version

--- template_bank_projects/run_workflow.sh
+++ template_bank_projects/run_workflow.sh
@@ -1,14 +1,13 @@
 WORKFLOW_NAME=pycbc_make_faithsim_workflow
 
-../pycbc_make_faithsim_pegasus \
+../pycbc_make_faithsim_workflow \
     --workflow-name ${WORKFLOW_NAME} \
     --config-files ../configuration_pycbc_faithsim.ini \
-    --output-dir output \
+    --output-dir output2 \
     --config-overrides \
         workflow:file-retention-level:all_triggers \
         'pegasus_profile:condor|accounting_group:ligo.dev.o4.cbc.uber.pycbcoffline' \
-        "pegasus_profile:condor|request_disk:10000000" \
-        "pegasus_profile:condor|request_gpus:1" \
-        "pegasus_profile:condor|request_memory:4096" \
+        "pegasus_profile:condor|request_disk:100000" \
+        "pegasus_profile:condor|request_memory:2048" \
         "pegasus_profile:condor|when_to_transfer_output:ON_SUCCESS" \
         "pegasus_profile:condor|success_exit_code:0" \
--- template_bank_projects/submit_workflow.sh
+++ template_bank_projects/submit_workflow.sh
@@ -1,10 +1,3 @@
-cd output
+cd output2
 
 pycbc_submit_dax \
-    --append-pegasus-property 'pegasus_profile:condor|accounting_group:ligo.dev.o4.cbc.uber.pycbcoffline' \
-    --append-pegasus-property 'pegasus.transfer.bypass.input.staging=true' \
-    --append-pegasus-property "pegasus_profile:condor|request_disk:10000000" \
-    --append-pegasus-property "pegasus_profile:condor|request_gpus:1" \
-    --append-pegasus-property "pegasus_profile:condor|request_memory:4096" \
-    --append-pegasus-property "pegasus_profile:condor|when_to_transfer_output:ON_SUCCESS" \
-    --append-pegasus-property "pegasus_profile:condor|success_exit_code:0" \
