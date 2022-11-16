473d8b64454f38d094cb90804ebba691afdcc521
GarethCabournDavies <gareth.cabourndavies@ligo.org>
2022-11-16 07:48:45 -0800

    Titos comments

--- pycbc/events/cuts.py
+++ pycbc/events/cuts.py
@@ -226,11 +226,14 @@ def apply_trigger_cuts(triggers, trigger_cut_dict):
             value = get_chisq_from_file_choice(triggers, chisq_choice)
             # Apply any previous cuts to the value for comparison
             value = value[idx_out]
-        elif (parameter in triggers
+        elif ((not hasattr(triggers, "file") and parameter in triggers)
                 or (hasattr(triggers, "file")
                     and parameter in triggers.file[triggers.ifo])):
             # parameter can be read direct from the trigger dictionary / file
-            value = triggers[parameter]
+            if parameter in triggers:
+                value = triggers[parameter]
+            else:
+                value = triggers.file[triggers.ifo][parameter]
             # Apply any previous cuts to the value for comparison
             value = value[idx_out]
         elif parameter in sngl_rank_keys:
