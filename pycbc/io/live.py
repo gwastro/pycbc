8343470014ea31f45de243f8d17c44aec638021d
Thomas Dent <thomas.dent@usc.es>
2022-11-30 13:22:10 +0100

    dot not underscore

--- pycbc/io/live.py
+++ pycbc/io/live.py
@@ -286,7 +286,7 @@ class CandidateForGraceDB(object):
 
         # Save multi-cpt p astro as json
         if self.astro_probs is not None:
-            self.multipa_file = 'pycbc_p_astro.json'
+            self.multipa_file = 'pycbc.p_astro.json'
             with open(self.multipa_file, 'w') as multipaf:
                 json.dump(self.astro_probs, multipaf)
             logging.info('Multi p_astro file saved as %s', self.multipa_file)
