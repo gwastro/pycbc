d1975fcb72710d9947c351c656b64c9ac00a8205
Prasia-Pankunni <116145091+Prasia-Pankunni@users.noreply.github.com>
2022-12-20 11:41:31 +0530

    Update sky_location.py

--- pycbc/distributions/sky_location.py
+++ pycbc/distributions/sky_location.py
@@ -33,7 +33,7 @@ class UniformSky(angular.UniformSolidAngle):
     _default_polar_angle = 'dec'
     _default_azimuthal_angle = 'ra'
 
-class FisherDist(angular.UniformSolidAngle):
+class FisherDist():
     """A distribution that returns a random (ra, dec) angle drawn from the
     Fisher distribution. Assume that the concentration parameter (kappa)
     is large so that we can use a Rayleigh distribution about the north
