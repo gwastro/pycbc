7d08ff377a3af8fd51b3e7d54fe9b031c735010a
P Prasia <prasia.p@ldas-pcdev2.gw.iucaa.in>
2023-03-22 10:33:09 +0530

    corrected

--- pycbc/conversions.py
+++ pycbc/conversions.py
@@ -848,7 +848,6 @@ def optimal_ra_from_detector(detector_name, tc):
         The name of the detector, e.g., 'H1'.
     tc : float
         The GPS time of the coalescence of the signal in the `ref_frame`.
-
     Returns
     -------
     float :
