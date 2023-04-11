863689ee052ba41106a2b92d23d202e09ea263ac
GarethCabournDavies <gareth.cabourndavies@ligo.org>
2023-04-11 02:47:07 -0700

    Remove clashing options

--- pycbc/tmpltbank/option_utils.py
+++ pycbc/tmpltbank/option_utils.py
@@ -131,9 +131,10 @@ def insert_base_bank_options(parser):
             help="Output file name. Required.")
     parser.add_argument('--f-low-column', type=str, metavar='NAME',
                         help='If given, store the lower frequency cutoff into '
-                             'column NAME of the single-inspiral table.')
+                             'column NAME of the single-inspiral table. '
+                             '(Requires an output file ending in .xml)')
     parser.add_argument('--output-f-final', action='store_true',
-            help="Include 'f_final' in the output.")
+            help="Include 'f_final' in the output hdf file.")
 
 def insert_metric_calculation_options(parser):
     """
