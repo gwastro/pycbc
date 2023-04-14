b885f3ebb24f7eb2bf59197df52fda20addd8e43
Tito Dal Canton <tito.canton@ligo.org>
2023-04-14 13:05:02 -0700

    Refactor using scipy's Rotation

--- pycbc/distributions/sky_location.py
+++ pycbc/distributions/sky_location.py
@@ -12,16 +12,18 @@
 # You should have received a copy of the GNU General Public License along
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
 """This modules provides classes for evaluating sky distributions in
-right acension and declination.
+right ascension and declination.
 """
 
-
+import logging
 import numpy
+from scipy.spatial.transform import Rotation
 from pycbc.distributions import angular
-from pycbc.transforms import new_z_to_euler, rotate_euler
 from pycbc import VARARGS_DELIM
 from pycbc.io import FieldArray
+
 
 class UniformSky(angular.UniformSolidAngle):
     """A distribution that is uniform on the sky. This is the same as
@@ -35,37 +37,69 @@ class UniformSky(angular.UniformSolidAngle):
     _default_polar_angle = 'dec'
     _default_azimuthal_angle = 'ra'
 
+
 class FisherSky():
-    """A distribution that returns a random (ra, dec) angle drawn from the
-    Fisher distribution. Assume that the concentration parameter (kappa)
-    is large so that we can use a Rayleigh distribution about the north
-    pole and rotate it to be centered at the (ra, dec) coordinate mu.
-    Assume kappa = 1 / (0.66*sigma)**2
-    As in UniformSky, the declination (dec) varies from pi/2 to-pi/2
-    and right ascension (ra) varies from 0 to 2pi. The angles
-    should be provided in (ra,dec) format in radians (the variable
-    angle_unit='rad' by default), rather than factors of pi, or
-    in degrees (in this case set angle_unit='deg').
+    """A distribution that returns a random angle drawn from an approximate
+    Fisher distribution. Assumes that the concentration parameter
+    kappa = 1 / (0.66*σ)**2
+    is large, so that we can use a Fisher distribution centered at the North
+    Pole (which factors as a uniform distribution for the right ascension, and
+    a Rayleigh distribution for the declination) and then rotate it to be
+    centered at the specified mean position. As in UniformSky, the declination
+    varies from π/2 to -π/2 and the right ascension varies from 0 to 2π.
+
     References:
       * http://en.wikipedia.org/wiki/Von_Mises-Fisher_distribution
       * http://arxiv.org/pdf/0902.0737v1 (states the Rayleigh limit)
+
+    Parameters
+    ----------
+    mean_ra: float
+        RA of the center of the distribution.
+    mean_dec: float
+        Declination of the center of the distribution.
+    sigma: float
+        Spread of the distribution.
+    angle_unit: str
+        Unit for the angle parameters: either "deg" or "rad".
     """
     name = 'fisher_sky'
     _params = ['ra', 'dec']
 
-    def __init__(self, angle_unit='rad', **params):
-        self.sigma = params['sigma']
-        if angle_unit == 'rad':
-            self.mu_values = numpy.array([params['mean_ra'],
-                                          params['mean_dec']])
-            self.kappa = 1./((0.66*params['sigma']))**2
-        elif angle_unit == 'deg':
-            self.mu_values = numpy.deg2rad([params['mean_ra'],
-                                            params['mean_dec']])
-            self.kappa = 1./((0.66*numpy.deg2rad(params['sigma'])))**2
-        else:
-            raise ValueError("Only deg or rad is allowed as unit")
-        self.alpha, self.beta = new_z_to_euler(self.mu_values)
+    def __init__(self, **params):
+        if params['angle_unit'] not in ['deg', 'rad']:
+            raise ValueError("Only deg or rad is allowed as angle unit")
+        mean_ra = params['mean_ra']
+        mean_dec = params['mean_dec']
+        sigma = params['sigma']
+        if params['angle_unit'] == 'deg':
+            mean_ra = numpy.deg2rad(mean_ra)
+            mean_dec = numpy.deg2rad(mean_dec)
+            sigma = numpy.deg2rad(sigma)
+        if mean_ra < 0 or mean_ra > 2 * numpy.pi:
+            raise ValueError(
+                f'The mean RA must be between 0 and 2π, {mean_ra} rad given'
+            )
+        if mean_dec < -numpy.pi/2 or mean_dec > numpy.pi/2:
+            raise ValueError(
+                'The mean declination must be between '
+                f'-π/2 and π/2, {mean_dec} rad given'
+            )
+        if sigma <= 0 or sigma > 2 * numpy.pi:
+            raise ValueError(
+                'Sigma must be positive and much smaller than 2π'
+            )
+        if sigma > 2:
+            logging.warning(
+                f'Warning: sigma = {sigma} rad is probably too large for the '
+                'Fisher approximation to be valid'
+            )
+        self.rayleigh_scale = 0.66 * sigma
+        # Prepare a rotation that puts the North Pole at the mean position
+        self.rotation = Rotation.from_euler(
+            'yz',
+            [numpy.pi / 2 - mean_dec, mean_ra]
+        )
 
     @property
     def params(self):
@@ -75,27 +109,56 @@ class FisherSky():
     def from_config(cls, cp, section, variable_args):
         tag = variable_args
         variable_args = variable_args.split(VARARGS_DELIM)
-        if not set(variable_args) == set(cls._params):
+        if set(variable_args) != set(cls._params):
             raise ValueError("Not all parameters used by this distribution "
                              "included in tag portion of section name")
         mean_ra = float(cp.get_opt_tag(section, 'mean_ra', tag))
         mean_dec = float(cp.get_opt_tag(section, 'mean_dec', tag))
         sigma = float(cp.get_opt_tag(section, 'sigma', tag))
         angle_unit = cp.get_opt_tag(section, 'angle_unit', tag)
-        return cls(mean_ra=mean_ra, mean_dec=mean_dec, sigma=sigma,
-                   angle_unit=angle_unit)
+        return cls(
+            mean_ra=mean_ra,
+            mean_dec=mean_dec,
+            sigma=sigma,
+            angle_unit=angle_unit
+        )
 
     def rvs(self, size):
-        arr = numpy.array([
-            numpy.random.rayleigh(scale=1./numpy.sqrt(self.kappa),
-                                  size=size),
-            numpy.random.uniform(low=0,
-                                 high=(2*numpy.pi),
-                                 size=size)]).T
-        euler = rotate_euler(arr, self.alpha, self.beta, 0)
-        rot_euler = FieldArray(size, dtype=[('ra', '<f8'), ('dec', '<f8')])
-        rot_euler['ra'], rot_euler['dec'] = euler[:, 0], euler[:, 1]
-        return rot_euler
+        # Draw samples from a distribution centered on the North pole
+        np_ra = numpy.random.uniform(
+            low=0,
+            high=(2*numpy.pi),
+            size=size
+        )
+        np_dec = numpy.random.rayleigh(
+            scale=self.rayleigh_scale,
+            size=size
+        )
+
+        # Convert the samples to intermediate cartesian representation
+        np_cart = numpy.empty(shape=(size, 3))
+        np_cart[:, 0] = numpy.cos(np_ra) * numpy.sin(np_dec)
+        np_cart[:, 1] = numpy.sin(np_ra) * numpy.sin(np_dec)
+        np_cart[:, 2] = numpy.cos(np_dec)
+
+        # Rotate the samples according to our pre-built rotation
+        rot_cart = self.rotation.apply(np_cart)
+
+        # Convert the samples back to spherical coordinates.
+        # Some unpleasant conditional operations are needed
+        # to get the right angle convention.
+        rot_radec = FieldArray(
+            size,
+            dtype=[
+                ('ra', '<f8'),
+                ('dec', '<f8')
+            ]
+        )
+        rot_radec['ra'] = numpy.arctan2(rot_cart[:, 1], rot_cart[:, 0])
+        neg_mask = rot_radec['ra'] < 0
+        rot_radec['ra'][neg_mask] += 2 * numpy.pi
+        rot_radec['dec'] = numpy.arcsin(rot_cart[:, 2])
+        return rot_radec
 
 
 __all__ = ['UniformSky', 'FisherSky']
