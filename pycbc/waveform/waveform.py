9e64dc001a87b8279b0e55ae49ded1842fa4917c
Shichao Wu <wushichao@mail.bnu.edu.cn>
2023-04-15 14:34:15 +0200

    Update waveform.py

--- pycbc/waveform/waveform.py
+++ pycbc/waveform/waveform.py
@@ -759,11 +759,13 @@ def _base_get_td_waveform_from_fd(template=None, rwrap=0.2, **params):
     kwds = props(template, **params)
     nparams = kwds.copy()
 
-    if rwrap == 0.2:
-        m_final, spin_final = get_final_from_initial(
-            mass1=nparams['mass1'], mass2=nparams['mass2'],
-            spin1z=nparams['spin1z'], spin2z=nparams['spin2z'])
-        rwrap = tau_from_final_mass_spin(m_final, spin_final) * 8
+    m_final, spin_final = get_final_from_initial(
+        mass1=nparams['mass1'], mass2=nparams['mass2'],
+        spin1z=nparams['spin1z'], spin2z=nparams['spin2z'])
+    t_damping = tau_from_final_mass_spin(m_final, spin_final) * 8
+
+    if rwrap < t_damping:
+        rwrap = t_damping
 
     if nparams['approximant'] not in _filter_time_lengths:
         raise ValueError("Approximant %s _filter_time_lengths function \
