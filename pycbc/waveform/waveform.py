5fbbe886096d310fb430a90450447f749e1f265b
Shichao Wu <wushichao@mail.bnu.edu.cn>
2023-04-16 12:59:38 +0200

    10 times tau

--- pycbc/waveform/waveform.py
+++ pycbc/waveform/waveform.py
@@ -762,7 +762,7 @@ def _base_get_td_waveform_from_fd(template=None, rwrap=0.2, **params):
     m_final, spin_final = get_final_from_initial(
         mass1=nparams['mass1'], mass2=nparams['mass2'],
         spin1z=nparams['spin1z'], spin2z=nparams['spin2z'])
-    t_damping = tau_from_final_mass_spin(m_final, spin_final) * 8
+    t_damping = tau_from_final_mass_spin(m_final, spin_final) * 10
 
     if rwrap < t_damping:
         rwrap = t_damping
