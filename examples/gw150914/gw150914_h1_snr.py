d40133b73ee3749daa7c65bebf290a8030a1a0f2
Tito Dal Canton <tito.dalcanton@ijclab.in2p3.fr>
2023-04-19 17:10:55 +0200

    Replace gw-openscience.org with gwosc.org (#4326)

--- examples/gw150914/gw150914_h1_snr.py
+++ examples/gw150914/gw150914_h1_snr.py
@@ -8,7 +8,7 @@ from pycbc.psd import welch, interpolate
 
 # Read data and remove low frequency content
 fname = 'H-H1_LOSC_4_V2-1126259446-32.gwf'
-url = "https://www.gw-openscience.org/GW150914data/" + fname
+url = "https://www.gwosc.org/GW150914data/" + fname
 urlretrieve(url, filename=fname)
 h1 = read_frame('H-H1_LOSC_4_V2-1126259446-32.gwf', 'H1:LOSC-STRAIN')
 h1 = highpass_fir(h1, 15, 8)
--- examples/inference/margtime/get.sh
+++ examples/inference/margtime/get.sh
@@ -4,5 +4,6 @@ for ifo in H-H1 L-L1
 do
     file=${ifo}_GWOSC_4KHZ_R1-1126257415-4096.gwf
     test -f ${file} && continue
-    curl -O --show-error --silent https://www.gw-openscience.org/eventapi/html/GWTC-1-confident/GW150914/v3/${ifo}_GWOSC_4KHZ_R1-1126257415-4096.gwf
+    curl -O -L --show-error --silent \
+        https://www.gwosc.org/eventapi/html/GWTC-1-confident/GW150914/v3/${ifo}_GWOSC_4KHZ_R1-1126257415-4096.gwf
 done
