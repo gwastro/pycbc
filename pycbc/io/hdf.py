534ad1593b647c33ee48cc5caae021d41badad1c
Gareth S Cabourn Davies <gareth.cabourndavies@ligo.org>
2023-07-04 13:16:12 +0100

    Codeclimate complaints

--- pycbc/io/hdf.py
+++ pycbc/io/hdf.py
@@ -1009,14 +1009,14 @@ class ForegroundTriggers(object):
             coinc_inspiral_row.snr = net_snrsq**0.5
             if exclusive:
                 coinc_inspiral_row.false_alarm_rate = \
-                        coinc_event_vals['fap_exc'][idx]
+                    coinc_event_vals['fap_exc'][idx]
                 coinc_inspiral_row.combined_far = \
-                        1./coinc_event_vals['ifar_exc'][idx]
+                    1./coinc_event_vals['ifar_exc'][idx]
             else:
                 coinc_inspiral_row.false_alarm_rate = \
-                        coinc_event_vals['fap'][idx]
+                    coinc_event_vals['fap'][idx]
                 coinc_inspiral_row.combined_far = \
-                        1./coinc_event_vals['ifar'][idx]
+                    1./coinc_event_vals['ifar'][idx]
             # Transform to Hz
             coinc_inspiral_row.combined_far = \
                                     coinc_inspiral_row.combined_far / YRJUL_SI
