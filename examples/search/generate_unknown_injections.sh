658933e306a78a3aed3e2f9ab84440ced657aa93
GarethCabournDavies <gareth.cabourndavies@ligo.org>
2023-07-05 14:15:46 -0700

    Some fixes to data for injection run

--- examples/search/generate_unknown_injections.sh
+++ examples/search/generate_unknown_injections.sh
@@ -2,22 +2,20 @@ if [[ ! -f injections.hdf ]]
 then
     echo -e "\\n\\n>> [`date`] Generating injections"
 
-    rm -rf ./strain
+    rm -f ./*-SIMULATED_STRAIN-*.gwf
 
     ./generate_injections.py
 fi
 
 start_time=1186740069
 end_time=1186743653
-if [[ ! -d strain ]]
-then
-    echo -e "\\n\\n>> [`date`] Generating simulated strain"
 
-    function simulate_strain { # detector PSD_model random_seed
-        mkdir -p strain/$1
+function simulate_strain { # detector PSD_model random_seed
 
-        out_path="strain/$1/$1-SIMULATED_STRAIN-{start}-{duration}.gwf"
-
+    out_path="$1-SIMULATED_STRAIN-{start}-{duration}.gwf"
+    if [[ ! -f $1-SIMULATED_STRAIN-1186740069-3584.gwf ]]
+    then
+        echo -e "\\n\\n>> [`date`] Generating simulated strain in $1"
         pycbc_condition_strain \
             --fake-strain $2 \
             --fake-strain-seed $3 \
@@ -29,14 +27,12 @@ then
             --channel-name $1:SIMULATED_STRAIN \
             --frame-duration 3584 \
             --injection-file injections.hdf \
-	    --fft-backends fftw
+            --fft-backends fftw
+    fi
+}
 
+simulate_strain H1 aLIGOMidLowSensitivityP1200087 1234
+simulate_strain L1 aLIGOMidLowSensitivityP1200087 2345
+simulate_strain V1 AdVEarlyLowSensitivityP1200087 3456
 
-	echo "${1::1} ${1}_simulated 1186740069 3584 $pwd/strain/$1/$1-SIMULATED_STRAIN-$start_time-3584.gwf" >> strain/$1/$1-cache.lcf
-    }
-    simulate_strain H1 aLIGOMidLowSensitivityP1200087 1234
-    simulate_strain L1 aLIGOMidLowSensitivityP1200087 2345
-    simulate_strain V1 AdVEarlyLowSensitivityP1200087 3456
-
-fi
 
--- examples/search/master.sh
+++ examples/search/master.sh
@@ -7,7 +7,7 @@ bash -e stats.sh
 bash -e generate_unknown_injections.sh
 bash -e gen.sh
 
-cp *.gwf output
+mv *.gwf output
 cd output
 bash -e ../submit.sh
 python ../check_job.py
