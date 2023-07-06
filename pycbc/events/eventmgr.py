fc698afce5e5d8aa857a43e15d30c58c23e3e3b7
Jake Buchanan <jacob.buchanan.19@cnu.edu>
2023-07-06 10:32:08 -0400

    Make event manager write slide offset

--- pycbc/events/eventmgr.py
+++ pycbc/events/eventmgr.py
@@ -725,6 +725,7 @@ class EventManagerCoherent(EventManagerMultiDetBase):
                         float(self.opt.sample_rate[ifo_str]) + \
                         self.opt.gps_start_time[ifo_str]
                 f['time_index'] = ifo_events['time_index']
+                f['slide_offset'] = ifo_events['slide_offset']
                 try:
                     # Precessing
                     template_sigmasq_plus = numpy.array(
