c6510d65a41c56d66cc68c22ede74617acb37a5e
Jake Buchanan <jacob.buchanan.19@cnu.edu>
2023-07-07 10:03:10 -0400

    Correct number of ids

--- pycbc/results/pygrb_postprocessing_utils.py
+++ pycbc/results/pygrb_postprocessing_utils.py
@@ -617,10 +617,9 @@ def load_injections(inj_file, vetoes, sim_table=False, label=None):
 # =============================================================================
 def load_time_slides(hdf_file_path):
     """Loads timeslides from PyGRB output file as a dictionary"""
-
     hdf_file = h5py.File(hdf_file_path, 'r')
     ifos = extract_ifos(hdf_file_path)
-    ids = numpy.arange(0, len(hdf_file[f'network/timeslides/{ifos[0]}'])-1)
+    ids = numpy.arange(len(hdf_file[f'network/timeslides/{ifos[0]}']))
     time_slide_dict = {
         slide_id: {
             ifo: hdf_file[f'network/timeslides/{ifo}'][slide_id] for ifo in ifos
