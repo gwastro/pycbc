a2f77f2bccbff4987569dacfb16e177425c407e1
Arthur Tolley <32394213+ArthurTolley@users.noreply.github.com>
2023-07-19 16:33:24 +0100

    Update module docstring copyright
    Co-authored-by: Gareth S Cabourn Davies <gareth.cabourndavies@ligo.org>

--- pycbc/live/snr_optimizer.py
+++ pycbc/live/snr_optimizer.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2023
+# Copyright (C) 2023 Arthur Tolley
 #
 # This program is free software; you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by the
