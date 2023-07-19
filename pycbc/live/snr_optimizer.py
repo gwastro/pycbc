c03ca1364175b237af9c2bc1d31fe77cd6a32f35
ArthurTolley <arthur.tolley@port.ac.uk>
2023-07-19 08:38:26 -0700

    Add gareth

--- pycbc/live/snr_optimizer.py
+++ pycbc/live/snr_optimizer.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2023 Arthur Tolley
+# Copyright (C) 2023 Arthur Tolley, Gareth Cabourn Davies
 #
 # This program is free software; you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by the
