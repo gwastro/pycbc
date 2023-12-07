d340bc6ef0d368f28c1c3e0893e08db104551127
Jacob Buchanan <83317255+jakeb245@users.noreply.github.com>
2023-12-07 07:45:52 -0500

    PyGRB postprocessing: segment writing/loading (#4542)
    * Try fixing load_segment_dict
    
    * Use segments/GPS time API more correctly
    
    * Use h5py correctly
    
    * Codeclimate
    
    * Add comment
    
    * condense segment list creation
    
    Co-authored-by: Francesco Pannarale <pannarale@users.noreply.github.com>
    
    * Plural start/end time in file output
    
    * Codeclimate
    
    ---------
    
    Co-authored-by: Francesco Pannarale <pannarale@users.noreply.github.com>

--- pycbc/events/eventmgr.py
+++ pycbc/events/eventmgr.py
@@ -596,7 +596,8 @@ class EventManagerMultiDetBase(EventManager):
 
 class EventManagerCoherent(EventManagerMultiDetBase):
     def __init__(self, opt, ifos, column, column_types, network_column,
-                 network_column_types, time_slides, psd=None, **kwargs):
+                 network_column_types, segments, time_slides,
+                 psd=None, **kwargs):
         super(EventManagerCoherent, self).__init__(
             opt, ifos, column, column_types, psd=None, **kwargs)
         self.network_event_dtype = \
@@ -612,6 +613,7 @@ class EventManagerCoherent(EventManagerMultiDetBase):
         self.event_index['network'] = 0
         self.template_event_dict['network'] = numpy.array(
             [], dtype=self.network_event_dtype)
+        self.segments = segments
         self.time_slides = time_slides
 
     def cluster_template_network_events(self, tcolumn, column, window_size,
@@ -703,6 +705,13 @@ class EventManagerCoherent(EventManagerMultiDetBase):
                     )
             else:
                 f[col] = network_events[col]
+        starts = []
+        ends = []
+        for seg in self.segments[self.ifos[0]]:
+            starts.append(seg.start_time.gpsSeconds)
+            ends.append(seg.end_time.gpsSeconds)
+        f['search/segments/start_times'] = starts
+        f['search/segments/end_times'] = ends
         # Individual ifo stuff
         for i, ifo in enumerate(self.ifos):
             tid = self.events['template_id'][self.events['ifo'] == i]
