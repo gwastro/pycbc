c92f321b7b971495500d1db79307d10afafab922
maxtrevor <max.trevor@ligo.org>
2023-12-13 13:28:06 -0800

    Rewrote dq workflow

--- pycbc/workflow/dq.py
+++ pycbc/workflow/dq.py
@@ -69,16 +69,6 @@ class PyCBCBinTriggerRatesDQExecutable(Executable):
         return node
 
 
-class PyCBCCombineDQExecutable(Executable):
-    current_retention_level = Executable.MERGED_TRIGGERS
-
-    def create_node(self, workflow, dq_files):
-        node = Node(self)
-        node.add_input_list_opt('--dq-files', dq_files)
-        node.new_output_file_opt(workflow.analysis_time, '.hdf', '--output-file')
-        return node
-
-
 def setup_dq_reranking(workflow, insps, bank,
                        analyzable_segs, analyzable_file,
                        dq_seg_file,
@@ -101,14 +91,14 @@ def setup_dq_reranking(workflow, insps, bank,
 
     for ifo in ifos:
         # get the dq labels and types for this ifo
-        ifo_dq_labels = [dq_label for dq_label, dq_ifo in zip(dq_labels, dq_ifos)
-                         if dq_ifo == ifo]
-        ifo_dq_types = [dq_type for dq_type, dq_ifo in zip(dq_types, dq_ifos)
-                        if dq_ifo == ifo]
+        ifo_dq_info = [(dq_label, dq_type) for dq_label, dq_type, dq_ifo
+                       in zip(dq_labels, dq_types, dq_ifos) if dq_ifo == ifo]
+        assert len(ifo_dq_info) == 1, f"Received more than one dq file for {ifo}"
+        dq_label, dq_type = ifo_dq_info[0]
 
         # get triggers for this ifo
         ifo_insp = [insp for insp in insps if (insp.ifo == ifo)]
-        assert len(ifo_insp) == 1
+        assert len(ifo_insp) == 1, f"Received more than one inspiral file for {ifo}"
         ifo_insp = ifo_insp[0]
 
         # calculate template bins for this ifo
@@ -122,82 +112,67 @@ def setup_dq_reranking(workflow, insps, bank,
         workflow += bin_templates_node
         template_bins_file = bin_templates_node.output_file
 
-        trigger_rate_files = FileList()
-        for dq_label, dq_type in zip(ifo_dq_labels, ifo_dq_types):
-            flag_file = None
+        flag_file = None
 
-            # if dq is a timeseries, need to convert it to a flag
-            if dq_type == 'timeseries':
-                if dq_label not in workflow.cp.get_subsections('workflow-datafind'):
-                    msg = """No workflow-datafind section with dq tag.
-                    Tags must be used in workflow-datafind sections "
-                    if more than one source of data is used.
-                    Strain data source must be tagged
-                    workflow-datafind-hoft.
-                    Consult the documentation for more info."""
-                    raise ValueError(msg)
+        # if dq is a timeseries, need to convert it to a flag
+        if dq_type == 'timeseries':
+            if dq_label not in workflow.cp.get_subsections('workflow-datafind'):
+                msg = """No workflow-datafind section with dq tag.
+                Tags must be used in workflow-datafind sections "
+                if more than one source of data is used.
+                Strain data source must be tagged
+                workflow-datafind-hoft.
+                Consult the documentation for more info."""
+                raise ValueError(msg)
 
-                # find timeseries frames
-                ifo_seg_dict = {ifo: analyzable_segs[ifo]}
-                dq_segs_for_file = {ifo+':'+dq_label: analyzable_segs[ifo]}
-                ts_dq_seg_file = SegFile.from_segment_list_dict(
-                    dq_label,
-                    dq_segs_for_file,
-                    extension='.xml',
-                    valid_segment=workflow.analysis_time,
-                    directory=output_dir)
-                datafind_files, dq_file, dq_segs, dq_name = setup_datafind_workflow(
-                    workflow,
-                    ifo_seg_dict,
-                    "datafind_dq",
-                    seg_file=ts_dq_seg_file,
-                    tags=dq_tags)
+            # find timeseries frames
+            ifo_seg_dict = {ifo: analyzable_segs[ifo]}
+            dq_segs_for_file = {ifo+':'+dq_label: analyzable_segs[ifo]}
+            ts_dq_seg_file = SegFile.from_segment_list_dict(
+                dq_label,
+                dq_segs_for_file,
+                extension='.xml',
+                valid_segment=workflow.analysis_time,
+                directory=output_dir)
+            datafind_files, dq_file, dq_segs, dq_name = setup_datafind_workflow(
+                workflow,
+                ifo_seg_dict,
+                "datafind_dq",
+                seg_file=ts_dq_seg_file,
+                tags=dq_tags)
 
-                # calculate dq flag from timeseries
-                calculate_flag_exe = PyCBCDQFlagFromTimeseriesExecutable(
-                    workflow.cp,
-                    'calculate_dqflag',
-                    ifos=ifo,
-                    out_dir=output_dir,
-                    tags=dq_tags)
-                calculate_flag_node = calculate_flag_exe.create_node(
-                    workflow,
-                    analyzable_segs,
-                    datafind_files)
-                workflow += calculate_flag_node
-                flag_file = calculate_flag_node.output_file
-            else:
-                assert dq_type == 'flag'
-                flag_file = dq_seg_file
-
-            # calculate trigger rates during dq flags
-            bin_triggers_exe = PyCBCBinTriggerRatesDQExecutable(
+            # calculate dq flag from timeseries
+            calculate_flag_exe = PyCBCDQFlagFromTimeseriesExecutable(
                 workflow.cp,
-                'bin_trigger_rates_dq',
+                'calculate_dqflag',
                 ifos=ifo,
                 out_dir=output_dir,
                 tags=dq_tags)
-            bin_triggers_node = bin_triggers_exe.create_node(
+            calculate_flag_node = calculate_flag_exe.create_node(
                 workflow,
-                ifo,
-                flag_file,
-                ifo_insp,
-                template_bins_file)
-            workflow += bin_triggers_node
-            trigger_rate_files += bin_triggers_node.output_files
+                analyzable_segs,
+                datafind_files)
+            workflow += calculate_flag_node
+            flag_file = calculate_flag_node.output_file
+        else:
+            assert dq_type == 'flag', f"Unknown dq type {dq_type}"
+            flag_file = dq_seg_file
 
-        # combine results from all dq inputs into one file
-        combine_dq_exe = PyCBCCombineDQExecutable(
+        # calculate trigger rates during dq flags
+        bin_triggers_exe = PyCBCBinTriggerRatesDQExecutable(
             workflow.cp,
-            'combine_dq',
+            'bin_trigger_rates_dq',
             ifos=ifo,
             out_dir=output_dir,
             tags=dq_tags)
-        combine_dq_node = combine_dq_exe.create_node(
+        bin_triggers_node = bin_triggers_exe.create_node(
             workflow,
-            trigger_rate_files)
-        workflow += combine_dq_node
-        output_files += combine_dq_node.output_files
-        output_labels += [f'{ifo}_dq']
+            ifo,
+            flag_file,
+            ifo_insp,
+            template_bins_file)
+        workflow += bin_triggers_node
+        output_files += bin_triggers_node.output_files
+        output_labels += [dq_label]
 
     return output_files, output_labels
