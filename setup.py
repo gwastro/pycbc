9b0e08a36d4e78254292d1a33625c277c5302aa8
Tito Dal Canton <tito.dalcanton@ijclab.in2p3.fr>
2024-01-19 18:39:45 +0100

    Set version to 2.1.4

--- setup.py
+++ setup.py
@@ -122,7 +122,7 @@ def get_version_info():
         vinfo = _version_helper.generate_git_version_info()
     except:
         vinfo = vdummy()
-        vinfo.version = '2.1.3'
+        vinfo.version = '2.1.4'
         vinfo.release = 'True'
 
     with open('pycbc/version.py', 'wb') as f:
