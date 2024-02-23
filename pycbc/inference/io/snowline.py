6ab458cfb00399be511cd9e5372d011850a7439d
Alex Nitz <alex.nitz@gmail.com>
2024-02-23 13:19:07 -0500

    Update snowline.py

--- pycbc/inference/io/snowline.py
+++ pycbc/inference/io/snowline.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2019 Collin Capano, Sumit Kumar, Alex Nitz
+# Copyright (C) 2024 Alex Nitz
 # This program is free software; you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by the
 # Free Software Foundation; either version 3 of the License, or (at your
