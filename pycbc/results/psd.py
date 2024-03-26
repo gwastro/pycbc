6aac1b79f6cd92c6cc3ec572d142680827235726
Thomas Dent <thomas.dent@usc.es>
2024-03-26 21:39:53 +0100

    Properly fix ASD plot minimum value (#4671)
    * Properly fix ASD plot minimum value
    
    Rather than a hard coded minimum use the minimum value we are going to plot.  Also put the calculation of the asd we are plotting on a single line rather than splitting over 2
    
    * Fix bug and grid
    
    * ignore samples above minimum freq rather than above 0
    
    * curr_psd does exist !
    
    * specify an older branch of BBHx in tox.ini (#4672) (#4674)
    
    * specify an older branch of BBHx in tox.ini
    
    * Update tox.ini
    
    Co-authored-by: Shichao Wu <wushichao@mail.bnu.edu.cn>
    
    ---------
    
    Co-authored-by: Shichao Wu <wushichao@mail.bnu.edu.cn>

--- pycbc/results/psd.py
+++ pycbc/results/psd.py
@@ -30,13 +30,12 @@ from pycbc.results import ifo_color
 from pycbc import DYN_RANGE_FAC
 
 
-def generate_asd_plot(psddict, output_filename):
+def generate_asd_plot(psddict, output_filename, f_min=10.):
     """
     Generate an ASD plot as used for upload to GraceDB.
 
     Parameters
     ----------
-
     psddict: dictionary
         A dictionary keyed on ifo containing the PSDs as
         FrequencySeries objects
@@ -44,22 +43,32 @@ def generate_asd_plot(psddict, output_filename):
     output_filename: string
         The filename for the plot to be saved to
 
+    f_min: float
+        Minimum frequency at which anything should be plotted
+
     Returns
     -------
         None
     """
     from matplotlib import pyplot as plt
     asd_fig, asd_ax = plt.subplots(1)
+    asd_min = [1E-24]  # Default minimum to plot
+
     for ifo in sorted(psddict.keys()):
         curr_psd = psddict[ifo]
-        # Can't plot log(0) so start from point 1
-        asd_ax.loglog(curr_psd.sample_frequencies[1:],
-                      curr_psd[1:] ** 0.5 / DYN_RANGE_FAC,
-                      c=ifo_color(ifo), label=ifo)
+        freqs = curr_psd.sample_frequencies
+        physical = (freqs >= f_min)  # Ignore lower frequencies
+        asd_to_plot = curr_psd[physical] ** 0.5 / DYN_RANGE_FAC
+        asd_min.append(min(asd_to_plot))
+        asd_ax.loglog(freqs[physical],
+                      asd_to_plot,
+                      c=ifo_color(ifo),
+                      label=ifo)
 
+    asd_ax.grid(True)
     asd_ax.legend()
-    asd_ax.set_xlim([10, 1300])
-    asd_ax.set_ylim([3E-24, 1E-20])
+    asd_ax.set_xlim([f_min, 1300])
+    asd_ax.set_ylim([min(asd_min), 1E-20])
     asd_ax.set_xlabel('Frequency (Hz)')
     asd_ax.set_ylabel('ASD')
     asd_fig.savefig(output_filename)
