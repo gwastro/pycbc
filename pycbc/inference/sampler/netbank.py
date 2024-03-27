165d70c4593992cc491c713a7f2991dc6fd96694
Alexander Harvey Nitz <alex.nitz@gmail.com>
2024-03-27 16:31:09 -0400

    fixes

--- pycbc/inference/sampler/netbank.py
+++ pycbc/inference/sampler/netbank.py
@@ -2,12 +2,14 @@
 some reconstruction supported by the likelihood model.
 """
 
-import numpy, tqdm, h5py
+import numpy, tqdm, h5py, logging
 from numpy.random import choice
+from scipy.special import logsumexp
+from pycbc.io import FieldArray
 
 from pycbc.inference.io import PosteriorFile
 from pycbc.inference import models
-from pycbc.pool import choose_pool'
+from pycbc.pool import choose_pool
 from .dummy import DummySampler
 
 from .base import (BaseSampler, setup_output)
@@ -39,7 +41,15 @@ class NetBank(DummySampler):
         self.num_samples = int(num_samples)
         self.pool = choose_pool(mpi=use_mpi, processes=nprocesses)
         self._samples = {}
-        
+          
+
+    def run(self):
+        logging.info('Retrieving params of parameter space nodes')
+        bparams = {}
+        with h5py.File(self.bank, 'r') as f:
+            for p in self.model.variable_params:
+                bparams[p] = f[p][:]
+ 
         logging.info('formatting input')
         f = h5py.File(self.mapfile, 'r')
 
@@ -48,25 +58,20 @@ class NetBank(DummySampler):
         params = f['params'][:]
 
         dmap = []
-        for i in range(len(mcb)):
+        for i in tqdm.tqdm(range(len(bparams[p]))):
             dmap.append([])
             select =  params[numpy.where(idx == i)[0]]
             dmap[i] = select  
+
         self.dmap = dmap
         self.lengths = numpy.array([len(x) for x in dmap])
-        f.close()      
-
-    def run(self):
-        logging.info('Retrieving params of parameter space nodes')
-        params = {}
-        with h5py.File(self.bank, 'r') as f:
-            for p in self.model.variable_params:
-                params[p] = f[p][:]
+        f.close()   
         
         logging.info('Calculating likelihood at nodes')
         node_loglrs = []
-        for i in tqdm.tqdm(range(len(params[p]))):
-            self.model.update(**{p: params[p][i] for p in self.model.variable_params}) 
+        for i in tqdm.tqdm(range(len(bparams[p]))):
+            pset = {p: bparams[p][i] for p in self.model.variable_params}
+            self.model.update(**pset)
             node_loglrs.append(self.model.loglr)
         node_loglrs = numpy.array(node_loglrs)
         loglr_bound = node_loglrs.max() - 25
@@ -90,26 +95,23 @@ class NetBank(DummySampler):
         logw3 = numpy.zeros(size)
         upsamp, expand = numpy.unique(psamp, return_inverse=True)
 
-        logging.info("Possible unique values", lengths[passed].sum())
-        logging.info("Templates drawn from", len(numpy.unique(draw)))
-        logging.info("Unique values first draw", len(upsamp))
+        logging.info("Possible unique values %s", self.lengths[passed].sum())
+        logging.info("Templates drawn from %s", len(numpy.unique(draw)))
+        logging.info("Unique values first draw %s", len(upsamp))
 
-        snrs2 = []
         for i, s in tqdm.tqdm(enumerate(upsamp), total=len(upsamp), position=0, leave=True):
-            self.model.update(**{p: params[p][i] for p in self.model.variable_params}) 
-            ll =  self.model.loglr
-            snrs2.append((ll*2)**0.5)
+            self.model.update(**{p: upsamp[p][i] for p in self.model.variable_params}) 
+            ll =  self.model.logl
             logw3[i] += ll
 
-        snrs2 = numpy.array(snrs2)[expand]
-        logw3 = logw3[expand] - numpy.array(lls)[draw]
+        logw3 = logw3[expand] - numpy.array(node_loglrs)[draw]
 
         logw3 -= logsumexp(logw3)
         weight2 = numpy.exp(logw3)
 
         draw2 = choice(len(psamp), size=200000, replace=True, p=weight2)
-        snrs2 = numpy.array(snrs2)[draw2]
-        print("Unique values second draw", len(numpy.unique(snrs2)))
+        logging.info("Unique values second draw %s", len(numpy.unique(psamp[draw2])))
+        logging.info("ESS = %s", 1.0 / (weight2 ** 2.0).sum())
 
         fsamp = FieldArray(len(draw2), dtype=params.dtype)
         for i in range(len(draw2)):
