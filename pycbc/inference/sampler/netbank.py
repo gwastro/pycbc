96e286365ff43f380f61a6ffc494256da8077d63
Alexander Harvey Nitz <alex.nitz@gmail.com>
2024-04-01 16:52:09 -0400

    fixes

--- pycbc/inference/sampler/netbank.py
+++ pycbc/inference/sampler/netbank.py
@@ -44,7 +44,7 @@ class NetBank(DummySampler):
     def __init__(self, model, *args, nprocesses=1, use_mpi=False,
                  mapfile=None,
                  loglr_region=25,
-                 node_draw_size=1e6,
+                 target_likelihood_calls=1e5,
                  resample_draw_size=2e5,
                  reconstruct = False,
                  **kwargs):
@@ -57,21 +57,19 @@ class NetBank(DummySampler):
         self.pool = choose_pool(mpi=use_mpi, processes=nprocesses)
         self._samples = {}
 
+        self.target_likelihood_calls = int(target_likelihood_calls)
         self.reconstruct = bool(reconstruct)
         self.loglr_region = float(loglr_region)
-        self.node_draw_size = int(float(node_draw_size))
         self.resample_draw_size = int(float(resample_draw_size))
 
     def run(self):
         logging.info('Retrieving params of parameter space nodes')
         with h5py.File(self.mapfile, 'r') as f:
             bparams = {p: f['bank'][p][:] for p in self.variable_params}
-            dmap = []
+            dmap = {}
             num_nodes = len(bparams[list(bparams.keys())[0]])
-            for i in range(num_nodes):
-                dmap.append(f['map'][str(i)][:])
-        dtype = dmap[0].dtype
-        lengths = numpy.array([len(x) for x in dmap])
+            lengths = numpy.array([len(f['map'][str(x)]) for x in range(num_nodes)])
+            dtype = f['map']['0'].dtype
 
         logging.info('Calculating likelihood at nodes')
         args = []
@@ -79,7 +77,9 @@ class NetBank(DummySampler):
             pset = {p: bparams[p][i] for p in self.model.variable_params}
             args.append(pset)
 
-        node_loglrs = numpy.array(self.pool.map(call_likelihood, args))
+        node_loglrs = list(tqdm.tqdm(self.pool.imap(call_likelihood, args),
+                                     total=len(args)))
+        node_loglrs = numpy.array(node_loglrs)
         loglr_bound = node_loglrs.max() - self.loglr_region
 
         logging.info('Drawing proposal samples from node regions')
@@ -89,27 +89,38 @@ class NetBank(DummySampler):
         logw2 -= logsumexp(logw2)
         weight = numpy.exp(logw2)
 
+        logging.info("...reading template bins")
+        with h5py.File(self.mapfile, 'r') as f:
+            for i in passed:
+                dmap[i] = f['map'][str(i)][:]
+
         logging.info("...draw template bins")
-        draw = choice(passed, size=self.node_draw_size, replace=True, p=weight)
-
+        self.minimum_bin_call = 100
+        drawcount = (weight * self.target_likelihood_calls).astype(numpy.int) + self.minimum_bin_call
+        k = drawcount > lengths[passed]
+        drawcount[k] = lengths[passed][k]
+        drawweight = weight / drawcount
+        total_draw = drawcount.sum()
+        
         logging.info('...drawn random points within bins')
-        psamp = FieldArray(self.node_draw_size, dtype=dtype)
-        for i in range(len(psamp)):
-            psamp[i] = choice(dmap[draw[i]])
+        psamp = FieldArray(total_draw, dtype=dtype)
+        pweight = numpy.zeros(total_draw, dtype=float)
+        j = 0
+        for i, c, w in zip(passed, drawcount, drawweight):
+            bdraw = choice(dmap[i], size=c, replace=False)
+            psamp[j:j+len(bdraw)] = FieldArray.from_records(bdraw, dtype=dtype)
+            pweight[j:j+len(bdraw)] = - node_loglrs[i] + numpy.log(w)
+            j += len(bdraw)
 
-        upsamp, idx, expand, count = numpy.unique(psamp,
-                                             return_index=True,
-                                             return_inverse=True,
-                                             return_counts=True)
         logging.info("Possible unique values %s", lengths[passed].sum())
-        logging.info("Templates drawn from %s", len(numpy.unique(draw)))
-        logging.info("Unique values first draw %s", len(upsamp))
+        logging.info("Templates drawn from %s", len(passed))
+        logging.info("Unique values first draw %s", len(psamp))
 
         # Calculate the likelihood values for the unique parameter space
         # points
         args = []
-        for i, s in enumerate(upsamp):
-            pset = {p: upsamp[p][i] for p in self.model.variable_params}
+        for i, s in enumerate(psamp):
+            pset = {p: psamp[p][i] for p in self.model.variable_params}
             args.append(pset)
 
         if self.reconstruct:
@@ -124,20 +135,20 @@ class NetBank(DummySampler):
 
         # Draw samples based on the actual likelihood relative to the
         # initial weights
-        logw3 = loglr_samp - numpy.array(node_loglrs)[draw[idx]] + numpy.log(count)
+        logw3 = loglr_samp + pweight
         logw3 -= logsumexp(logw3)
         weight2 = numpy.exp(logw3)
 
-        draw2 = choice(len(upsamp), size=self.resample_draw_size,
+        draw2 = choice(len(psamp), size=self.resample_draw_size,
                        replace=True, p=weight2)
         logging.info("Unique values second draw %s",
-                     len(numpy.unique(upsamp[draw2])))
+                     len(numpy.unique(psamp[draw2])))
         logging.info("ESS = %s", 1.0 / (weight2 ** 2.0).sum())
 
         # Prepare the equally weighted output samples
         fsamp = FieldArray(len(draw2), dtype=dtype)
         for i in range(len(draw2)):
-            fsamp[i] = upsamp[draw2[i]]
+            fsamp[i] = psamp[draw2[i]]
 
         self._samples = {p: fsamp[p] for p in self.model.variable_params}
         self._samples['loglikelihood'] = loglr_samp[draw2]
