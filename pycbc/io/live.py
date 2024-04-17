e9ed745842def3aa72efd5d609df19349867990f
GarethCabournDavies <gareth.cabourndavies@ligo.org>
2024-04-17 03:39:15 -0700

    Move lots of significance fits functions into shared module

--- pycbc/io/live.py
+++ pycbc/io/live.py
@@ -617,6 +617,151 @@ def filter_file(filename, start_time, end_time):
 
     return ((file_start + duration) >= start_time) and (file_start <= end_time)
 
+def add_live_trigger_selection_options(parser):
+    """
+    Add options required for obtaining the right set of PyCBC live triggers
+    into an argument parser
+    """
+    finding_group = parser.add_argument_group('Trigger Finding')
+    finding_group.add_argument(
+        "--trigger-directory",
+        metavar="PATH",
+        required=True,
+        help="Directory containing trigger files, directory "
+             "can contain subdirectories. Required."
+    )
+    finding_group.add_argument(
+        "--gps-start-time",
+        type=int,
+        required=True,
+        help="Start time of the analysis. Integer, required"
+    )
+    finding_group.add_argument(
+        "--gps-end-time",
+        type=int,
+        required=True,
+        help="End time of the analysis. Integer, required"
+    )
+    finding_group.add_argument(
+        "--date-directories",
+        action="store_true",
+        help="Are the triggers stored in directories according "
+             "to the date?"
+    )
+    default_dd_format = "%Y_%m_%d"
+    finding_group.add_argument(
+        "--date-directory-format",
+        default=default_dd_format,
+        help="Format of date, see datetime strftime "
+             "documentation for details. Default: "
+             "%%Y_%%m_%%d"
+    )
+    finding_group.add_argument(
+        "--file-identifier",
+        default="H1L1V1-Live",
+        help="String required in filename to be considered for "
+             "analysis. Default: 'H1L1V1-Live'."
+    )
+
+def add_live_significance_trigger_pruning_options(parser):
+    pruning_group = parser.add_argument_group("Trigger pruning")
+    pruning_group.add_argument(
+        "--prune-loudest",
+        type=int,
+        help="Maximum number of loudest trigger clusters to "
+             "remove from each bin."
+    )
+    pruning_group.add_argument(
+        "--prune-window",
+        type=float,
+        help="Window (seconds) either side of the --prune-loudest "
+             "loudest triggers in each duration bin to remove."
+    )
+    pruning_group.add_argument(
+        "--prune-stat-threshold",
+        type=float,
+        help="Minimum statistic value to consider a "
+             "trigger for pruning."
+    )
+
+def verify_live_significance_trigger_pruning_options(args, parser):
+    """
+    Verify options used to select appropriate triggers for single significance fits
+    """
+    # Pruning options are mutually required or not needed
+    prune_options = [args.prune_loudest, args.prune_window,
+                     args.prune_stat_threshold]
+
+def add_live_significance_duration_bin_options(parser):
+    durbin_group = parser.add_argument_group('Duration Bins')
+    durbin_group.add_argument(
+        "--duration-bin-edges",
+        nargs='+',
+        type=float,
+        help="Durations to use for bin edges. "
+             "Use if specifying exact bin edges, "
+             "Not compatible with --duration-bin-start, "
+             "--duration-bin-end and --num-duration-bins"
+    )
+    durbin_group.add_argument(
+        "--duration-bin-start",
+        type=float,
+        help="Shortest duration to use for duration bins."
+             "Not compatible with --duration-bins, requires "
+             "--duration-bin-end and --num-duration-bins."
+    )
+    durbin_group.add_argument(
+        "--duration-bin-end", type=float,
+        help="Longest duration to use for duration bins."
+    )
+    durbin_group.add_argument(
+        "--duration-from-bank",
+        help="Path to the template bank file to get max/min "
+             "durations from."
+    )
+    durbin_group.add_argument(
+        "--num-duration-bins",
+        type=int,
+        help="How many template duration bins to split the bank "
+             "into before fitting."
+    )
+    durbin_group.add_argument(
+        "--duration-bin-spacing",
+        choices=['linear','log'],
+        default='log',
+        help="How to set spacing for bank split "
+             "if using --num-duration-bins and "
+             "--duration-bin-start + --duration-bin-end "
+             "or --duration-from-bank."
+    )
+
+def verify_live_significance_duration_bin_options(args, parser):
+    if any(prune_options) and not all(prune_options):
+        parser.error("Require all or none of --prune-loudest, "
+                     "--prune-window and --prune-stat-threshold")
+
+    # Check the bin options
+    if args.duration_bin_edges:
+        if (args.duration_bin_start or args.duration_bin_end or
+            args.duration_from_bank or args.num_duration_bins):
+            parser.error("Cannot use --duration-bin-edges with "
+                         "--duration-bin-start, --duration-bin-end, "
+                         "--duration-from-bank or --num-duration-bins.")
+    else:
+        if not args.num_duration_bins:
+            parser.error("--num-duration-bins must be set if not using "
+                         "--duration-bin-edges.")
+        if not ((args.duration_bin_start and args.duration_bin_end) or
+                args.duration_from_bank):
+            parser.error("--duration-bin-start & --duration-bin-end or "
+                         "--duration-from-bank must be set if not using "
+                         "--duration-bin-edges.")
+    if args.duration_bin_end and \
+            args.duration_bin_end <= args.duration_bin_start:
+        parser.error("--duration-bin-end must be greater than "
+                     "--duration-bin-start, got "
+                     f"{args.duration_bin_end} and {args.duration_bin_start}")
+
 
 def find_trigger_files(directory, gps_start_time, gps_end_time,
                        id_string='*', date_directories=False,
@@ -661,5 +806,55 @@ def find_trigger_files(directory, gps_start_time, gps_end_time,
 
     return sorted(matching_files)
 
+def find_trigger_files_from_cli(args):
+    """
+    Wrapper around the find_trigger_files function to use when called using
+    options from the add_live_trigger_selection_options function
+    """
+    return liveio.find_trigger_files(
+            args.directory,
+            args.gps_start_time,
+            args.gps_end_time,
+            id_string=args.file_identifier,
+            date_directories=args.date_directories,
+            date_directory_format=args.date_directory_format
+        )
 
-__all__ = ['CandidateForGraceDB', 'gracedb_tag_with_version', 'find_trigger_files']
+def duration_bins_from_cli(args):
+    """Create the duration bins from CLI options.
+    """
+    if args.duration_bin_edges:
+        # direct bin specification
+        return np.array(args.duration_bin_edges)
+    # calculate bins from min/max and number
+    min_dur = args.duration_bin_start
+    max_dur = args.duration_bin_end
+    if args.duration_from_bank:
+        # read min/max duration directly from the bank itself
+        with h5py.File(args.duration_from_bank, 'r') as bank_file:
+            temp_durs = bank_file['template_duration'][:]
+        min_dur, max_dur = min(temp_durs), max(temp_durs)
+    if args.duration_bin_spacing == 'log':
+        return np.logspace(
+            np.log10(min_dur),
+            np.log10(max_dur),
+            args.num_duration_bins + 1
+        )
+    if args.duration_bin_spacing == 'linear':
+        return np.linspace(
+            min_dur,
+            max_dur,
+            args.num_duration_bins + 1
+        )
+    raise RuntimeError("Invalid duration bin specification")
+
+__all__ = [
+    'CandidateForGraceDB', 'gracedb_tag_with_version',
+    'gracedb_tag_with_version', 'add_live_trigger_selection_options',
+    'add_live_significance_trigger_pruning_options',
+    'verify_live_significance_trigger_pruning_options',
+    'add_live_significance_duration_bin_options',
+    'verify_live_significance_duration_bin_options',
+    'find_trigger_files', 'find_trigger_files_from_cli',
+    'duration_bins_from_cli',
+]
