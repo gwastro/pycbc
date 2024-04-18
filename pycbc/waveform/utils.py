02f4d6f6a822be4d86fce78351f9d6a34ce0e87c
Collin Capano <cdcapano@gmail.com>
2024-04-18 21:49:19 +0000

    add function to waveform utils to redshift time-domain waveforms

--- pycbc/waveform/utils.py
+++ pycbc/waveform/utils.py
@@ -480,3 +480,37 @@ def fd_to_td(htilde, delta_t=None, left_window=None, right_window=None,
         start, end = right_window
         htilde = fd_taper(htilde, start, end, side='right', beta=right_beta)
     return htilde.to_timeseries(delta_t=delta_t)
+
+
+def redshift_td_waveform(srch, z, return_fd=False):
+    """Redshifts a time-domain waveform.
+
+    The waveform will be stretched in time by :math:`(1+z)` and its
+    (time-domain) amplitude increased by :math:`(1+z)`. A time shift is also
+    applied to the waveform so that the merger occurs at the same point in
+    the time-series as before the redshift.
+
+    Parameters
+    ----------
+    srch : TimeSeries
+        The waveform to redshift.
+    z : float
+        The redshift to apply.
+
+    Returns
+    -------
+    TimeSeries
+        The red-shifted waveform.
+    """
+    stretched_srch = (1+z) * srch
+    stretched_srch._delta_t *= 1+z
+    # the merger was originally at t=0. Stretching dt will make the merger
+    # happen later. Need to shift back.
+    tcindex = -srch.start_time / srch.delta_t
+    shift = tcindex * srch.delta_t * (1+z)
+    stretched_srchtilde = stretched_srch.to_frequencyseries()
+    # apply time shift to account for redshift
+    apply_fd_time_shift(stretched_srchtilde, -shift, copy=False)
+    if return_fd:
+        return stretched_srchtilde
+    return stretched_srchtilde.to_timeseries()
