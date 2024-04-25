4209b1f6f603194c5b9dbc92060e7005b8b8fa73
Alexander Harvey Nitz <alex.nitz@gmail.com>
2024-04-25 12:12:48 -0400

    more reverts

--- tools/docker_build_dist.sh
+++ tools/docker_build_dist.sh
@@ -50,7 +50,7 @@ if [ "x${PYCBC_CONTAINER}" == "xpycbc_rhel_virtualenv" ]; then
   mkdir -p ${CVMFS_PATH}
 
   VENV_PATH=${CVMFS_PATH}/pycbc-${SOURCE_TAG}
-  virtualenv -p python39 ${VENV_PATH}
+  virtualenv -p python3.9 ${VENV_PATH}
   echo 'export PYTHONUSERBASE=${VIRTUAL_ENV}/.local' >> ${VENV_PATH}/bin/activate
   echo "export XDG_CACHE_HOME=\${HOME}/cvmfs-pycbc-${SOURCE_TAG}/.cache" >> ${VENV_PATH}/bin/activate
   source ${VENV_PATH}/bin/activate
