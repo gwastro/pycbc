1b9d5be204b6366bd2b7ca17196de0140e367856
Alexander Harvey Nitz <alex.nitz@gmail.com>
2024-05-13 16:22:45 -0400

    modularize

--- pycbc/inference/sampler/netbank.py
+++ pycbc/inference/sampler/netbank.py
@@ -60,41 +60,7 @@ class NetBank(DummySampler):
         self.reconstruct = bool(reconstruct)
         self.loglr_region = float(loglr_region)
 
-    def run(self):
-        logging.info('Retrieving params of parameter space nodes')
-        with h5py.File(self.mapfile, 'r') as f:
-            bparams = {p: f['bank'][p][:] for p in self.variable_params}
-            dmap = {}
-            num_nodes = len(bparams[list(bparams.keys())[0]])
-            lengths = numpy.array([len(f['map'][str(x)]) for x in range(num_nodes)])
-            dtype = f['map']['0'].dtype
-
-        logging.info('Calculating likelihood at nodes')
-        args = []
-        for i in range(num_nodes):
-            pset = {p: bparams[p][i] for p in self.model.variable_params}
-            args.append(pset)
-
-        node_loglrs = list(tqdm.tqdm(self.pool.imap(call_likelihood, args),
-                                     total=len(args)))
-         
-                                    
-         
-        node_loglrs = numpy.array(node_loglrs)
-        loglr_bound = node_loglrs[~numpy.isnan(node_loglrs)].max() - self.loglr_region
-
-        logging.info('Drawing proposal samples from node regions')
-        logw = node_loglrs + numpy.log(lengths)
-        passed = numpy.where((node_loglrs > loglr_bound)  & ~numpy.isnan(node_loglrs))[0]
-        logw2 = logw[passed]
-        logw2 -= logsumexp(logw2)
-        weight = numpy.exp(logw2)
-
-        logging.info("...reading template bins")
-        with h5py.File(self.mapfile, 'r') as f:
-            for i in passed:
-                dmap[i] = f['map'][str(i)][:]
-
+    def sample_round(self, weight, node_loglrs, dmap, lengths, passed):
         logging.info("...draw template bins")
         drawcount = (weight * self.target_likelihood_calls).astype(int)
 
@@ -114,12 +80,12 @@ class NetBank(DummySampler):
         total_draw = drawcount.sum()
 
         logging.info('...drawn random points within bins')
-        psamp = FieldArray(total_draw, dtype=dtype)
+        psamp = FieldArray(total_draw, dtype=self.dtype)
         pweight = numpy.zeros(total_draw, dtype=float)
         j = 0
         for i, c, w in zip(passed, drawcount, drawweight):
             bdraw = choice(dmap[i], size=c, replace=False)
-            psamp[j:j+len(bdraw)] = FieldArray.from_records(bdraw, dtype=dtype)
+            psamp[j:j+len(bdraw)] = FieldArray.from_records(bdraw, dtype=self.dtype)
             pweight[j:j+len(bdraw)] = - node_loglrs[i] + numpy.log(w)
             j += len(bdraw)
 
@@ -134,38 +100,62 @@ class NetBank(DummySampler):
             pset = {p: psamp[p][i] for p in self.model.variable_params}
             args.append(pset)
 
-        if self.reconstruct:
-            logging.info('Reconstructing any marginalized params...')
-            res = list(tqdm.tqdm(self.pool.imap(call_rlikelihood, args),
-                       total=len(args)))
-            loglr_samp = numpy.array([r[0] for r in res])
-            rec = [r[1] for r in res]
-        else:
-            loglr_samp = numpy.array(list(tqdm.tqdm(self.pool.imap(call_likelihood, args),
+        loglr_samp = numpy.array(list(tqdm.tqdm(self.pool.imap(call_likelihood, args),
                                      total=len(args))))
 
-        # Draw samples based on the actual likelihood relative to the
+        # Calculate the weights from the actual likelihood relative to the
         # initial weights
         logw3 = loglr_samp + pweight
         logw3 -= logsumexp(logw3)
         weight2 = numpy.exp(logw3)
+        return psamp, loglr_samp, weight2
+
+    def run(self):
+        logging.info('Retrieving params of parameter space nodes')
+        with h5py.File(self.mapfile, 'r') as f:
+            bparams = {p: f['bank'][p][:] for p in self.variable_params}
+            dmap = {}
+            num_nodes = len(bparams[list(bparams.keys())[0]])
+            lengths = numpy.array([len(f['map'][str(x)]) for x in range(num_nodes)])
+            self.dtype = f['map']['0'].dtype
+
+        logging.info('Calculating likelihood at nodes')
+        args = []
+        for i in range(num_nodes):
+            pset = {p: bparams[p][i] for p in self.model.variable_params}
+            args.append(pset)
+
+        node_loglrs = list(tqdm.tqdm(self.pool.imap(call_likelihood, args),
+                                     total=len(args)))
+        node_loglrs = numpy.array(node_loglrs)
+        loglr_bound = node_loglrs[~numpy.isnan(node_loglrs)].max() - self.loglr_region
+
+        logging.info('Drawing proposal samples from node regions')
+        logw = node_loglrs + numpy.log(lengths)
+        passed = numpy.where((node_loglrs > loglr_bound)  & ~numpy.isnan(node_loglrs))[0]
+        logw2 = logw[passed]
+        logw2 -= logsumexp(logw2)
+        weight = numpy.exp(logw2)
+
+        logging.info("...reading template bins")
+        with h5py.File(self.mapfile, 'r') as f:
+            for i in passed:
+                dmap[i] = f['map'][str(i)][:]
+
+        # Sample from posterior
+        psamp, loglr_samp, weight2 = self.sample_round(weight, node_loglrs, dmap, lengths, passed)
         ess = 1.0 / (weight2 ** 2.0).sum()
         logging.info("ESS = %s", ess)
 
+        # Prepare the equally weighted output samples
         draw2 = choice(len(psamp), size=int(ess * 5),
                        replace=True, p=weight2)
         logging.info("Unique values second draw %s",
                      len(numpy.unique(psamp[draw2])))
 
-        # Prepare the equally weighted output samples
-        fsamp = FieldArray(len(draw2), dtype=dtype)
+        fsamp = FieldArray(len(draw2), dtype=self.dtype)
         for i in range(len(draw2)):
             fsamp[i] = psamp[draw2[i]]
 
         self._samples = {p: fsamp[p] for p in self.model.variable_params}
         self._samples['loglikelihood'] = loglr_samp[draw2]
-
-        if self.reconstruct:
-            for k in rec[0]:
-                values = [r[k] for r in rec]
-                self._samples[k] = numpy.array(values)[draw2]
