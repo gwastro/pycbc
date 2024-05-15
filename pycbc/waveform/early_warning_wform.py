4f94ccbfc158bd984bdfd0b26f6177dd5a231e51
mj-will <michaeljw1@googlemail.com>
2024-05-15 10:45:07 +0100

    remove incorrect PSD lower frequency cutoff

--- pycbc/waveform/early_warning_wform.py
+++ pycbc/waveform/early_warning_wform.py
@@ -384,9 +384,9 @@ def generate_early_warning_psds(
     # Use same PSD for A & E
     psd = pycbc.psd.from_txt(psd_file, flen, 1./tlen, 1./tlen, is_asd_file=False)
     if low_freq_cutoff is not None:
-        # Set all values below frequency cutoff to inf
-        # Is 1e30 safe?
-        psd.data[psd.sample_frequencies < low_freq_cutoff] = 1e30
+        raise NotImplementedError(
+            "Lower frequency cutoff is not implemented yet!"
+        )
 
     psd_kern = PSDFirKernel()
 
