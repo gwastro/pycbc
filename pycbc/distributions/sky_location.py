9114055cc6db3cadc8943fbf3ef0a0d8bf3466ca
Julien Greuter <julien_g@LAPTOP-N3R34PGU>
2024-08-08 12:02:19 +0200

    end of test 'ring'

--- pycbc/distributions/sky_location.py
+++ pycbc/distributions/sky_location.py
@@ -245,7 +245,7 @@ class HealpixSky:
             alpha_min = 2*numpy.pi
             
             rasterized_map = healpix_map.rasterize(                            # A COMPRENDRE ? marche pas si RING, (code qui ne s'arrete plus) 
-                scheme = 'ring',
+                scheme = 'NESTED',
                 nside = nside
                 )                                                              
             data = rasterized_map.data
