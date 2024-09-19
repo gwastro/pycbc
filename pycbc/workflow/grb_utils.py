e364aea1cc94cbb24845a3988b0f9fad1af94fb4
Francesco Pannarale <francesco.pannarale@ligo.org>
2024-09-19 08:31:52 -0700

    Renaming make_info_table to make_pygrb_info_table; removing 4 cp.get calls from make_pygrb_info_table

--- pycbc/workflow/grb_utils.py
+++ pycbc/workflow/grb_utils.py
@@ -560,7 +560,8 @@ def make_pygrb_plot(workflow, exec_name, out_dir,
     return node, node.output_files
 
 
-def make_info_table(workflow, exec_name, out_dir, in_files=None, tags=None):
+def make_pygrb_info_table(workflow, exec_name, out_dir, in_files=None,
+                          tags=None):
     """
     Setup a job to create an html snippet with the GRB trigger information
     or exlusion distances information.
@@ -578,11 +579,6 @@ def make_info_table(workflow, exec_name, out_dir, in_files=None, tags=None):
 
     # Options
     if exec_name == 'pygrb_grb_info_table':
-        node.add_opt('--trigger-time',
-                     workflow.cp.get('workflow', 'trigger-time'))
-        node.add_opt('--ra', workflow.cp.get('workflow', 'ra'))
-        node.add_opt('--dec', workflow.cp.get('workflow', 'dec'))
-        node.add_opt('--sky-error', workflow.cp.get('workflow', 'sky-error'))
         node.add_opt('--ifos', ' '.join(workflow.ifos))
     elif exec_name == 'pygrb_exclusion_dist_table':
         node.add_input_opt('--input-files', in_files)
