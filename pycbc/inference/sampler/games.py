e08f5ae99984b78a05a173c956bd430d1efbe908
Alexander Harvey Nitz <alex.nitz@gmail.com>
2024-10-07 16:18:02 -0400

    remove unused options

--- pycbc/inference/sampler/games.py
+++ pycbc/inference/sampler/games.py
@@ -14,33 +14,31 @@ from .dummy import DummySampler
 from .base import (BaseSampler, setup_output)
 
 def call_likelihood(params):
-    """ Accessor to update the global model and call its reconstruction
-    routine.
+    """ Accessor to update the global model
     """
     models._global_instance.update(**params)
     return models._global_instance.loglikelihood
-
-def call_rlikelihood(params):
-    """ Accessor to update the global model and call its reconstruction
-    routine.
-    """
-    models._global_instance.update(**params)
-    logl = models._global_instance.loglikelihood
-    models._global_instance.update(**params)
-    rec = models._global_instance.reconstruct()
-    return logl, rec
 
 class OutOfSamples(Exception):
     pass
 
 class GameSampler(DummySampler):
-    """Direct monte-carlo sampler using a preconstructed parameter space
+    """Direct importance sampling using a preconstructed parameter space
     mapping file.
 
     Parameters
     ----------
     model : Model
         An instance of a model from ``pycbc.inference.models``.
+    mapfile : str
+        Path to the pre-generated file containing the pre-mapped prior volume
+    loglr_region: int
+        Only use regions from the prior volume tiling that are within
+        this loglr difference of the maximum tile.
+    target_likelihood_calls: int
+        Try to use this many likelihood calls in each round of the analysis.
+    rounds: int
+        The number of iterations to use before terminated.
     """
     name = 'games'
 
@@ -48,7 +46,6 @@ class GameSampler(DummySampler):
                  mapfile=None,
                  loglr_region=25,
                  target_likelihood_calls=1e5,
-                 reconstruct = False,
                  rounds = 1,
                  **kwargs):
         super().__init__(model, *args)
@@ -65,10 +62,10 @@ class GameSampler(DummySampler):
         self._samples = {}
 
         self.target_likelihood_calls = int(target_likelihood_calls)
-        self.reconstruct = bool(reconstruct)
         self.loglr_region = float(loglr_region)
 
     def draw_samples_from_bin(self, i, size):
+        """ Get samples from the binned prior space """
         if i not in self.draw:
             self.draw[i] = numpy.arange(0, len(self.dmap[i]))
 
@@ -86,6 +83,17 @@ class GameSampler(DummySampler):
         return self.dmap[i][selected]
 
     def sample_round(self, bin_weight, node_idx, lengths):
+        """ Sample from the posterior using pre-binned sets of points and
+        the weighting factor of each bin.
+        
+        bin_weight: Array
+            The weighting importance factor of each bin of the prior space
+        node_idx: Array
+            The set of ids into the prebinned prior volume to use. This should
+            map to the given weights.
+        lengths: Array
+            The size of each bin, used to self-normalize
+        """
         logging.info("...draw template bins")
         drawcount = (bin_weight * self.target_likelihood_calls).astype(int)
 
@@ -112,7 +120,8 @@ class GameSampler(DummySampler):
         j = 0
         for i, (c, w) in enumerate(zip(drawcount, drawweight)):
             bdraw = self.draw_samples_from_bin(node_idx[i], c)
-            psamp[j:j+len(bdraw)] = FieldArray.from_records(bdraw, dtype=self.dtype)
+            psamp[j:j+len(bdraw)] = FieldArray.from_records(bdraw,
+                                                            dtype=self.dtype)
             pweight[j:j+len(bdraw)] = numpy.log(bin_weight[i]) - numpy.log(w)
             bin_id[j:j+len(bdraw)] = i
             j += len(bdraw)
@@ -128,8 +137,9 @@ class GameSampler(DummySampler):
             pset = {p: psamp[p][i] for p in self.model.variable_params}
             args.append(pset)
 
-        loglr_samp = numpy.array(list(tqdm.tqdm(self.pool.imap(call_likelihood, args),
-                                     total=len(args))))
+        loglr_samp = list(tqdm.tqdm(self.pool.imap(call_likelihood, args),
+                                    total=len(args)))
+        loglr_samp = numpy.array(loglr_samp)
 
         # Calculate the weights from the actual likelihood relative to the
         # initial weights
@@ -138,22 +148,8 @@ class GameSampler(DummySampler):
         weight2 = numpy.exp(logw3)
         return psamp, loglr_samp, weight2, bin_id
 
-    def calculate_evidence(self, node_loglrs, node_lengths, loglrsamp, bin_id):
-        logz = node_loglrs * 1
-
-        #s = bin_id.argsort()
-        #bin_id = bin_id[s]
-        #loglrsamp = loglrsamp[s]
-
-        bid = numpy.unique(bin_id)
-        for i in bid:
-            x = numpy.where(i == bin_id)[0]
-            logz[i] = logsumexp(loglrsamp[x]) - numpy.log(len(x))
-
-        logz = (logz + numpy.log(node_lengths) - numpy.log(node_lengths.sum()))
-        return logsumexp(logz)
-
     def run(self):
+        """ Produce posterior samples """
         logging.info('Retrieving params of parameter space nodes')
         with h5py.File(self.mapfile, 'r') as f:
             bparams = {p: f['bank'][p][:] for p in self.variable_params}
@@ -196,7 +192,9 @@ class GameSampler(DummySampler):
 
         for i in range(self.rounds):
             try:
-                psamp_v, loglr_samp_v, weight2_v, bin_id = self.sample_round(weight/weight.sum(), passed, lengths[passed])
+                psamp_v, loglr_samp_v, weight2_v, bin_id = \
+                    self.sample_round(weight / weight.sum(),
+                                      passed, lengths[passed])
             except OutOfSamples:
                 logging.info("No more samples to draw from")
                 break
@@ -219,10 +217,6 @@ class GameSampler(DummySampler):
 
             ess = 1.0 / ((weight2/weight2.sum()) ** 2.0).sum()
             logging.info("ESS = %s", ess)
-
-            logz = self.calculate_evidence(node_loglrs, lengths, loglr_samp, bin_ids)
-            dlogz = (loglr_samp * weight2).sum() / weight2.sum() + numpy.log(len(bin_id)) - numpy.log(lengths.sum())
-            logging.info("dlogz = %s, %s", dlogz, logz)
 
         # Prepare the equally weighted output samples
         self.meta['ncalls'] = len(weight2)
