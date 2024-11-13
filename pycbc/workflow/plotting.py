477dbb055b3261b77736757988fb72859e772050
GarethCabournDavies <gareth.cabourndavies@ligo.org>
2024-11-13 04:30:10 -0800

    Some fixes to allow the joined bank to be plotted

--- pycbc/workflow/plotting.py
+++ pycbc/workflow/plotting.py
@@ -575,25 +575,29 @@ def make_template_bin_table(workflow, dq_file, out_dir, tags=None):
     return node.output_files[0]
 
 
-def make_bank_compression_plot(workflow, bank_files, out_dir, tags=None):
+def make_bank_compression_plots(workflow, bank_files, out_dir, tags=None):
     tags = [] if tags is None else tags
     makedir(out_dir)
-    node = PlotExecutable(
-        workflow.cp,
-        "plot_bank_compression",
-        ifos=workflow.ifos,
-        out_dir=out_dir,
-    tags=tags).create_node()
+    secs = workflow.cp.get_subsections("plot_bank_compression")
+    files = FileList([])
+    for tag in secs:
+        node = PlotExecutable(
+            workflow.cp,
+            "plot_bank_compression",
+            ifos=workflow.ifos,
+            out_dir=out_dir,
+            tags=[tag] + tags
+        ).create_node()
+        node.add_input_list_opt(
+            "--bank-files",
+            bank_files
+        )
 
-    node.add_input_list_opt(
-        "--bank-files",
-        bank_files
-    )
-
-    node.new_output_file_opt(
-        workflow.analysis_time,
-        '.png',
-        '--output-file'
-    )
-    workflow += node
-    return node.output_files[0]
+        node.new_output_file_opt(
+            workflow.analysis_time,
+            '.png',
+            '--output-file'
+        )
+        workflow += node
+        files += node.output_files
+    return files
