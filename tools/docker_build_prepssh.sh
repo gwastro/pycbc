69e6c40801f46d8acd8da946d5237e03eb440e7a
Tito Dal Canton <tito.dalcanton@ijclab.in2p3.fr>
2025-03-26 15:07:46 +0100

    Cleanup stuff that handles the Docker image and CVMFS release (#5091)
    * Drop Python setup stage from CI
    
    * Remove redundant commands from prepssh
    
    * Make Dockerfile more readable
    
    * Remove code duplication and simplify logic in in push_image.sh
    
    * Remove redundant bits in Dockerfile
    
    * Cleanup and comment docker-install.sh

--- tools/docker_build_prepssh.sh
+++ tools/docker_build_prepssh.sh
@@ -1,6 +1,5 @@
 mkdir -p ~/.ssh
-touch ~/.ssh/id_rsa ~/.ssh/ldg_user ~/.ssh/ldg_token
-chmod 600 ~/.ssh/id_rsa ~/.ssh/ldg_user ~/.ssh/ldg_token
+touch ~/.ssh/ldg_user ~/.ssh/ldg_token
 echo "${OSG_ACCESS}" > ~/.ssh/id_rsa
 echo -e "Host sugwg-test1.phy.syr.edu\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config ;
 echo -e "Host sugwg-condor.phy.syr.edu\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config ;
