f4ddc976628c8d95c64a8846e8de2a7888dfd05d
Sumit Kumar <sum33it@users.noreply.github.com>
2025-07-11 13:50:28 +0200

    Update examples/inference/calibration_configuration_files/generate_calibration_config.sh
    Co-authored-by: Gareth S Cabourn Davies <gareth.cabourndavies@ligo.org>

--- examples/inference/calibration_configuration_files/generate_calibration_config.sh
+++ examples/inference/calibration_configuration_files/generate_calibration_config.sh
@@ -3,7 +3,7 @@
 # These files can be downloaded from
 # https://dcc.ligo.org/T2100313/public
 
-CALIB_ENV_FILE_PATH=/holohome/sumit.kumar/data/GWOSC/calibration_uncertainty_o1_o2_o3
+CALIB_ENV_FILE_PATH=./calibration_uncertainty_files
 
 # Generate calibration configuration file for GW150914
 pycbc_inference_create_calibration_config --calibration-files-path ${CALIB_ENV_FILE_PATH} --ifos H1 L1 --minimum-frequency H1:20 L1:20 --maximum-frequency H1:1000 L1:1000  --gps-time 1126259462.43 --correction-type H1:data L1:data
