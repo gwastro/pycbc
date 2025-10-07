f8c422efb218bfd92816131dcb667b9c1a4f0cee
GarethCabournDavies <gareth.cabourndavies@ligo.org>
2025-10-07 06:23:18 -0700

    remove from setup.py as well

--- setup.py
+++ setup.py
@@ -46,10 +46,7 @@ install_requires = setup_requires + [
     'setuptools',
     'gwdatafind',
     'pegasus-wms.api >= 5.1.1',
-    # FIXME igwn-ligolw 2.1.0 + lalsuite 7.25.1 produce errors arising from the
-    # old python-ligo-lw module. Remove pin when we have moved to a lalsuite
-    # that no longer depends on python-ligo-lw.
-    'igwn-ligolw<2.1.0',
+    'igwn-ligolw',
     'igwn-segments',
     'lalsuite!=7.2',
     'lscsoft-glue>=1.59.3',
