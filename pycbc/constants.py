e557a35f991d5423190b7c71320d824baf954856
Gareth S Cabourn Davies <gareth.cabourn-davies@port.ac.uk>
2025-11-03 16:51:53 +0000

    work through replacing lal constants

--- pycbc/constants.py
+++ pycbc/constants.py
@@ -6,7 +6,7 @@ based on a preferred order of availability. This helps minimize dependency bloat
 by relying on smaller, more general packages (astropy/numpy) when the large
 LALSuite dependency is not available.
 
-The priority order for constant lookup is: LAL > Astropy > NumPy
+The priority order for constant lookup is: LAL > Astropy / SciPy / NumPy
 """
 
 import logging
@@ -40,18 +40,23 @@ _FALLBACK_MAPPING = {
     'PC_SI': aconstants.pc.value,           # Parsec
     'REARTH_SI': aconstants.R_earth.value,  # Earth equatorial radius
     'YRJUL_SI': aunits.year.to(aunits.s),   # years in seconds
-    'PI': np.pi
+    'PI': np.pi,
+    'TWOPI': 2 * np.pi,
+    'GAMMA': np.euler_gamma,
 }
 
 # We need to define some constants from astropy values
 MSUN_SI = _FALLBACK_MAPPING['MSUN_SI']
 C_SI = _FALLBACK_MAPPING['C_SI']
 G_SI = _FALLBACK_MAPPING['G_SI']
+
 MTSUN_SI = MSUN_SI * G_SI / (C_SI ** 3)
+MRSUN_SI = MSUN_SI * G_SI / (C_SI * C_SI)
 
 # Add in these hybrid constants
 _FALLBACK_MAPPING.update({
-    'MTSUN_SI': MTSUN_SI
+    'MTSUN_SI': MTSUN_SI,
+    'MRSUN_SI': MRSUN_SI
 })
 
 # Define the Constant Lookup Function
@@ -94,9 +99,14 @@ def get_constant(name):
 
 C_SI = get_constant('C_SI')
 G_SI = get_constant('G_SI')
+
 MTSUN_SI = get_constant('MTSUN_SI')
+MRSUN_SI = get_constant('MTSUN_SI')
 MSUN_SI = get_constant('MSUN_SI')
+
 PC_SI = get_constant('PC_SI')
 REARTH_SI = get_constant('REARTH_SI')
 YRJUL_SI = get_constant('YRJUL_SI')
+
 PI = get_constant('PI')
+GAMMA = get_constant('PI')
