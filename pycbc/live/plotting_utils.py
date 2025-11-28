3ac8036ee44ce3003d143684939fe614c1a68f67
Gareth S Cabourn Davies <gareth.cabourn-davies@port.ac.uk>
2025-11-28 12:22:36 +0000

    copilot suggestions

--- pycbc/live/plotting_utils.py
+++ pycbc/live/plotting_utils.py
@@ -6,9 +6,9 @@ import datetime
 
 logger = logging.getLogger('pycbc.live.plotting_utils')
 
-def strip_time(time):
+def strip_time(gps_time):
     """Strip off the time information, give gps time at the previous midnight"""
-    midnight_date = Time(time, format='gps', scale='utc').to_datetime().date()
+    midnight_date = Time(gps_time, format='gps', scale='utc').to_datetime().date()
     midnight_dt = datetime.datetime.combine(midnight_date, dtt.min)
     return Time(midnight_dt, format='datetime').gps, midnight_date.strftime("%Y-%m-%d")
 
