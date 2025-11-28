8311de5f5542cf7b392f60bbb8fe89daa3ab2b66
Gareth S Cabourn Davies <gareth.cabourn-davies@port.ac.uk>
2025-11-28 16:18:28 +0000

    Revert to use proper imports for the tests

--- test/test_live_plotting_utils.py
+++ test/test_live_plotting_utils.py
@@ -1,22 +1,10 @@
-import os
-import importlib.util
 import datetime
 import unittest
 from astropy.time import Time
 
-
-def load_plotting_utils():
-    base = os.path.dirname(__file__)
-    path = os.path.abspath(os.path.join(base, '..', 'pycbc', 'live', 'plotting_utils.py'))
-    spec = importlib.util.spec_from_file_location('temp_plotting_utils', path)
-    module = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(module)
-    return module
-
+from pycbc.live import plotting_utils as pu
 
 class TestStripTime(unittest.TestCase):
-    def setUp(self):
-        self.mod = load_plotting_utils()
 
     def _expected(self, gps_time):
         # compute expected midnight GPS and date string using astropy
@@ -27,7 +15,7 @@ class TestStripTime(unittest.TestCase):
 
     def test_integer_gps(self):
         gps = 1262304000  # arbitrary known GPS second
-        midnight, date_str = self.mod.strip_time(gps)
+        midnight, date_str = pu.strip_time(gps)
         exp_midnight, exp_date = self._expected(gps)
         self.assertEqual(date_str, exp_date)
         # allow small floating point diffs
@@ -35,14 +23,14 @@ class TestStripTime(unittest.TestCase):
 
     def test_float_gps(self):
         gps = 1262304000.5
-        midnight, date_str = self.mod.strip_time(gps)
+        midnight, date_str = pu.strip_time(gps)
         exp_midnight, exp_date = self._expected(gps)
         self.assertEqual(date_str, exp_date)
         self.assertAlmostEqual(midnight, exp_midnight, places=6)
 
     def test_midnight_is_before_or_equal_and_within_day(self):
         gps = 1262304000 + 3600 * 12  # midday
-        midnight, date_str = self.mod.strip_time(gps)
+        midnight, _ = pu.strip_time(gps)
         # midnight should be <= provided time and within 24 hours difference
         self.assertLessEqual(midnight, gps)
         self.assertLess(gps - midnight, 86400)
