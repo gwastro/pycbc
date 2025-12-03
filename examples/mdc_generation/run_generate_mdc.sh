70ac9af5f10314aa486c4c5acf74bb193f7eb69d
WuShichao <shichao.wu@aei.mpg.de>
2025-12-02 23:36:02 -0500

    update

--- examples/mdc_generation/run_generate_mdc.sh
+++ examples/mdc_generation/run_generate_mdc.sh
@@ -7,7 +7,7 @@
 pycbc_generate_mock_data --ifo-list H1 L1 V1 --low-frequency-cutoff H1:20 L1:20 V1:20 --psd-model H1:aLIGOZeroDetHighPower L1:aLIGOZeroDetHighPower V1:aLIGOZeroDetHighPower --fake-strain-seed H1:1234 L1:2345 V1:3456 --gps-start-time 1257294808 --gps-end-time 1257294908 --sample-rate 2048 --channel-name H1:SIMULATED_STRAIN L1:SIMULATED_STRAIN V1:SIMULATED_STRAIN --tag HLV_NOISE 
 
 # Generate Noise + Signals frame files
-pycbc_generate_mock_data --ifo-list H1 L1 V1 --low-frequency-cutoff H1:20 L1:20 V1:20 --psd-model H1:aLIGOZeroDetHighPower L1:aLIGOZeroDetHighPower V1:aLIGOZeroDetHighPower --fake-strain-seed H1:1234 L1:2345 V1:3456 --gps-start-time 1257294808 --gps-end-time 1257294908 --sample-rate 2048 --channel-name H1:SIMULATED_STRAIN L1:SIMULATED_STRAIN V1:SIMULATED_STRAIN --tag HLV_NOISE_PLUS_SIGNAL --injection-file injection_10inj.hdf
+pycbc_generate_mock_data --ifo-list H1 L1 V1 --low-frequency-cutoff H1:20 L1:20 V1:20 --psd-model H1:aLIGOZeroDetHighPower L1:aLIGOZeroDetHighPower V1:aLIGOZeroDetHighPower --fake-strain-seed H1:1234 L1:2345 V1:3456 --gps-start-time 1257294808 --gps-end-time 1257294908 --sample-rate 2048 --channel-name H1:SIMULATED_STRAIN L1:SIMULATED_STRAIN V1:SIMULATED_STRAIN --tag HLV_NOISE_SIGNAL --injection-file injection_10inj.hdf
 
 # Generate Signal (with Zeronoise) frame files 
 pycbc_generate_mock_data --ifo-list H1 L1 V1 --low-frequency-cutoff H1:20 L1:20 V1:20 --psd-model H1:zeroNoise L1:zeroNoise V1:zeroNoise --fake-strain-seed H1:1234 L1:2345 V1:3456 --gps-start-time 1257294808 --gps-end-time 1257294908 --sample-rate 2048 --channel-name H1:SIMULATED_STRAIN L1:SIMULATED_STRAIN V1:SIMULATED_STRAIN --tag HLV_ZERONOISE_SIGNAL --injection-file injection_10inj.hdf
--- examples/mdc_generation/run_generate_mdc_lisa.sh
+++ examples/mdc_generation/run_generate_mdc_lisa.sh
@@ -4,33 +4,64 @@
 ./make_injections_lisa.sh
 
 # Generate noise without signals frame files
-pycbc_generate_mock_data --ifo-list LISA_A LISA_E LISA_T \
-                         --low-frequency-cutoff LISA_A:1e-4 LISA_E:1e-4 LISA_T:1e-4 \
-                         --psd-model LISA_A:analytical_psd_lisa_tdi_AE LISA_E:analytical_psd_lisa_tdi_AE LISA_T:analytical_psd_lisa_tdi_T \
-                         --fake-strain-seed LISA_A:100 LISA_E:150 LISA_T:200 \
-                         --gps-start-time 2121224.274911478 --gps-end-time 4799924.274911478 --sample-rate 0.2 \
-                         --len-arm 2.5e9 --acc-noise-level 2.4e-15 --oms-noise-level 7.9e-12 --tdi '2.0' \
-                         --channel-name LISA_A:SIMULATED_STRAIN LISA_E:SIMULATED_STRAIN LISA_T:SIMULATED_STRAIN \
-                         --tag LISA_NOISE
+pycbc_generate_mock_data \
+    --ifo-list LISA_A LISA_E LISA_T \
+    --low-frequency-cutoff LISA_A:1e-4 LISA_E:1e-4 LISA_T:1e-4 \
+    --psd-model LISA_A:analytical_psd_lisa_tdi_AE \
+                LISA_E:analytical_psd_lisa_tdi_AE \
+                LISA_T:analytical_psd_lisa_tdi_T \
+    --fake-strain-seed LISA_A:100 LISA_E:150 LISA_T:200 \
+    --gps-start-time 0 \
+    --gps-end-time 31536000 \
+    --sample-rate 0.2 \
+    --len-arm 2.5e9 \
+    --acc-noise-level 2.4e-15 \
+    --oms-noise-level 7.9e-12 \
+    --tdi '2.0' \
+    --channel-name LISA_A:SIMULATED_STRAIN \
+                   LISA_E:SIMULATED_STRAIN \
+                   LISA_T:SIMULATED_STRAIN \
+    --tag LISA_NOISE \
+    --fake-strain-filter-duration 31536000
 
 # Generate noise + signals frame files
-pycbc_generate_mock_data --ifo-list LISA_A LISA_E LISA_T \
-                         --low-frequency-cutoff LISA_A:1e-4 LISA_E:1e-4 LISA_T:1e-4 \
-                         --psd-model LISA_A:analytical_psd_lisa_tdi_AE LISA_E:analytical_psd_lisa_tdi_AE LISA_T:analytical_psd_lisa_tdi_T \
-                         --fake-strain-seed LISA_A:100 LISA_E:150 LISA_T:200 \
-                         --gps-start-time 2121224.274911478 --gps-end-time 4799924.274911478 --sample-rate 0.2 \
-                         --len-arm 2.5e9 --acc-noise-level 2.4e-15 --oms-noise-level 7.9e-12 --tdi '2.0' \
-                         --channel-name LISA_A:SIMULATED_STRAIN LISA_E:SIMULATED_STRAIN LISA_T:SIMULATED_STRAIN \
-                         --injection-file injections_lisa.hdf \
-                         --tag LISA_NOISE_PLUS_SIGNAL
+    pycbc_generate_mock_data \
+        --ifo-list LISA_A LISA_E LISA_T \
+        --low-frequency-cutoff LISA_A:1e-4 LISA_E:1e-4 LISA_T:1e-4 \
+        --psd-model LISA_A:analytical_psd_lisa_tdi_AE \
+                    LISA_E:analytical_psd_lisa_tdi_AE \
+                    LISA_T:analytical_psd_lisa_tdi_T \
+        --fake-strain-seed LISA_A:100 LISA_E:150 LISA_T:200 \
+        --gps-start-time 0 \
+        --gps-end-time 31536000 \
+        --sample-rate 0.2 \
+        --len-arm 2.5e9 \
+        --acc-noise-level 2.4e-15 \
+        --oms-noise-level 7.9e-12 \
+        --tdi '2.0' \
+        --channel-name LISA_A:SIMULATED_STRAIN \
+                       LISA_E:SIMULATED_STRAIN \
+                       LISA_T:SIMULATED_STRAIN \
+        --injection-file injections_lisa.hdf \
+        --tag LISA_NOISE_SIGNAL \
+        --fake-strain-filter-duration 31536000
 
 # Generate Signal (with Zeronoise) frame files 
-pycbc_generate_mock_data --ifo-list LISA_A LISA_E LISA_T \
-                         --low-frequency-cutoff LISA_A:1e-4 LISA_E:1e-4 LISA_T:1e-4 \
-                         --psd-model LISA_A:zeroNoise LISA_E:zeroNoise LISA_T:zeroNoise \
-                         --fake-strain-seed LISA_A:100 LISA_E:150 LISA_T:200 \
-                         --gps-start-time 2121224.274911478 --gps-end-time 4799924.274911478 --sample-rate 0.2 \
-                         --len-arm 2.5e9 --acc-noise-level 2.4e-15 --oms-noise-level 7.9e-12 --tdi '2.0' \
-                         --channel-name LISA_A:SIMULATED_STRAIN LISA_E:SIMULATED_STRAIN LISA_T:SIMULATED_STRAIN \
-                         --injection-file injections_lisa.hdf \
-                         --tag LISA_ZERONOISE_SIGNAL 
+    pycbc_generate_mock_data \
+        --ifo-list LISA_A LISA_E LISA_T \
+        --low-frequency-cutoff LISA_A:1e-4 LISA_E:1e-4 LISA_T:1e-4 \
+        --psd-model LISA_A:zeroNoise LISA_E:zeroNoise LISA_T:zeroNoise \
+        --fake-strain-seed LISA_A:100 LISA_E:150 LISA_T:200 \
+        --gps-start-time 0 \
+        --gps-end-time 31536000 \
+        --sample-rate 0.2 \
+        --len-arm 2.5e9 \
+        --acc-noise-level 2.4e-15 \
+        --oms-noise-level 7.9e-12 \
+        --tdi '2.0' \
+        --channel-name LISA_A:SIMULATED_STRAIN \
+                       LISA_E:SIMULATED_STRAIN \
+                       LISA_T:SIMULATED_STRAIN \
+        --injection-file injections_lisa.hdf \
+        --tag LISA_ZERONOISE_SIGNAL \
+        --fake-strain-filter-duration 31536000
