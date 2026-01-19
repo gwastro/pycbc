744ac07ed7cde3a4a9bf8349e1fc0ccb6e16bcb5
Ian Harry <ian.harry@ligo.org>
2026-01-19 08:40:52 -0800

    Simplify log

--- pycbc/filter/simd_correlate.py
+++ pycbc/filter/simd_correlate.py
@@ -57,7 +57,7 @@ if l2_cache_size is not None:
     # Since we need 3 vectors fitting in L2 cache, divide by 3
     # We find the nearest power-of-two that fits, and the length
     # of the single-precision complex array that fits into that size.
-    pow2 = int(_np.log(l2_cache_size/3.0)/_np.log(2.0))
+    pow2 = int(_np.log2(l2_cache_size / 3.0))
     default_segsize = pow(2, pow2) // _np.dtype(_np.complex64).itemsize
 else:
     # Seems to work for Sandy Bridge/Ivy Bridge/Haswell, for now?
