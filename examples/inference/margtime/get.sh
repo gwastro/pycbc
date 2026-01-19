7fb2d898096e48ca30ccced2277314790e390cf0
Tito Dal Canton <tito.dalcanton@ijclab.in2p3.fr>
2026-01-19 15:50:19 +0100

    CI: cache example GW data (#5256)
    * CI: cache example GW data
    
    * Debug prints
    
    * Add more paths and fix some incorrect curl usage
    
    * Revert debug prints

--- examples/inference/margtime/get.sh
+++ examples/inference/margtime/get.sh
@@ -5,5 +5,5 @@ do
     file=${ifo}_GWOSC_4KHZ_R1-1126257415-4096.gwf
     test -f ${file} && continue
     curl -O -L --show-error --silent \
-        https://www.gwosc.org/eventapi/html/GWTC-1-confident/GW150914/v3/${ifo}_GWOSC_4KHZ_R1-1126257415-4096.gwf
+        https://www.gwosc.org/eventapi/html/GWTC-1-confident/GW150914/v3/${file}
 done
