0614f08678f27758b228a20f386df97139ad57da
Alex Correia <alcorrei@syr.edu>
2026-03-11 15:44:32 -0400

    remove prints; fix docs

--- pycbc/types/timeseries.py
+++ pycbc/types/timeseries.py
@@ -705,7 +705,7 @@ class TimeSeries(Array):
 
         Returns
         -------
-        data: pycbc.types.TimeSeris
+        data: pycbc.types.TimeSeries
             Gated time series
         """
         data = self.copy() if copy else self
@@ -726,7 +726,6 @@ class TimeSeries(Array):
             rindex_time = float(self.start_time + rindex * self.delta_t)
             offset = rindex_time - (time + window)
             if offset == 0:
-                print(paint_method)
                 if paint_method == 'toeplitz':
                     return gate_and_paint(data, lindex, rindex, invpsd, copy=False)
                 elif paint_method == 'matmul':
