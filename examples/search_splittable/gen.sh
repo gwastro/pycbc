7af953a4388bdae1a60a27bacd532e0de5ba1738
kkacanja <keisi.kacanja@gmail.com>
2026-03-27 10:32:03 -0400

    cleanup

--- examples/search_splittable/gen.sh
+++ examples/search_splittable/gen.sh
@@ -4,6 +4,6 @@ set -e
 pycbc_make_offline_search_workflow \
 --workflow-name gw \
 --output-dir output \
---config-files analysis.ini plotting.ini executables.ini injections_minimal.ini \
+--config-files analysis.ini ../search/plotting.ini ../search/executables.ini ../search/injections_minimal.ini \
 --config-overrides results_page:output-path:$(pwd)/html \
 --submit-now
