6768c034f166ee10f46494937ccd1cbc5fbd21ba
Ben McKallip <mckallipb@southwestern.edu>
2026-04-20 19:35:18 -0500

    Refactored testing data to be stored in an hdf5 file (https://github.com/gwastro/pycbc/pull/5146)

--- test/test_matchedfilter.py
+++ test/test_matchedfilter.py
@@ -195,7 +195,7 @@ class TestMatchedFilter(unittest.TestCase):
     
     def test_optimized_match_valid(self):
         with self.context:
-            NL_waveform, L_waveform, ligo_psd = testing_data.get_match_testing_waveforms()
+            NL_waveform, L_waveform, ligo_psd = testing_data.load_match_testing_waveforms_from_hdf5()
 
             NL_waveform = FrequencySeries(NL_waveform, delta_f=0.25)
             L_waveform = FrequencySeries(L_waveform, delta_f=0.25)
