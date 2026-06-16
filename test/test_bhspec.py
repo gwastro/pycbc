748d7db6917871102e70545a39bd8ac7fae6ac8b
Ian Harry <ian.harry@ligo.org>
2026-06-16 14:27:43 -0700

    Revert this change

--- test/test_bhspec.py
+++ test/test_bhspec.py
@@ -35,7 +35,10 @@ class TestBHSpecModel(unittest.TestCase):
             frame_file = frame_file.split(":")[-1]
             if not os.path.exists(frame_file):
                 url = os.path.join(cls.frame_files_url, frame_file)
-                frame_file = get_file(url, cache=True)
+                frame_file = get_file(url, cache=False)
+                # Naming matters here, so we move the file to something named
+                # in accordance with the frame-file specification.
+                shutil.move(tmp_path, frame_file)
 
         # Load expected parameter values and expected loglikelihood from
         # the JSON file
