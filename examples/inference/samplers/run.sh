8f36463631781fc763be89f30989283631ff7815
Ian Harry <ian.harry@ligo.org>
2026-07-10 16:45:40 +0100

    Avoid cpnest on mac, doesn't work

--- examples/inference/samplers/run.sh
+++ examples/inference/samplers/run.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
-for f in cpnest_stub.ini emcee_stub.ini emcee_pt_stub.ini dynesty_stub.ini ultranest_stub.ini epsie_stub.ini nessai_stub.ini snowline_stub.ini; do
+SAMPLERS="emcee_stub.ini emcee_pt_stub.ini dynesty_stub.ini ultranest_stub.ini epsie_stub.ini nessai_stub.ini snowline_stub.ini"
+PLOT_INPUTS="emcee_stub.ini.hdf:emcee emcee_pt_stub.ini.hdf:emcee_pt dynesty_stub.ini.hdf:dynesty ultranest_stub.ini.hdf:ultranest epsie_stub.ini.hdf:espie nessai_stub.ini.hdf:nessai snowline_stub.ini.hdf:snowline"
+
+if [ "$(uname)" != "Darwin" ]; then
+    SAMPLERS="cpnest_stub.ini $SAMPLERS"
+    PLOT_INPUTS="$PLOT_INPUTS cpnest_stub.ini.hdf:cpnest"
+fi
+
+for f in $SAMPLERS; do
         echo $f
-	pycbc_inference \
+        pycbc_inference \
         --config-files `dirname $0`/simp.ini `dirname $0`/$f \
         --output-file $f.hdf \
         --nprocesses 1 \
@@ -9,15 +17,7 @@ for f in cpnest_stub.ini emcee_stub.ini emcee_pt_stub.ini dynesty_stub.ini ultra
         --force
 done
 
-pycbc_inference_plot_posterior --input-file \
-emcee_stub.ini.hdf:emcee \
-emcee_pt_stub.ini.hdf:emcee_pt \
-dynesty_stub.ini.hdf:dynesty \
-ultranest_stub.ini.hdf:ultranest \
-epsie_stub.ini.hdf:espie \
-cpnest_stub.ini.hdf:cpnest \
-nessai_stub.ini.hdf:nessai \
-snowline_stub.ini.hdf:snowline \
+pycbc_inference_plot_posterior --input-file $PLOT_INPUTS \
 --output-file sample.png \
 --plot-contours \
 --plot-marginal \
