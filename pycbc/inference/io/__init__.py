ae462279eb3c9305db9777b36698a0938f0bce86
Collin Capano <cdcapano@gmail.com>
2026-07-30 18:39:55 -0400

    add back support for emcee_pt files

--- pycbc/inference/io/__init__.py
+++ pycbc/inference/io/__init__.py
@@ -31,6 +31,7 @@ from pycbc.io.hdf import (dump_state, load_state)
 
 from pycbc.inference.option_utils import (ParseLabelArg, ParseParametersArg)
 from .emcee import EmceeFile
+from .emcee_pt import EmceePTFile
 from .ptemcee import PTEmceeFile
 from .cpnest import CPNestFile
 from .multinest import MultinestFile
@@ -43,6 +44,7 @@ from .txt import InferenceTXTFile
 
 filetypes = {
     EmceeFile.name: EmceeFile,
+    EmceePTFile.name: EmceePTFile,
     PTEmceeFile.name: PTEmceeFile,
     CPNestFile.name: CPNestFile,
     MultinestFile.name: MultinestFile,
