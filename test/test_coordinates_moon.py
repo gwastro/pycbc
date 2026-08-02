bd5909d783deb59e1253dd9974edcc7cd57ccc57
WuShichao <shichao.wu@aei.mpg.de>
2026-08-02 21:04:15 +0200

    Add Jacopo Tissino and Jan Harms as co-authors of the LGWA integration modules
    This work builds directly on Tissino et al. 2026 (arXiv:2606.04918) --
    both the physics motivating the reference-point frame and the
    lgwa_response package (Jacopo Tissino) whose antenna-pattern geometry
    pycbc.detector.space._LGWA_detector reimplements and cross-checks
    against, with Jan Harms' guidance on the underlying LGWA science.

--- test/test_coordinates_moon.py
+++ test/test_coordinates_moon.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2026  Shichao Wu, Alex Nitz
+# Copyright (C) 2026  Shichao Wu, Alex Nitz, Jacopo Tissino, Jan Harms
 #
 # This program is free software; you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by the
